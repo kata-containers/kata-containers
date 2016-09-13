38e041dc9d1ff81b593223ee4586f18350530a6d
Samuel Ortiz <sameo@linux.intel.com>
2016-09-13 12:31:24 +0200

    qemu: Add Device unit tests
    We add a NVDIMM, a filesystem and an empty device.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- qemu_test.go
+++ qemu_test.go
@@ -32,6 +32,13 @@ func testAppend(structure interface{}, expected string, t *testing.T) {
 		}
 
 		params = appendMachine([]string{}, config)
+
+	case Device:
+		config := Config{
+			Devices: []Device{s},
+		}
+
+		params = appendDevices([]string{}, config)
 	}
 
 	result := strings.Join(params, " ")
@@ -55,4 +62,34 @@ func TestAppendEmptyMachine(t *testing.T) {
 	machine := Machine{}
 
 	testAppend(machine, "", t)
+}
+
+var deviceNVDIMMString = "-device nvdimm,id=nv0,memdev=mem0"
+
+func TestAppendDeviceNVDIMM(t *testing.T) {
+	device := Device{
+		Type:   "nvdimm",
+		ID:     "nv0",
+		MemDev: "mem0",
+	}
+
+	testAppend(device, deviceNVDIMMString, t)
+}
+
+var deviceFSString = "-device virtio-9p-pci,fsdev=workload9p,mount_tag=rootfs"
+
+func TestAppendDeviceFS(t *testing.T) {
+	device := Device{
+		Type:     "virtio-9p-pci",
+		FSDev:    "workload9p",
+		MountTag: "rootfs",
+	}
+
+	testAppend(device, deviceFSString, t)
+}
+
+func TestAppendEmptyDevice(t *testing.T) {
+	device := Device{}
+
+	testAppend(device, "", t)
 }
