4f92cf9d2f55e78fa7a3e74653e44812cf450008
Hui Zhu <teawater@hyper.sh>
2017-12-08 09:09:04 +0800

    TestShimOps: Change os.Stdin to pty.Open
    Got some errors with test:
    INFO[0000] Error getting size error="inappropriate ioctl for device" name=kata-shim pid=96301
    INFO[0000] Error getting size error="inappropriate ioctl for device" name=kata-shim pid=96301
    The reason is os.Stdin in test is not tty.
    
    This patch replace os.Stdin with github.com/kr/pty.Open to handle the issues.
    
    Signed-off-by: Hui Zhu <teawater@hyper.sh>

--- shim_test.go
+++ shim_test.go
@@ -9,8 +9,8 @@ import (
 	"os/signal"
 	"sync"
 	"testing"
-	"os"
 
+	"github.com/kr/pty"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -42,7 +42,11 @@ func TestShimOps(t *testing.T) {
 	assert.Nil(t, err, "%s", err)
 	defer shim.agent.Close()
 
-	shim.resizeTty(os.Stdin)
+	_, tty, err := pty.Open()
+	assert.Nil(t, err, "%s", err)
+	defer tty.Close()
+
+	shim.resizeTty(tty)
 
 	wg := &sync.WaitGroup{}
 	shim.proxyStdio(wg)
@@ -50,7 +54,7 @@ func TestShimOps(t *testing.T) {
 	sigc := shim.forwardAllSignals()
 	defer signal.Stop(sigc)
 
-	shim.monitorTtySize(os.Stdin)
+	shim.monitorTtySize(tty)
 
 	status, err := shim.wait()
 	assert.Nil(t, err, "%s", err)
