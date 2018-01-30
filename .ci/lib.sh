1c488951e07763173599f0ab5cf567b6183ff127
James O. D. Hunt <james.o.hunt@intel.com>
2018-01-30 14:48:06 +0000

    CI: Enable checkcommits
    Run the central static check script to enable `checkcommits`.
    
    Fixes #8.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

---
+++ .ci/lib.sh
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+export tests_repo="github.com/kata-containers/tests"
+export tests_repo_dir="$GOPATH/src/$tests_repo"
+
+clone_tests_repo()
+{
+	# KATA_CI_NO_NETWORK is (has to be) ignored if there is
+	# no existing clone.
+	if [ -d "$tests_repo_dir" -a -n "$KATA_CI_NO_NETWORK" ]
+	then
+		return
+	fi
+
+	go get -d -u "$tests_repo" || true
+}
+
+run_static_checks()
+{
+	clone_tests_repo
+	bash "$tests_repo_dir/.ci/static-checks.sh"
+}
