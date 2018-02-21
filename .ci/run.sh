184632d240673c312b9e2ae3d96f078f706e2a2b
Salvador Fuentes <salvador.fuentes@intel.com>
2018-02-21 09:09:05 -0600

    CI: Add jenkins_ci_job.sh
    jenkins_ci_job.sh will be used by the kata-containers repos
    to setup the environment and execute the functional and
    integration tests.
    
    Fixes #90.
    
    Signed-off-by: Salvador Fuentes <salvador.fuentes@intel.com>

---
+++ .ci/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright (c) 2017-2018 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# This script will execute the Kata Containers Test Suite. 
+
+set -e
+
+export RUNTIME="kata-runtime"
+
+sudo -E PATH="$PATH" bash -c "make check"
