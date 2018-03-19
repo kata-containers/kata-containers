734c387f4ed7bbcb993c5029ec5266468b1cdb4f
James O. D. Hunt <james.o.hunt@intel.com>
2018-03-19 20:13:02 +0000

    CI: Add virtcontainers setup
    Update the CI setup script to handle virtcontainers CI setup too.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -14,3 +14,9 @@ clone_tests_repo
 pushd "${tests_repo_dir}"
 .ci/setup.sh
 popd
+
+echo "Setup virtcontainers environment"
+chronic sudo -E PATH=$PATH bash -c "${cidir}/../virtcontainers/utils/virtcontainers-setup.sh"
+
+echo "Install virtcontainers"
+make -C "${cidir}/../virtcontainers" && chronic sudo make -C "${cidir}/../virtcontainers" install
