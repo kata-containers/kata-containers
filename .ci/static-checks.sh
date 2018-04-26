191089fa8f323244fff74c54ae16c630d553dd7c
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2018-04-26 08:37:34 -0500

    ci: Add scripts needed by CI
    Add required files to execute CI.
    
    Fixes: #21
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

---
+++ .ci/static-checks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright (c) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# This script should run any static analysis check
+# It is called by the CI setup
+true
