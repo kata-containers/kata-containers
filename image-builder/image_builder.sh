e86380aab79f0e8b3ee8019a65c45c229ab9e25b
Penny Zheng <Penny.Zheng@arm.com>
2018-06-12 02:03:48 +0000

    rootfs: add '-rm' to delete intermediate container
    If we set env USE_DOCKER true, we will use container as development
    environment. After docker run command, this temporary container
    would be no use. we could add -rm flag to automatically delete
    intermediate container.
    
    Fixes: #115
    
    Signed-off-by: Penny Zheng <penny.zheng@arm.com>

--- image-builder/image_builder.sh
+++ image-builder/image_builder.sh
@@ -98,6 +98,7 @@ if [ -n "${USE_DOCKER}" ] ; then
 	# In case Clear Containers Runtime is installed we dont want to hit issue:
 	#https://github.com/clearcontainers/runtime/issues/828
 	docker run  \
+		--rm \
 		--runtime runc  \
 		--privileged \
 		--env IMG_SIZE="${IMG_SIZE}" \
