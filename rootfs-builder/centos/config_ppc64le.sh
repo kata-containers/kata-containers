22f04db80d7431168f9683ff9ef5d58a27a52e50
Nitesh Konkar <niteshkonkar@in.ibm.com>
2018-06-29 21:15:43 +0530

    travis: Enable travis ci for ppc64le
    Fixes: #122
    
    Signed-off-by: Nitesh Konkar <niteshkonkar@in.ibm.com>

---
+++ rootfs-builder/centos/config_ppc64le.sh
@@ -0,0 +1,18 @@
+#
+# Copyright (c) 2018 IBM
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Base Repos
+BASE_URL="http://mirror.centos.org/altarch/${OS_VERSION}/os/${ARCH}/"
+
+# Additional Repos
+CENTOS_UPDATES_URL="http://mirror.centos.org/altarch/${OS_VERSION}/updates/${ARCH}/"
+
+CENTOS_EXTRAS_URL="http://mirror.centos.org/altarch/${OS_VERSION}/extras/${ARCH}/"
+
+CENTOS_PLUS_URL="http://mirror.centos.org/altarch/${OS_VERSION}/centosplus/${ARCH}/"
+
+GPG_KEY_ARCH_URL="http://mirror.centos.org/altarch/7/os/ppc64le/RPM-GPG-KEY-CentOS-SIG-AltArch-7-ppc64le"
+
+GPG_KEY_ARCH_FILE="RPM-GPG-KEY-CentOS-SIG-AltArch-7-ppc64le"
