976b883501c956762f85a303a094b58370bde0d1
Eric Ernst <eric.ernst@intel.com>
2018-07-10 23:06:31 +0000

    kata-deploy: cleanup, updated for 1.1.0
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- kata-deploy/scripts/remove-kata-crio.sh
+++ kata-deploy/scripts/remove-kata-crio.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "deleting kata artifacts"
 rm -rf /opt/kata/
-rm -rf /usr/sahre/defaults/kata-containers
+rm -rf /usr/share/defaults/kata-containers
 mv /etc/crio/crio.conf.bak /etc/crio/crio.conf
