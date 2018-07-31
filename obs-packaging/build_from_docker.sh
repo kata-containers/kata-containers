afe2dff5c3ca9300df548d1cd955bd2c5704bab2
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2018-07-31 18:59:38 -0500

    obs: create-repo-branch.sh: New script to create OBS repos.
    If want to create pkgs based in new branch, this script will create
    all the empty repositories in OBS for each kata package.
    
    Then we can point use the rest of scripts to push changes to this new repo.
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- obs-packaging/build_from_docker.sh
+++ obs-packaging/build_from_docker.sh
@@ -12,49 +12,27 @@ set -o pipefail
 
 script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 script_name="$(basename "${BASH_SOURCE[0]}")"
-cache_dir=${PWD}/obs-cache
 #where packaing repo lives
 packaging_repo_dir=$(cd "${script_dir}/.." && pwd)
-#where results will be stored
-host_datadir="${PWD}/pkgs"
-obs_image="obs-kata"
 export USE_DOCKER=1
 http_proxy=${http_proxy:-}
 https_proxy=${https_proxy:-}
 no_proxy=${no_proxy:-}
 PUSH=${PUSH:-}
 
+# shellcheck source=scripts/obs-docker.sh
+source "${script_dir}/scripts/obs-docker.sh"
+
 GO_ARCH=$(go env GOARCH)
 export GO_ARCH
 
-docker_run() {
-	local cmd="$@"
-	sudo docker run \
-		--rm \
-		-v "${HOME}/.ssh":/root/.ssh \
-		-v "${HOME}/.gitconfig":/root/.gitconfig \
-		-v /etc/profile:/etc/profile \
-		--env GO_ARCH="${GO_ARCH}" \
-		--env http_proxy="${http_proxy}" \
-		--env https_proxy="${https_proxy}" \
-		--env no_proxy="${no_proxy}" \
-		--env PUSH="${PUSH}" \
-		--env DEBUG="${DEBUG:-}" \
-		--env OBS_SUBPROJECT="${OBS_SUBPROJECT:-}" \
-		-v "${HOME}/.bashrc":/root/.bashrc \
-		-v "$cache_dir":/var/tmp/osbuild-packagecache/ \
-		-v "$packaging_repo_dir":${packaging_repo_dir} \
-		-v "$host_datadir":/var/packaging \
-		-v "$HOME/.oscrc":/root/.oscrc \
-		-ti "$obs_image" bash -c "${cmd}"
-}
 usage() {
 	msg="${1:-}"
 	exit_code=$"${2:-0}"
 	cat <<EOT
 ${msg}
 Usage:
-${script_name} <kata-branch>
+${script_name} <kata-branch/tag>
 EOT
 	exit "${exit_code}"
 }
@@ -69,16 +47,8 @@ main() {
 	image_tarball=$(find . -name 'kata-containers-'"${branch}"'-*.tar.gz')
 	[ -f "${image_tarball}" ] || die "image not found"
 	popd >>/dev/null
-	sudo docker build \
-		--build-arg http_proxy="${http_proxy}" \
-		--build-arg https_proxy="${https_proxy}" \
-		--build-arg GO_ARCH="${GO_ARCH}" \
-		-t $obs_image "${script_dir}"
-
-	#Create/update OBS repository for branch
-	#docker_run "${packaging_repo_dir}/obs-packaging/create-pkg-branch.sh ${branch}"
 	#Build all kata packages
 	docker_run "${packaging_repo_dir}/obs-packaging/build_all.sh ${branch}"
 }
 
-main $@
+main "$@"
