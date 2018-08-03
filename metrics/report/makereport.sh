6e204b3afe8995339cc6696413414d02d84c676f
Graham Whaley <graham.whaley@intel.com>
2018-08-03 17:54:32 +0100

    metrics: reportgen: Add metrics report generation scripts
    Add a set of scripts and Dockerfiles that allow metrics data to
    be gathered, processed and presented in a mostly automated and
    repeatable manner.
    
    The package provides:
     - A script to gather metrics data for the system under test
     - A set of Dockerfile/RMarkdown/R files for processing and presenting
      the gathered data
     - A script to drive the Dockerfile/R files to generate a PDF report.
    
    The scripts can process 1-n sets of data. If n=2 then some parts of
    the report will add an additional set of 'diff' data for easier
    comparison.
    
    Fixes: #596
    
    Signed-off-by: Graham Whaley <graham.whaley@intel.com>

---
+++ metrics/report/makereport.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Copyright (c) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Take the data found in subdirectories of the metrics 'results' directory,
+# and turn them into a PDF report. Use a Dockerfile containing all the tooling
+# and scripts we need to do that.
+
+set -e
+
+SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
+source "${SCRIPT_PATH}/../lib/common.bash"
+
+IMAGE="${IMAGE:-metrics-report}"
+DOCKERFILE="${SCRIPT_PATH}/report_dockerfile/Dockerfile"
+
+HOSTINPUTDIR="${SCRIPT_PATH}/../results"
+RENVFILE="${HOSTINPUTDIR}/Env.R"
+HOSTOUTPUTDIR="${SCRIPT_PATH}/output"
+
+GUESTINPUTDIR="/inputdir/"
+GUESTOUTPUTDIR="/outputdir/"
+
+setup() {
+	echo "Checking Dockerfile"
+	check_dockerfiles_images "$IMAGE" "$DOCKERFILE"
+
+	mkdir -p "$HOSTOUTPUTDIR" && true
+
+	echo "inputdir=\"${GUESTINPUTDIR}\"" > ${RENVFILE}
+	echo "outputdir=\"${GUESTOUTPUTDIR}\"" >> ${RENVFILE}
+
+	# A bit of a hack to get an R syntax'd list of dirs to process
+	# Also, need it as not host-side dir path - so short relative names
+	resultdirs="$(cd ${HOSTINPUTDIR}; ls -dx */)"
+	resultdirslist=$(echo ${resultdirs} | sed 's/ \+/", "/g')
+	echo "resultdirs=c(" >> ${RENVFILE}
+	echo "	\"${resultdirslist}\"" >> ${RENVFILE}
+	echo ")" >> ${RENVFILE}
+}
+
+run() {
+	docker run -ti --rm -v ${HOSTINPUTDIR}:${GUESTINPUTDIR} -v ${HOSTOUTPUTDIR}:${GUESTOUTPUTDIR} ${IMAGE}
+}
+
+setup
+run
+ls -la ${HOSTOUTPUTDIR}/*
