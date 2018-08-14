d14b9463da4b0bf915ca1d9bab073dc0c61e54bd
Eric Ernst <eric.ernst@intel.com>
2018-08-14 13:06:44 -0700

    kata-deploy: changes for 1.2.0 release
    1.2.0 release changed the tarball file layout for the
    Kata artifacts.  Adjust scripts accordingly.
    
    Fixes: #142
    
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- kata-deploy/scripts/install-kata-crio.sh
+++ kata-deploy/scripts/install-kata-crio.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
 echo "copying kata artifacts onto host"
-cp -R /opt/kata-artifacts/bin /opt/kata/
-mkdir /opt/kata/share
-mv /opt/kata/bin/qemu /opt/kata/share/
+cp -R /opt/kata-artifacts/opt/kata/* /opt/kata/
 chmod +x /opt/kata/bin/*
-cp /opt/kata-artifacts/configuration.toml /usr/share/defaults/kata-containers/configuration.toml
 
-# Update Kata configuration for /opt/kata path usage
-sed -i 's!/usr.*kata-containers/!/opt/kata/bin/!' /usr/share/defaults/kata-containers/configuration.toml
-sed -i 's!/usr/bin/!/opt/kata/bin/!' /usr/share/defaults/kata-containers/configuration.toml
-sed -i 's!qemu-lite!qemu!' /usr/share/defaults/kata-containers/configuration.toml
-
+# Configure crio to use Kata:
 echo "Set Kata containers as default runtime in CRI-O for untrusted workloads"
 cp /etc/crio/crio.conf /etc/crio/crio.conf.bak
 sed -i '/runtime_untrusted_workload = /c\runtime_untrusted_workload = "/opt/kata/bin/kata-runtime"' /etc/crio/crio.conf
--- kata-deploy/scripts/remove-kata-crio.sh
+++ kata-deploy/scripts/remove-kata-crio.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 echo "deleting kata artifacts"
 rm -rf /opt/kata/
-rm -rf /usr/share/defaults/kata-containers
 mv /etc/crio/crio.conf.bak /etc/crio/crio.conf
