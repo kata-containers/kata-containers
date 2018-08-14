2d07689ffec165431aa18e3762d9ade07f112998
Eric Ernst <eric.ernst@intel.com>
2018-08-14 13:06:44 -0700

    kata-deploy: changes for 1.2.0 release
    1.2.0 release changed the tarball file layout for the
    Kata artifacts.  Adjust scripts accordingly.
    
    Fixes: #142
    
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- kata-deploy/scripts/remove-kata-containerd.sh
+++ kata-deploy/scripts/remove-kata-containerd.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo "delete kata artifacts"
 rm -rf /opt/kata
-rm -rf /usr/share/defaults/kata-containers
 rm -f /etc/containerd/config.toml
 
 if [ -f /etc/containerd/config.toml.bak ]; then
