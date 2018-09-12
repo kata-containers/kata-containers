d75841ef23802e63a9103227ac243fb8a7c5ae2b
Peng Tao <bergwolf@gmail.com>
2018-09-12 11:27:26 +0800

    ut: add more UTs
    Let's make codecov happier;)
    
    Signed-off-by: Peng Tao <bergwolf@gmail.com>

--- virtcontainers/cc_proxy_test.go
+++ virtcontainers/cc_proxy_test.go
@@ -7,10 +7,22 @@ package virtcontainers
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCCProxyStart(t *testing.T) {
 	proxy := &ccProxy{}
 
 	testProxyStart(t, nil, proxy)
+}
+
+func TestCCProxy(t *testing.T) {
+	proxy := &ccProxy{}
+	assert := assert.New(t)
+
+	err := proxy.stop(0)
+	assert.Nil(err)
+
+	assert.False(proxy.consoleWatched())
 }
