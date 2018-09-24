a588140bc617cd000ff00aa5859f50b61c5f38b8
Nitesh Konkar <niteshkonkar@in.ibm.com>
2018-09-25 01:39:43 +0530

    ci: Improve debugging info on travis CI run
    Fixes: #174
    
    Signed-off-by: Nitesh Konkar niteshkonkar@in.ibm.com

--- .ci/run.sh
+++ .ci/run.sh
@@ -12,4 +12,4 @@ export GOPATH="${GOPATH:-/tmp/go}"
 
 script_dir="$(dirname $(readlink -f $0))"
 
-sudo -E PATH="$PATH" bash "${script_dir}/../tests/test_images.sh"
+sudo -E PATH="$PATH" bash -x "${script_dir}/../tests/test_images.sh"
