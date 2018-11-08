49d8e673985deb0130bff8b0c0d3742ffa63bdcd
Nitesh Konkar <niteshkonkar@in.ibm.com>
2018-11-09 00:27:44 +0530

    rootfs: Conditionally add libseccomp support in rootfs image
    If the rootfs is built with SECCOMP=yes environment
    variable then include libseccomp package inside the
    rootfs image. Else do not include it.
    
    Fixes: #155
    
    Signed-off-by: Nitesh Konkar niteshkonkar@in.ibm.com

--- rootfs-builder/euleros/config.sh
+++ rootfs-builder/euleros/config.sh
@@ -25,3 +25,5 @@ ARCH_EXCLUDE_LIST=()
 # Allow the build to fail without generating an error.
 # For more info see: https://github.com/kata-containers/osbuilder/issues/190
 BUILD_CAN_FAIL=1
+
+[ "$SECCOMP" = "yes" ] && PACKAGES+=" libseccomp" || true
--- rootfs-builder/rootfs.sh
+++ rootfs-builder/rootfs.sh
@@ -326,6 +326,7 @@ if [ -n "${USE_DOCKER}" ] ; then
 		--env EXTRA_PKGS="${EXTRA_PKGS}" \
 		--env OSBUILDER_VERSION="${OSBUILDER_VERSION}" \
 		--env INSIDE_CONTAINER=1 \
+		--env SECCOMP="${SECCOMP}" \
 		-v "${script_dir}":"/osbuilder" \
 		-v "${ROOTFS_DIR}":"/rootfs" \
 		-v "${script_dir}/../scripts":"/scripts" \
@@ -352,7 +353,7 @@ pushd "${GOPATH_LOCAL}/src/${GO_AGENT_PKG}"
 [ -n "${AGENT_VERSION}" ] && git checkout "${AGENT_VERSION}" && OK "git checkout successful"
 make clean
 make INIT=${AGENT_INIT}
-make install DESTDIR="${ROOTFS_DIR}" INIT=${AGENT_INIT}
+make install DESTDIR="${ROOTFS_DIR}" INIT=${AGENT_INIT} SECCOMP=${SECCOMP}
 popd
 
 AGENT_DIR="${ROOTFS_DIR}/usr/bin"
