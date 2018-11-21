b0b57cf474321d7c39c54481cc4edf4d849d35d0
Salvador Fuentes <salvador.fuentes@intel.com>
2018-11-21 12:27:43 -0600

    k8s: Update scripts for Kubernetes 1.12
    Update init and cleanup scripts.
    - Use updated flannel config file, taken from the
      documentation documents, which is now downloaded from
      the coreos/flannel repository.
    - Use a yaml configuration file to initialize kubeadm.
    - Remove workaround to delete pods after the kubeadm reset.
    
    Fixes: #925.
    Fixes: #296.
    
    Signed-off-by: Salvador Fuentes <salvador.fuentes@intel.com>

--- integration/kubernetes/cleanup_env.sh
+++ integration/kubernetes/cleanup_env.sh
@@ -4,31 +4,34 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# This script is used to reset the kubernetes cluster
 
 SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
 source "${SCRIPT_PATH}/../../lib/common.bash"
 
+cri_runtime="${CRI_RUNTIME:-crio}"
+
+case "${cri_runtime}" in
+containerd)
+	cri_runtime_socket="/run/containerd/containerd.sock"
+	;;
+crio)
+	cri_runtime_socket="/var/run/crio/crio.sock"
+	;;
+*)
+	echo "Runtime ${cri_runtime} not supported"
+	;;
+esac
+
 export KUBECONFIG=/etc/kubernetes/admin.conf
-sudo -E kubeadm reset --cri-socket=/var/run/crio/crio.sock
+sudo -E kubeadm reset -f --cri-socket="${cri_runtime_socket}"
 
-# Workaround to delete pods using crictl
-# Needed until https://github.com/kubernetes/kubeadm/issues/748
-# gets fixed
-for ctr in $(sudo crictl ps --quiet); do
-	sudo crictl stop "$ctr"
-	sudo crictl rm "$ctr"
-done
-for pod in $(sudo crictl pods --quiet); do
-	sudo crictl stopp "$pod"
-	sudo crictl rmp "$pod"
-done
-
-sudo systemctl stop crio
+sudo systemctl stop "${cri_runtime}"
 
 sudo ip link set dev cni0 down
 sudo ip link set dev flannel.1 down
 sudo ip link del cni0
 sudo ip link del flannel.1
 
-# Check no processes are left behind
+# Check no kata processes are left behind after reseting kubernetes
 check_processes
