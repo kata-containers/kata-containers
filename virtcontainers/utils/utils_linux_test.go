d67a81b8a9d1dab154cf3bcef5c615f210535a7c
Alice Frosi <afrosi@de.ibm.com>
2018-11-28 17:02:05 +0100

    virtcontainers: change uint32 to uint64 for ioctl
    The PR changes the parameter args from uint32 to uint64 for ioctl function.
    That leads to an endianess bug.
    
    Fixes: #947
    
    Signed-off-by: Alice Frosi <afrosi@de.ibm.com>

--- virtcontainers/utils/utils_linux_test.go
+++ virtcontainers/utils/utils_linux_test.go
@@ -15,7 +15,7 @@ import (
 func TestFindContextID(t *testing.T) {
 	assert := assert.New(t)
 
-	ioctlFunc = func(fd uintptr, request int, arg1 uint32) error {
+	ioctlFunc = func(fd uintptr, request int, arg1 uint64) error {
 		return errors.New("ioctl")
 	}
 
