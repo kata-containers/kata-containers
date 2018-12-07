18bd138dcbf71420f2238790507e882b797aa049
Salvador Fuentes <salvador.fuentes@intel.com>
2018-12-07 14:10:21 -0600

    k8s: add configuration for runtimeClass feature
    We will now be able to use the runtimeClass feature.
    In addition, the old trusted/untrusted annotations
    will also work.
    
    To configure cri-o and k8s to use the new runtimeClass feature,
    simply set `use_runtime_class=true` as an environment variable.
    
    Fixes: #982.
    
    Signed-off-by: Salvador Fuentes <salvador.fuentes@intel.com>

--- .ci/install_kubernetes.sh
+++ .ci/install_kubernetes.sh
@@ -5,10 +5,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-set -e
+set -o errexit
+set -o nounset
+set -o pipefail
 
 cidir=$(dirname "$0")
 source "${cidir}/lib.sh"
+
+# `use_runtime_class` should be set to:
+# - true if we will test using k8s RuntimeClass feature or
+# - false (default) if we will test using the old trusted/untrusted annotations.
+use_runtime_class=${use_runtime_class:-false}
 
 echo "Install Kubernetes components"
 
@@ -27,3 +34,10 @@ EOF"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 chronic sudo -E apt update
 chronic sudo -E apt install --allow-downgrades -y kubelet="$kubernetes_version" kubeadm="$kubernetes_version" kubectl="$kubernetes_version"
+
+if [ "${use_runtime_class}"  == true ]; then
+	kubelet_systemd_file="/etc/systemd/system/kubelet.service.d/10-kubeadm.conf"
+	feature_gate="--feature-gates RuntimeClass=true"
+	echo "Configure Kubelet service to enable RuntimeClass feature"
+	sudo sed -i "s/ExecStart=\/.*$/& $feature_gate/" "$kubelet_systemd_file"
+fi
