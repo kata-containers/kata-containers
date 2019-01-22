42837a9b4cff3d8b01b8dd6152908d766ba37d1d
Nitesh Konkar <niteshkonkar@in.ibm.com>
2019-01-22 22:04:52 +0530

    runtime: Check SMT=on/off only for P8 or lesser
    Set testCPUInfoTemplate to systems /proc/cpuinfo
    and check if SMT is on/off only on P8 and lower
    systems as Power 9 systems support virtualization
    irrespective of SMT being on/off.
    
    Fixes: #1114
    
    Signed-off-by: Nitesh Konkar niteshkonkar@in.ibm.com

--- cli/kata-check_data_ppc64le_test.go
+++ cli/kata-check_data_ppc64le_test.go
@@ -5,110 +5,20 @@
 
 package main
 
-const testCPUInfoTemplate = `
-processor	: 0
-cpu		: POWER8E (raw), altivec supported
-clock		: 3690.000000MHz
-revision	: 2.1 (pvr 004b 0201)
+import (
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+)
 
-processor	: 8
-cpu		: POWER8E (raw), altivec supported
-clock		: 3690.000000MHz
-revision	: 2.1 (pvr 004b 0201)
+var testCPUInfoTemplate = setTestCPUInfoTemplate()
 
-processor	: 16
-cpu		: POWER8E (raw), altivec supported
-clock		: 2360.000000MHz
-revision	: 2.1 (pvr 004b 0201)
+func setTestCPUInfoTemplate() string {
 
-processor	: 24
-cpu		: POWER8E (raw), altivec supported
-clock		: 2061.000000MHz
-revision	: 2.1 (pvr 004b 0201)
+	var kataLog *logrus.Entry
+	content, err := ioutil.ReadFile("/proc/cpuinfo")
 
-processor	: 32
-cpu		: POWER8E (raw), altivec supported
-clock		: 3690.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 40
-cpu		: POWER8E (raw), altivec supported
-clock		: 3690.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 48
-cpu		: POWER8E (raw), altivec supported
-clock		: 3690.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 56
-cpu		: POWER8E (raw), altivec supported
-clock		: 3690.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 64
-cpu		: POWER8E (raw), altivec supported
-clock		: 3690.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 72
-cpu		: POWER8E (raw), altivec supported
-clock		: 3059.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 80
-cpu		: POWER8E (raw), altivec supported
-clock		: 2693.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 88
-cpu		: POWER8E (raw), altivec supported
-clock		: 2061.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 96
-cpu		: POWER8E (raw), altivec supported
-clock		: 3690.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 104
-cpu		: POWER8E (raw), altivec supported
-clock		: 2061.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 112
-cpu		: POWER8E (raw), altivec supported
-clock		: 2061.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 120
-cpu		: POWER8E (raw), altivec supported
-clock		: 2061.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 128
-cpu		: POWER8E (raw), altivec supported
-clock		: 3690.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 136
-cpu		: POWER8E (raw), altivec supported
-clock		: 2061.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 144
-cpu		: POWER8E (raw), altivec supported
-clock		: 2294.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-processor	: 152
-cpu		: POWER8E (raw), altivec supported
-clock		: 2560.000000MHz
-revision	: 2.1 (pvr 004b 0201)
-
-timebase	: 512000000
-platform	: PowerNV
-model		: 8247-22L
-machine		: PowerNV 8247-22L
-firmware	: OPAL v3
-`
+	if err != nil {
+		kataLog.WithError(err).Error("failed to read file /proc/cpuinfo")
+	}
+	return string(content)
+}
