sameo/runtime-1

virtcontainers: Add a Store manager
Each virtcontainers module/component should be able to get a handler on
a Store for loading component specific items. The Store manager is an
internal Store layer for tracking all created Stores.

Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

virtcontainers/store/manager.go
@@ -7,6 +7,7 @@ package store
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 	"sync"
 
@@ -85,6 +86,48 @@ type Store struct {
 	scheme string
 	path   string
 	host   string
+}
+
+type manager struct {
+	sync.RWMutex
+	stores map[string]*Store
+}
+
+var stores = &manager{stores: make(map[string]*Store)}
+
+func (m *manager) addStore(s *Store) (err error) {
+	if s == nil {
+		return nil
+	}
+
+	m.Lock()
+	defer m.Unlock()
+
+	if m.stores[s.root] != nil {
+		return fmt.Errorf("Store %s already added", s.root)
+	}
+
+	m.stores[s.root] = s
+
+	return nil
+}
+
+func (m *manager) removeStore(root string) {
+	m.Lock()
+	defer m.Unlock()
+
+	delete(m.stores, root)
+}
+
+func (m *manager) findStore(root string) *Store {
+	m.RLock()
+	defer m.RUnlock()
+
+	if s := m.stores[root]; s != nil {
+		return s
+	}
+
+	return nil
 }
 
 // New will return a new virtcontainers Store.

kata-containers/kata-containers

virtcontainers/store/manager.go
@@ -7,6 +7,7 @@ package store
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 	"sync"
 
@@ -85,6 +86,48 @@ type Store struct {
 	scheme string
 	path   string
 	host   string
+}
+
+type manager struct {
+	sync.RWMutex
+	stores map[string]*Store
+}
+
+var stores = &manager{stores: make(map[string]*Store)}
+
+func (m *manager) addStore(s *Store) (err error) {
+	if s == nil {
+		return fmt.Errorf("Store can not be nil")
+	}
+
+	if s.url == "" {
+		return fmt.Errorf("Store URL can not be nil")
+	}
+
+	m.Lock()
+	defer m.Unlock()
+
+	if m.stores[s.url] != nil {
+		return fmt.Errorf("Store %s already added", s.url)
+	}
+
+	m.stores[s.url] = s
+
+	return nil
+}
+
+func (m *manager) removeStore(url string) {
+	m.Lock()
+	defer m.Unlock()
+
+	delete(m.stores, url)
+}
+
+func (m *manager) findStore(url string) *Store {
+	m.RLock()
+	defer m.RUnlock()
+
+	return m.stores[url]
 }
 
 // New will return a new virtcontainers Store.