sameo/runtime-1

virtcontainers: store: Keep track of newly created Stores
When a component creates a new store from a given root path, we add it
to the store manager and return it back when another component asks for
it.

Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

virtcontainers/store/manager.go
@@ -134,18 +134,29 @@ func (m *manager) findStore(root string) *Store {
 // If there is already a Store for the root path, we will re-use it.
 // Otherwise a new Store is created.
 func New(ctx context.Context, root string) (*Store, error) {
+	// Do we already have such store?
+	if s := stores.findStore(root); s != nil {
+		return s, nil
+	}
+
 	u, err := url.Parse(root)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Store{
+	s := &Store{
 		ctx:    ctx,
 		root:   root,
 		scheme: u.Scheme,
 		path:   u.Path,
 		host:   u.Host,
-	}, nil
+	}
+
+	if err := stores.addStore(s); err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
 
 var storeLog = logrus.WithField("source", "virtcontainers/store")

kata-containers/kata-containers

virtcontainers/store/manager.go
@@ -134,18 +134,29 @@ func (m *manager) findStore(url string) *Store {
 // If there is already a Store for the URL, we will re-use it.
 // Otherwise a new Store is created.
 func New(ctx context.Context, storeURL string) (*Store, error) {
+	// Do we already have such store?
+	if s := stores.findStore(storeURL); s != nil {
+		return s, nil
+	}
+
 	u, err := url.Parse(storeURL)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Store{
+	s := &Store{
 		ctx:    ctx,
 		url:    storeURL,
 		scheme: u.Scheme,
 		path:   u.Path,
 		host:   u.Host,
-	}, nil
+	}
+
+	if err := stores.addStore(s); err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
 
 var storeLog = logrus.WithField("source", "virtcontainers/store")