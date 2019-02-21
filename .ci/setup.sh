618aeba54097f158f6958bc558e76c578314333c
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2019-02-21 07:42:44 -0600

    ci: Run all CI test.
    run all CI test to increase testing coverage on kernel config changes.
    
    Fixes: #346
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -20,3 +20,24 @@ if [ "$ID" == ubuntu ]; then
 	echo "Install kernel dependencies"
 	sudo -E apt install -y libelf-dev bc gcc
 fi
+
+export tests_repo="${tests_repo:-github.com/kata-containers/tests}"
+export tests_repo_dir="$GOPATH/src/$tests_repo"
+
+clone_tests_repo()
+{
+	# KATA_CI_NO_NETWORK is (has to be) ignored if there is
+	# no existing clone.
+	if [ -d "$tests_repo_dir" -a -n "${KATA_CI_NO_NETWORK:-}" ]
+	then
+		return
+	fi
+
+	go get -d -u "$tests_repo" || true
+}
+
+clone_tests_repo
+
+pushd "${tests_repo_dir}"
+.ci/setup.sh
+popd
