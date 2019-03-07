2456ac52eb4151d3370bd20de9968fcaf1d07eab
Julio Montes <julio.montes@intel.com>
2019-03-07 08:45:52 -0600

    pkg: reduce memory footprint
    Reduce memory footprint ~7% by disabling some systemd services like
    systemd-journald and systemd-udevd, those services are just consuming memory
    and are not needed. For example kata-agent logs the errors through the proxy.
    
    fixes #1339
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

--- pkg/katautils/config_test.go
+++ pkg/katautils/config_test.go
@@ -1401,7 +1401,7 @@ func TestUpdateRuntimeConfigurationInvalidKernelParams(t *testing.T) {
 		GetKernelParamsFunc = savedFunc
 	}()
 
-	GetKernelParamsFunc = func(needSystemd bool) []vc.Param {
+	GetKernelParamsFunc = func(needSystemd, trace bool) []vc.Param {
 		return []vc.Param{
 			{
 				Key:   "",
