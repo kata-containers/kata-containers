13d7bc8e45f5481b28646e4a12baa038e55de7a7
Ruidong Cao <caoruidong@huawei.com>
2019-03-09 03:29:27 +0800

    Network: remove Physical field in VethEndpoint
    This a bool field. It is useless because veth endpoint is never a
    physical endpoint.
    
    Fixes #1343
    
    Signed-off-by: Ruidong Cao <caoruidong@huawei.com>

--- virtcontainers/veth_endpoint.go
+++ virtcontainers/veth_endpoint.go
@@ -15,7 +15,6 @@ import (
 type VethEndpoint struct {
 	NetPair            NetworkInterfacePair
 	EndpointProperties NetworkInfo
-	Physical           bool
 	EndpointType       EndpointType
 	PCIAddr            string
 }
