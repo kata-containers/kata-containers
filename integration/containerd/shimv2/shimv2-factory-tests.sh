eda0ed93d11be3f56aca9ff9c61a8ca242a0e725
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2019-04-04 07:23:15 -0600

    test: shimv2 test not working on RHEL
    As well as SLES, openSUSE the shimv2 tests are not working on
    RHEL.
    
    Fixes #1422
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- integration/containerd/shimv2/shimv2-factory-tests.sh
+++ integration/containerd/shimv2/shimv2-factory-tests.sh
@@ -13,7 +13,7 @@ source "${SCRIPT_PATH}/../../../metrics/lib/common.bash"
 source /etc/os-release || source /usr/lib/os-release
 extract_kata_env
 
-if [[ "$ID" =~ ^opensuse.*$ ]] || [ "$ID" == sles ]; then
+if [[ "$ID" =~ ^opensuse.*$ ]] || [ "$ID" == sles ] || [ "$ID" == rhel ]; then
 	issue="https://github.com/kata-containers/tests/issues/1251"
 	echo "Skip shimv2 on $ID, see: $issue"
 	exit
