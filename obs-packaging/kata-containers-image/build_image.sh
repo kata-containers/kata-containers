ac7eb355e47db1603a147a2bfd8eb8be279b21a6
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2019-04-30 13:55:24 -0500

    image: build: rename varaible
    kata_osbuilder_vesion is already defined in versions.txt
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- obs-packaging/kata-containers-image/build_image.sh
+++ obs-packaging/kata-containers-image/build_image.sh
@@ -32,7 +32,7 @@ arch_target="$(uname -m)"
 kata_version="master"
 
 # osbuilder info
-kata_osbuilder_version="${KATA_OSBUILDER_VERSION:-}"
+osbuider_version="${KATA_OSBUILDER_VERSION:-}"
 # Agent version
 agent_version="${AGENT_VERSION:-}"
 if [ -z "${agent_version}" ]; then
@@ -68,9 +68,9 @@ create_tarball() {
 	agent_sha=$(get_repo_hash "${GOPATH}/src/github.com/kata-containers/agent")
 	#reduce sha size for short names
 	agent_sha=${agent_sha:0:${short_commit_length}}
-	tarball_name="kata-containers-${kata_osbuilder_version}-${agent_sha}-${arch_target}.tar.gz"
-	image_name="kata-containers-image_${img_distro}_${kata_osbuilder_version}_agent_${agent_sha}.img"
-	initrd_name="kata-containers-initrd_${initrd_distro}_${kata_osbuilder_version}_agent_${agent_sha}.initrd"
+	tarball_name="kata-containers-${osbuider_version}-${agent_sha}-${arch_target}.tar.gz"
+	image_name="kata-containers-image_${img_distro}_${osbuider_version}_agent_${agent_sha}.img"
+	initrd_name="kata-containers-initrd_${initrd_distro}_${osbuider_version}_agent_${agent_sha}.initrd"
 
 	mv "${tmp_dir}/osbuilder/kata-containers.img" "${image_name}"
 	mv "${tmp_dir}/osbuilder/kata-containers-initrd.img" "${initrd_name}"
@@ -107,7 +107,7 @@ main() {
 		esac
 	done
 	# osbuilder info
-	[ -n "${kata_osbuilder_version}" ] || kata_osbuilder_version="${kata_version}"
+	[ -n "${osbuider_version}" ] || osbuider_version="${kata_version}"
 	# Agent version
 	[ -n "${agent_version}" ] || agent_version="${kata_version}"
 
@@ -126,7 +126,7 @@ main() {
 	shift "$((OPTIND - 1))"
 	git clone "$osbuilder_url" "${tmp_dir}/osbuilder"
 	pushd "${tmp_dir}/osbuilder"
-	git checkout "${kata_osbuilder_version}"
+	git checkout "${osbuider_version}"
 	build_initrd
 	build_image
 	create_tarball
