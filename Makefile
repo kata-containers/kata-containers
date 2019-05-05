dong-liuliu/runtime

virtiofs: Add cache option
Several cache modes are supported by virtio-fs.  They affect the
performance and consistency characteristics of the file system.

For the time being cache="none" is recommended, but the other modes can
be experimented with.

Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

Makefile
@@ -149,6 +149,7 @@ DEFVIRTIOFS := false
 DEFVIRTIOFSDAEMON :=
 # Default DAX mapping cache size in GiB
 DEFVIRTIOFSCACHESIZE := 2
+DEFVIRTIOFSCACHE := none
 DEFBLOCKSTORAGEDRIVER := virtio-scsi
 DEFENABLEIOTHREADS := false
 DEFENABLEMEMPREALLOC := false
@@ -227,6 +228,7 @@ USER_VARS += DEFDISABLEBLOCK
 USER_VARS += DEFVIRTIOFS
 USER_VARS += DEFVIRTIOFSDAEMON
 USER_VARS += DEFVIRTIOFSCACHESIZE
+USER_VARS += DEFVIRTIOFSCACHE
 USER_VARS += DEFBLOCKSTORAGEDRIVER
 USER_VARS += DEFENABLEIOTHREADS
 USER_VARS += DEFENABLEMEMPREALLOC
@@ -397,6 +399,7 @@ $(GENERATED_FILES): %: %.in Makefile VERSION
 		-e "s|@DEFVIRTIOFS@|$(DEFVIRTIOFS)|g" \
 		-e "s|@DEFVIRTIOFSDAEMON@|$(DEFVIRTIOFSDAEMON)|g" \
 		-e "s|@DEFVIRTIOFSCACHESIZE@|$(DEFVIRTIOFSCACHESIZE)|g" \
+		-e "s|@DEFVIRTIOFSCACHE@|$(DEFVIRTIOFSCACHE)|g" \
 		-e "s|@DEFBLOCKSTORAGEDRIVER@|$(DEFBLOCKSTORAGEDRIVER)|g" \
 		-e "s|@DEFENABLEIOTHREADS@|$(DEFENABLEIOTHREADS)|g" \
 		-e "s|@DEFENABLEMEMPREALLOC@|$(DEFENABLEMEMPREALLOC)|g" \

kata-containers/kata-containers

Makefile
@@ -160,6 +160,7 @@ DEFSHAREDFS := virtio-9p
 DEFVIRTIOFSDAEMON :=
 # Default DAX mapping cache size in MiB
 DEFVIRTIOFSCACHESIZE := 8192
+DEFVIRTIOFSCACHE := always
 DEFENABLEIOTHREADS := false
 DEFENABLEMEMPREALLOC := false
 DEFENABLEHUGEPAGES := false
@@ -327,6 +328,7 @@ USER_VARS += DEFBLOCKSTORAGEDRIVER_QEMU
 USER_VARS += DEFSHAREDFS
 USER_VARS += DEFVIRTIOFSDAEMON
 USER_VARS += DEFVIRTIOFSCACHESIZE
+USER_VARS += DEFVIRTIOFSCACHE
 USER_VARS += DEFENABLEIOTHREADS
 USER_VARS += DEFENABLEMEMPREALLOC
 USER_VARS += DEFENABLEHUGEPAGES
@@ -466,6 +468,7 @@ $(GENERATED_FILES): %: %.in $(MAKEFILE_LIST) VERSION .git-commit
 		-e "s|@DEFSHAREDFS@|$(DEFSHAREDFS)|g" \
 		-e "s|@DEFVIRTIOFSDAEMON@|$(DEFVIRTIOFSDAEMON)|g" \
 		-e "s|@DEFVIRTIOFSCACHESIZE@|$(DEFVIRTIOFSCACHESIZE)|g" \
+		-e "s|@DEFVIRTIOFSCACHE@|$(DEFVIRTIOFSCACHE)|g" \
 		-e "s|@DEFENABLEIOTHREADS@|$(DEFENABLEIOTHREADS)|g" \
 		-e "s|@DEFENABLEMEMPREALLOC@|$(DEFENABLEMEMPREALLOC)|g" \
 		-e "s|@DEFENABLEHUGEPAGES@|$(DEFENABLEHUGEPAGES)|g" \