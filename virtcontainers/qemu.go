933967daae1a873b6cabc59d8932af649809f233
Peng Tao <bergwolf@hyper.sh>
2019-05-06 18:01:05 +0800

    qemu: stop qemu process when virtiofsd quits
    If virtiofsd fails to initialize and stops unexpected,
    qemu might hang forever. We just stop the qemu process.
    Resource cleanup will be done by others.
    
    Fixes: #1690
    Signed-off-by: Peng Tao <bergwolf@hyper.sh>

--- virtcontainers/qemu.go
+++ virtcontainers/qemu.go
@@ -645,6 +645,7 @@ func (q *qemu) startSandbox(timeout int) error {
 				}
 			}
 			q.Logger().Info("virtiofsd quits")
+			q.stopSandbox()
 		}()
 		timeoutDuration := time.Duration(timeout) * time.Second
 		select {
