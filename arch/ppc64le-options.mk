29d18e9aa3a7e998b5f23376ae57efb516868350
Penny Zheng <penny.zheng@arm.com>
2019-05-14 15:05:18 +0800

    filter_scheme: clean redundant filter scheme scripts
    Let's clean all duplicated codes and just retain the specific
    configuration file.
    
    Fixes: #1570
    
    Signed-off-by: Penny Zheng <penny.zheng@arm.com>

--- arch/ppc64le-options.mk
+++ arch/ppc64le-options.mk
@@ -3,8 +3,12 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+# configuration file
+CONFIG := .ci/ppc64le/configuration_ppc64le.yaml
+
 # union for 'make test'
-UNION := $(shell bash -f .ci/ppc64le/filter_test_ppc64le.sh)
+UNION := $(shell bash -c '.ci/filter/filter_test_union.sh $(CONFIG)')
 
 # skiped test suites for docker integration tests
-SKIP := $(shell bash -f .ci/ppc64le/filter_docker_ppc64le.sh)
+SKIP := $(shell bash -c '.ci/filter/filter_docker_test.sh $(CONFIG)')
+
