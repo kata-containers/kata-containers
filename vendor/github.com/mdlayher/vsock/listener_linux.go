c931cf939590bb148c975a792034576b86a82039
Eric Ernst <eric.ernst@intel.com>
2019-06-07 15:36:15 -0700

    shimv2: Revert shimv2, vendor commits
    This reverts:
     - 590ed09 vendor: update gogo/protobuf, containerd and agent vendors
     - eabfd99 shimv2: Improve shim shutdown logic
    
    These introduce a regression for starting pods with k8s 1.14 + contaienr
    1.2.6
    
    Fixes: #1781
    
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- vendor/github.com/mdlayher/vsock/listener_linux.go
+++ vendor/github.com/mdlayher/vsock/listener_linux.go
@@ -4,7 +4,6 @@ package vsock
 
 import (
 	"net"
-	"time"
 
 	"golang.org/x/sys/unix"
 )
@@ -14,14 +13,13 @@ var _ net.Listener = &listener{}
 // A listener is the net.Listener implementation for connection-oriented
 // VM sockets.
 type listener struct {
-	fd   listenFD
+	fd   fd
 	addr *Addr
 }
 
 // Addr and Close implement the net.Listener interface for listener.
-func (l *listener) Addr() net.Addr                { return l.addr }
-func (l *listener) Close() error                  { return l.fd.Close() }
-func (l *listener) SetDeadline(t time.Time) error { return l.fd.SetDeadline(t) }
+func (l *listener) Addr() net.Addr { return l.addr }
+func (l *listener) Close() error   { return l.fd.Close() }
 
 // Accept accepts a single connection from the listener, and sets up
 // a net.Conn backed by conn.
@@ -32,34 +30,49 @@ func (l *listener) Accept() (net.Conn, error) {
 	}
 
 	savm := sa.(*unix.SockaddrVM)
-	remote := &Addr{
+	remoteAddr := &Addr{
 		ContextID: savm.CID,
 		Port:      savm.Port,
 	}
 
-	return newConn(cfd, l.addr, remote)
+	return newConn(cfd, l.addr.fileName(), l.addr, remoteAddr)
 }
 
-// listen is the entry point for Listen on Linux.
-func listen(cid, port uint32) (*Listener, error) {
-	lfd, err := newListenFD()
+// listenStream is the entry point for ListenStream on Linux.
+func listenStream(port uint32) (net.Listener, error) {
+	var cid uint32
+	if err := localContextID(sysFS{}, &cid); err != nil {
+		return nil, err
+	}
+
+	fd, err := unix.Socket(unix.AF_VSOCK, unix.SOCK_STREAM, 0)
 	if err != nil {
 		return nil, err
 	}
 
-	return listenLinux(lfd, cid, port)
+	lfd := &sysFD{fd: fd}
+	return listenStreamLinuxHandleError(lfd, cid, port)
 }
 
-// listenLinux is the entry point for tests on Linux.
-func listenLinux(lfd listenFD, cid, port uint32) (l *Listener, err error) {
-	defer func() {
-		if err != nil {
-			// If any system calls fail during setup, the socket must be closed
-			// to avoid file descriptor leaks.
-			_ = lfd.EarlyClose()
-		}
-	}()
+// listenStreamLinuxHandleError ensures that any errors from listenStreamLinux
+// result in the socket being cleaned up properly.
+func listenStreamLinuxHandleError(lfd fd, cid, port uint32) (net.Listener, error) {
+	l, err := listenStreamLinux(lfd, cid, port)
+	if err != nil {
+		// If any system calls fail during setup, the socket must be closed
+		// to avoid file descriptor leaks.
+		_ = lfd.Close()
+		return nil, err
+	}
 
+	return l, nil
+}
+
+// TODO(mdlayher): fine-tune this number instead of just picking one.
+const listenBacklog = 32
+
+// listenStreamLinux is the entry point for tests on Linux.
+func listenStreamLinux(lfd fd, cid, port uint32) (net.Listener, error) {
 	// Zero-value for "any port" is friendlier in Go than a constant.
 	if port == 0 {
 		port = unix.VMADDR_PORT_ANY
@@ -70,26 +83,20 @@ func listenLinux(lfd listenFD, cid, port uint32) (l *Listener, err error) {
 		Port: port,
 	}
 
+	if err := lfd.SetNonblock(true); err != nil {
+		return nil, err
+	}
+
 	if err := lfd.Bind(sa); err != nil {
 		return nil, err
 	}
 
-	if err := lfd.Listen(unix.SOMAXCONN); err != nil {
+	if err := lfd.Listen(listenBacklog); err != nil {
 		return nil, err
 	}
 
 	lsa, err := lfd.Getsockname()
 	if err != nil {
-		return nil, err
-	}
-
-	// Done with blocking mode setup, transition to non-blocking before the
-	// caller has a chance to start calling things concurrently that might make
-	// the locking situation tricky.
-	//
-	// Note: if any calls fail after this point, lfd.Close should be invoked
-	// for cleanup because the socket is now non-blocking.
-	if err := lfd.SetNonblocking("vsock-listen"); err != nil {
 		return nil, err
 	}
 
@@ -99,10 +106,8 @@ func listenLinux(lfd listenFD, cid, port uint32) (l *Listener, err error) {
 		Port:      lsavm.Port,
 	}
 
-	return &Listener{
-		l: &listener{
-			fd:   lfd,
-			addr: addr,
-		},
+	return &listener{
+		fd:   lfd,
+		addr: addr,
 	}, nil
 }
