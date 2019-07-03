f8b84d7ebad0644f92751773040162349354efd2
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2019-07-03 07:06:08 -0500

    ci: Fix versions_checker.sh
    Version checker does to work today
    
    - Allow to detect stabe branches
    
    Fixes #1581
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- .ci/versions_checker.sh
+++ .ci/versions_checker.sh
@@ -12,20 +12,29 @@
 # This ensures that the rest
 # of the components are merged before the runtime
 
-set -e
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
 
 GOPATH=${GOPATH:-${HOME}/go}
 
 PACKAGING_REPO="github.com/kata-containers/packaging"
 RUNTIME_REPO="github.com/kata-containers/runtime"
+KATA_BRANCH=${target_branch:-master}
 
 go get -d "${PACKAGING_REPO}" || true
 
-check_changes=$(git diff "${GOPATH}/src/${RUNTIME_REPO}/VERSION")
+if ! check_changes=$(git diff --name-only "origin/${KATA_BRANCH}" | grep VERSION); then
+	echo "No changes in VERSION file - this is not a bump - nothing to check"
+	exit 0
+fi
 version_to_check=$(cat "${GOPATH}/src/${RUNTIME_REPO}/VERSION")
 
 if [ ! -z "$check_changes" ]; then
+	echo "Changes detected on VERSION"
+	echo "Check versions in branch ${KATA_BRANCH}"
 	pushd "${GOPATH}/src/${PACKAGING_REPO}"
-	./release/tag_repos.sh pre-release "${version_to_check}" 
+	./release/tag_repos.sh -b "${KATA_BRANCH}" pre-release "${version_to_check}"
 	popd
 fi
