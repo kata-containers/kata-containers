31ddb4d4529c4f710803c01810d4f0436ef84989
lifupan <lifupan@gmail.com>
2019-08-06 11:54:40 +0800

    virtcontainers: add watchconsole for no_proxy type
    For no proxy type, we also need the feature
    of watch hypervisor's console to help debug.
    
    Fixes:#1932
    
    Signed-off-by: lifupan <lifupan@gmail.com>

--- virtcontainers/kata_builtin_proxy.go
+++ virtcontainers/kata_builtin_proxy.go
@@ -5,36 +5,17 @@
 
 package virtcontainers
 
-import (
-	"bufio"
-	"fmt"
-	"io"
-	"net"
-
-	"github.com/sirupsen/logrus"
-)
-
-var buildinProxyConsoleProto = consoleProtoUnix
+import "fmt"
 
 // This is a kata builtin proxy implementation of the proxy interface. Kata proxy
 // functionality is implemented inside the virtcontainers library.
 type kataBuiltInProxy struct {
-	sandboxID string
-	conn      net.Conn
-}
-
-// check if the proxy has watched the vm console.
-func (p *kataBuiltInProxy) consoleWatched() bool {
-	return p.conn != nil
+	proxyBuiltin
 }
 
 func (p *kataBuiltInProxy) validateParams(params proxyParams) error {
 	if len(params.id) == 0 || len(params.agentURL) == 0 || len(params.consoleURL) == 0 {
 		return fmt.Errorf("Invalid proxy parameters %+v", params)
-	}
-
-	if params.logger == nil {
-		return fmt.Errorf("Invalid proxy parameter: proxy logger is not set")
 	}
 
 	return nil
@@ -48,76 +29,5 @@ func (p *kataBuiltInProxy) start(params proxyParams) (int, string, error) {
 		return -1, "", err
 	}
 
-	if p.consoleWatched() {
-		return -1, "", fmt.Errorf("kata builtin proxy running for sandbox %s", params.id)
-	}
-
-	params.logger.Debug("Starting builtin kata proxy")
-
-	p.sandboxID = params.id
-
-	if params.debug {
-		err := p.watchConsole(buildinProxyConsoleProto, params.consoleURL, params.logger)
-		if err != nil {
-			p.sandboxID = ""
-			return -1, "", err
-		}
-	}
-
-	return params.hid, params.agentURL, nil
-}
-
-// stop is the proxy stop implementation for kata builtin proxy.
-func (p *kataBuiltInProxy) stop(pid int) error {
-	if p.conn != nil {
-		p.conn.Close()
-		p.conn = nil
-		p.sandboxID = ""
-	}
-	return nil
-}
-
-func (p *kataBuiltInProxy) watchConsole(proto, console string, logger *logrus.Entry) (err error) {
-	var (
-		scanner *bufio.Scanner
-		conn    net.Conn
-	)
-
-	switch proto {
-	case consoleProtoUnix:
-		conn, err = net.Dial("unix", console)
-		if err != nil {
-			return err
-		}
-	// TODO: add pty console support for kvmtools
-	case consoleProtoPty:
-		fallthrough
-	default:
-		return fmt.Errorf("unknown console proto %s", proto)
-	}
-
-	p.conn = conn
-
-	go func() {
-		scanner = bufio.NewScanner(conn)
-		for scanner.Scan() {
-			logger.WithFields(logrus.Fields{
-				"sandbox":   p.sandboxID,
-				"vmconsole": scanner.Text(),
-			}).Debug("reading guest console")
-		}
-
-		if err := scanner.Err(); err != nil {
-			if err == io.EOF {
-				logger.Info("console watcher quits")
-			} else {
-				logger.WithError(err).WithFields(logrus.Fields{
-					"console-protocol": proto,
-					"console-socket":   console,
-				}).Error("Failed to read agent logs")
-			}
-		}
-	}()
-
-	return nil
+	return p.proxyBuiltin.start(params)
 }
