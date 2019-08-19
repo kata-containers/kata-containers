6534357925c59e43f5e61a8fee7f5e2490f95038
Ace-Tang <aceapril@126.com>
2019-08-19 15:02:35 +0800

    shim-v2: add network stat in metric
    improve metric message, add network stat, base on agent PR: #538 and
    containerd/cgroup PR #81
    
    Fixes: #1976
    
    Signed-off-by: ZeroMagic <anthonyliu@zju.edu.cn>
    Signed-off-by: Ace-Tang <aceapril@126.com>

--- virtcontainers/container.go
+++ virtcontainers/container.go
@@ -194,9 +194,25 @@ type CgroupStats struct {
 	HugetlbStats map[string]HugetlbStats `json:"hugetlb_stats,omitempty"`
 }
 
+// NetworkStats describe all network stats.
+type NetworkStats struct {
+	// Name is the name of the network interface.
+	Name string `json:"name,omitempty"`
+
+	RxBytes   uint64 `json:"rx_bytes,omitempty"`
+	RxPackets uint64 `json:"rx_packets,omitempty"`
+	RxErrors  uint64 `json:"rx_errors,omitempty"`
+	RxDropped uint64 `json:"rx_dropped,omitempty"`
+	TxBytes   uint64 `json:"tx_bytes,omitempty"`
+	TxPackets uint64 `json:"tx_packets,omitempty"`
+	TxErrors  uint64 `json:"tx_errors,omitempty"`
+	TxDropped uint64 `json:"tx_dropped,omitempty"`
+}
+
 // ContainerStats describes a container stats.
 type ContainerStats struct {
-	CgroupStats *CgroupStats
+	CgroupStats  *CgroupStats
+	NetworkStats []*NetworkStats
 }
 
 // ContainerResources describes container resources
