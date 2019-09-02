alicefr/runtime

bridges: abstraction of bridge type
The abstraction of the bridge type to add additional types.

Signed-off-by: Alice Frosi <afrosi@de.ibm.com>

virtcontainers/types/bridges_test.go
@@ -12,15 +12,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAddRemoveDevice(t *testing.T) {
+func testAddRemoveDevice(t *testing.T, b *Bridge) {
 	assert := assert.New(t)
-
-	// create a bridge
-	bridges := []*PCIBridge{{make(map[uint32]string), PCI, "rgb123", 5}}
 
 	// add device
 	devID := "abc123"
-	b := bridges[0]
+
 	addr, err := b.AddDevice(devID)
 	assert.NoError(err)
 	if addr < 1 {
@@ -35,13 +32,21 @@ func TestAddRemoveDevice(t *testing.T) {
 	assert.NoError(err)
 
 	// add device when the bridge is full
-	bridges[0].Address = make(map[uint32]string)
-	for i := uint32(1); i <= pciBridgeMaxCapacity; i++ {
-		bridges[0].Address[i] = fmt.Sprintf("%d", i)
+	b.Address = make(map[uint32]string)
+	for i := uint32(1); i <= b.bridgeMaxCapacity; i++ {
+		b.Address[i] = fmt.Sprintf("%d", i)
 	}
 	addr, err = b.AddDevice(devID)
 	assert.Error(err)
 	if addr != 0 {
 		assert.Fail("address should be 0")
 	}
+}
+
+func TestAddRemoveDevicePCI(t *testing.T) {
+
+	// create a pci bridge
+	bridges := []*Bridge{{make(map[uint32]string), "rgb123", 5, PCI, pciBridgeMaxCapacity}}
+
+	testAddRemoveDevice(t, bridges[0])
 }

kata-containers/kata-containers

Fixes: #1153
Co-authored-by: Jan Schintag <jan.schintag@de.ibm.com>

virtcontainers/types/bridges_test.go
@@ -12,15 +12,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAddRemoveDevice(t *testing.T) {
+func testAddRemoveDevice(t *testing.T, b *Bridge) {
 	assert := assert.New(t)
-
-	// create a bridge
-	bridges := []*PCIBridge{{make(map[uint32]string), PCI, "rgb123", 5}}
 
 	// add device
 	devID := "abc123"
-	b := bridges[0]
+
 	addr, err := b.AddDevice(devID)
 	assert.NoError(err)
 	if addr < 1 {
@@ -35,13 +32,21 @@ func TestAddRemoveDevice(t *testing.T) {
 	assert.NoError(err)
 
 	// add device when the bridge is full
-	bridges[0].Address = make(map[uint32]string)
-	for i := uint32(1); i <= pciBridgeMaxCapacity; i++ {
-		bridges[0].Address[i] = fmt.Sprintf("%d", i)
+	b.Devices = make(map[uint32]string)
+	for i := uint32(1); i <= b.MaxCapacity; i++ {
+		b.Devices[i] = fmt.Sprintf("%d", i)
 	}
 	addr, err = b.AddDevice(devID)
 	assert.Error(err)
 	if addr != 0 {
 		assert.Fail("address should be 0")
 	}
+}
+
+func TestAddRemoveDevicePCI(t *testing.T) {
+
+	// create a pci bridge
+	bridges := []*Bridge{{make(map[uint32]string), "rgb123", 5, PCI, pciBridgeMaxCapacity}}
+
+	testAddRemoveDevice(t, bridges[0])
 }