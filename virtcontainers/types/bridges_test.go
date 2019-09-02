alicefr/runtime

ccw: add ccw type as bridge
On IBM Z, CCW bus type can be additionally used.

Signed-off-by: Alice Frosi <afrosi@de.ibm.com>

virtcontainers/types/bridges_test.go
@@ -50,3 +50,11 @@ func TestAddRemoveDevicePCI(t *testing.T) {
 
 	testAddRemoveDevice(t, bridges[0])
 }
+
+func TestAddRemoveDeviceCCW(t *testing.T) {
+
+	// create a CCW bridge
+	bridges := []*Bridge{{make(map[uint32]string), "rgb123", 5, CCW, ccwBridgeMaxCapacity}}
+
+	testAddRemoveDevice(t, bridges[0])
+}

kata-containers/kata-containers

Fixes: #1153
Reviewed-by: Jan Schintag <jan.schintag@de.ibm.com>

virtcontainers/types/bridges_test.go
@@ -46,7 +46,15 @@ func testAddRemoveDevice(t *testing.T, b *Bridge) {
 func TestAddRemoveDevicePCI(t *testing.T) {
 
 	// create a pci bridge
-	bridges := []*Bridge{{make(map[uint32]string), "rgb123", 5, PCI, pciBridgeMaxCapacity}}
+	bridges := []*Bridge{{make(map[uint32]string), "rgb123", 5, PCI, PCIBridgeMaxCapacity}}
+
+	testAddRemoveDevice(t, bridges[0])
+}
+
+func TestAddRemoveDeviceCCW(t *testing.T) {
+
+	// create a CCW bridge
+	bridges := []*Bridge{{make(map[uint32]string), "rgb123", 5, CCW, CCWBridgeMaxCapacity}}
 
 	testAddRemoveDevice(t, bridges[0])
 }