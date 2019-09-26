gabibeyer/runtime

vc: make cgroup usage configurable if rootless
rootless execution does not yet support cgroups, so if running
rootlessly skip the cgroup creation and deletion.

Fixes: 1877

Signed-off-by: Gabi Beyer <gabrielle.n.beyer@intel.com>

virtcontainers/sandbox.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/kata-containers/agent/protocols/grpc"
+	"github.com/kata-containers/runtime/pkg/rootless"
 	"github.com/kata-containers/runtime/virtcontainers/device/api"
 	"github.com/kata-containers/runtime/virtcontainers/device/config"
 	"github.com/kata-containers/runtime/virtcontainers/device/drivers"
@@ -755,8 +756,10 @@ func (s *Sandbox) Delete() error {
 		}
 	}
 
-	if err := s.deleteCgroups(); err != nil {
-		return err
+	if !rootless.IsRootless() {
+		if err := s.deleteCgroups(); err != nil {
+			return err
+		}
 	}
 
 	globalSandboxList.removeSandbox(s.id)

kata-containers/kata-containers

virtcontainers/sandbox.go
@@ -26,6 +26,7 @@ import (
 	"github.com/vishvananda/netlink"
 
 	"github.com/kata-containers/agent/protocols/grpc"
+	"github.com/kata-containers/runtime/pkg/rootless"
 	"github.com/kata-containers/runtime/virtcontainers/device/api"
 	"github.com/kata-containers/runtime/virtcontainers/device/config"
 	"github.com/kata-containers/runtime/virtcontainers/device/drivers"
@@ -764,8 +765,10 @@ func (s *Sandbox) Delete() error {
 		}
 	}
 
-	if err := s.cgroupsDelete(); err != nil {
-		return err
+	if !rootless.IsRootless() {
+		if err := s.cgroupsDelete(); err != nil {
+			return err
+		}
 	}
 
 	globalSandboxList.removeSandbox(s.id)