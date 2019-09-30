5adc0f644d8b15b70306bb304d4b5b90c8cf00c8
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2019-09-30 11:29:18 -0500

    ci: Add cache for qemu experimental
    This will build qemu experimental using a cache jenkins job.
    
    Fixes #1967
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- .ci/install_qemu_experimental.sh
+++ .ci/install_qemu_experimental.sh
@@ -25,6 +25,7 @@ arch=$("${cidir}"/kata-arch.sh -d)
 INSTALL_LOCATION="/tmp/qemu-virtiofs-static/opt/kata/bin/"
 QEMU_PATH="/opt/kata/bin/qemu-virtiofs-system-x86_64"
 VIRTIOFS_PATH="/opt/kata/bin/virtiofsd"
+qemu_experimental_latest_build_url="http://jenkins.katacontainers.io/job/qemu-experimental-$(uname -m)/lastSuccessfulBuild/artifact/artifacts"
 
 uncompress_experimental_qemu() {
 	local qemu_tar_location="$1"
@@ -32,9 +33,19 @@ uncompress_experimental_qemu() {
 	sudo tar -xf "${qemu_tar_location}" -C /
 }
 
+install_cached_qemu_experimental() {
+	info "Installing cached experimental QEMU"
+	curl -fL --progress-bar "${qemu_experimental_latest_build_url}/${QEMU_TAR}" -o "${QEMU_TAR}" || return 1
+	curl -fsOL "${qemu_experimental_latest_build_url}/sha256sum-${QEMU_TAR}" || return 1
+	sha256sum -c "sha256sum-${QEMU_TAR}" || return 1
+	uncompress_static_qemu "${QEMU_TAR}"
+	sudo -E ln -s "${QEMU_PATH}" "/usr/bin"
+	sudo -E ln -s "${VIRTIOFS_PATH}" "/usr/bin"
+}
+
 build_and_install_static_experimental_qemu() {
 	build_experimental_qemu
-	uncompress_experimental_qemu "${QEMU_TAR}"
+	uncompress_experimental_qemu "${KATA_TESTS_CACHEDIR}/${QEMU_TAR}"
 	sudo -E ln -s "${QEMU_PATH}" "/usr/bin"
 	sudo -E ln -s "${VIRTIOFS_PATH}" "/usr/bin"
 }
@@ -43,14 +54,21 @@ build_experimental_qemu() {
 	mkdir -p "${GOPATH}/src"
 	go get -d "$PACKAGING_REPO" || true
 	"${GOPATH}/src/${PACKAGING_REPO}/static-build/qemu-virtiofs/build-static-qemu-virtiofs.sh"
+	sudo mkdir -p "${KATA_TESTS_CACHEDIR}"
+	sudo mv "${QEMU_TAR}" "${KATA_TESTS_CACHEDIR}"
 }
 
 main() {
 	if [ "$arch" != "x86_64" ]; then
 		die "Unsupported architecture: $arch"
 	fi
-	mkdir -p "${QEMU_LOCATION}"
-	build_and_install_static_experimental_qemu
+	cached_qemu_experimental_version=$(curl -sfL "${qemu_experimental_latest_build_url}/latest") || cached_qemu_experimental_version="none"
+	mkdir -p "${INSTALL_LOCATION}"
+	if [ "$cached_qemu_experimental_version" == "$CURRENT_QEMU_TAG" ]; then
+		install_cached_qemu_experimental || build_and_install_static_experimental_qemu
+	else
+		build_and_install_static_experimental_qemu
+	fi
 }
 
 main
