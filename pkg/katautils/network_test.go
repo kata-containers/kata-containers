929c4e7e3d241c39e7c490b10047add0b57d65f7
Archana Shinde <archana.m.shinde@intel.com>
2019-10-07 13:53:40 -0700

    network: Change NewNS() call
    NewNS() api has been moved to testutils package in the cni
    plugin repo.
    
    Signed-off-by: Archana Shinde <archana.m.shinde@intel.com>

--- pkg/katautils/network_test.go
+++ pkg/katautils/network_test.go
@@ -14,6 +14,7 @@ import (
 	"testing"
 
 	"github.com/containernetworking/plugins/pkg/ns"
+	"github.com/containernetworking/plugins/pkg/testutils"
 	ktu "github.com/kata-containers/runtime/pkg/katatestutils"
 	vc "github.com/kata-containers/runtime/virtcontainers"
 	"github.com/stretchr/testify/assert"
@@ -127,7 +128,7 @@ func TestSetupNetworkNamespace(t *testing.T) {
 	assert.Error(err)
 
 	// Existent netns path
-	n, err := ns.NewNS()
+	n, err := testutils.NewNS()
 	assert.NoError(err)
 	config = &vc.NetworkConfig{
 		NetNSPath: n.Path(),
