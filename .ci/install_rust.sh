8f82773fdf1b64cd5bcdda00ade782e03241091c
Yang Bo <bo@hyper.sh>
2019-10-31 15:02:22 +0800

    rust: Use version from versions.yaml or command line.
    Fixes: #2030
    
    Signed-off-by: Yang Bo <bo@hyper.sh>

--- .ci/install_rust.sh
+++ .ci/install_rust.sh
@@ -10,16 +10,19 @@ set -e
 
 cidir=$(dirname "$0")
 rustarch=$(${cidir}/kata-arch.sh --rust)
-release="nightly"
+# release="nightly"
 # recent functional version
-version="2019-10-04"
+version="${1:-""}"
+if [ -z "${version}" ]; then
+	version=$(get_version "languages.rust.meta.newest-version")
+fi
 
 if ! command -v rustup > /dev/null; then
 	curl https://sh.rustup.rs -sSf | sh
 fi
 
-rustup toolchain install ${release}-${version}-${rustarch}-unknown-linux-gnu
-rustup default ${release}-${version}-${rustarch}-unknown-linux-gnu
+rustup toolchain install ${version}
+rustup default ${version}
 rustup target install ${rustarch}-unknown-linux-musl
 rustup component add rustfmt
 sudo ln -sf /usr/bin/g++ /bin/musl-g++
