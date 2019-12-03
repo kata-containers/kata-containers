28b8f4e93b458cb95f1c4dc2bf58c2b40af83595
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2019-12-03 15:03:30 -0600

    ci: Add cache for cloud hypervisor
    This adds the installation for cached cloud hypervisor.
    
    Fixes #2120
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- .ci/install_cloud_hypervisor.sh
+++ .ci/install_cloud_hypervisor.sh
@@ -16,7 +16,7 @@ cidir=$(dirname "$0")
 arch=$("${cidir}"/kata-arch.sh -d)
 # Where real kata build script exist, via docker build to avoid install all deps
 packaging_repo="github.com/kata-containers/packaging"
-
+latest_build_url="${jenkins_url}/job/cloud-hypervisor-nightly-$(uname -m)/${cached_artifacts_path}"
 source "${cidir}/lib.sh"
 
 
@@ -44,8 +44,37 @@ install_clh() {
 	popd
 }
 
+install_cached_clh(){
+	local cloud_hypervisor_binary=${1:-}
+	[ -z "${cloud_hypervisor_binary}" ] && die "empty binary format"
+	info "Installing ${cloud_hypervisor_binary}"
+	local cloud_hypervisor_path="/usr/bin"
+	local cloud_hypervisor_name="cloud-hypervisor"
+	sudo -E curl -fL --progress-bar "${latest_build_url}"/"${cloud_hypervisor_binary_name}" -o "${cloud_hypervisor_binary_path}" || return 1
+}
+
+install_prebuilt_clh() {
+	local cloud_hypervisor_path="/usr/bin"
+	pushd "${cloud_hypervisor_path}" > /dev/null
+	info "Verify download checksum"
+	sudo -E curl -fsOL "${latest_build_url}/sha256sum-cloud-hypervisor" || return 1
+	sudo sha256sum -c "sha256sum-cloud-hypervisor" || return 1
+	popd > /dev/null
+}
+
 main() {
-	install_clh
+	current_cloud_hypervisor_version=$(get_version "assets.hypervisor.cloud_hypervisor.version")
+	cached_cloud_hypervisor_version=$(curl -sfL "${latest_build_url}/latest") || cached_cloud_hypervisor_version="none"
+	info "current cloud hypervisor : ${current_cloud_hypervisor_version}"
+	info "cached cloud hypervisor  : ${cached_cloud_hypervisor_version}"
+	if [ "$cached_cloud_hypervisor_version" == "$current_cloud_hypervisor_version" ] && [ "$arch" == "x86_64" ]; then
+		if ! install_prebuilt_clh; then
+			info "failed to install cached cloud hypervisor, trying to build from source"
+			install_clh
+		fi
+	else
+		install_clh
+	fi
 }
 
 main "$@"
