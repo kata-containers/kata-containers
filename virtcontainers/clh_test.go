0afeb527ff87820f0415c6173ce74803862d93ac
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2019-12-05 21:48:57 +0000

    clh: clh http  unit test
    Add initial unit test around http client
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- virtcontainers/clh_test.go
+++ virtcontainers/clh_test.go
@@ -4,3 +4,21 @@
 //
 
 package virtcontainers
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCloudHypervisorAddVSock(t *testing.T) {
+	assert := assert.New(t)
+	clh := cloudHypervisor{}
+
+	clh.addVSock(1, "path")
+	assert.Equal(clh.vmconfig.Vsock[0].Cid, int64(1))
+	assert.Equal(clh.vmconfig.Vsock[0].Sock, "path")
+
+	clh.addVSock(2, "path2")
+	assert.Equal(clh.vmconfig.Vsock[1].Cid, int64(2))
+	assert.Equal(clh.vmconfig.Vsock[1].Sock, "path2")
+}
