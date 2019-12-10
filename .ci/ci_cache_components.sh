kata-containers/kata-containers

commit 06da90d9a26a366d75b0dc56565a3d8ed2098752

ci: remove nemu refernces
nemu is deprecated on master, lets drop code related to it

Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

.ci/ci_cache_components.sh
@@ -5,7 +5,7 @@
 #
 # This script help us to build the
 # cache of several kata components like
-# qemu, kernel, nemu and images with the
+# qemu, kernel and images with the
 # main purpose that being to be used at
 # the CI to reduce the execution time
 
@@ -66,16 +66,6 @@ cache_image_artifacts() {
 	create_image_tar "${image_name}"
 }
 
-# This builds nemu by specifying the nemu version from the
-# runtime versions file and the nemu tar file name
-cache_nemu_artifacts() {
-	pushd "${tests_repo_dir}"
-	local nemu_version=$(get_version "assets.hypervisor.nemu.version")
-	local nemu_tar="kata-nemu-static.tar.gz"
-	popd
-	create_cache_asset "${nemu_tar}" "${nemu_version}"
-}
-
 # This builds the initrd image by specifying the image initrd version
 # from /usr/share/kata-containers and the image initrd name
 cache_image_initrd_artifacts() {
@@ -97,13 +87,12 @@ create_image_tar(){
 	rm "${getinfo}"
 }
 
-# This function receives as arguments the component name (qemu, nemu, kernel, etc)
+# This function receives as arguments the component name (qemu, kernel, etc)
 # as well as the version
 create_cache_asset() {
 	local component_name="$1"
 	local component_version="$2"
 	local check_image=$(echo "${component_version}" | grep kata || true)
-	local check_nemu=$(echo "${component_name}" | grep nemu || true)
 	local check_qemu=$(echo "${component_name}" | grep qemu || true)
 	local check_kernel=$(echo "${component_name}" | grep -E '\<vmlinu[xz]\>' || true)
 	local check_initrd=$(echo "${component_name}" | grep initrd || true)
@@ -124,11 +113,6 @@ create_cache_asset() {
 		echo "${component_version}" >  "latest"
 	fi
 
-	# In the case of nemu and qemu we have the tar file in
-	# a specific location
-	if [ ! -z "${check_nemu}" ] || [ ! -z "${check_qemu}" ]; then
-		cp -a "${KATA_TESTS_CACHEDIR}/${component_name}" .
-	fi
 
 	# In the case of the kernel we have it in the kata dir
 	if [ ! -z "${check_kernel}" ]; then
@@ -159,7 +143,6 @@ Options:
 -h      Shows help
 -i      Run image cache
 -k      Run kernel cache
--n      Run nemu cache
 -q      Run qemu cache
 -r      Run image initrd cache
 
@@ -183,9 +166,6 @@ main() {
 		k)
 			build_kernel="true"
  			;;
-		n)
-			build_nemu="true"
-			;;
 		q)
 			build_qemu="true"
 			;;
@@ -199,7 +179,6 @@ main() {
 	[[ -z "$build_kernel" ]] && \
 	[[ -z "$build_qemu" ]] && \
 	[[ -z "$build_qemu_experimental" ]] && \
- 	[[ -z "$build_nemu" ]] && \
 	[[ -z "$build_image" ]] && \
 	[[ -z "$build_image_initrd" ]] && \
 		usage && die "Must choose at least one option"
@@ -213,8 +192,6 @@ main() {
 	[ "$build_qemu" == "true" ] && cache_qemu_artifacts
 
 	[ "$build_qemu_experimental" == "true" ] && cache_qemu_experimental_artifacts
-
-	[ "$build_nemu" == "true" ] && cache_nemu_artifacts
 
 	[ "$build_image" == "true" ] && cache_image_artifacts
 

.ci/install_kata.sh
@@ -25,9 +25,6 @@ echo "Install Kata Containers Kernel"
 if [ "$KATA_HYPERVISOR" == "firecracker" ]; then
 	echo "Install Firecracker"
 	"${cidir}/install_firecracker.sh"
-elif [ "$KATA_HYPERVISOR" == "nemu" ]; then
-	echo "Install Nemu"
-	"${cidir}/install_nemu.sh"
 else
 	if [ "$experimental_qemu" == "true" ]; then
 		echo "Install experimental Qemu"

.ci/install_nemu.sh
@@ -1,89 +1,0 @@
-#!/bin/bash
-#
-# Copyright (c) 2018 Intel Corporation
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-set -o errexit
-set -o nounset
-set -o pipefail
-set -o errtrace
-
-cidir=$(dirname "$0")
-source "${cidir}/lib.sh"
-source "${cidir}/../lib/common.bash"
-source /etc/os-release || source /usr/lib/os-release
-
-versions_file="${cidir}/../versions.yaml"
-arch=$("${cidir}"/kata-arch.sh -d)
-latest_build_url="${jenkins_url}/job/nemu-nightly-${arch}/${cached_artifacts_path}"
-nemu_repo=$(get_version "assets.hypervisor.nemu.url")
-nemu_version=$(get_version "assets.hypervisor.nemu.version")
-NEMU_TAR="kata-nemu-static.tar.gz"
-
-install_nemu() {
-	info "build nemu from source"
-	PACKAGING_REPO="github.com/kata-containers/packaging"
-	case "$arch" in
-	x86_64)
-		local nemu_bin="nemu-system-${arch}"
-		;;
-	*)
-		die "Unsupported architecture: $arch"
-		;;
-	esac
-
-	go get -d "${PACKAGING_REPO}" || true
-
-	prefix="${KATA_NEMU_DESTDIR}" ${GOPATH}/src/${PACKAGING_REPO}/static-build/nemu/build-static-nemu.sh "${arch}"
-	sudo tar -xvf ${NEMU_TAR} -C /
-	# We need to move the tar file to a specific location so we
-	# can know where it is and then we can perform the build cache
-	# operations
-	sudo mkdir -p "${KATA_TESTS_CACHEDIR}"
-	sudo mv ${NEMU_TAR} ${KATA_TESTS_CACHEDIR}
-}
-
-install_firmware() {
-	local firmware="OVMF.fd"
-	local firmware_repo=$(get_version "assets.hypervisor.nemu-ovmf.url")
-	local firmware_version=$(get_version "assets.hypervisor.nemu-ovmf.version")
-	local firmware_dir="/usr/share/nemu/firmware"
-
-	sudo mkdir -p "${firmware_dir}"
-	curl -LO "${firmware_repo}/releases/download/${firmware_version}/${firmware}"
-	sudo install -o root -g root -m 0644 "${firmware}" "${firmware_dir}"
-	rm -f "${firmware}"
-}
-
-install_prebuilt_nemu() {
-	sudo -E curl -fL --progress-bar "${latest_build_url}/${NEMU_TAR}" -o "${NEMU_TAR}" || return 1
-	info "Install pre-built nemu version"
-	sudo tar -xvf "${NEMU_TAR}" -C /
-
-	info "Verify download checksum"
-	sudo -E curl -fsOL "${latest_build_url}/sha256sum-${NEMU_TAR}" || return 1
-	sudo sha256sum -c "sha256sum-${NEMU_TAR}" || return 1
-}
-
-main() {
-	cached_nemu_version=$(curl -sfL "${latest_build_url}/latest") || cached_nemu_version="none"
-	info "current nemu : ${nemu_version}"
-	info "cached nemu  : ${cached_nemu_version}"
-	# Currently in our CI we are only installing and running nemu on x86_64 that is the
-	# main reason of why we will only install the prebuilt nemu on this arch, this
-	# can change once that we have nemu installation on other archs
-	if [ "$cached_nemu_version" == "$nemu_version" ] && [ "$arch" == "x86_64" ]; then
-		# If installing nemu fails,
-		# then build and install it from sources.
-		if ! install_prebuilt_nemu; then
-			info "failed to install cached nemu, trying to build from source"
-			install_nemu
-		fi
-	else
-		install_nemu
-	fi
-}
-
-main

.ci/install_runtime.sh
@@ -60,10 +60,6 @@ if [ "$KATA_HYPERVISOR" = "firecracker" ]; then
 	sudo sed -i '/jailer_path/d' "${PKGDEFAULTSDIR}/configuration.toml"
 fi
 
-if [ "$KATA_HYPERVISOR" = "nemu" ]; then
-	echo "Enable nemu configuration.toml"
-	sudo mv "${PKGDEFAULTSDIR}/configuration-nemu.toml" "${PKGDEFAULTSDIR}/configuration.toml"
-fi
 
 if [ "$KATA_HYPERVISOR" = "qemu" ] && [ "$experimental_qemu" == "true" ]; then
 	echo "Enable qemu virtiofs configuration.toml"
@@ -106,19 +102,6 @@ fi
 
 if [ "$KATA_HYPERVISOR" == "qemu" ]; then
 	echo "Add kata-runtime as a new/default Docker runtime."
-	"${cidir}/../cmd/container-manager/manage_ctr_mgr.sh" docker configure -r kata-runtime -f
-elif [ "$KATA_HYPERVISOR" == "nemu" ]; then
-	echo "Configure Nemu as Kata Hypervisor"
-	sudo crudini --set "${runtime_config_path}" hypervisor.qemu machine_type \"virt\"
-	sudo crudini --set "${runtime_config_path}" hypervisor.qemu firmware \"${KATA_NEMU_DESTDIR}/share/kata-nemu/OVMF.fd\"
-	case "$arch" in
-	x86_64)
-		sudo crudini --set "${runtime_config_path}" hypervisor.qemu path \"${KATA_NEMU_DESTDIR}/bin/nemu-system-${arch}\"
-		;;
-	*)
-		die "Unsupported architecture: $arch"
-		;;
-	esac
 	"${cidir}/../cmd/container-manager/manage_ctr_mgr.sh" docker configure -r kata-runtime -f
 else
 	echo "Kata runtime will not set as a default in Docker"

.ci/setup_env_centos.sh
@@ -45,7 +45,6 @@ declare -A minimal_packages=( \
 declare -A packages=( \
 	[kata_containers_dependencies]="libtool libtool-ltdl-devel device-mapper-persistent-data lvm2 device-mapper-devel libtool-ltdl" \
 	[qemu_dependencies]="libcap-devel libcap-ng-devel libattr-devel libcap-ng-devel librbd1-devel flex libfdt-devel" \
-	[nemu_dependencies]="brlapi" \
 	[kernel_dependencies]="elfutils-libelf-devel flex pkgconfig" \
 	[crio_dependencies]="glibc-static libseccomp-devel libassuan-devel libgpg-error-devel device-mapper-libs btrfs-progs-devel util-linux libselinux-devel" \
 	[bison_binary]="bison" \

.ci/setup_env_fedora.sh
@@ -24,7 +24,6 @@ declare -A packages=( \
 	[general_dependencies]="dnf-plugins-core python pkgconfig util-linux libgpg-error-devel" \
 	[kata_containers_dependencies]="libtool automake autoconf bc pixman numactl-libs" \
 	[qemu_dependencies]="libcap-devel libattr-devel libcap-ng-devel zlib-devel pixman-devel librbd-devel" \
-	[nemu_dependencies]="brlapi" \
 	[kernel_dependencies]="elfutils-libelf-devel flex" \
 	[crio_dependencies]="btrfs-progs-devel device-mapper-devel glib2-devel glibc-devel glibc-static gpgme-devel libassuan-devel libseccomp-devel libselinux-devel" \
 	[bison_binary]="bison" \

.ci/setup_env_rhel.sh
@@ -30,7 +30,6 @@ declare -A minimal_packages=( \
 declare -A packages=(
 	[kata_containers_dependencies]="libtool libtool-ltdl-devel device-mapper-persistent-data lvm2 device-mapper-devel libtool-ltdl bzip2 m4 patch gettext-devel automake alien autoconf bc pixman-devel coreutils" \
 	[qemu_dependencies]="libcap-devel libcap-ng-devel libattr-devel libcap-ng-devel librbd1-devel flex libfdt-devel" \
-	[nemu_dependencies]="brlapi" \
 	[kernel_dependencies]="elfutils-libelf-devel flex" \
 	[crio_dependencies]="glibc-static libseccomp-devel libassuan-devel libgpg-error-devel device-mapper-libs btrfs-progs-devel util-linux gpgme-devel glib2-devel glibc-devel libselinux-devel pkgconfig" \
 	[bison_binary]="bison" \

.ci/setup_env_ubuntu.sh
@@ -26,7 +26,6 @@ declare -A minimal_packages=( \
 declare -A packages=( \
 	[kata_containers_dependencies]="libtool automake autotools-dev autoconf bc alien libpixman-1-dev coreutils" \
 	[qemu_dependencies]="libcap-dev libattr1-dev libcap-ng-dev librbd-dev" \
-	[nemu_dependencies]="libbrlapi0.6" \
 	[kernel_dependencies]="libelf-dev flex" \
 	[crio_dependencies]="libglib2.0-dev libseccomp-dev libapparmor-dev libgpgme11-dev thin-provisioning-tools" \
 	[bison_binary]="bison" \

commit 583d4550e47f9d229f0889a15e4a991754222112

ci: Fix ci_cache_components.sh script to locate qemu
In the case of qemu and qemu experimental we have the tar file in a
specific location and we need to check it. This PR fixes the regression
that was introduced by the commit 06da90d9a26a366d75b0dc56565a3d8ed2098752.

Fixes #2152

Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

.ci/ci_cache_components.sh
@@ -121,6 +121,10 @@ create_cache_asset() {
 		echo "${component_version}" >  "latest"
 	fi
 
+	# In the case of qemu we have the tar at a specific location
+	if [ ! -z "${check_qemu}" ]; then
+		cp -a "${KATA_TESTS_CACHEDIR}/${component_name}" .
+	fi
 
 	# In the case of the kernel we have it in the kata dir
 	if [ ! -z "${check_kernel}" ]; then