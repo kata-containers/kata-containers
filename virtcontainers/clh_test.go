8d66ea7d56869572129e7a41825729a9a16d8d13
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2019-12-13 22:42:06 +0000

    clh: add Client Interface and bootVM test
    Add interface with the same methods of client,
    this will help to decouple the implementation
    and help use to do mock testing.
    
    Add Mock client and add bootVM unit test
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- virtcontainers/clh_test.go
+++ virtcontainers/clh_test.go
@@ -6,9 +6,40 @@
 package virtcontainers
 
 import (
-	"github.com/stretchr/testify/assert"
+	"context"
+	"net/http"
 	"testing"
+
+	chclient "github.com/kata-containers/runtime/virtcontainers/pkg/cloud-hypervisor/client"
+	"github.com/stretchr/testify/assert"
 )
+
+type clhClientMock struct {
+	vmInfo chclient.VmInfo
+}
+
+func (c *clhClientMock) VmmPingGet(ctx context.Context) (chclient.VmmPingResponse, *http.Response, error) {
+	return chclient.VmmPingResponse{}, nil, nil
+}
+
+func (c *clhClientMock) ShutdownVMM(ctx context.Context) (*http.Response, error) {
+	return nil, nil
+}
+
+func (c *clhClientMock) CreateVM(ctx context.Context, vmConfig chclient.VmConfig) (*http.Response, error) {
+	c.vmInfo.State = clhStateCreated
+	return nil, nil
+}
+
+//nolint:golint
+func (c *clhClientMock) VmInfoGet(ctx context.Context) (chclient.VmInfo, *http.Response, error) {
+	return c.vmInfo, nil, nil
+}
+
+func (c *clhClientMock) BootVM(ctx context.Context) (*http.Response, error) {
+	c.vmInfo.State = clhStateRunning
+	return nil, nil
+}
 
 func TestCloudHypervisorAddVSock(t *testing.T) {
 	assert := assert.New(t)
@@ -21,4 +52,13 @@ func TestCloudHypervisorAddVSock(t *testing.T) {
 	clh.addVSock(2, "path2")
 	assert.Equal(clh.vmconfig.Vsock[1].Cid, int64(2))
 	assert.Equal(clh.vmconfig.Vsock[1].Sock, "path2")
+}
+
+func TestCloudHypervisorBootVM(t *testing.T) {
+	clh := &cloudHypervisor{}
+	clh.APIClient = &clhClientMock{}
+	var ctx context.Context
+	if err := clh.bootVM(ctx); err != nil {
+		t.Errorf("cloudHypervisor.bootVM() error = %v", err)
+	}
 }
