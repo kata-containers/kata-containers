ff7d9af9a5c32cadef83a1ad29bdc976d91ff4e7
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2020-01-16 14:44:36 -0600

    ci: Remove alien package
    The alien package is not needed so we can remove it.
    
    Fixes #2231
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- .ci/setup_env_ubuntu.sh
+++ .ci/setup_env_ubuntu.sh
@@ -24,7 +24,7 @@ declare -A minimal_packages=( \
 )
 
 declare -A packages=( \
-	[kata_containers_dependencies]="libtool automake autotools-dev autoconf bc alien libpixman-1-dev coreutils" \
+	[kata_containers_dependencies]="libtool automake autotools-dev autoconf bc libpixman-1-dev coreutils" \
 	[qemu_dependencies]="libcap-dev libattr1-dev libcap-ng-dev librbd-dev" \
 	[kernel_dependencies]="libelf-dev flex" \
 	[crio_dependencies]="libglib2.0-dev libseccomp-dev libapparmor-dev libgpgme11-dev thin-provisioning-tools" \
