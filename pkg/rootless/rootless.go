9c18e4a969cb3d4b84d38cb4b2963196e68c6160
Julio Montes <julio.montes@intel.com>
2020-01-28 15:23:35 +0000

    rootless: use libcontainer API to detect rootless
    libcontainer already has an API to detect if the runtime is running rootless.
    Use libcontainer API instead of reinventing the wheel.
    
    fixes #2415
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

--- pkg/rootless/rootless.go
+++ pkg/rootless/rootless.go
@@ -20,20 +20,17 @@
 package rootless
 
 import (
-	"bufio"
 	"context"
 	"crypto/rand"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"runtime"
-	"strconv"
 	"strings"
 	"sync"
 
 	"github.com/containernetworking/plugins/pkg/ns"
-	"github.com/pkg/errors"
+	"github.com/opencontainers/runc/libcontainer/system"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 )
@@ -53,13 +50,12 @@ var (
 	// which user-specific non-essential runtime files are stored.
 	rootlessDir = os.Getenv("XDG_RUNTIME_DIR")
 
-	// uidMapPath defines the location of the uid_map file to
-	// determine whether a user is root or not
-	uidMapPath = "/proc/self/uid_map"
-
 	rootlessLog = logrus.WithFields(logrus.Fields{
 		"source": "rootless",
 	})
+
+	//The function is declared this way for mocking in unit tests
+	IsRootless = isRootlessFunc
 )
 
 // SetLogger sets up a logger for the rootless pkg
@@ -68,71 +64,24 @@ func SetLogger(ctx context.Context, logger *logrus.Entry) {
 	rootlessLog = logger.WithFields(fields)
 }
 
-// setRootless reads a uid_map file, compares the UID of the
-// user inside the container vs on the host. If the host UID
-// is not root, but the container ID is, it can be determined
-// the user is running rootlessly.
-func setRootless() error {
-	initRootless = true
-	file, err := os.Open(uidMapPath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	buf := bufio.NewReader(file)
-	for {
-		line, _, err := buf.ReadLine()
-		if err != nil {
-			if err == io.EOF {
-				return nil
-			}
-			return err
-		}
-		if line == nil {
-			return nil
-		}
-
-		var parseError = errors.Errorf("Failed to parse uid map file %s", uidMapPath)
-		// if the container id (id[0]) is 0 (root inside the container)
-		// has a mapping to the host id (id[1]) that is not root, then
-		// it can be determined that the host user is running rootless
-		ids := strings.Fields(string(line))
-		// do some sanity checks
-		if len(ids) != 3 {
-			return parseError
-		}
-		userNSUid, err := strconv.ParseUint(ids[0], 10, 0)
-		if err != nil {
-			return parseError
-		}
-		hostUID, err := strconv.ParseUint(ids[1], 10, 0)
-		if err != nil {
-			return parseError
-		}
-		rangeUID, err := strconv.ParseUint(ids[2], 10, 0)
-		if err != nil || rangeUID == 0 {
-			return parseError
-		}
-
-		if userNSUid == 0 && hostUID != 0 {
-			rootlessLog.Info("Running as rootless")
-			isRootless = true
-			return nil
-		}
-	}
-}
-
 // IsRootless states whether kata is being ran with root or not
-func IsRootless() bool {
+func isRootlessFunc() bool {
 	rLock.Lock()
+	defer rLock.Unlock()
 	if !initRootless {
-		err := setRootless()
-		if err != nil {
-			rootlessLog.WithError(err).Error("Unable to determine if running rootless")
+		initRootless = true
+		isRootless = true
+		// --rootless and --systemd-cgroup options must honoured
+		// but with the current implementation this is not possible
+		// https://github.com/kata-containers/runtime/issues/2412
+		if os.Geteuid() != 0 {
+			return true
 		}
+		if system.RunningInUserNS() {
+			return true
+		}
+		isRootless = false
 	}
-	rLock.Unlock()
 	return isRootless
 }
 
