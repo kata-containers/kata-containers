dd2762fdadeea6c0e12d71e33ee591f702ffcd8f
Julio Montes <julio.montes@intel.com>
2020-01-31 19:42:47 +0000

    virtcontainers/persist: introduce mock fs driver
    Mock FS driver can be used in unit testing to allow
    
    Mock fs driver inherits from FS and may overwrite its methods. All files
    and directories created by this driver are under a path accessible for all
    users, this path is created under the system temporal directory.
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

---
+++ virtcontainers/persist/fs/mockfs.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2020 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package fs
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	persistapi "github.com/kata-containers/runtime/virtcontainers/persist/api"
+)
+
+type MockFS struct {
+	// inherit from FS. Overwrite if needed.
+	*FS
+}
+
+func MockStorageRootPath() string {
+	return filepath.Join(os.TempDir(), "vc", "mockfs")
+}
+
+func MockRunStoragePath() string {
+	return filepath.Join(MockStorageRootPath(), sandboxPathSuffix)
+}
+
+func MockRunVMStoragePath() string {
+	return filepath.Join(MockStorageRootPath(), vmPathSuffix)
+}
+
+func MockStorageDestroy() {
+	os.RemoveAll(MockStorageRootPath())
+}
+
+func MockFSInit() (persistapi.PersistDriver, error) {
+	driver, err := Init()
+	if err != nil {
+		return nil, fmt.Errorf("Could not create Mock FS driver: %v", err)
+	}
+
+	fsDriver, ok := driver.(*FS)
+	if !ok {
+		return nil, fmt.Errorf("Could not create Mock FS driver")
+	}
+
+	fsDriver.storageRootPath = MockStorageRootPath()
+	fsDriver.driverName = "mockfs"
+
+	return &MockFS{fsDriver}, nil
+}
