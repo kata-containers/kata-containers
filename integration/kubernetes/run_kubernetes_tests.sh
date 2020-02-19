1980cc60fcdb646ffc439aa231ba7a62e039ece0
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2020-02-19 20:49:59 +0000

    test: k8s: skip faling test for clh.
    - cpu is not implmented
    - blk is not implemented
    - sysctl is not enabled
    
    Fixes: #2318
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- integration/kubernetes/run_kubernetes_tests.sh
+++ integration/kubernetes/run_kubernetes_tests.sh
@@ -38,10 +38,8 @@ trap '${kubernetes_dir}/cleanup_env.sh' EXIT
 systemctl is-active --quiet docker || sudo systemctl start docker
 
 K8S_TEST_UNION=("k8s-attach-handlers.bats" \
-	"k8s-block-volume.bats" \
 	"k8s-configmap.bats" \
 	"k8s-copy-file.bats" \
-	"k8s-cpu-ns.bats" \
 	"k8s-credentials-secrets.bats" \
 	"k8s-custom-dns.bats" \
 	"k8s-empty-dirs.bats" \
@@ -61,13 +59,26 @@ K8S_TEST_UNION=("k8s-attach-handlers.bats" \
 	"k8s-scale-nginx.bats" \
 	"k8s-security-context.bats" \
 	"k8s-shared-volume.bats" \
-	"k8s-sysctls.bats" \
-	"k8s-number-cpus.bats" \
 	"k8s-uts+ipc-ns.bats" \
 	"k8s-volume.bats" \
 	"nginx.bats" \
 	"k8s-hugepages.bats")
 
+if [ "${KATA_HYPERVISOR:-}" == "cloud-hypervisor" ]; then
+	blk_issue="https://github.com/kata-containers/tests/issues/2318"
+	cpu_issue="https://github.com/kata-containers/tests/issues/2325"
+	sysctl_issue="https://github.com/kata-containers/tests/issues/2324"
+	info "$KATA_HYPERVISOR hotplug is not implemented:"
+	info "blk ${blk_issue}"
+	info "cpu ${cpu_issue}"
+	info "$KATA_HYPERVISOR sysctl is failing:"
+	info "sysctls: ${sysctl_issue}"
+else
+	K8S_TEST_UNION+=("k8s-block-volume.bats")
+	K8S_TEST_UNION+=("k8s-cpu-ns.bats")
+	K8S_TEST_UNION+=("k8s-number-cpus.bats")
+	K8S_TEST_UNION+=("k8s-sysctls.bats")
+fi
 # we may need to skip a few test cases when running on non-x86_64 arch
 if [ -f "${cidir}/${arch}/configuration_${arch}.yaml" ]; then
 	config_file="${cidir}/${arch}/configuration_${arch}.yaml"
