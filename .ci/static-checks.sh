997312e574aee0db007feb8f91e9cfbc5c075a2b
Graham Whaley <graham.whaley@intel.com>
2020-04-30 13:26:29 +0100

    CI: enable Travis for static checks
    Enable the Travis CI configuration to perform static CI checks
    on PRs to this repo.
    
    Fixes: #1031
    
    Signed-off-by: Graham Whaley <graham.whaley@intel.com>

--- .ci/static-checks.sh
+++ .ci/static-checks.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #
-# Copyright (c) 2018 Intel Corporation
+# Copyright (c) 2018,2020 Intel Corporation
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
-set -o errexit
-set -o nounset
-set -o pipefail
+set -e
 
-# This script should run any static analysis check
-# It is called by the CI setup
-true
+cidir=$(dirname "$0")
+source "${cidir}/lib.sh"
+
+# Run static checks
+run_static_checks
