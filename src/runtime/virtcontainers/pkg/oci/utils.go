2e093dfd8ba55db3cee8fe8df47619a26c1faa1c
Christophe de Dinechin <dinechin@redhat.com>
2020-05-15 15:59:30 +0200

    config: Fix typo in function name
    There was an extra 'p' in addHypervisorVirtioFsOverrides.
    
    Fixes: #901
    
    Signed-off-by: Christophe de Dinechin <dinechin@redhat.com>

--- src/runtime/virtcontainers/pkg/oci/utils.go
+++ src/runtime/virtcontainers/pkg/oci/utils.go
@@ -385,7 +385,7 @@ func addHypervisorConfigOverrides(ocispec specs.Spec, config *vc.SandboxConfig, 
 		return err
 	}
 
-	if err := addHypervisporVirtioFsOverrides(ocispec, config, runtime); err != nil {
+	if err := addHypervisorVirtioFsOverrides(ocispec, config, runtime); err != nil {
 		return err
 	}
 
@@ -671,7 +671,7 @@ func addHypervisorBlockOverrides(ocispec specs.Spec, sbConfig *vc.SandboxConfig)
 	return nil
 }
 
-func addHypervisporVirtioFsOverrides(ocispec specs.Spec, sbConfig *vc.SandboxConfig, runtime RuntimeConfig) error {
+func addHypervisorVirtioFsOverrides(ocispec specs.Spec, sbConfig *vc.SandboxConfig, runtime RuntimeConfig) error {
 	if value, ok := ocispec.Annotations[vcAnnotations.SharedFS]; ok {
 		supportedSharedFS := []string{config.Virtio9P, config.VirtioFS}
 		valid := false
