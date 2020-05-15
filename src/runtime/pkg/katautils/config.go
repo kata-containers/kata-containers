c2a186b18c91541c8917a3695dfde2b71af5baa0
Christophe de Dinechin <dinechin@redhat.com>
2020-05-15 18:42:30 +0200

    config: Protect vhost_user_store_path against annotation attacks
    This path could be used to overwrite data on the host.
    
    Fixes: #901
    
    Signed-off-by: Christophe de Dinechin <dinechin@redhat.com>

--- src/runtime/pkg/katautils/config.go
+++ src/runtime/pkg/katautils/config.go
@@ -680,6 +680,7 @@ func newQemuHypervisorConfig(h hypervisor) (vc.HypervisorConfig, error) {
 		DisableVhostNet:         h.DisableVhostNet,
 		EnableVhostUserStore:    h.EnableVhostUserStore,
 		VhostUserStorePath:      h.vhostUserStorePath(),
+		VhostUserStorePathList:  h.VhostUserStorePathList,
 		GuestHookPath:           h.guestHookPath(),
 		RxRateLimiterMaxRate:    rxRateLimiterMaxRate,
 		TxRateLimiterMaxRate:    txRateLimiterMaxRate,
