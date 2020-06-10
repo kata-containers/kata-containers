5a0a904c961ba0d79592bab50edcf34102ff517e
James O. D. Hunt <james.o.hunt@intel.com>
2020-06-10 11:24:46 +0100

    shimv2: Add a "--version" cli option
    All components should support a `--version` option to allow clear
    identification of the version of the component being used.
    
    Note that the build changes are required to allow the shim binary to
    access the golang code generated by the build (such as the `version`
    variable).
    
    Fixes: #307.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- src/runtime/cli/containerd-shim-kata-v2/main.go
+++ src/runtime/cli/containerd-shim-kata-v2/main.go
@@ -6,9 +6,14 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/containerd/containerd/runtime/v2/shim"
 	"github.com/kata-containers/kata-containers/src/runtime/containerd-shim-v2"
 )
+
+const shim_id = "io.containerd.kata.v2"
 
 func shimConfig(config *shim.Config) {
 	config.NoReaper = true
@@ -16,5 +21,11 @@ func shimConfig(config *shim.Config) {
 }
 
 func main() {
-	shim.Run("io.containerd.kata.v2", containerdshim.New, shimConfig)
+
+	if len(os.Args) == 2 && os.Args[1] == "--version" {
+		fmt.Printf("%s containerd shim: id: %q, version: %s, commit: %v\n", project, shim_id, version, commit)
+		os.Exit(0)
+	}
+
+	shim.Run(shim_id, containerdshim.New, shimConfig)
 }
