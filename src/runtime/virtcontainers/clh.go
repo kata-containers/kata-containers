853e4f854d29d82b5dda54012b57ec6c9a7c6a6a
Bo Chen <chen.bo@intel.com>
2020-06-26 17:29:54 -0700

    clh: Remove the use of deprecated '--memory file=' parameter
    [ Port from runtime commit 30b40f5505fd46d23b89eb5fb38301d2f7454f35 ]
    
    Along with the release of cloud-hypervisor v0.8.0, this option has been
    deprecated. clh now enforces to use the alternative controls,
    e.g. "shared" and "hugepages", which can infer the backing file
    paths. Also, we don't use "hugepages" in kata, so we are fine now as the
    "shared" control  is already enabled.
    
    Fixes: #461
    
    Signed-off-by: Bo Chen <chen.bo@intel.com>

--- src/runtime/virtcontainers/clh.go
+++ src/runtime/virtcontainers/clh.go
@@ -221,7 +221,6 @@ func (clh *cloudHypervisor) createSandbox(ctx context.Context, id string, networ
 	// Set initial memomory size of the virtual machine
 	// Convert to int64 openApiClient only support int64
 	clh.vmconfig.Memory.Size = int64((utils.MemUnit(clh.config.MemorySize) * utils.MiB).ToBytes())
-	clh.vmconfig.Memory.File = "/dev/shm"
 	// shared memory should be enabled if using vhost-user(kata uses virtiofsd)
 	clh.vmconfig.Memory.Shared = true
 	hostMemKb, err := getHostMemorySizeKb(procMemInfo)
