8185897e2a1643d497acaafbe235132e4acba044
Pradipta Kr. Banerjee <pradipta.banerjee@gmail.com>
2020-06-29 21:06:28 -0700

    cli: Fix kata-env output on Power
    [ port from runtime commit 9ac39116b08148de8e66abfca2e5407bc153af87 ]
    
    kata-env output always shows "VMContainerCapable=false" on Power.
    This patch fixes the same.
    
    Signed-off-by: bpradipt@in.ibm.com
    Signed-off-by: Peng Tao <bergwolf@hyper.sh>

--- src/runtime/cli/kata-env_ppc64le_test.go
+++ src/runtime/cli/kata-env_ppc64le_test.go
@@ -10,7 +10,7 @@ import "testing"
 func getExpectedHostDetails(tmpdir string) (HostInfo, error) {
 	expectedVendor := ""
 	expectedModel := "POWER8"
-	expectedVMContainerCapable := false
+	expectedVMContainerCapable := true
 	return genericGetExpectedHostDetails(tmpdir, expectedVendor, expectedModel, expectedVMContainerCapable)
 }
 
