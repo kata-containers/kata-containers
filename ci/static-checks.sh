766ae832a4aeaa0d6147928adb996e843a9e0f5f
Peng Tao <bergwolf@hyper.sh>
2020-07-02 10:48:51 +0800

    test: test
    test
    
    Signed-off-by: Peng Tao <bergwolf@hyper.sh>

--- ci/static-checks.sh
+++ ci/static-checks.sh
@@ -9,4 +9,13 @@ set -e
 cidir=$(dirname "$0")
 source "${cidir}/lib.sh"
 
+/usr/bin/git version
+/usr/bin/git branch
+pwd
+/usr/bin/git show
+/usr/bin/git show HEAD~
+/usr/bin/git show HEAD~2
+/usr/bin/git show HEAD~3
+/usr/bin/git show HEAD~4
+
 run_static_checks
