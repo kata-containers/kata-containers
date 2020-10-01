f4b864a33a2c58be4c08e29f9557bb8b9687f698
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2020-10-01 15:54:26 -0500

    Revert "agent: remove unreachable code"
    This reverts commit 7019e72c7b5c9b4559fe1fb2ea0837ca955f9991.

--- src/agent/rustjail/src/container.rs
+++ src/agent/rustjail/src/container.rs
@@ -340,6 +340,8 @@ pub fn init_child() {
             return;
         }
     }
+
+    std::process::exit(-1);
 }
 
 fn do_init_child(cwfd: RawFd) -> Result<()> {
@@ -645,6 +647,8 @@ fn do_init_child(cwfd: RawFd) -> Result<()> {
     }
 
     do_exec(&args);
+
+    Err(anyhow!("fail to create container"))
 }
 
 impl BaseContainer for LinuxContainer {
