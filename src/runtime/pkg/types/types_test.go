d658129695ce20d446923585e9988140e17d11ee
bin liu <bin@hyper.sh>
2020-10-15 15:05:24 +0800

    kata-monitor: use regexp to check if runtime is kata containers
    To support a few common configurations for Kata, including:
    
    - `io.containerd.kata.v2`
    - `io.containerd.kata-qemu.v2`
    - `io.containerd.kata-clh.v2`
    
    `kata-monintor` changes to use regexp instead of direct string comparison.
    
    Fixes: #957
    
    Signed-off-by: bin liu <bin@hyper.sh>

---
+++ src/runtime/pkg/types/types_test.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2020 Ant Group
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+package types
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKataRuntimeNameRegexp(t *testing.T) {
+	assert := assert.New(t)
+
+	runtimeNameRegexp, err := regexp.Compile(KataRuntimeNameRegexp)
+	assert.NoError(err)
+
+	// valid Kata containers name
+	assert.Equal(true, runtimeNameRegexp.MatchString("io.containerd.kata.v2"))
+	assert.Equal(true, runtimeNameRegexp.MatchString("io.containerd.kataclh.v2"))
+	assert.Equal(true, runtimeNameRegexp.MatchString("io.containerd.kata-clh.v2"))
+	assert.Equal(true, runtimeNameRegexp.MatchString("io.containerd.kata.1.2.3-clh.4.v2"))
+
+	// invalid Kata containers name
+	assert.Equal(false, runtimeNameRegexp.MatchString("io2containerd.kata.v2"))
+	assert.Equal(false, runtimeNameRegexp.MatchString("io.c3ontainerd.kata.v2"))
+	assert.Equal(false, runtimeNameRegexp.MatchString("io.containerd.runc.v1"))
+}
