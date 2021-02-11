2e2749ad3f302d7456ad802fcb9c3e3bacb4ba12
Eric Ernst <eric.g.ernst@gmail.com>
2021-02-11 14:04:57 -0800

    runtime: clh-config: add runtime hooks to the clh toml
    Today hooks are only described in the QEMU toml. This shouldn't be VMM
    specific -- let's make sure these are advertised for Cloud Hypervisor as
    well.
    
    Fixes: #1401
    
    Signed-off-by: Eric Ernst <eric.g.ernst@gmail.com>

--- src/runtime/pkg/katautils/config.go
+++ src/runtime/pkg/katautils/config.go
@@ -873,6 +873,7 @@ func newClhHypervisorConfig(h hypervisor) (vc.HypervisorConfig, error) {
 		HotplugVFIOOnRootBus:    h.HotplugVFIOOnRootBus,
 		PCIeRootPort:            h.PCIeRootPort,
 		DisableVhostNet:         true,
+		GuestHookPath:           h.guestHookPath(),
 		VirtioFSExtraArgs:       h.VirtioFSExtraArgs,
 		SGXEPCSize:              defaultSGXEPCSize,
 		EnableAnnotations:       h.EnableAnnotations,
