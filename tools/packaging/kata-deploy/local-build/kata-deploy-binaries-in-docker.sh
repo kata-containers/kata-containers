0c5ded4bd78bfea9481c76fa69a742ccca33e0e3
Carlos Venegas <jos.c.venegas.munoz@intel.com>
2021-06-29 15:25:46 +0000

    kata-deploy: build kata only with docker in host
    Add script to build kata using docker.
    
    Allow build kata-deploy binaries using docker.
    kata-deploy-binaries-in-docker.sh is a wrapper of
    kata-deploy-binaries.sh it will call kata-deploy-binaries.sh in a
    container with all the dependencies installed.
    
    Signed-off-by: Carlos Venegas <jos.c.venegas.munoz@intel.com>

---
+++ tools/packaging/kata-deploy/local-build/kata-deploy-binaries-in-docker.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Copyright (c) 2018-2021 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+script_dir=$(dirname "$(readlink -f "$0")")
+kata_dir=$(realpath "${script_dir}/../../../../")
+kata_deploy_create="${script_dir}/kata-deploy-binaries.sh"
+uid=$(id -u ${USER})
+gid=$(id -g ${USER})
+
+TTY_OPT="-i"
+NO_TTY="${NO_TTY:-false}"
+[ -t 1 ] &&  [ "${NO_TTY}"  == "false" ] && TTY_OPT="-it"
+
+if [ "${script_dir}" != "${PWD}" ]; then
+	ln -sf "${script_dir}/build" "${PWD}/build"
+fi
+
+install_yq_script_path="${script_dir}/../../../../ci/install_yq.sh"
+
+cp "${install_yq_script_path}" "${script_dir}/dockerbuild/install_yq.sh"
+
+docker build -q -t build-kata-deploy \
+	--build-arg IMG_USER="${USER}" \
+	--build-arg UID=${uid} \
+	--build-arg GID=${gid} \
+	"${script_dir}/dockerbuild/"
+
+docker run ${TTY_OPT} \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	--user ${uid}:${gid} \
+	--env USER=${USER} -v "${kata_dir}:${kata_dir}" \
+	--rm \
+	-w ${script_dir} \
+	build-kata-deploy "${kata_deploy_create}" $@
+
