3533a5b61d69d425a11effac0f22ae46311d879c
Carlos Venegas <jos.c.venegas.munoz@intel.com>
2021-06-29 15:51:25 +0000

    Packaging: stop using GOPATH for yq.
    Use the yq installed in the env.  Needed
    to build kata from docker. The container builder
    has not an initial Go env.
    
    Signed-off-by: Carlos Venegas <jos.c.venegas.munoz@intel.com>

--- tools/packaging/kernel/build-kernel.sh
+++ tools/packaging/kernel/build-kernel.sh
@@ -14,7 +14,6 @@ set -o pipefail
 
 readonly script_name="$(basename "${BASH_SOURCE[0]}")"
 readonly script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-kata_version="${kata_version:-}"
 
 #project_name
 readonly project_name="kata-containers"
@@ -493,13 +492,9 @@ main() {
 	# If not kernel version take it from versions.yaml
 	if [ -z "$kernel_version" ]; then
 		if [[ ${experimental_kernel} == "true" ]]; then
-			kernel_version=$(get_from_kata_deps "assets.kernel-experimental.tag" "${kata_version}")
-			#Remove extra 'v'
-			kernel_version="${kernel_version#v}"
+			kernel_version=$(get_from_kata_deps "assets.kernel-experimental.tag")
 		else
-			kernel_version=$(get_from_kata_deps "assets.kernel.version" "${kata_version}")
-			#Remove extra 'v'
-			kernel_version="${kernel_version#v}"
+			kernel_version=$(get_from_kata_deps "assets.kernel.version")
 		fi
 	fi
 
