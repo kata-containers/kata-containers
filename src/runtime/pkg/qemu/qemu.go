502e40cd0cd1b3014e731ff28a2a9bc3c9f149d9
Fabiano Fidêncio <fidencio@redhat.com>
2021-08-18 08:37:55 +0200

    qemu: Modify the license format to make CI happy
    Both GoVMM & kata-containers are licensed under Apache-2.0 license.
    However, as the format used by GoVMM is not understood by the
    kata-containers CI, let's modify its format and make it match with what
    our CI can understand.
    
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
    Message-Id: <20210818063755.1038141-1-fidencio@redhat.com>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- src/runtime/pkg/qemu/qemu.go
+++ src/runtime/pkg/qemu/qemu.go
@@ -1,27 +1,8 @@
-/*
 // Copyright contributors to the Virtual Machine Manager for Go project
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// SPDX-License-Identifier: Apache-2.0
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-*/
 
-// Package qemu provides methods and types for launching and managing QEMU
-// instances.  Instances can be launched with the LaunchQemu function and
-// managed thereafter via QMPStart and the QMP object that this function
-// returns.  To manage a qemu instance after it has been launched you need
-// to pass the -qmp option during launch requesting the qemu instance to create
-// a QMP unix domain manageent socket, e.g.,
-// -qmp unix:/tmp/qmp-socket,server,nowait.  For more information see the
-// example below.
 package qemu
 
 import (
