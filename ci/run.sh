dddbcde1254f61dcfa12b076f5d48df725c0ba93
Jakob Naucke <jakob.naucke@ibm.com>
2021-08-25 16:34:38 +0200

    ci: Temporarily skip agent shutdown test on s390x
    see https://github.com/kata-containers/tests/issues/3878 for tracking
    
    Fixes: #2507
    Signed-off-by: Jakob Naucke <jakob.naucke@ibm.com>

--- ci/run.sh
+++ ci/run.sh
@@ -13,5 +13,6 @@ clone_tests_repo
 
 pushd ${tests_repo_dir}
 .ci/run.sh
-tracing/test-agent-shutdown.sh
+# temporary fix, see https://github.com/kata-containers/tests/issues/3878
+[ "$(uname -m)" != "s390x" ] && tracing/test-agent-shutdown.sh
 popd
