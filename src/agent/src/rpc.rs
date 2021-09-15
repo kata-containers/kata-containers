1a94c9c6fc73d793d4f889a27df8c59e576a9910
stevenhorsman <steven@uk.ibm.com>
2021-09-15 18:04:41 +0100

    agent: Add CreateContainer support for pre-pulled bundle
    - Add a check in setup_bundle to see if the bundle already exists
    and if it does then skip the setup.
    
    Fixes: #2617
    
    Co-authored-by: Dave Hay <david_hay@uk.ibm.com>
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- src/agent/src/rpc.rs
+++ src/agent/src/rpc.rs
@@ -1657,20 +1657,27 @@ fn setup_bundle(cid: &str, spec: &mut Spec) -> Result<PathBuf> {
     let config_path = bundle_path.join("config.json");
     let rootfs_path = bundle_path.join("rootfs");
 
-    fs::create_dir_all(&rootfs_path)?;
-    baremount(
-        &spec_root.path,
-        rootfs_path.to_str().unwrap(),
-        "bind",
-        MsFlags::MS_BIND,
-        "",
-        &sl!(),
-    )?;
+    let rootfs_exists = Path::new(&rootfs_path).exists();
+    info!(
+        sl!(),
+        "The rootfs_path is {:?} and exists: {}", rootfs_path, rootfs_exists
+    );
+
+    if !rootfs_exists {
+        fs::create_dir_all(&rootfs_path)?;
+        baremount(
+            &spec_root.path,
+            rootfs_path.to_str().unwrap(),
+            "bind",
+            MsFlags::MS_BIND,
+            "",
+            &sl!(),
+        )?;
+    }
     spec.root = Some(Root {
         path: rootfs_path.to_str().unwrap().to_owned(),
         readonly: spec_root.readonly,
     });
-
     info!(
         sl!(),
         "{:?}",
