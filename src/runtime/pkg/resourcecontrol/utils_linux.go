a9626682af8350ccde96eb33deb995fed5d3e9d3
Samuel Ortiz <s.ortiz@apple.com>
2021-11-24 17:15:06 +0100

    virtcontainers: resourcecontrol: Add skeleton for Darwin
    Cgroups do not exist on Darwin, so use an empty implementation for
    resourcecontrol for the time being. In the process, ensure that the
    utilized cgroup handling (ie, isSystemdCgroup) is kept in general file,
    since we use this to help assess/constrain the container spec we pass to
    the guest.
    
    Fixes: #6051
    
    Signed-off-by: Samuel Ortiz <s.ortiz@apple.com>
    Signed-off-by: Eric Ernst <eric_ernst@apple.com>

--- src/runtime/pkg/resourcecontrol/utils_linux.go
+++ src/runtime/pkg/resourcecontrol/utils_linux.go
@@ -17,9 +17,6 @@ import (
 	"github.com/opencontainers/runc/libcontainer/cgroups/systemd"
 )
 
-// DefaultResourceControllerID runtime-determined location in the cgroups hierarchy.
-const DefaultResourceControllerID = "/vc"
-
 // ValidCgroupPathV1 returns a valid cgroup path for cgroup v1.
 // see https://github.com/opencontainers/runtime-spec/blob/master/config-linux.md#cgroups-path
 func ValidCgroupPathV1(path string, systemdCgroup bool) (string, error) {
