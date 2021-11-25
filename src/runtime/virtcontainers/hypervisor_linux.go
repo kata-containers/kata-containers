26b3f0017c6f4756cd5280949695104cd6e7424c
Samuel Ortiz <s.ortiz@apple.com>
2021-11-25 08:55:08 +0100

    virtcontainers: Split hypervisor into Linux and OS agnostic bits
    Keep all the OS agnostic bits in the hypervisor.go and
    hypervisor_ARCH.go files.
    
    Fixes #3614
    
    Signed-off-by: Eric Ernst <eric_ernst@apple.com>
    Signed-off-by: Samuel Ortiz <s.ortiz@apple.com>

---
+++ src/runtime/virtcontainers/hypervisor_linux.go
@@ -0,0 +1,45 @@
+// Copyright (c) 2021 Apple Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package virtcontainers
+
+import (
+	"fmt"
+
+	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/types"
+	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/utils"
+)
+
+func generateVMSocket(id string, vmStogarePath string) (interface{}, error) {
+	vhostFd, contextID, err := utils.FindContextID()
+	if err != nil {
+		return nil, err
+	}
+
+	return types.VSock{
+		VhostFd:   vhostFd,
+		ContextID: contextID,
+		Port:      uint32(vSockPort),
+	}, nil
+}
+
+// NewHypervisor returns an hypervisor from a hypervisor type.
+func NewHypervisor(hType HypervisorType) (Hypervisor, error) {
+
+	switch hType {
+	case QemuHypervisor:
+		return &qemu{}, nil
+	case FirecrackerHypervisor:
+		return &firecracker{}, nil
+	case AcrnHypervisor:
+		return &Acrn{}, nil
+	case ClhHypervisor:
+		return &cloudHypervisor{}, nil
+	case MockHypervisor:
+		return &mockHypervisor{}, nil
+	default:
+		return nil, fmt.Errorf("Unknown hypervisor type %s", hType)
+	}
+}
