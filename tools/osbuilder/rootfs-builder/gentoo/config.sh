f61e31cd840049a31bb16793fa4fe14ba9d4c7be
Peng Tao <bergwolf@hyper.sh>
2021-11-29 11:17:41 +0800

    osbuilder: add coreutils to guest rootfs
    So that the debug console is more useful. In the meantime, remove
    iptables as it is not used by kata-agent any more.
    
    Fixes: #3138
    Signed-off-by: Peng Tao <bergwolf@hyper.sh>

--- tools/osbuilder/rootfs-builder/gentoo/config.sh
+++ tools/osbuilder/rootfs-builder/gentoo/config.sh
@@ -11,7 +11,7 @@ OS_VERSION=${OS_VERSION:-latest}
 OS_NAME=${OS_NAME:-"gentoo"}
 
 # packages to be installed by default
-PACKAGES="sys-apps/systemd net-firewall/iptables net-misc/chrony"
+PACKAGES="sys-apps/systemd sys-apps/coreutils net-misc/chrony"
 
 # Init process must be one of {systemd,kata-agent}
 INIT_PROCESS=systemd
