10d5bc7ad5e702ecf67b64c80c6a7ed43a245f0f
James O. D. Hunt <james.o.hunt@intel.com>
2021-11-30 13:24:04 +0000

    rustjail: Wrap remaining nix errors with anyhow
    Replace `Result` values that use a "bare" `nix` `Error` like this:
    
    ```rust
    return Err(nix::Error::EINVAL.into());
    ```
    
    ... to the following which wraps the nix` error in an `anyhow` call for
    consistency with the other errors returned by `rustjail`:
    
    ```rust
    return Err(anyhow!(nix::Error::EINVAL));
    ```
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- src/agent/rustjail/src/cgroups/fs/mod.rs
+++ src/agent/rustjail/src/cgroups/fs/mod.rs
@@ -174,7 +174,7 @@ impl CgroupManager for Manager {
                 freezer_controller.freeze()?;
             }
             _ => {
-                return Err(nix::Error::EINVAL.into());
+                return Err(anyhow!(nix::Error::EINVAL));
             }
         }
 
