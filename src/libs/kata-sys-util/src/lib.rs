a3c612d54d3355da615d272498d9c7769e32ce04
Liu Jiang <gerry@linux.alibaba.com>
2021-12-08 23:12:13 +0800

    libs/sys-util: add wrappers for mount and fs
    Add some wrappers for mount and fs syscall.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>
    Signed-off-by: Bin Liu <bin@hyper.sh>
    Signed-off-by: Fupan Li <lifupan@gmail.com>
    Signed-off-by: Huamin Tang <huamin.thm@alibaba-inc.com>
    Signed-off-by: Lei Wang <wllenyj@linux.alibaba.com>
    Signed-off-by: Quanwei Zhou <quanweiZhou@linux.alibaba.com>

--- src/libs/kata-sys-util/src/lib.rs
+++ src/libs/kata-sys-util/src/lib.rs
@@ -2,3 +2,25 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 //
+
+#[macro_use]
+extern crate slog;
+
+pub mod fs;
+pub mod mount;
+
+// Convenience macro to obtain the scoped logger
+#[macro_export]
+macro_rules! sl {
+    () => {
+        slog_scope::logger()
+    };
+}
+
+#[macro_export]
+macro_rules! eother {
+    () => (std::io::Error::new(std::io::ErrorKind::Other, ""));
+    ($fmt:expr, $($arg:tt)*) => ({
+        std::io::Error::new(std::io::ErrorKind::Other, format!($fmt, $($arg)*))
+    })
+}
