5b89c1df2fb105ed3afbf68d946ee7b19cdd5263
Liu Jiang <gerry@linux.alibaba.com>
2021-12-09 11:51:38 +0800

    libs/types: add kata-types crate under src/libs
    Add kata-types crate to host constants and data types shared by multiple
    Kata Containers components.
    
    Fixes: #3305
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>
    Signed-off-by: Fupan Li <lifupan@gmail.com>
    Signed-off-by: Huamin Tang <huamin.thm@alibaba-inc.com>
    Signed-off-by: Lei Wang <wllenyj@linux.alibaba.com>
    Signed-off-by: yanlei <yl.on.the.way@gmail.com>

---
+++ src/libs/kata-types/src/annotations/mod.rs
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 Alibaba Cloud
+// Copyright (c) 2019 Ant Group
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/// CRI-containerd specific annotations.
+pub mod cri_containerd;
+
+/// CRI-O specific annotations.
+pub mod crio;
+
+/// Dockershim specific annotations.
+pub mod dockershim;
