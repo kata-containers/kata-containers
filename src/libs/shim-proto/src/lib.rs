364cec4a9851e419b5a54be24a5482a4c6e7471f
Zack <zmlcc@linux.alibaba.com>
2021-12-30 14:45:16 +0800

    shim-proto: provide rust support for containerd shim v2 protobuf
    This crate provides protobuf message and ttrpc service definitions
    for the containerd shim v2 protocol.
    This crate is designed to be published on `crates.io`, so it contains
    seperate examples, `README.md` and full `LICENSE` file.
    
    Fixes: #3250
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>
    Signed-off-by: Zack <zmlcc@linux.alibaba.com>

---
+++ src/libs/shim-proto/src/lib.rs
@@ -0,0 +1,15 @@
+// Copyright (c) 2021 Alibaba Cloud
+// Copyright (c) 2021 Ant Group
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#![allow(clippy::all)]
+
+pub mod empty;
+pub mod events_task;
+pub mod metrics;
+pub mod mount;
+pub mod shim;
+pub mod shim_ttrpc;
+pub mod task;
