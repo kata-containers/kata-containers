c1ce67d9050b1a36f88c8349cd9e54dce329f6c5
Matt Layher <mdlayher@gmail.com>
2022-02-04 20:06:12 -0500

    runtime: use github.com/mdlayher/vsock@v1.1.0
    Fixes #3625
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- src/runtime/vendor/github.com/mdlayher/vsock/conn_linux.go
+++ src/runtime/vendor/github.com/mdlayher/vsock/conn_linux.go
@@ -1,72 +1,55 @@
-//+build linux
+//go:build linux
+// +build linux
 
 package vsock
 
 import (
+	"github.com/mdlayher/socket"
 	"golang.org/x/sys/unix"
 )
 
-// newConn creates a Conn using a connFD, immediately setting the connFD to
-// non-blocking mode for use with the runtime network poller.
-func newConn(cfd connFD, local, remote *Addr) (*Conn, error) {
-	// Note: if any calls fail after this point, cfd.Close should be invoked
-	// for cleanup because the socket is now non-blocking.
-	if err := cfd.SetNonblocking(local.fileName()); err != nil {
-		return nil, err
-	}
-
-	return &Conn{
-		fd:     cfd,
-		local:  local,
-		remote: remote,
-	}, nil
-}
-
 // dial is the entry point for Dial on Linux.
-func dial(cid, port uint32) (*Conn, error) {
-	cfd, err := newConnFD()
+func dial(cid, port uint32, _ *Config) (*Conn, error) {
+	// TODO(mdlayher): Config default nil check and initialize. Pass options to
+	// socket.Config where necessary.
+
+	c, err := socket.Socket(unix.AF_VSOCK, unix.SOCK_STREAM, 0, "vsock", nil)
 	if err != nil {
 		return nil, err
 	}
 
-	return dialLinux(cfd, cid, port)
-}
-
-// dialLinux is the entry point for tests on Linux.
-func dialLinux(cfd connFD, cid, port uint32) (c *Conn, err error) {
-	defer func() {
-		if err != nil {
-			// If any system calls fail during setup, the socket must be closed
-			// to avoid file descriptor leaks.
-			_ = cfd.EarlyClose()
-		}
-	}()
-
-	rsa := &unix.SockaddrVM{
-		CID:  cid,
-		Port: port,
-	}
-
-	if err := cfd.Connect(rsa); err != nil {
+	sa := &unix.SockaddrVM{CID: cid, Port: port}
+	rsa, err := c.Connect(sa)
+	if err != nil {
+		_ = c.Close()
 		return nil, err
 	}
 
-	lsa, err := cfd.Getsockname()
+	// TODO(mdlayher): getpeername(2) appears to return nil in the GitHub CI
+	// environment, so in the event of a nil sockaddr, fall back to the previous
+	// method of synthesizing the remote address.
+	if rsa == nil {
+		rsa = sa
+	}
+
+	lsa, err := c.Getsockname()
 	if err != nil {
+		_ = c.Close()
 		return nil, err
 	}
 
 	lsavm := lsa.(*unix.SockaddrVM)
+	rsavm := rsa.(*unix.SockaddrVM)
 
-	local := &Addr{
-		ContextID: lsavm.CID,
-		Port:      lsavm.Port,
-	}
-
-	remote := &Addr{
-		ContextID: cid,
-		Port:      port,
-	}
-
-	return newConn(cfd, local, remote)
+	return &Conn{
+		c: c,
+		local: &Addr{
+			ContextID: lsavm.CID,
+			Port:      lsavm.Port,
+		},
+		remote: &Addr{
+			ContextID: rsavm.CID,
+			Port:      rsavm.Port,
+		},
+	}, nil
 }
