579c704f0c498f6bdcf52ddf587df955b5fca1bf
Zhongtao Hu <zhongtaohu.tim@linux.alibaba.com>
2022-02-11 17:10:23 +0800

    libs/types:fix styles and implementation details
    1. Some Nit problems are fixed
    2. Make the code more readable
    3. Modify some implementation details
    
    Signed-off-by: Zhongtao Hu <zhongtaohu.tim@linux.alibaba.com>

--- src/libs/kata-types/src/annotations/thirdparty.rs
+++ src/libs/kata-types/src/annotations/thirdparty.rs
@@ -9,6 +9,4 @@
 /// Annotation to enable SGX.
 ///
 /// Hardware-based isolation and memory encryption.
-// Supported suffixes are: Ki | Mi | Gi | Ti | Pi | Ei . For example: 4Mi
-// For more information about supported suffixes see https://physics.nist.gov/cuu/Units/binary.html
 pub const SGXEPC: &str = "sgx.intel.com/epc";
