4c164afbac3df0611a03f7dc774833ee51184178
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-02-24 09:39:51 +0100

    versions: Update Cloud Hypervisor to 5343e09e7b8db
    Let's bump the Cloud Hypervisor version to 5343e09e7b8db, as that brings
    a few fixes we're interested in, such as:
    
    * hypervisor, vmm: Handle TDX hypercalls with INVALID_OPERAND
      - https://github.com/cloud-hypervisor/cloud-hypervisor/pull/3723
        - This is needed for the TDX support on the cloud hypervisor driver,
          which is part of this very same series.
    
    * openapi: Update the PciBdf types
      - https://github.com/cloud-hypervisor/cloud-hypervisor/pull/3748
        - This is needed due to a change in a DeviceNode field, which would
          cause a marshalling / demarshalling error when running with a
          version of cloud-hypervisor that includes the TDX fixes mentioned
          above.
    
    * scripts: dev_cli: Don't quote $features_build
    * scripts: dev_cli: Add --features option
      - https://github.com/cloud-hypervisor/cloud-hypervisor/pull/3773
        - This is needed due to changes in the scripts used to build Cloud
          Hypervisor, which are used as part of Kata Containers CIs and
          github actions.
    
          Due to this change, we're also adapting the build scripts as part
          of this very same commit.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- src/runtime/virtcontainers/pkg/cloud-hypervisor/client/model_device_node.go
+++ src/runtime/virtcontainers/pkg/cloud-hypervisor/client/model_device_node.go
@@ -19,7 +19,7 @@ type DeviceNode struct {
 	Id        *string                   `json:"id,omitempty"`
 	Resources *[]map[string]interface{} `json:"resources,omitempty"`
 	Children  *[]string                 `json:"children,omitempty"`
-	PciBdf    *int32                    `json:"pci_bdf,omitempty"`
+	PciBdf    *string                   `json:"pci_bdf,omitempty"`
 }
 
 // NewDeviceNode instantiates a new DeviceNode object
@@ -136,9 +136,9 @@ func (o *DeviceNode) SetChildren(v []string) {
 }
 
 // GetPciBdf returns the PciBdf field value if set, zero value otherwise.
-func (o *DeviceNode) GetPciBdf() int32 {
+func (o *DeviceNode) GetPciBdf() string {
 	if o == nil || o.PciBdf == nil {
-		var ret int32
+		var ret string
 		return ret
 	}
 	return *o.PciBdf
@@ -146,7 +146,7 @@ func (o *DeviceNode) GetPciBdf() int32 {
 
 // GetPciBdfOk returns a tuple with the PciBdf field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *DeviceNode) GetPciBdfOk() (*int32, bool) {
+func (o *DeviceNode) GetPciBdfOk() (*string, bool) {
 	if o == nil || o.PciBdf == nil {
 		return nil, false
 	}
@@ -162,8 +162,8 @@ func (o *DeviceNode) HasPciBdf() bool {
 	return false
 }
 
-// SetPciBdf gets a reference to the given int32 and assigns it to the PciBdf field.
-func (o *DeviceNode) SetPciBdf(v int32) {
+// SetPciBdf gets a reference to the given string and assigns it to the PciBdf field.
+func (o *DeviceNode) SetPciBdf(v string) {
 	o.PciBdf = &v
 }
 
