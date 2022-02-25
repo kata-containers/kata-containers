4393707abef07e3f8a7966836f4fc8dae01cc772
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-02-25 21:18:07 +0100

    tools: clh: Fix unbound variable
    4c164afbac3df0611a03f7dc774833ee51184178 renamed extra_build_args to
    features, but did it only in one place, leading to:
    ```
    21:15:28 /home/jenkins/workspace/kata-containers-2.0-ubuntu-ARM-PR/go/src/github.com/kata-containers/kata-containers/tools/packaging/static-build/cloud-hypervisor/build-static-clh.sh: line 55: features: unbound variable
    21:15:29 make[1]: *** [tools/packaging/kata-deploy/local-build/Makefile:30: cloud-hypervisor-tarball-build] Error 1
    ```
    
    Fixes: #3775
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tools/packaging/static-build/cloud-hypervisor/build-static-clh.sh
+++ tools/packaging/static-build/cloud-hypervisor/build-static-clh.sh
@@ -16,7 +16,7 @@ ARCH=$(uname -m)
 script_dir=$(dirname $(readlink -f "$0"))
 kata_version="${kata_version:-}"
 force_build_from_source="${force_build_from_source:-false}"
-extra_build_args="${extra_build_args:-}"
+features="${features:-}"
 
 source "${script_dir}/../../scripts/lib.sh"
 
@@ -68,7 +68,7 @@ if [ "${ARCH}" == "aarch64" ]; then
     force_build_from_source="true"
 fi
 
-if [ -n "${extra_build_args}" ]; then
+if [ -n "${features}" ]; then
     info "As an extra build argument has been passed to the script, forcing to build from source"
     force_build_from_source="true"
 fi
