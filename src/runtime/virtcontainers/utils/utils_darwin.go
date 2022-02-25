5be188cc29a1281aa972d8db5103d1f4083e1fce
Eric Ernst <eric_ernst@apple.com>
2022-02-25 11:43:57 -0800

    utils: Add darwin stub
    Add a stub for utils_darwin to facilitate building this package on
    Darwin. We can probably drop this empty stub if we have better
    abstraction for the various parts of virtcontainers that call it
    today...
    
    Fixes:# 3777
    
    Signed-off-by: Eric Ernst <eric_ernst@apple.com>

---
+++ src/runtime/virtcontainers/utils/utils_darwin.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Apple Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package utils
+
+func GetDevicePathAndFsTypeOptions(mountPoint string) (devicePath, fsType string, fsOptions []string, err error) {
+	return
+}
