4be7185aa48f793428701b1b73f201e60b45d598
Tim Zhang <tim@hyper.sh>
2022-03-26 17:33:41 +0800

    runtime-rs: runtime part implement
    Fixes: #3785
    Signed-off-by: Tim Zhang <tim@hyper.sh>
    Signed-off-by: Zhongtao Hu <zhongtaohu.tim@linux.alibaba.com>
    Signed-off-by: Quanwei Zhou <quanweiZhou@linux.alibaba.com>

--- src/runtime-rs/crates/runtimes/virt_container/src/lib.rs
+++ src/runtime-rs/crates/runtimes/virt_container/src/lib.rs
@@ -3,12 +3,25 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 //
+
+#[macro_use]
+extern crate slog;
+
+logging::logger_with_subsystem!(sl, "virt-container");
+
+mod container_manager;
+pub mod health_check;
+pub mod sandbox;
+
 use std::sync::Arc;
 
-use anyhow::Result;
+use agent::kata::KataAgent;
+use anyhow::{Context, Result};
 use async_trait::async_trait;
 use common::{message::Message, RuntimeHandler, RuntimeInstance};
-use kata_types::config::{hypervisor::register_hypervisor_plugin, DragonballConfig};
+use hypervisor::Hypervisor;
+use kata_types::config::{hypervisor::register_hypervisor_plugin, DragonballConfig, TomlConfig};
+use resource::ResourceManager;
 use tokio::sync::mpsc::Sender;
 
 unsafe impl Send for VirtContainer {}
@@ -34,14 +47,64 @@ impl RuntimeHandler for VirtContainer {
 
     async fn new_instance(
         &self,
-        _sid: &str,
-        _msg_sender: Sender<Message>,
+        sid: &str,
+        msg_sender: Sender<Message>,
     ) -> Result<RuntimeInstance> {
-        todo!()
+        let (toml_config, _) = TomlConfig::load_from_file("").context("load config")?;
+
+        // TODO: new sandbox and container manager
+        // TODO: get from hypervisor
+        let hypervisor = new_hypervisor(&toml_config)
+            .await
+            .context("new hypervisor")?;
+
+        // get uds from hypervisor and get config from toml_config
+        let agent = Arc::new(KataAgent::new(kata_types::config::Agent {
+            debug: true,
+            enable_tracing: false,
+            server_port: 1024,
+            log_port: 1025,
+            dial_timeout_ms: 10,
+            reconnect_timeout_ms: 3_000,
+            request_timeout_ms: 30_000,
+            health_check_request_timeout_ms: 90_000,
+            kernel_modules: Default::default(),
+            container_pipe_size: 0,
+            debug_console_enabled: false,
+        }));
+
+        let resource_manager = Arc::new(ResourceManager::new(
+            sid,
+            agent.clone(),
+            hypervisor.clone(),
+            &toml_config,
+        )?);
+        let pid = std::process::id();
+
+        let sandbox = sandbox::VirtSandbox::new(
+            sid,
+            msg_sender,
+            agent.clone(),
+            hypervisor,
+            resource_manager.clone(),
+        )
+        .await
+        .context("new virt sandbox")?;
+        let container_manager =
+            container_manager::VirtContainerManager::new(sid, pid, agent, resource_manager);
+        Ok(RuntimeInstance {
+            sandbox: Arc::new(sandbox),
+            container_manager: Arc::new(container_manager),
+        })
     }
 
     fn cleanup(&self, _id: &str) -> Result<()> {
         // TODO
         Ok(())
     }
+}
+
+async fn new_hypervisor(_toml_config: &TomlConfig) -> Result<Arc<dyn Hypervisor>> {
+    // TODO: implement ready hypervisor
+    todo!()
 }
