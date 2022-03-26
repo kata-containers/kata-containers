6d8d79ada47cea70a8beab029e4ecb50631fbfc2
Quanwei Zhou <quanweiZhou@linux.alibaba.com>
2022-03-26 17:33:41 +0800

    libs: enhance kata-sys-util and kata-types
    Fixes: #3785
    Signed-off-by: Quanwei Zhou <quanweiZhou@linux.alibaba.com>

---
+++ src/libs/kata-sys-util/src/rand/mod.rs
@@ -0,0 +1,10 @@
+// Copyright (c) 2019-2022 Alibaba Cloud
+// Copyright (c) 2019-2022 Ant Group
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+mod random_bytes;
+pub use random_bytes::RandomBytes;
+mod uuid;
+pub use uuid::UUID;
