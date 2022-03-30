75e282b4c19e49ad7d8ef246a1992e20816f8989
Quanwei Zhou <quanweiZhou@linux.alibaba.com>
2022-03-30 10:13:17 +0800

    runtime-rs: hypervisor base define
    Responsible for VM manager, such as Qemu, Dragonball
    
    Fixes: #3785
    Signed-off-by: Quanwei Zhou <quanweiZhou@linux.alibaba.com>

---
+++ src/runtime-rs/crates/hypervisor/src/device/mod.rs
@@ -0,0 +1,36 @@
+// Copyright (c) 2019-2022 Alibaba Cloud
+// Copyright (c) 2019-2022 Ant Group
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+mod block;
+pub use block::BlockConfig;
+mod network;
+pub use network::{Address, NetworkConfig};
+mod share_fs_device;
+pub use share_fs_device::ShareFsDeviceConfig;
+mod vfio;
+pub use vfio::{bind_device_to_host, bind_device_to_vfio, VfioBusMode, VfioConfig};
+mod share_fs_mount;
+pub use share_fs_mount::{ShareFsMountConfig, ShareFsMountType, ShareFsOperation};
+mod vsock;
+pub use vsock::VsockConfig;
+
+use std::fmt;
+
+#[derive(Debug)]
+pub enum Device {
+    Block(BlockConfig),
+    Network(NetworkConfig),
+    ShareFsDevice(ShareFsDeviceConfig),
+    Vfio(VfioConfig),
+    ShareFsMount(ShareFsMountConfig),
+    Vsock(VsockConfig),
+}
+
+impl fmt::Display for Device {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
