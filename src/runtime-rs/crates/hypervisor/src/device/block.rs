7204acf47a7918969134f0e3cf46a5a31e51d4a5
Quanwei Zhou <quanweiZhou@linux.alibaba.com>
2022-03-30 10:13:17 +0800

    runtime-rs: hypervisor base define
    Responsible for VM manager, such as Qemu, Dragonball
    
    Fixes: #3785
    Signed-off-by: Quanwei Zhou <quanweiZhou@linux.alibaba.com>

---
+++ src/runtime-rs/crates/hypervisor/src/device/block.rs
@@ -0,0 +1,24 @@
+// Copyright (c) 2019-2022 Alibaba Cloud
+// Copyright (c) 2019-2022 Ant Group
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#[derive(Debug)]
+pub struct BlockConfig {
+    /// Unique identifier of the drive.
+    pub id: String,
+
+    /// Path of the drive.
+    pub path_on_host: String,
+
+    /// If set to true, the drive is opened in read-only mode. Otherwise, the
+    /// drive is opened as read-write.
+    pub is_readonly: bool,
+
+    /// Don't close `path_on_host` file when dropping the device.
+    pub no_drop: bool,
+
+    /// device index
+    pub index: u64,
+}
---
+++ src/runtime-rs/crates/hypervisor/src/lib.rs
@@ -0,0 +1,49 @@
+// Copyright (c) 2019-2022 Alibaba Cloud
+// Copyright (c) 2019-2022 Ant Group
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#[macro_use]
+extern crate slog;
+
+logging::logger_with_subsystem!(sl, "hypervisor");
+
+pub mod device;
+
+use std::collections::HashMap;
+
+use anyhow::Result;
+use async_trait::async_trait;
+use kata_types::config::hypervisor::Hypervisor as HypervisorConfig;
+
+#[derive(Debug)]
+pub struct VcpuThreadIds {
+    /// List of tids of vcpu threads (vcpu index, tid)
+    pub vcpus: HashMap<u32, u32>,
+}
+
+#[async_trait]
+pub trait Hypervisor: Send + Sync {
+    // vm manager
+    async fn prepare_vm(&self, id: &str, netns: Option<String>) -> Result<()>;
+    async fn start_vm(&self, timeout: i32) -> Result<()>;
+    async fn stop_vm(&self) -> Result<()>;
+    async fn pause_vm(&self) -> Result<()>;
+    async fn save_vm(&self) -> Result<()>;
+    async fn resume_vm(&self) -> Result<()>;
+
+    // device manager
+    async fn add_device(&self, device: device::Device) -> Result<()>;
+    async fn remove_device(&self, device: device::Device) -> Result<()>;
+
+    // utils
+    async fn get_agent_socket(&self) -> Result<String>;
+    async fn disconnect(&self);
+    async fn hypervisor_config(&self) -> HypervisorConfig;
+    async fn get_thread_ids(&self) -> Result<VcpuThreadIds>;
+    async fn get_pids(&self) -> Result<Vec<u32>>;
+    async fn cleanup(&self) -> Result<()>;
+    async fn check(&self) -> Result<()>;
+    async fn get_jailer_root(&self) -> Result<String>;
+}
