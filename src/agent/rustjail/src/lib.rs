c69c9f370d3be96b6f8c2e7886e9468e4f9e70e1
Braden Rayhorn <bradenrayhorn@fastmail.com>
2022-04-12 20:04:54 -0500

    rustjail: add tests for parse_mount_table
    Add tests for parse_mount_table function in rustjail/src/mount.rs.
    Includes some minor refactoring improve the testability of the
    function and improve its error values.
    
    Fixes: #4082
    
    Signed-off-by: Braden Rayhorn <bradenrayhorn@fastmail.com>

--- src/agent/rustjail/src/lib.rs
+++ src/agent/rustjail/src/lib.rs
@@ -523,6 +523,31 @@ mod tests {
         };
     }
 
+    // Parameters:
+    //
+    // 1: expected Result
+    // 2: actual Result
+    // 3: string used to identify the test on error
+    #[macro_export]
+    macro_rules! assert_result {
+        ($expected_result:expr, $actual_result:expr, $msg:expr) => {
+            if $expected_result.is_ok() {
+                let expected_value = $expected_result.as_ref().unwrap();
+                let actual_value = $actual_result.unwrap();
+                assert!(*expected_value == actual_value, "{}", $msg);
+            } else {
+                assert!($actual_result.is_err(), "{}", $msg);
+
+                let expected_error = $expected_result.as_ref().unwrap_err();
+                let expected_error_msg = format!("{:?}", expected_error);
+
+                let actual_error_msg = format!("{:?}", $actual_result.unwrap_err());
+
+                assert!(expected_error_msg == actual_error_msg, "{}", $msg);
+            }
+        };
+    }
+
     #[test]
     fn test_process_grpc_to_oci() {
         #[derive(Debug)]
