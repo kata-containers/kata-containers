03bc89ab0b46328e230092d4b873d0d93f2272ed
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-05-03 16:01:28 +0200

    clh: Rely on Cloud Hypervisor for generating the device ID
    We're currently hitting a race condition on the Cloud Hypervisor's
    driver code when quickly removing and adding a block device.
    
    This happens because the device removal is an asynchronous operation,
    and we currently do *not* monitor events coming from Cloud Hypervisor to
    know when the device was actually removed.  Together with this, the
    sandbox code doesn't know about that and when a new device is attached
    it'll quickly assign what may be the very same ID to the new device,
    leading to the Cloud Hypervisor's driver trying to hotplug a device with
    the very same ID of the device that was not yet removed.
    
    This is, in a nutshell, why the tests with Cloud Hypervisor and
    devmapper have been failing every now and then.
    
    The workaround taken to solve the issue is basically *not* passing down
    the device ID to Cloud Hypervisor and simply letting Cloud Hypervisor
    itself generate those, as Cloud Hypervisor does it in a manner that
    avoids such conflicts.  With this addition we have then to keep a map of
    the device ID and the Cloud Hypervisor's generated ID, so we can
    properly remove the device.
    
    This workaround will probably stay for a while, at least till someone
    has enough cycles to implement a way to watch the device removal event
    and then properly act on that.  Spoiler alert, this will be a complex
    change that may not even be worth it considering the race can be avoided
    with this commit.
    
    Fixes: #4196
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>
    (cherry picked from commit 33a8b705588d6f65163e05bf83119db4360b04ac)
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- src/runtime/virtcontainers/clh_test.go
+++ src/runtime/virtcontainers/clh_test.go
@@ -384,6 +384,7 @@ func TestCloudHypervisorHotplugAddBlockDevice(t *testing.T) {
 	clh := &cloudHypervisor{}
 	clh.config = clhConfig
 	clh.APIClient = &clhClientMock{}
+	clh.devicesIds = make(map[string]string)
 
 	clh.config.BlockDeviceDriver = config.VirtioBlock
 	err = clh.hotplugAddBlockDevice(&config.BlockDrive{Pmem: false})
@@ -406,6 +407,7 @@ func TestCloudHypervisorHotplugRemoveDevice(t *testing.T) {
 	clh := &cloudHypervisor{}
 	clh.config = clhConfig
 	clh.APIClient = &clhClientMock{}
+	clh.devicesIds = make(map[string]string)
 
 	_, err = clh.HotplugRemoveDevice(context.Background(), &config.BlockDrive{}, BlockDev)
 	assert.NoError(err, "Hotplug remove block device expected no error")
