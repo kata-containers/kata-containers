ffa8ac037bb4d2b6a2ecc5a9e42f3f90598bb520
wllenyj <wllenyj@linux.alibaba.com>
2022-05-05 17:36:04 +0800

    dragonball: add virtio vsock device manager.
    Added VsockDeviceMgr struct to manage all vsock devices.
    
    Fixes: #4257
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>
    Signed-off-by: wllenyj <wllenyj@linux.alibaba.com>
    Signed-off-by: Chao Wu <chaowu@linux.alibaba.com>

--- src/dragonball/src/device_manager/mod.rs
+++ src/dragonball/src/device_manager/mod.rs
@@ -16,6 +16,19 @@ use dbs_legacy_devices::ConsoleHandler;
 use dbs_utils::epoll_manager::EpollManager;
 use kvm_ioctls::VmFd;
 
+#[cfg(feature = "dbs-virtio-devices")]
+use dbs_device::resources::ResourceConstraint;
+#[cfg(feature = "dbs-virtio-devices")]
+use dbs_virtio_devices as virtio;
+#[cfg(feature = "dbs-virtio-devices")]
+use dbs_virtio_devices::{
+    mmio::{
+        MmioV2Device, DRAGONBALL_FEATURE_INTR_USED, DRAGONBALL_FEATURE_PER_QUEUE_NOTIFY,
+        DRAGONBALL_MMIO_DOORBELL_SIZE, MMIO_DEFAULT_CFG_SIZE,
+    },
+    VirtioDevice,
+};
+
 use crate::address_space_manager::GuestAddressSpaceImpl;
 use crate::error::StartMicrovmError;
 use crate::resource_manager::ResourceManager;
@@ -28,6 +41,18 @@ pub use self::console_manager::ConsoleManager;
 
 mod legacy;
 pub use self::legacy::{Error as LegacyDeviceError, LegacyDeviceManager};
+
+#[cfg(feature = "virtio-vsock")]
+/// Device manager for user-space vsock devices.
+pub mod vsock_dev_mgr;
+#[cfg(feature = "virtio-vsock")]
+use self::vsock_dev_mgr::VsockDeviceMgr;
+
+macro_rules! info(
+    ($l:expr, $($args:tt)+) => {
+        slog::info!($l, $($args)+; slog::o!("subsystem" => "device_manager"))
+    };
+);
 
 /// Errors related to device manager operations.
 #[derive(Debug, thiserror::Error)]
@@ -53,10 +78,21 @@ pub enum DeviceMgrError {
     /// Failure from legacy device manager.
     #[error(transparent)]
     LegacyManager(legacy::Error),
+
+    #[cfg(feature = "dbs-virtio-devices")]
+    /// Error from Virtio subsystem.
+    #[error(transparent)]
+    Virtio(virtio::Error),
 }
 
 /// Specialized version of `std::result::Result` for device manager operations.
 pub type Result<T> = ::std::result::Result<T, DeviceMgrError>;
+
+/// Type of the dragonball virtio devices.
+#[cfg(feature = "dbs-virtio-devices")]
+pub type DbsVirtioDevice = Box<
+    dyn VirtioDevice<GuestAddressSpaceImpl, virtio_queue::QueueState, vm_memory::GuestRegionMmap>,
+>;
 
 /// Type of the dragonball virtio mmio devices.
 #[cfg(feature = "dbs-virtio-devices")]
@@ -240,6 +276,8 @@ pub struct DeviceManager {
 
     pub(crate) con_manager: ConsoleManager,
     pub(crate) legacy_manager: Option<LegacyDeviceManager>,
+    #[cfg(feature = "virtio-vsock")]
+    pub(crate) vsock_manager: VsockDeviceMgr,
 }
 
 impl DeviceManager {
@@ -259,6 +297,8 @@ impl DeviceManager {
             logger: logger.new(slog::o!()),
             con_manager: ConsoleManager::new(epoll_manager, logger),
             legacy_manager: None,
+            #[cfg(feature = "virtio-vsock")]
+            vsock_manager: VsockDeviceMgr::default(),
         }
     }
 
@@ -386,6 +426,9 @@ impl DeviceManager {
         self.create_legacy_devices(&mut ctx)?;
         self.init_legacy_devices(dmesg_fifo, com1_sock_path, &mut ctx)?;
 
+        #[cfg(feature = "virtio-vsock")]
+        self.vsock_manager.attach_devices(&mut ctx)?;
+
         ctx.generate_kernel_boot_args(kernel_config)
             .map_err(StartMicrovmError::DeviceManager)?;
 
@@ -422,6 +465,138 @@ impl DeviceManager {
             Ok((range.0, range.1, irq))
         } else {
             Err(DeviceMgrError::GetDeviceResource)
+        }
+    }
+
+    /// Create an Virtio MMIO transport layer device for the virtio backend device.
+    pub fn create_mmio_virtio_device(
+        device: DbsVirtioDevice,
+        ctx: &mut DeviceOpContext,
+        use_shared_irq: bool,
+        use_generic_irq: bool,
+    ) -> std::result::Result<Arc<DbsMmioV2Device>, DeviceMgrError> {
+        let features = DRAGONBALL_FEATURE_INTR_USED | DRAGONBALL_FEATURE_PER_QUEUE_NOTIFY;
+        DeviceManager::create_mmio_virtio_device_with_features(
+            device,
+            ctx,
+            Some(features),
+            use_shared_irq,
+            use_generic_irq,
+        )
+    }
+
+    /// Create an Virtio MMIO transport layer device for the virtio backend device with specified
+    /// features.
+    pub fn create_mmio_virtio_device_with_features(
+        device: DbsVirtioDevice,
+        ctx: &mut DeviceOpContext,
+        features: Option<u32>,
+        use_shared_irq: bool,
+        use_generic_irq: bool,
+    ) -> std::result::Result<Arc<DbsMmioV2Device>, DeviceMgrError> {
+        // Every emulated Virtio MMIO device needs a 4K configuration space,
+        // and another 4K space for per queue notification.
+        const MMIO_ADDRESS_DEFAULT: ResourceConstraint = ResourceConstraint::MmioAddress {
+            range: None,
+            align: 0,
+            size: MMIO_DEFAULT_CFG_SIZE + DRAGONBALL_MMIO_DOORBELL_SIZE,
+        };
+        let mut requests = vec![MMIO_ADDRESS_DEFAULT];
+        device.get_resource_requirements(&mut requests, use_generic_irq);
+        let resources = ctx
+            .res_manager
+            .allocate_device_resources(&requests, use_shared_irq)
+            .map_err(|_| DeviceMgrError::GetDeviceResource)?;
+
+        let virtio_dev = match MmioV2Device::new(
+            ctx.vm_fd.clone(),
+            ctx.get_vm_as()?,
+            ctx.irq_manager.clone(),
+            device,
+            resources,
+            features,
+        ) {
+            Ok(d) => d,
+            Err(e) => return Err(DeviceMgrError::Virtio(e)),
+        };
+
+        Self::register_mmio_virtio_device(Arc::new(virtio_dev), ctx)
+    }
+
+    /// Teardown the Virtio MMIO transport layer device associated with the virtio backend device.
+    pub fn destroy_mmio_virtio_device(
+        device: Arc<dyn DeviceIo>,
+        ctx: &mut DeviceOpContext,
+    ) -> std::result::Result<(), DeviceMgrError> {
+        Self::destroy_mmio_device(device.clone(), ctx)?;
+
+        let mmio_dev = device
+            .as_any()
+            .downcast_ref::<DbsMmioV2Device>()
+            .ok_or(DeviceMgrError::InvalidOperation)?;
+
+        mmio_dev.remove();
+
+        Ok(())
+    }
+
+    fn destroy_mmio_device(
+        device: Arc<dyn DeviceIo>,
+        ctx: &mut DeviceOpContext,
+    ) -> std::result::Result<(), DeviceMgrError> {
+        // unregister IoManager
+        Self::deregister_mmio_virtio_device(&device, ctx)?;
+
+        // unregister Resource manager
+        let resources = device.get_assigned_resources();
+        ctx.res_manager.free_device_resources(&resources);
+
+        Ok(())
+    }
+
+    /// Create an Virtio MMIO transport layer device for the virtio backend device.
+    pub fn register_mmio_virtio_device(
+        device: Arc<DbsMmioV2Device>,
+        ctx: &mut DeviceOpContext,
+    ) -> std::result::Result<Arc<DbsMmioV2Device>, DeviceMgrError> {
+        let (mmio_base, mmio_size, irq) = Self::get_virtio_device_info(&device)?;
+        info!(
+            ctx.logger(),
+            "create virtio mmio device 0x{:x}@0x{:x}, irq: 0x{:x}", mmio_size, mmio_base, irq
+        );
+        let resources = device.get_trapped_io_resources();
+
+        let mut tx = ctx.io_context.begin_tx();
+        if let Err(e) = ctx
+            .io_context
+            .register_device_io(&mut tx, device.clone(), &resources)
+        {
+            ctx.io_context.cancel_tx(tx);
+            Err(DeviceMgrError::IoManager(e))
+        } else {
+            ctx.virtio_devices.push(device.clone());
+            ctx.io_context.commit_tx(tx);
+            Ok(device)
+        }
+    }
+
+    /// Deregister a Virtio MMIO device from IoManager
+    pub fn deregister_mmio_virtio_device(
+        device: &Arc<dyn DeviceIo>,
+        ctx: &mut DeviceOpContext,
+    ) -> std::result::Result<(), DeviceMgrError> {
+        let resources = device.get_trapped_io_resources();
+        info!(
+            ctx.logger(),
+            "unregister mmio virtio device: {:?}", resources
+        );
+        let mut tx = ctx.io_context.begin_tx();
+        if let Err(e) = ctx.io_context.unregister_device_io(&mut tx, &resources) {
+            ctx.io_context.cancel_tx(tx);
+            Err(DeviceMgrError::IoManager(e))
+        } else {
+            ctx.io_context.commit_tx(tx);
+            Ok(())
         }
     }
 }
--- src/dragonball/src/error.rs
+++ src/dragonball/src/error.rs
@@ -9,18 +9,33 @@
 
 //! Error codes for the virtual machine monitor subsystem.
 
+#[cfg(feature = "dbs-virtio-devices")]
+use dbs_virtio_devices::Error as VirtIoError;
+
+use crate::device_manager;
+
 /// Errors associated with starting the instance.
 #[derive(Debug, thiserror::Error)]
 pub enum StartMicrovmError {
-    /// The device manager was not configured.
-    #[error("the device manager failed to manage devices: {0}")]
-    DeviceManager(#[source] crate::device_manager::DeviceMgrError),
-
-    /// Cannot add devices to the Legacy I/O Bus.
-    #[error("failure in managing legacy device: {0}")]
-    LegacyDevice(#[source] crate::device_manager::LegacyDeviceError),
-
     /// Cannot read from an Event file descriptor.
     #[error("failure while reading from EventFd file descriptor")]
     EventFd,
+
+    /// The device manager was not configured.
+    #[error("the device manager failed to manage devices: {0}")]
+    DeviceManager(#[source] device_manager::DeviceMgrError),
+
+    /// Cannot add devices to the Legacy I/O Bus.
+    #[error("failure in managing legacy device: {0}")]
+    LegacyDevice(#[source] device_manager::LegacyDeviceError),
+
+    #[cfg(feature = "virtio-vsock")]
+    /// Failed to create the vsock device.
+    #[error("cannot create virtio-vsock device: {0}")]
+    CreateVsockDevice(#[source] VirtIoError),
+
+    #[cfg(feature = "virtio-vsock")]
+    /// Cannot initialize a MMIO Vsock Device or add a device to the MMIO Bus.
+    #[error("failure while registering virtio-vsock device: {0}")]
+    RegisterVsockDevice(#[source] device_manager::DeviceMgrError),
 }
