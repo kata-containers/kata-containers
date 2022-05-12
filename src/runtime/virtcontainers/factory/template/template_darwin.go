46004506eadf171849695d5f242f8f3cfc1975fb
Eric Ernst <eric_ernst@apple.com>
2022-05-12 13:57:38 -0700

    virtcontainers: Split the factory package into Linux and Darwin bits
    - split template
    - split factory
    - add stubs for darwin
    
    Signed-off-by: Eric Ernst <eric_ernst@apple.com>

---
+++ src/runtime/virtcontainers/factory/template/template_darwin.go
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2022 Apple, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// template implements base vm factory with vm templating.
+
+package template
+
+type template struct{}
