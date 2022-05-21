e708ef3c7d586ecabef6d0720895cb0b7b1ce1de
Yibo Zhuang <yibzhuang@gmail.com>
2022-05-20 18:42:47 -0700

    runtime: direct-volume stats use correct name
    Today the shim does a translation when doing
    direct-volume stats where it takes the source and
    returns the mount path within the guest.
    
    The source for a direct-assigned volume is actually
    the device path on the host and not the publish
    volume path.
    
    This change will perform a lookup of the mount info
    during direct-volume stats to ensure that the
    device path is provided to the shim for querying
    the volume stats.
    
    Fixes: #4297
    
    Signed-off-by: Yibo Zhuang <yibzhuang@gmail.com>

--- src/runtime/cmd/kata-runtime/kata-volume.go
+++ src/runtime/cmd/kata-runtime/kata-volume.go
@@ -130,8 +130,14 @@ func Stats(volumePath string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	urlSafeDevicePath := url.PathEscape(volumePath)
-	body, err := shimclient.DoGet(sandboxId, defaultTimeout, containerdshim.DirectVolumeStatUrl+"/"+urlSafeDevicePath)
+	volumeMountInfo, err := volume.VolumeMountInfo(volumePath)
+	if err != nil {
+		return nil, err
+	}
+
+	urlSafeDevicePath := url.PathEscape(volumeMountInfo.Device)
+	body, err := shimclient.DoGet(sandboxId, defaultTimeout,
+		fmt.Sprintf("%s?%s=%s", containerdshim.DirectVolumeStatUrl, containerdshim.DirectVolumePathKey, urlSafeDevicePath))
 	if err != nil {
 		return nil, err
 	}
@@ -144,8 +150,13 @@ func Resize(volumePath string, size uint64) error {
 	if err != nil {
 		return err
 	}
+	volumeMountInfo, err := volume.VolumeMountInfo(volumePath)
+	if err != nil {
+		return err
+	}
+
 	resizeReq := containerdshim.ResizeRequest{
-		VolumePath: volumePath,
+		VolumePath: volumeMountInfo.Device,
 		Size:       size,
 	}
 	encoded, err := json.Marshal(resizeReq)
