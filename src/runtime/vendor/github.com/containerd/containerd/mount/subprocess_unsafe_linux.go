d6924182f38e06d9e104ecda194d2dbe9e965a8d
Megan Wright <megan.wright@ibm.com>
2022-06-20 10:51:09 +0100

    vendor: Update CC containerd version
    Re-vendor to pickup confidential-containers containerd 1.6.6 rebase
    Fixes: #4489
    
    Signed-off-by: Megan Wright <megan.wright@ibm.com>

--- src/runtime/vendor/github.com/containerd/containerd/mount/subprocess_unsafe_linux.go
+++ src/runtime/vendor/github.com/containerd/containerd/mount/subprocess_unsafe_linux.go
@@ -14,4 +14,17 @@
    limitations under the License.
 */
 
-package plugin
+package mount
+
+import (
+	_ "unsafe" // required for go:linkname.
+)
+
+//go:linkname beforeFork syscall.runtime_BeforeFork
+func beforeFork()
+
+//go:linkname afterFork syscall.runtime_AfterFork
+func afterFork()
+
+//go:linkname afterForkInChild syscall.runtime_AfterForkInChild
+func afterForkInChild()
