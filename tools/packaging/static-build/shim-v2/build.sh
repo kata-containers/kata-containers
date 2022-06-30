7ceeeba9a2ca0b0200f5921b1faba0a35d411224
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-06-30 12:48:20 +0200

    static-build: Allow removing shipped shim-v2 configs
    Let's add a new "REMOVE_VMM_CONFIGS" environment variable that can be
    passsed to the script responsible for building Kata Containers.
    
    Right now this is not useful for the `main` or `stable` branch, but for
    the CC release we only have been working and testing with QEMU and Cloud
    Hypervisor.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tools/packaging/static-build/shim-v2/build.sh
+++ tools/packaging/static-build/shim-v2/build.sh
@@ -20,6 +20,7 @@ PREFIX=${PREFIX:-/opt/kata}
 container_image="shim-v2-builder"
 
 EXTRA_OPTS="${EXTRA_OPTS:-""}"
+REMOVE_VMM_CONFIGS="${REMOVE_VMM_CONFIGS:-""}"
 
 sudo docker build  --build-arg GO_VERSION="${GO_VERSION}" -t "${container_image}" "${script_dir}"
 
@@ -40,6 +41,10 @@ sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 
 sudo sed -i -e '/^initrd =/d' "${DESTDIR}/${PREFIX}/share/defaults/kata-containers/configuration-qemu.toml"
 sudo sed -i -e '/^initrd =/d' "${DESTDIR}/${PREFIX}/share/defaults/kata-containers/configuration-fc.toml"
+
+for vmm in ${REMOVE_VMM_CONFIGS}; do
+	sudo rm "${DESTDIR}/${PREFIX}/share/defaults/kata-containers/configuration-$vmm.toml"
+done
 
 pushd "${DESTDIR}/${PREFIX}/share/defaults/kata-containers"
 	sudo ln -sf "configuration-qemu.toml" configuration.toml
