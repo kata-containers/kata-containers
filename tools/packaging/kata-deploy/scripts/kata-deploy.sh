4bd17359d093d05fb2a4b0f13486a84eaaebd9f4
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-07-08 12:44:43 +0200

    kata-deploy: Rely on the configured config path
    Instead of passing a `KATA_CONF_FILE` environament variable, let's rely
    on the configured (in the container engine) config path, as both
    containerd and CRI-O support it, and we're using this for both of them.
    
    Fixes: #4608
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tools/packaging/kata-deploy/scripts/kata-deploy.sh
+++ tools/packaging/kata-deploy/scripts/kata-deploy.sh
@@ -98,10 +98,7 @@ function configure_different_shims_base() {
 			fi
 		fi
 
-		cat << EOF | tee "$shim_file"
-#!/usr/bin/env bash
-KATA_CONF_FILE=/opt/kata/share/defaults/kata-containers/configuration-${shim}.toml /opt/kata/bin/containerd-shim-kata-v2 "\$@"
-EOF
+		ln -sf /opt/kata/bin/containerd-shim-kata-v2 "${shim_file}"
 		chmod +x "$shim_file"
 
 		if [ "${shim}" == "${default_shim}" ]; then
