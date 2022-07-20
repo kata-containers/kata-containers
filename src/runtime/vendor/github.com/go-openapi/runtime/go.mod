23df0bc745021bcce5093324be0bf0412f793a12
gntouts <gntouts@nubificus.co.uk>
2022-07-20 16:07:21 +0000

    versions: Update Firecracker version to v1.1.0
    This patch upgrades Firecracker version from v0.23.4 to v1.1.0
    
    * Generate swagger models for v1.1.0 (from firecracker.yaml)
    * Replace ht_enabled param to smt (API change)
    * Remove NUMA-related jailer param --node 0
    
    Fixes: #4673
    Depends-on: github.com/kata-containers/tests#4968
    
    Signed-off-by: George Ntoutsos <gntouts@nubificus.co.uk>
    Signed-off-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- src/runtime/vendor/github.com/go-openapi/runtime/go.mod
+++ src/runtime/vendor/github.com/go-openapi/runtime/go.mod
@@ -1,20 +1,17 @@
 module github.com/go-openapi/runtime
 
 require (
-	github.com/docker/go-units v0.3.3
-	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8 // indirect
-	github.com/go-openapi/analysis v0.17.2
-	github.com/go-openapi/errors v0.17.2
-	github.com/go-openapi/jsonpointer v0.17.2 // indirect
-	github.com/go-openapi/jsonreference v0.17.2 // indirect
-	github.com/go-openapi/loads v0.17.2
-	github.com/go-openapi/spec v0.17.2
-	github.com/go-openapi/strfmt v0.17.2
-	github.com/go-openapi/swag v0.17.2
-	github.com/go-openapi/validate v0.17.2
-	github.com/google/uuid v1.1.0 // indirect
-	github.com/stretchr/testify v1.2.2
-	golang.org/x/net v0.0.0-20181201002055-351d144fa1fc // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v2 v2.2.2
+	github.com/docker/go-units v0.4.0
+	github.com/go-openapi/analysis v0.19.10
+	github.com/go-openapi/errors v0.19.6
+	github.com/go-openapi/loads v0.19.5
+	github.com/go-openapi/spec v0.19.8
+	github.com/go-openapi/strfmt v0.19.5
+	github.com/go-openapi/swag v0.19.9
+	github.com/go-openapi/validate v0.19.10
+	github.com/stretchr/testify v1.6.1
+	gopkg.in/yaml.v2 v2.3.0
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
+
+go 1.13
--- src/runtime/virtcontainers/pkg/firecracker/client/operations/describe_instance_parameters.go
+++ src/runtime/virtcontainers/pkg/firecracker/client/operations/describe_instance_parameters.go
@@ -13,56 +13,69 @@ import (
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
 	cr "github.com/go-openapi/runtime/client"
-
-	strfmt "github.com/go-openapi/strfmt"
+	"github.com/go-openapi/strfmt"
 )
 
-// NewDescribeInstanceParams creates a new DescribeInstanceParams object
-// with the default values initialized.
+// NewDescribeInstanceParams creates a new DescribeInstanceParams object,
+// with the default timeout for this client.
+//
+// Default values are not hydrated, since defaults are normally applied by the API server side.
+//
+// To enforce default values in parameter, use SetDefaults or WithDefaults.
 func NewDescribeInstanceParams() *DescribeInstanceParams {
-
 	return &DescribeInstanceParams{
-
 		timeout: cr.DefaultTimeout,
 	}
 }
 
 // NewDescribeInstanceParamsWithTimeout creates a new DescribeInstanceParams object
-// with the default values initialized, and the ability to set a timeout on a request
+// with the ability to set a timeout on a request.
 func NewDescribeInstanceParamsWithTimeout(timeout time.Duration) *DescribeInstanceParams {
-
 	return &DescribeInstanceParams{
-
 		timeout: timeout,
 	}
 }
 
 // NewDescribeInstanceParamsWithContext creates a new DescribeInstanceParams object
-// with the default values initialized, and the ability to set a context for a request
+// with the ability to set a context for a request.
 func NewDescribeInstanceParamsWithContext(ctx context.Context) *DescribeInstanceParams {
-
 	return &DescribeInstanceParams{
-
 		Context: ctx,
 	}
 }
 
 // NewDescribeInstanceParamsWithHTTPClient creates a new DescribeInstanceParams object
-// with the default values initialized, and the ability to set a custom HTTPClient for a request
+// with the ability to set a custom HTTPClient for a request.
 func NewDescribeInstanceParamsWithHTTPClient(client *http.Client) *DescribeInstanceParams {
-
 	return &DescribeInstanceParams{
 		HTTPClient: client,
 	}
 }
 
-/*DescribeInstanceParams contains all the parameters to send to the API endpoint
-for the describe instance operation typically these are written to a http.Request
+/* DescribeInstanceParams contains all the parameters to send to the API endpoint
+   for the describe instance operation.
+
+   Typically these are written to a http.Request.
 */
 type DescribeInstanceParams struct {
 	timeout    time.Duration
 	Context    context.Context
 	HTTPClient *http.Client
+}
+
+// WithDefaults hydrates default values in the describe instance params (not the query body).
+//
+// All values with no default are reset to their zero value.
+func (o *DescribeInstanceParams) WithDefaults() *DescribeInstanceParams {
+	o.SetDefaults()
+	return o
+}
+
+// SetDefaults hydrates default values in the describe instance params (not the query body).
+//
+// All values with no default are reset to their zero value.
+func (o *DescribeInstanceParams) SetDefaults() {
+	// no default values defined for this parameter
 }
 
 // WithTimeout adds the timeout to the describe instance params
