17fae54079db85814a28569fbc1f49692f31825a
quanwei.zqw <quanwei.zqw@alibaba-inc.com>
2022-07-28 15:21:54 +0800

    runtime-rs: fix stop failed in azure
    Fix the stop failed in azure.
    
    Fixes: #4740
    Signed-off-by: Quanwei Zhou <quanweiZhou@linux.alibaba.com>

--- src/runtime-rs/crates/runtimes/virt_container/src/container_manager/process.rs
+++ src/runtime-rs/crates/runtimes/virt_container/src/container_manager/process.rs
@@ -133,10 +133,20 @@ impl Process {
         let io_name = io_name.to_string();
         let logger = self.logger.new(o!("io name" => io_name));
         let _ = tokio::spawn(async move {
-            match tokio::io::copy(&mut reader, &mut writer).await {
-                Err(e) => warn!(logger, "io: failed to copy stream {}", e),
-                Ok(length) => warn!(logger, "io: stop to copy stream length {}", length),
-            };
+            loop {
+                match tokio::io::copy(&mut reader, &mut writer).await {
+                    Err(e) => {
+                        if let Some(error_code) = e.raw_os_error() {
+                            if error_code == libc::EAGAIN {
+                                continue;
+                            }
+                        }
+                        warn!(logger, "io: failed to copy stream {}", e);
+                    }
+                    Ok(length) => warn!(logger, "io: stop to copy stream length {}", length),
+                };
+                break;
+            }
 
             wgw.done();
         });
