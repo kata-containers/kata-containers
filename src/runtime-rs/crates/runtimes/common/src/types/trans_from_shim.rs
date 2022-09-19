156f8e2e58288ff3964df6b4ddba9643b670f4eb
Zhongtao Hu <zhongtaohu.tim@linux.alibaba.com>
2022-09-19 20:36:52 +0800

    kata-deploy: get the config path from cri options
    get the config path for runtime-rs from cri options
    
    Fixes: #5000
    Signed-off-by: Zhongtao Hu <zhongtaohu.tim@linux.alibaba.com>

--- src/runtime-rs/crates/runtimes/common/src/types/trans_from_shim.rs
+++ src/runtime-rs/crates/runtimes/common/src/types/trans_from_shim.rs
@@ -4,18 +4,16 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-use std::{
-    convert::{From, TryFrom},
-    path::PathBuf,
-};
-
-use anyhow::{Context, Result};
-use containerd_shim_protos::api;
-use kata_types::mount::Mount;
-
 use super::{
     ContainerConfig, ContainerID, ContainerProcess, ExecProcessRequest, KillRequest, Request,
     ResizePTYRequest, ShutdownRequest, UpdateRequest,
+};
+use anyhow::{Context, Result};
+use containerd_shim_protos::api;
+use kata_types::mount::Mount;
+use std::{
+    convert::{From, TryFrom},
+    path::PathBuf,
 };
 
 fn trans_from_shim_mount(from: api::Mount) -> Mount {
@@ -42,6 +40,11 @@ fn trans_from_shim_mount(from: api::Mount) -> Mount {
 impl TryFrom<api::CreateTaskRequest> for Request {
     type Error = anyhow::Error;
     fn try_from(from: api::CreateTaskRequest) -> Result<Self> {
+        let options = if from.has_options() {
+            Some(from.get_options().get_value().to_vec())
+        } else {
+            None
+        };
         Ok(Request::CreateContainer(ContainerConfig {
             container_id: from.id.clone(),
             bundle: from.bundle.clone(),
@@ -52,6 +55,7 @@ impl TryFrom<api::CreateTaskRequest> for Request {
                 .map(trans_from_shim_mount)
                 .collect(),
             terminal: from.terminal,
+            options,
             stdin: (!from.stdin.is_empty()).then(|| from.stdin.clone()),
             stdout: (!from.stdout.is_empty()).then(|| from.stdout.clone()),
             stderr: (!from.stderr.is_empty()).then(|| from.stderr.clone()),
