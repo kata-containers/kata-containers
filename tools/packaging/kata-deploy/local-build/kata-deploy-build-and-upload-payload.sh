c57f8ff669e733c0c3c37515d327d60656797760
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-10-19 13:00:40 +0200

    packaging: Expand the vars on build-and-upload-payload.sh
    Just for the sake of avoiding issues in the future.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tools/packaging/kata-deploy/local-build/kata-deploy-build-and-upload-payload.sh
+++ tools/packaging/kata-deploy/local-build/kata-deploy-build-and-upload-payload.sh
@@ -5,23 +5,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-KATA_DEPLOY_DIR="`dirname $0`/../../kata-deploy-cc"
+KATA_DEPLOY_DIR="`dirname ${0}`/../../kata-deploy-cc"
 KATA_DEPLOY_ARTIFACT="${1:-"kata-static.tar.xz"}"
 REGISTRY="${2:-"quay.io/confidential-containers/runtime-payload"}"
 TAG="${3:-}"
 
-echo "Copying $KATA_DEPLOY_ARTIFACT to $KATA_DEPLOY_DIR"
-cp $KATA_DEPLOY_ARTIFACT $KATA_DEPLOY_DIR
+echo "Copying ${KATA_DEPLOY_ARTIFACT} to ${KATA_DEPLOY_DIR}"
+cp ${KATA_DEPLOY_ARTIFACT} ${KATA_DEPLOY_DIR}
 
-pushd $KATA_DEPLOY_DIR
+pushd ${KATA_DEPLOY_DIR}
 
 IMAGE_TAG="${REGISTRY}:kata-containers-$(git rev-parse HEAD)"
 
 echo "Building the image"
-docker build --tag $IMAGE_TAG .
+docker build --tag ${IMAGE_TAG} .
 
 echo "Pushing the image to quay.io"
-docker push $IMAGE_TAG
+docker push ${IMAGE_TAG}
 
 if [ -n "${TAG}" ]; then
 	ADDITIONAL_TAG="${REGISTRY}:${TAG}"
