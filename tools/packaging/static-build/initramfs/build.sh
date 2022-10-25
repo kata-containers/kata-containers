880c163c83026d17153f62be70f728b06cc2c20b
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-10-25 15:09:11 +0200

    packaging: Add infra to push the initramfs builder image
    Let's add the needed infra for only building and pushing the initramfs
    builder image to the Kata Containers' quay.io registry.
    
    Fixes: #7084
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>
    (cherry picked from commit 111ad87828e479dd986ac1a158637d195c0283bd)

--- tools/packaging/static-build/initramfs/build.sh
+++ tools/packaging/static-build/initramfs/build.sh
@@ -34,8 +34,10 @@ package_output_dir="${package_output_dir:-}"
 
 container_image="${BUILDER_REGISTRY}:initramfs-cryptsetup-${cryptsetup_version}-lvm2-${lvm2_version}-$(get_last_modification ${repo_root_dir} ${script_dir})"
 
-sudo docker pull ${container_image} || sudo docker build \
-	-t "${container_image}" "${script_dir}"
+sudo docker pull ${container_image} || (sudo docker build \
+	-t "${container_image}" "${script_dir}" && \
+	# No-op unless PUSH_TO_REGISTRY is exported as "yes"
+	push_to_registry "${container_image}")
 
 sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	-w "${PWD}" \
