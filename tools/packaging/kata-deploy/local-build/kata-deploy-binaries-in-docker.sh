959dc3226bee9c9eb56f9c6a7e6279a5707b4e54
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-10-25 20:16:18 +0200

    Revert "packaging: Add infra to push the kata-deploy builder image"
    This reverts commit fe8b246ae4f3945fc5816cefcee5cf67bf233a59.
    
    The reason this has to be reverted is because we cannot cache an image
    that has a specific user, uid, gid, docker_host_id, and expect that to
    work equally on different machines.  Unfortunately, this is one of the
    images that cannot be cached at all.

--- tools/packaging/kata-deploy/local-build/kata-deploy-binaries-in-docker.sh
+++ tools/packaging/kata-deploy/local-build/kata-deploy-binaries-in-docker.sh
@@ -41,15 +41,12 @@ fi
 
 container_image="${CC_BUILDER_REGISTRY}:build-kata-deploy-$(get_last_modification ${kata_dir} ${script_dir})"
 
-docker pull "${container_image}" || \
-	(docker build -q -t "${container_image}" \
-		--build-arg IMG_USER="${USER}" \
-		--build-arg UID=${uid} \
-		--build-arg GID=${gid} \
-		--build-arg HOST_DOCKER_GID=${docker_gid} \
-		"${script_dir}/dockerbuild/" && \
- 	 # No-op unless PUSH_TO_REGISTRY is exported as "yes"
-	 push_to_registry "${container_image}" "no")
+docker pull "${container_image}" || docker build -q -t "${container_image}" \
+	--build-arg IMG_USER="${USER}" \
+	--build-arg UID=${uid} \
+	--build-arg GID=${gid} \
+	--build-arg HOST_DOCKER_GID=${docker_gid} \
+	"${script_dir}/dockerbuild/"
 
 docker run \
 	--privileged \
