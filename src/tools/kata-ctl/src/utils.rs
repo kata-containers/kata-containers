e80a9f09fac3b166eca996f7bcee79b820fc2367
Archana Shinde <archana.m.shinde@intel.com>
2022-11-17 14:04:07 -0800

    utils: Add utility function to fetch the kernel version.
    Add functionality to get kernel version and related unit tests.
    This is intended to be used in the kata-env command going forward.
    
    Fixes: #5688
    
    Signed-off-by: Archana Shinde <archana.m.shinde@intel.com>

--- src/tools/kata-ctl/src/utils.rs
+++ src/tools/kata-ctl/src/utils.rs
@@ -5,7 +5,8 @@
 
 #![allow(dead_code)]
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
+use std::fs;
 
 const NON_PRIV_USER: &str = "nobody";
 
@@ -21,13 +22,73 @@ pub fn drop_privs() -> Result<()> {
     Ok(())
 }
 
+const PROC_VERSION_FILE: &str = "/proc/version";
+
+pub fn get_kernel_version(proc_version_file: &str) -> Result<String> {
+    let contents = fs::read_to_string(proc_version_file)
+        .context(format!("Failed to read file {}", proc_version_file))?;
+
+    let fields: Vec<&str> = contents.split_whitespace().collect();
+
+    if fields.len() < 3 {
+        return Err(anyhow!("unexpected contents in file {}", proc_version_file));
+    }
+
+    let kernel_version = String::from(fields[2]);
+    Ok(kernel_version)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Write;
+    use tempfile::tempdir;
 
     #[test]
     fn test_drop_privs() {
         let res = drop_privs();
         assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_kernel_version_empty_input() {
+        let res = get_kernel_version("").unwrap_err().to_string();
+        let err_msg = format!("Failed to read file {}", "");
+        assert_eq!(res, err_msg);
+    }
+
+    #[test]
+    fn test_kernel_version_valid_input() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("proc-version");
+        let path = file_path.clone();
+        let mut file = fs::File::create(file_path).unwrap();
+        writeln!(
+            file,
+            "Linux version 5.15.0-75-generic (buildd@lcy02-amd64-045)"
+        )
+        .unwrap();
+        let kernel = get_kernel_version(path.to_str().unwrap()).unwrap();
+        assert_eq!(kernel, "5.15.0-75-generic");
+    }
+
+    #[test]
+    fn test_kernel_version_system_input() {
+        let res = get_kernel_version(PROC_VERSION_FILE);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_kernel_version_invalid_input() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("proc-version");
+        let path = file_path.clone();
+        let mut file = fs::File::create(file_path).unwrap();
+        writeln!(file, "Linux-version-5.15.0-75-generic").unwrap();
+        let actual = get_kernel_version(path.to_str().unwrap())
+            .unwrap_err()
+            .to_string();
+        let expected = format!("unexpected contents in file {}", path.to_str().unwrap());
+        assert_eq!(actual, expected);
     }
 }
