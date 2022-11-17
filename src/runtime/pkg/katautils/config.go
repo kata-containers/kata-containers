38d55e91938c6f137ad4e6b0e3aad446d03152c3
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-11-17 13:11:05 +0100

    golang: Stop using io/ioutils
    The package has been deprecated as part of 1.16 and the same
    functionality is now provided by either the io or the os package.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- src/runtime/pkg/katautils/config.go
+++ src/runtime/pkg/katautils/config.go
@@ -10,7 +10,6 @@ package katautils
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -1410,7 +1409,7 @@ func decodeDropIns(mainConfigPath string, tomlConf *tomlConfig) error {
 	configDir := filepath.Dir(mainConfigPath)
 	dropInDir := filepath.Join(configDir, "config.d")
 
-	files, err := ioutil.ReadDir(dropInDir)
+	files, err := os.ReadDir(dropInDir)
 	if err != nil {
 		if !os.IsNotExist(err) {
 			return fmt.Errorf("error reading %q directory: %s", dropInDir, err)
