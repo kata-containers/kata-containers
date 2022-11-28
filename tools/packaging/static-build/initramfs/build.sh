2c6b3d114c94d99cb09eead9a1295c9420c1c2a6
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2022-11-28 19:44:38 +0100

    initramfs: get_from_kata_deps does't require a "kata_version" arg
    It's been dropped for a long time now, as part of
    a09e58fa80ae81c0d336d9a782da4f38f9492da1.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tools/packaging/static-build/initramfs/build.sh
+++ tools/packaging/static-build/initramfs/build.sh
@@ -15,17 +15,16 @@ readonly default_install_dir="$(cd "${script_dir}/../../kernel" && pwd)"
 
 source "${script_dir}/../../scripts/lib.sh"
 
-kata_version="${kata_version:-}"
 cryptsetup_repo="${cryptsetup_repo:-}"
 cryptsetup_version="${cryptsetup_version:-}"
 lvm2_repo="${lvm2_repo:-}"
 lvm2_version="${lvm2_version:-}"
 package_output_dir="${package_output_dir:-}"
 
-[ -n "${cryptsetup_repo}" ] || cryptsetup_repo=$(get_from_kata_deps "externals.cryptsetup.url" "${kata_version}")
-[ -n "${cryptsetup_version}" ] || cryptsetup_version=$(get_from_kata_deps "externals.cryptsetup.version" "${kata_version}")
-[ -n "${lvm2_repo}" ] || lvm2_repo=$(get_from_kata_deps "externals.lvm2.url" "${kata_version}")
-[ -n "${lvm2_version}" ] || lvm2_version=$(get_from_kata_deps "externals.lvm2.version" "${kata_version}")
+[ -n "${cryptsetup_repo}" ] || cryptsetup_repo=$(get_from_kata_deps "externals.cryptsetup.url")
+[ -n "${cryptsetup_version}" ] || cryptsetup_version=$(get_from_kata_deps "externals.cryptsetup.version")
+[ -n "${lvm2_repo}" ] || lvm2_repo=$(get_from_kata_deps "externals.lvm2.url")
+[ -n "${lvm2_version}" ] || lvm2_version=$(get_from_kata_deps "externals.lvm2.version")
 
 [ -n "${cryptsetup_repo}" ] || die "Failed to get cryptsetup repo"
 [ -n "${cryptsetup_version}" ] || die "Failed to get cryptsetup version"
