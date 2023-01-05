2607336b3b21ad3ccf91d7fc39c498cbc0213e29
Danny Canter <danny@dcantah.dev>
2023-01-05 02:53:22 -0800

    schedcore: Make buildable on !linux
    Fixes: #5983
    
    sched-core only makes sense on Linux hosts. Let's add stub/error for
    other platforms.
    
    Signed-off-by: Eric Ernst <eric_ernst@apple.com>
    Signed-off-by: Danny Canter <danny@dcantah.dev>

--- src/runtime/pkg/utils/schedcore.go
+++ src/runtime/pkg/utils/schedcore.go
@@ -5,10 +5,6 @@
 
 package utils
 
-import (
-	"golang.org/x/sys/unix"
-)
-
 // PidType is the type of provided pid value and how it should be treated
 type PidType int
 
@@ -24,13 +20,3 @@ const (
 	// ProcessGroup affects all processes in the group
 	ProcessGroup PidType = pidTypeProcessGroupId
 )
-
-// Create a new sched core domain
-func Create(t PidType) error {
-	return unix.Prctl(unix.PR_SCHED_CORE, unix.PR_SCHED_CORE_CREATE, 0, uintptr(t), 0)
-}
-
-// ShareFrom shares the sched core domain from the provided pid
-func ShareFrom(pid uint64, t PidType) error {
-	return unix.Prctl(unix.PR_SCHED_CORE, unix.PR_SCHED_CORE_SHARE_FROM, uintptr(pid), uintptr(t), 0)
-}
