43ce3f7588c6b1e411b36f23e73a196fb4a68861
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-03-08 21:13:34 +0100

    packaging: Simplify get_last_modification()
    There's no need to pass repo_root_dir to get_last_modification() as the
    variable used everywhere is exported from that very same file.
    
    Fixes: #6431
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tools/packaging/static-build/virtiofsd/build.sh
+++ tools/packaging/static-build/virtiofsd/build.sh
@@ -48,7 +48,7 @@ case ${ARCH} in
 		;;
 esac
 
-container_image="${VIRTIOFSD_CONTAINER_BUILDER:-${BUILDER_REGISTRY}:virtiofsd-${virtiofsd_toolchain}-${libc}-$(get_last_modification ${repo_root_dir} ${script_dir})-$(uname -m)}"
+container_image="${VIRTIOFSD_CONTAINER_BUILDER:-${BUILDER_REGISTRY}:virtiofsd-${virtiofsd_toolchain}-${libc}-$(get_last_modification ${script_dir})-$(uname -m)}"
 
 sudo docker pull ${container_image} || \
 	(sudo docker build \
