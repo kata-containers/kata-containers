84cedf83d49b86660331c4b376ba825ceec91fc8
Dan Mihai <dmihai@microsoft.com>
2023-04-25 01:46:33 +0000

    Support stdin/stdout as YAML file
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- src/tools/genpolicy/src/utils.rs
+++ src/tools/genpolicy/src/utils.rs
@@ -3,3 +3,36 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+use log::info;
+
+pub struct InOutFiles {
+    pub yaml_file: Option<String>,
+    pub rules_file: String,
+    pub infra_data_file: String,
+    pub output_policy_file: Option<String>,
+}
+
+impl InOutFiles {
+    pub fn new(
+        yaml_file: Option<String>,
+        input_files_path: Option<String>,
+        output_policy_file: Option<String>,
+    ) -> Self {
+        let mut input_path = ".".to_string();
+        if let Some(path) = input_files_path {
+            input_path = path.clone();
+        }
+        let rules_file = input_path.to_owned() + "/rules.rego";
+        info!("Rules file: {:?}", &rules_file);
+
+        let infra_data_file = input_path.to_owned() + "/data.json";
+        info!("Infra data file: {:?}", &infra_data_file);
+
+        Self {
+            yaml_file,
+            rules_file,
+            infra_data_file,
+            output_policy_file,
+        }
+    }
+}
