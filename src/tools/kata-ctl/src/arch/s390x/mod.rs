77519fd12016bd2ac41736bdffeceb19a5f572de
Jayant Singh <jayant.singh@intel.com>
2023-04-28 23:54:49 +0000

    kata-ctl: Switch to slog logging; add --log-level, --json-logging args
    Fixes: #5401, #6654
    
    - Switch kata-ctl from eprintln!()/println!() to structured logging via
      the logging library which uses slog.
    - Adds a new create_term_logger() library call which enables printing
      log messages to the terminal via a less verbose / more human readable
      terminal format with colors.
    - Adds --log-level argument to select the minimum log level of printed messages.
    - Adds --json-logging argument to switch to logging in JSON format.
    
    Co-authored-by: Byron Marohn <byron.marohn@intel.com>
    Co-authored-by: Luke Phillips <lucas.phillips@intel.com>
    Signed-off-by: Jayant Singh <jayant.singh@intel.com>
    Signed-off-by: Byron Marohn <byron.marohn@intel.com>
    Signed-off-by: Luke Phillips <lucas.phillips@intel.com>
    Signed-off-by: Kelby Madal-Hellmuth <kelby.madal-hellmuth@intel.com>
    Signed-off-by: Liz Lawrens <liz.lawrens@intel.com>

--- src/tools/kata-ctl/src/arch/s390x/mod.rs
+++ src/tools/kata-ctl/src/arch/s390x/mod.rs
@@ -13,6 +13,7 @@ mod arch_specific {
     use crate::utils;
     use anyhow::{anyhow, Result};
     use nix::unistd::Uid;
+    use slog::{info, o, warn};
     use std::collections::HashMap;
     use std::io::BufRead;
     use std::io::BufReader;
@@ -21,6 +22,12 @@ mod arch_specific {
     const CPUINFO_FEATURES_TAG: &str = "features";
     const CPU_FEATURES_REQ: &[&str] = &["sie"];
 
+    macro_rules! sl {
+        () => {
+            slog_scope::logger().new(o!("subsystem" => "s390x"))
+        };
+    }
+
     #[allow(dead_code)]
     pub const ARCH_CPU_VENDOR_FIELD: &str = check::GENERIC_CPU_VENDOR_FIELD;
     #[allow(dead_code)]
@@ -28,7 +35,7 @@ mod arch_specific {
 
     // check cpu
     fn check_cpu() -> Result<()> {
-        println!("INFO: check CPU: s390x");
+        info!(sl!(), "check CPU: s390x");
 
         let cpu_info = check::get_single_cpu_info(check::PROC_CPUINFO, CPUINFO_DELIMITER)?;
 
@@ -42,14 +49,14 @@ mod arch_specific {
 
         let missing_cpu_features = check::check_cpu_flags(&cpu_features, CPU_FEATURES_REQ)?;
         if !missing_cpu_features.is_empty() {
-            eprintln!("WARNING: Missing CPU flags {:?}", missing_cpu_features);
+            warn!(sl!(), "Missing CPU flags {:?}", missing_cpu_features);
         }
 
         Ok(())
     }
 
     pub fn check(_args: &str) -> Result<()> {
-        println!("INFO: check: s390x");
+        info!(sl!(), "check: s390x");
 
         let _cpu_result = check_cpu();
 
--- src/tools/kata-ctl/src/ops/check_ops.rs
+++ src/tools/kata-ctl/src/ops/check_ops.rs
@@ -15,7 +15,15 @@ use crate::types::*;
 
 use anyhow::{anyhow, Result};
 
+use slog::{info, o, warn};
+
 const NAME: &str = "kata-ctl";
+
+macro_rules! sl {
+    () => {
+        slog_scope::logger().new(o!("subsystem" => "check_ops"))
+    };
+}
 
 // This function retrieves the cmd function passes as argument
 fn get_builtin_check_func(name: CheckType) -> Result<BuiltinCmdFp> {
@@ -55,14 +63,12 @@ fn print_check_list() -> Result<()> {
     let cmds = get_client_cmd_details();
 
     if cmds.is_empty() {
-        println!("Checks not found!\n");
+        warn!(sl!(), "Checks not found!\n");
 
         return Ok(());
     }
 
-    cmds.iter().for_each(|n| println!(" - {}", n));
-
-    println!();
+    cmds.iter().for_each(|n| info!(sl!(), " - {}", n));
 
     Ok(())
 }
@@ -125,6 +131,6 @@ pub fn handle_metrics(_args: MetricsCommand) -> Result<()> {
 pub fn handle_version() -> Result<()> {
     let version = version::get().unwrap();
 
-    println!("{} version {:?} (type: rust)", NAME, version);
+    info!(sl!(), "{} version {:?} (type: rust)", NAME, version);
     Ok(())
 }
