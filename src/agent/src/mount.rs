6c6243413cb40a31b1819840636eeffe075412c4
Wedson Almeida Filho <walmeida@microsoft.com>
2023-06-02 14:19:14 -0300

    agent: fix the order of the layers in overlayfs
    When passed the option lowerdir=/a:/b:/c, overlayfs interprets /c as the
    base layer, /b as the next one, and /a as the top layer. This is in
    reverse order of the OCI image spec, so we need to apply this reversal
    before mounting with overlayfs.
    
    Signed-off-by: Wedson Almeida Filho <walmeida@microsoft.com>

--- src/agent/src/mount.rs
+++ src/agent/src/mount.rs
@@ -543,6 +543,7 @@ async fn virtio_blk_storage_handler(
             logger,
             "mounts are: {}", layers.join(":")
         );
+        layers.reverse();
         let status = std::process::Command::new("mount_tar.sh")
             .arg(layers.join(":"))
             .arg(&mount_point)
