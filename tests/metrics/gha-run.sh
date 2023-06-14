4bbfcfaf152a80b54f1f3f786d00d242b3f7e872
David Esparza <david.esparza.borquez@intel.com>
2023-06-14 15:53:06 -0600

    tests: Move tests helper script to this repo
    The common.sh script includes helper functions used in
    our metrics tests, so we are gradually adding more
    metrics used in kata.
    
    Fixes: #7108
    
    Signed-off-by: David Esparza <david.esparza.borquez@intel.com>

--- tests/metrics/gha-run.sh
+++ tests/metrics/gha-run.sh
@@ -10,22 +10,14 @@ set -o nounset
 set -o pipefail
 
 metrics_dir="$(dirname "$(readlink -f "$0")")"
-source "${metrics_dir}/lib/common.bash"
-
-function init_env() {
-    metrics_onetime_init
-    disable_ksm
-}
 
 function run_test_launchtimes() {
     hypervisor="${1}"
 
     echo "Running launchtimes tests: "
-    init_env
 
     if [ "${hypervisor}" = 'qemu' ]; then
         echo "qemu"
-        bash time/launch_times.sh -i public.ecr.aws/ubuntu/ubuntu:latest -n 20
     elif [ "${hypervisor}" = 'clh' ]; then
         echo "clh"
     fi
