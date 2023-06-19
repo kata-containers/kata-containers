ee3206e930961bb7ff6a246754b83d59b1d87a63
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2023-06-19 17:55:19 +0000

    gha: Check kata env for metrics tests
    This PR will check that kata is properly install for the metrics gha.
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- tests/metrics/gha-run.sh
+++ tests/metrics/gha-run.sh
@@ -12,24 +12,28 @@ set -o pipefail
 metrics_dir="$(dirname "$(readlink -f "$0")")"
 
 function run_test_launchtimes() {
-    hypervisor="${1}"
+	hypervisor="${1}"
 
-    echo "Running launchtimes tests: "
+	echo "Running launchtimes tests: "
 
-    if [ "${hypervisor}" = 'qemu' ]; then
-        echo "qemu"
-    elif [ "${hypervisor}" = 'clh' ]; then
-        echo "clh"
-    fi
+	if [ "${hypervisor}" = 'qemu' ]; then
+		echo "qemu"
+		echo "Check kata installation"
+		kata-runtime kata-env
+		echo "Kata config:"
+		cat $(kata-runtime kata-env  --json | jq .Runtime.Config.Path -r)
+	elif [ "${hypervisor}" = 'clh' ]; then
+		echo "clh"
+	fi
 }
 
 function main() {
-    action="${1:-}"
-    case "${action}" in
-        run-test-launchtimes-qemu) run_test_launchtimes "qemu" ;;
-        run-test-launchtimes-clh) run_test_launchtimes "clh" ;;
-        *) >&2 echo "Invalid argument"; exit 2 ;;
-    esac
+	action="${1:-}"
+	case "${action}" in
+		run-test-launchtimes-qemu) run_test_launchtimes "qemu" ;;
+		run-test-launchtimes-clh) run_test_launchtimes "clh" ;;
+		*) >&2 echo "Invalid argument"; exit 2 ;;
+	esac
 }
 
 main "$@"
