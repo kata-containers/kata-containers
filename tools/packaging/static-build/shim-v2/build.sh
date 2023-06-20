554b5c7fa01e2106c5b55033f6377a91855cd1ab
Jianyong Wu <jianyong.wu@arm.com>
2023-06-20 22:30:37 +0800

    static-build: enable cross build for shim-v2
    shim-v2 has go and rust code. For rust code, we use messense/rust-musl-cross
    to build for speed up as it doesn't depends on qemu emulation. Build go
    code based on docker buildx as it doesn't support cross build now.
    
    Fixes: #6557
    Signed-off-by: Jianyong Wu <jianyong.wu@arm.com>

--- tools/packaging/static-build/shim-v2/build.sh
+++ tools/packaging/static-build/shim-v2/build.sh
@@ -16,6 +16,7 @@ VMM_CONFIGS="qemu fc"
 
 GO_VERSION=${GO_VERSION}
 RUST_VERSION=${RUST_VERSION}
+CC=""
 
 DESTDIR=${DESTDIR:-${PWD}}
 PREFIX=${PREFIX:-/opt/kata}
@@ -23,29 +24,43 @@ container_image="${SHIM_V2_CONTAINER_BUILDER:-$(get_shim_v2_image_name)}"
 
 EXTRA_OPTS="${EXTRA_OPTS:-""}"
 
+[ "${CROSS_BUILD}" == "true" ] && container_image_bk="${container_image}" && container_image="${container_image}-cross-build"
 sudo docker pull ${container_image} || \
-       	(sudo docker build  \
+	(sudo docker ${BUILDX} build ${PLATFORM}  \
 		--build-arg GO_VERSION="${GO_VERSION}" \
 		--build-arg RUST_VERSION="${RUST_VERSION}" \
 		-t "${container_image}" \
 		"${script_dir}" && \
 	 push_to_registry "${container_image}")
 
-arch=$(uname -m)
+arch=${ARCH:-$(uname -m)}
+GCC_ARCH=${arch}
 if [ ${arch} = "ppc64le" ]; then
+	GCC_ARCH="powerpc64le"
 	arch="ppc64"
 fi
 
+#Build rust project using cross build musl image to speed up
+[[ "${CROSS_BUILD}" == "true" && ${ARCH} != "s390x" ]] && container_image="messense/rust-musl-cross:${GCC_ARCH}-musl" && CC=${GCC_ARCH}-unknown-linux-musl-gcc
+
 sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+	--env CROSS_BUILD=${CROSS_BUILD} \
+	--env ARCH=${ARCH} \
+	--env CC="${CC}" \
 	-w "${repo_root_dir}/src/runtime-rs" \
 	"${container_image}" \
 	bash -c "git config --global --add safe.directory ${repo_root_dir} && make PREFIX=${PREFIX} QEMUCMD=qemu-system-${arch}"
 
 sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+	--env CROSS_BUILD=${CROSS_BUILD} \
+        --env ARCH=${ARCH} \
+        --env CC="${CC}" \
 	-w "${repo_root_dir}/src/runtime-rs" \
 	"${container_image}" \
 	bash -c "git config --global --add safe.directory ${repo_root_dir} && make PREFIX="${PREFIX}" DESTDIR="${DESTDIR}" install"
-	
+
+[ "${CROSS_BUILD}" == "true" ] && container_image="${container_image_bk}-cross-build"
+
 sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	-w "${repo_root_dir}/src/runtime" \
 	"${container_image}" \
