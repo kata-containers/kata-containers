4dbba5215f12413babad133cf23a862f93c27783
Wainer dos Santos Moschetta <wainersm@redhat.com>
2023-07-04 13:50:21 -0300

    shim-v2: moved measured rootfs logic to its builder
    Moved the measure rootfs logic from kata-deploy-binaries.sh to the
    shim-v2's builder script so that the former get less bloated with
    components's specific code.
    
    Fixes #6674
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- tools/packaging/static-build/shim-v2/build.sh
+++ tools/packaging/static-build/shim-v2/build.sh
@@ -25,6 +25,16 @@ container_image="${SHIM_V2_CONTAINER_BUILDER:-$(get_shim_v2_image_name)}"
 EXTRA_OPTS="${EXTRA_OPTS:-""}"
 
 [ "${CROSS_BUILD}" == "true" ] && container_image_bk="${container_image}" && container_image="${container_image}-cross-build"
+if [ "${MEASURED_ROOTFS}" == "yes" ]; then
+	EXTRA_OPTS+=" DEFSERVICEOFFLOAD=true"
+	if [ -f "${repo_root_dir}/tools/osbuilder/root_hash.txt" ]; then
+		info "Enable rootfs measurement config"
+		root_hash=$(sudo sed -e 's/Root hash:\s*//g;t;d' "${repo_root_dir}/tools/osbuilder/root_hash.txt")
+		root_measure_config="rootfs_verity.scheme=dm-verity rootfs_verity.hash=${root_hash}"
+		EXTRA_OPTS+=" ROOTMEASURECONFIG=\"${root_measure_config}\""
+	fi
+fi
+
 sudo docker pull ${container_image} || \
 	(sudo docker ${BUILDX} build ${PLATFORM}  \
 		--build-arg GO_VERSION="${GO_VERSION}" \
