3bd72fd945696cc43c281527d06426570cc55f29
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-07-12 18:17:04 +0200

    tests: common: Move a few utility functions to common.bash
    Those functions were originally introduced as part of the
    `metrics/gha-run.sh` file, but those will be very hand at the time we
    start adding more tests.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tests/common.bash
+++ tests/common.bash
@@ -254,3 +254,74 @@ function compress_metrics_results_dir()
 	cd "${path_results}" && tar -czf "${tarball_fname}" *.json && cd -
 	info "tarball generated: ${tarball_fname}"
 }
+
+function create_symbolic_links() {
+	local KATA_HYPERVISOR="${1}"
+
+	local link_configuration_file="/opt/kata/share/defaults/kata-containers/configuration.toml"
+	local source_configuration_file="/opt/kata/share/defaults/kata-containers/configuration-${KATA_HYPERVISOR}.toml"
+
+	if [ "${KATA_HYPERVISOR}" != 'qemu' ] && [ "${KATA_HYPERVISOR}" != 'clh' ]; then
+		die "Failed to set the configuration.toml: '${KATA_HYPERVISOR}' is not recognized as a valid hypervisor name."
+	fi
+
+	sudo ln -sf "${source_configuration_file}" "${link_configuration_file}"
+}
+
+# Configures containerd
+function overwrite_containerd_config() {
+	containerd_config="/etc/containerd/config.toml"
+	sudo rm "${containerd_config}"
+	sudo tee "${containerd_config}" << EOF
+version = 2
+[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+  SystemdCgroup = true
+
+[plugins]
+  [plugins."io.containerd.grpc.v1.cri"]
+    [plugins."io.containerd.grpc.v1.cri".containerd]
+      default_runtime_name = "kata"
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.kata]
+          runtime_type = "io.containerd.kata.v2"
+EOF
+}
+
+function install_kata() {
+	local kata_tarball="kata-static.tar.xz"
+	declare -r katadir="/opt/kata"
+	declare -r destdir="/"
+	declare -r local_bin_dir="/usr/local/bin/"
+
+	# Removing previous kata installation
+	sudo rm -rf "${katadir}"
+
+	pushd "${kata_tarball_dir}"
+	sudo tar -xvf "${kata_tarball}" -C "${destdir}"
+	popd
+
+	# create symbolic links to kata components
+	for b in "${katadir}/bin/*" ; do
+		sudo ln -sf "${b}" "${local_bin_dir}/$(basename $b)"
+	done
+
+	check_containerd_config_for_kata
+	restart_containerd_service
+	install_checkmetrics
+}
+
+function check_containerd_config_for_kata() {
+	# check containerd config
+	declare -r line1="default_runtime_name = \"kata\""
+	declare -r line2="runtime_type = \"io.containerd.kata.v2\""
+	declare -r num_lines_containerd=2
+	declare -r containerd_path="/etc/containerd/config.toml"
+	local count_matches=$(grep -ic  "$line1\|$line2" "${containerd_path}")
+
+	if [ "${count_matches}" = "${num_lines_containerd}" ]; then
+		info "containerd ok"
+	else
+		info "overwriting containerd configuration w/ a valid one"
+		overwrite_containerd_config
+	fi
+}
