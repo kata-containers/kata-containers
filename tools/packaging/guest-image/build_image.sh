64260102c1d2f9df3dba970ee0b38d0244d3d56c
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-07-26 08:55:48 +0200

    fixup! Merge remote-tracking branch 'upstream/main' into CCv0-merge-25th-july

--- tools/packaging/guest-image/build_image.sh
+++ tools/packaging/guest-image/build_image.sh
@@ -21,65 +21,45 @@ readonly osbuilder_dir="$(cd "${repo_root_dir}/tools/osbuilder" && pwd)"
 
 export GOPATH=${GOPATH:-${HOME}/go}
 
-final_image_name="kata-containers"
-final_initrd_name="kata-containers-initrd"
-image_initrd_extension=".img"
-
 arch_target="$(uname -m)"
-final_image_name="kata-containers"
-final_initrd_name="kata-containers-initrd"
+final_artifact_name="kata-containers"
 image_initrd_extension=".img"
 
 build_initrd() {
 	info "Build initrd"
-	info "initrd os: $initrd_distro"
-	info "initrd os version: $initrd_os_version"
-	local rootfs_build_dest="${builddir}/initrd-image"
-	export DISTRO="$initrd_distro"
-	export OS_VERSION="${initrd_os_version}"
-	export USE_DOCKER=1
-	export AGENT_INIT="yes"
-	# ROOTFS_BUILD_DEST is a Make variable
-	# SNP will also use the SEV guest module
-	if [[ "${AA_KBC:-}" == "offline_sev_kbc" || "${AA_KBC:-}" == "online_sev_kbc" ]]; then
-		config_version=$(get_config_version)
-		kernel_version="$(get_from_kata_deps "assets.kernel.sev.version")"
-		kernel_version=${kernel_version#v}
-		module_dir="${repo_root_dir}/tools/packaging/kata-deploy/local-build/build/kernel-sev/builddir/kata-linux-${kernel_version}-${config_version}/lib/modules/${kernel_version}"
-		sudo -E PATH="$PATH" make rootfs ROOTFS_BUILD_DEST="${rootfs_build_dest}" KERNEL_MODULES_DIR="${module_dir}"
-	else
-		sudo -E PATH="$PATH" make rootfs ROOTFS_BUILD_DEST="${rootfs_build_dest}"
-	fi
-
-	if [ -n "${INCLUDE_ROOTFS:-}" ]; then
-		sudo cp -RL --preserve=mode "${INCLUDE_ROOTFS}/." "${rootfs_build_dest}/${initrd_distro}_rootfs/"
-	fi
-	sudo -E PATH="$PATH" make initrd ROOTFS_BUILD_DEST="${rootfs_build_dest}"
-	mv "kata-containers-initrd.img" "${install_dir}/${initrd_name}"
+	info "initrd os: $os_name"
+	info "initrd os version: $os_version"
+	sudo -E PATH="$PATH" make initrd \
+		DISTRO="$os_name" \
+		DEBUG="${DEBUG:-}" \
+		OS_VERSION="${os_version}" \
+		ROOTFS_BUILD_DEST="${builddir}/initrd-image" \
+		USE_DOCKER=1 \
+		AGENT_INIT="yes"
+	mv "kata-containers-initrd.img" "${install_dir}/${artifact_name}"
 	(
 		cd "${install_dir}"
-		ln -sf "${initrd_name}" "${final_initrd_name}${image_initrd_extension}"
+		ln -sf "${artifact_name}" "${final_artifact_name}${image_initrd_extension}"
 	)
 }
 
 build_image() {
 	info "Build image"
-	info "image os: $img_distro"
-	info "image os version: $img_os_version"
+	info "image os: $os_name"
+	info "image os version: $os_version"
 	sudo -E PATH="${PATH}" make image \
-		DISTRO="${img_distro}" \
+		DISTRO="${os_name}" \
 		DEBUG="${DEBUG:-}" \
 		USE_DOCKER="1" \
-		IMG_OS_VERSION="${img_os_version}" \
+		IMG_OS_VERSION="${os_version}" \
 		ROOTFS_BUILD_DEST="${builddir}/rootfs-image"
-	mv -f "kata-containers.img" "${install_dir}/${image_name}"
+	mv -f "kata-containers.img" "${install_dir}/${artifact_name}"
 	if [ -e "root_hash.txt" ]; then
-		[ -z "${root_hash_suffix}" ] && root_hash_suffix=vanilla
-		mv "${repo_root_dir}/tools/osbuilder/root_hash.txt" "${repo_root_dir}/tools/osbuilder/root_hash_${root_hash_suffix}.txt"
+	    cp root_hash.txt "${install_dir}/"
 	fi
 	(
 		cd "${install_dir}"
-		ln -sf "${image_name}" "${final_image_name}${image_initrd_extension}"
+		ln -sf "${artifact_name}" "${final_artifact_name}${image_initrd_extension}"
 	)
 }
 
@@ -93,6 +73,8 @@ Usage:
 ${script_name} [options]
 
 Options:
+ --osname=${os_name}
+ --osversion=${os_version}
  --imagetype=${image_type}
  --prefix=${prefix}
  --destdir=${destdir}
@@ -108,49 +90,25 @@ main() {
 	prefix="/opt/kata"
 	image_suffix=""
 	image_initrd_suffix=""
-	root_hash_suffix=""
 	builddir="${PWD}"
 	while getopts "h-:" opt; do
 		case "$opt" in
 		-)
 			case "${OPTARG}" in
+			osname=*)
+				os_name=${OPTARG#*=}
+				;;
+			osversion=*)
+				os_version=${OPTARG#*=}
+				;;
 			imagetype=image)
 				image_type=image
-				#image information
-				img_distro=$(get_from_kata_deps "assets.image.architecture.${arch_target}.name")
-				img_os_version=$(get_from_kata_deps "assets.image.architecture.${arch_target}.version")
-				image_name="kata-${img_distro}-${img_os_version}.${image_type}"
 				;;
 			imagetype=initrd)
 				image_type=initrd
-				#initrd information
-				initrd_distro=$(get_from_kata_deps "assets.initrd.architecture.${arch_target}.name")
-				initrd_os_version=$(get_from_kata_deps "assets.initrd.architecture.${arch_target}.version")
-				initrd_name="kata-${initrd_distro}-${initrd_os_version}.${image_type}"
 				;;
 			image_initrd_suffix=*)
 				image_initrd_suffix=${OPTARG#*=}
-				if [ "${image_initrd_suffix}" == "sev" ]; then
-					initrd_distro=$(get_from_kata_deps "assets.initrd.architecture.${arch_target}.sev.name")
-					initrd_os_version=$(get_from_kata_deps "assets.initrd.architecture.${arch_target}.sev.version")
-					initrd_name="kata-${initrd_distro}-${initrd_os_version}-${image_initrd_suffix}.${image_type}"
-					final_initrd_name="${final_initrd_name}-${image_initrd_suffix}"
-				elif [ "${image_initrd_suffix}" == "tdx" ]; then
-					img_distro=$(get_from_kata_deps "assets.image.architecture.${arch_target}.name")
-					img_os_version=$(get_from_kata_deps "assets.image.architecture.${arch_target}.version")
-					image_name="kata-${img_distro}-${img_os_version}-${image_initrd_suffix}.${image_type}"
-					final_image_name="${final_image_name}-${image_initrd_suffix}"
-
-					initrd_distro=$(get_from_kata_deps "assets.initrd.architecture.${arch_target}.name")
-					initrd_os_version=$(get_from_kata_deps "assets.initrd.architecture.${arch_target}.version")
-					initrd_name="kata-${initrd_distro}-${initrd_os_version}-${image_initrd_suffix}.${image_type}"
-					final_initrd_name="${final_initrd_name}-${image_initrd_suffix}"
-				elif [ -n "${image_initrd_suffix}" ]; then
-					die "Invalid image_initrd_suffix ${image_initrd_suffix}"
-				fi
-				;;
-			root_hash_suffix=*)
-				root_hash_suffix=${OPTARG#*=}
 				;;
 			prefix=*)
 				prefix=${OPTARG#*=}
@@ -179,7 +137,16 @@ main() {
 
 	echo "build ${image_type}"
 
+	if [ "${image_type}" = "initrd" ]; then
+		final_artifact_name+="-initrd"
+	fi
 
+	if [ -n "${image_initrd_suffix}" ]; then
+		artifact_name="kata-${os_name}-${os_version}-${image_initrd_suffix}.${image_type}"
+		final_artifact_name+="-${image_initrd_suffix}"
+	else
+		artifact_name="kata-${os_name}-${os_version}.${image_type}"
+	fi
 
 	install_dir="${destdir}/${prefix}/share/kata-containers/"
 	readonly install_dir
