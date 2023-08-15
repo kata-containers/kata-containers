ce6adecd0a28a18e112c62608db73975352a99f5
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-08-15 16:24:59 +0200

    gha: kata-deploy: Add run-kata-deploy-tests.sh
    This will have the same function as run-k8s-tests.sh has, but for
    kata-deploy.
    
    Right now it doesn't have any tests, and the command to actually run the
    tests is commented out, but right now this is just a placeholder that
    will be populated sooner than later.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

---
+++ tests/functional/kata-deploy/run-kata-deploy-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2023 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+kata_deploy_dir=$(dirname "$(readlink -f "$0")")
+source "${kata_deploy_dir}/../../common.bash"
+
+if [ -n "${KATA_DEPLOY_TEST_UNION:-}" ]; then
+	KATA_DEPLOY_TEST_UNION=($KATA_DEPLOY_TEST_UNION)
+else
+	KATA_DEPLOY_TEST_UNION=( \
+	)
+fi
+
+info "Run tests"
+for KATA_DEPLOY_TEST_ENTRY in ${KATA_DEPLOY_TEST_UNION[@]}
+do
+	#bats "${KATA_DEPLOY_TEST_ENTRY}"
+done
