436e046e8d48fb436c301c77d0eeeb898455d6c6
shixuanqing <1356292400@qq.com>
2023-08-15 08:36:38 +0000

    runtime: create network monitor for the Go runtime
    During the era of Kata 1.0, a network monitor was implemented using Go.
    However, following the architectural changes in Kata 2.0 and 3.0,
    support for Docker was discontinued, leading to the removal of the associated network monitor.
    With the reintegration of Docker support into Kata, the necessity to reestablish the network monitor has arisen.
    
    Fixes: #7040
    
    Signed-off-by: shixuanqing <1356292400@qq.com>
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>
    Signed-off-by: HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
    
    Co-authored-by: Sebastien Boeuf <sebastien.boeuf@intel.com>
    Co-authored-by: HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>

--- tests/integration/docker/gha-run.sh
+++ tests/integration/docker/gha-run.sh
@@ -30,6 +30,53 @@ function run() {
 
 	info "Running docker with Kata Containers (${KATA_HYPERVISOR})"
 	sudo docker run --rm --runtime io.containerd.kata-${KATA_HYPERVISOR}.v2 --entrypoint nping "${image}" --tcp-connect -c 2 -p 80 www.github.com
+
+	# Test the network monitor
+	info "Running docker with Kata Containers (${KATA_HYPERVISOR})"
+	net_name="my-net"
+	container_id=
+
+	cleanup() {
+		if [[ -n "$container_id" ]]; then
+			info "Stop container"
+			docker stop "$container_id"
+			info "Delete container"
+			docker rm "$container_id"
+		fi
+		if docker network inspect "$net_name" &>/dev/null; then
+			info "Delete network"
+			docker network rm "$net_name"
+		fi
+	}
+	trap 'cleanup; trap - RETURN' RETURN
+
+	container_id=$(sudo docker run -d --runtime "io.containerd.kata-${KATA_HYPERVISOR}.v2" busybox)
+
+	if [ -z "$container_id" ]; then
+		die "Failed to create docker container"
+	fi
+	info "Create a docker network '$net_name'"
+	docker network create "$net_name"
+
+	info "Connect the container to '$net_name' network"
+	docker network connect "$net_name" "$container_id"
+	sleep 3
+	mac_address=$(docker network inspect "$net_name" | grep -A5 "$container_id" | grep '"MacAddress"' | awk -F'"' '{print $4}')
+	if [ -z "$mac_address" ]; then
+		die "Failed to get MacAddress"
+	fi
+	if docker exec -i $container_id ip a | grep "$mac_address"; then
+		info "Disconnect container from '$net_name' network"
+		docker network disconnect "$net_name" "$container_id"
+		sleep 3
+		if docker exec -i $container_id ip a | grep "$mac_address"; then
+			die "Failed to disconnect from '$net_name'"
+		fi
+	else
+		die "Failed to connect to '$net_name'"
+	fi
+
+	# cleanup is deferred with trap above
 }
 
 function main() {
