508f1bba15e28a05a1abbda79f74d9362a4a34b6
Dan Mihai <dmihai@microsoft.com>
2023-08-21 02:09:35 +0000

    gha: capture additional kata-deploy output
    10 lines can be insufficient for diagnostics.
    
    Fixes: #7707
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>
    (cherry picked from commit 400eb8874322ac2dc20c0e2809e0ed704f256e5d)

--- tests/integration/kubernetes/gha-run.sh
+++ tests/integration/kubernetes/gha-run.sh
@@ -54,7 +54,7 @@ function deploy_kata() {
     fi
 
     echo "::group::kata-deploy logs"
-    kubectl -n kube-system logs -l name=kata-deploy
+    kubectl -n kube-system logs --tail=100 -l name=kata-deploy
     echo "::endgroup::"
 
     echo "::group::Runtime classes"
