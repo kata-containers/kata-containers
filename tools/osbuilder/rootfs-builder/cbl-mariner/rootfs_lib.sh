a392d1405c86d74f0db570b46102d12a72ac5c22
Dan Mihai <Daniel.Mihai@microsoft.com>
2023-08-30 18:36:46 -0700

    rootfs: delete some of the mariner packages (#45)
    * rootfs: delete some of the mariner packages
    
    Delete some of the mariner packages from the Guest image, for faster
    TEE memory measurement.
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- tools/osbuilder/rootfs-builder/cbl-mariner/rootfs_lib.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/rootfs_lib.sh
@@ -22,5 +22,75 @@ build_rootfs()
 	info "install packages for rootfs"
 	$DNF install ${EXTRA_PKGS} ${PACKAGES}
 
+	# Reduce the image size, for faster TEE memory measurement.
+	local MARINER_REMOVED_PACKAGES=( \
+		"bash" \
+		"bc" \
+		"bridge-utils" \
+		"bzip2" \
+		"chkconfig" \
+		"coreutils" \
+		"cracklib-dicts" \
+		"curl" \
+		"curl-libs" \
+		"cyrus-sasl-lib" \
+		"e2fsprogs" \
+		"expat" \
+		"file" \
+		"findutils" \
+		"gdbm" \
+		"gmp" \
+		"gnupg2" \
+		"gpgme" \
+		"grep" \
+		"gzip" \
+		"iana-etc" \
+		"iproute" \
+		"iputils" \
+		"krb5" \
+		"libarchive" \
+		"libassuan" \
+		"libdb" \
+		"libksba" \
+		"libpwquality" \
+		"libsolv" \
+		"libssh2" \
+		"libtool" \
+		"libuv" \
+		"libxml2" \
+		"lua-libs" \
+		"mariner-rpm-macros" \
+		"ncurses" \
+		"nghttp2" \
+		"net-tools" \
+		"nettle" \
+		"newt" \
+		"npth" \
+		"openldap" \
+		"openssh-clients" \
+		"openssl" \
+		"pinentry" \
+		"pcre" \
+		"procps-ng" \
+		"readline" \
+		"rpm" \
+		"rpm-libs" \
+		"sed" \
+		"shadow-utils" \
+		"sqlite-libs" \
+		"slang" \
+		"sudo" \
+		"tar" \
+		"tzdata" \
+		"xz" \
+		"zstd-libs" \
+	)
+
+	for MARINER_REMOVED_PACKAGE in ${MARINER_REMOVED_PACKAGES[@]}
+	do
+		info "removing package ${MARINER_REMOVED_PACKAGE}"
+		rpm -e "${MARINER_REMOVED_PACKAGE}" --nodeps --root=${ROOTFS_DIR}
+	done
+
 	rm -rf ${ROOTFS_DIR}/usr/share/{bash-completion,cracklib,doc,info,locale,man,misc,pixmaps,terminfo,zoneinfo,zsh}
 }
