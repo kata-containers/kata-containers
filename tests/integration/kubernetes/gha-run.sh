7c12463d3d5680aca85c3ed9c4a95873bdeef36b
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-09-07 12:26:47 +0200

    ci: k8s: Use the proper command for sleep
    `wait` waits for a job to complete, not a number of seconds.  Not sure
    how I got that wrong in the first place, but it's what it's.
    
    Fixes: #6542
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tests/integration/kubernetes/gha-run.sh
+++ tests/integration/kubernetes/gha-run.sh
@@ -144,7 +144,7 @@ function deploy_k3s() {
 	curl -sfL https://get.k3s.io | sh -
 
 	# This is an arbitrary value that came up from local tests
-	wait 240s
+	sleep 240s
 }
 
 function deploy_k8s() {
