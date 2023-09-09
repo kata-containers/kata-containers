f86711ad8897d9a5b69a2b89f3e57810da4e066a
Yuan-Zhuo <yuanzhuo0118@outlook.com>
2023-09-09 13:56:43 +0800

    agent: optimize the code of systemd cgroup manager
    1. Directly support CgroupManager::freeze through systemd API.
    2. Avoid always passing unit_name by storing it into DBusClient.
    3. Realize CgroupManager::destroy more accurately by killing systemd unit rather than stop it.
    4. Ignore no such unit error when destroying systemd unit.
    5. Update zbus version and corresponding interface file.
    
    Acknowledgement: error handling for no such systemd unit error refers to
    
    Fixes: #7080, #7142, #7143, #7166
    
    Signed-off-by: Yuan-Zhuo <yuanzhuo0118@outlook.com>
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

--- src/agent/rustjail/src/cgroups/systemd/common.rs
+++ src/agent/rustjail/src/cgroups/systemd/common.rs
@@ -6,7 +6,10 @@
 pub const DEFAULT_SLICE: &str = "system.slice";
 pub const SLICE_SUFFIX: &str = ".slice";
 pub const SCOPE_SUFFIX: &str = ".scope";
-pub const UNIT_MODE: &str = "replace";
+pub const WHO_ENUM_ALL: &str = "all";
+pub const SIGNAL_KILL: i32 = nix::sys::signal::SIGKILL as i32;
+pub const UNIT_MODE_REPLACE: &str = "replace";
+pub const NO_SUCH_UNIT_ERROR: &str = "org.freedesktop.systemd1.NoSuchUnit";
 
 pub type Properties<'a> = Vec<(&'a str, zbus::zvariant::Value<'a>)>;
 
