813bfdec0178e59569eba366063553e0def033ab
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-09-13 13:10:14 +0200

    ci: docker: nerdtl: Use io.containerd.kata-${KATA_HYPERVISOR}.io
    This will ensure that we're calling the correct binary for the
    hypervisor.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tests/integration/nerdctl/gha-run.sh
+++ tests/integration/nerdctl/gha-run.sh
@@ -67,7 +67,7 @@ function run() {
 	sudo nerdctl run --rm --entrypoint nping instrumentisto/nmap --tcp-connect -c 2 -p 80 www.github.com
 
 	info "Running nerdctl with Kata Containers (${KATA_HYPERVISOR})"
-	sudo nerdctl run --rm --runtime io.containerd.kata.v2 --entrypoint nping instrumentisto/nmap --tcp-connect -c 2 -p 80 www.github.com
+	sudo nerdctl run --rm --runtime io.containerd.kata-${KATA_HYPERVISOR}.v2 --entrypoint nping instrumentisto/nmap --tcp-connect -c 2 -p 80 www.github.com
 }
 
 function main() {
