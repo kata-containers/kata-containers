dbfe6512fc095286860613f303cedcc04ef11747
lisongqian <mail@lisongqian.cn>
2023-09-23 16:08:47 +0800

    dragonball: vcpu metrics change to be recorded per vcpu
    In this commit, the vcpu metrics in Dragonball will be changed to record per-vcpu.
    
    Fixes: #7248
    
    Signed-off-by: lisongqian <mail@lisongqian.cn>

--- src/dragonball/src/metric.rs
+++ src/dragonball/src/metric.rs
@@ -2,14 +2,19 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 
-use dbs_utils::metric::{IncMetric, SharedIncMetric};
+use dbs_utils::metric::SharedIncMetric;
 use lazy_static::lazy_static;
 use serde::Serialize;
 
 lazy_static! {
-    /// Static instance used for handling metrics.
+    /// # Static instance used for handling metrics.
+    ///
+    /// Using a big lock over the DragonballMetrics since we have various device metric types
+    /// and the write operation is only used when creating or removing devices, it has a low
+    /// competitive overhead.
     pub static ref METRICS: RwLock<DragonballMetrics> = RwLock::new(DragonballMetrics::default());
 }
 
@@ -50,9 +55,121 @@ pub struct SignalMetrics {
 #[derive(Default, Serialize)]
 pub struct DragonballMetrics {
     /// Metrics related to a vcpu's functioning.
-    pub vcpu: VcpuMetrics,
+    pub vcpu: HashMap<u32, Arc<VcpuMetrics>>,
     /// Metrics related to seccomp filtering.
     pub seccomp: SeccompMetrics,
     /// Metrics related to signals.
     pub signals: SignalMetrics,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+    use std::thread;
+
+    use dbs_utils::metric::IncMetric;
+
+    use crate::metric::{VcpuMetrics, METRICS};
+
+    #[test]
+    fn test_read_map() {
+        let metrics = Arc::new(VcpuMetrics::default());
+        let vcpu_id: u32 = u32::MIN;
+        METRICS
+            .write()
+            .unwrap()
+            .vcpu
+            .insert(vcpu_id, metrics.clone());
+        metrics.failures.inc();
+        assert_eq!(
+            METRICS
+                .read()
+                .unwrap()
+                .vcpu
+                .get(&vcpu_id)
+                .unwrap()
+                .failures
+                .count(),
+            1
+        );
+    }
+
+    #[test]
+    fn test_metrics_count() {
+        let metrics = Arc::new(VcpuMetrics::default());
+        let vcpu_id: u32 = 65535;
+        METRICS
+            .write()
+            .unwrap()
+            .vcpu
+            .insert(vcpu_id, metrics.clone());
+
+        let metrics1 = metrics.clone();
+        let thread1 = thread::spawn(move || {
+            for _i in 0..10 {
+                metrics1.exit_io_in.inc();
+            }
+        });
+
+        let metrics2 = metrics.clone();
+        let thread2 = thread::spawn(move || {
+            for _i in 0..10 {
+                metrics2.exit_io_in.inc();
+            }
+        });
+        thread1.join().unwrap();
+        thread2.join().unwrap();
+        assert_eq!(
+            METRICS
+                .read()
+                .unwrap()
+                .vcpu
+                .get(&vcpu_id)
+                .unwrap()
+                .exit_io_in
+                .count(),
+            20
+        );
+    }
+
+    #[test]
+    fn test_rw_lock() {
+        let metrics = Arc::new(VcpuMetrics::default());
+        let vcpu_id: u32 = u32::MAX;
+        METRICS
+            .write()
+            .unwrap()
+            .vcpu
+            .insert(vcpu_id, metrics.clone());
+
+        let write_thread = thread::spawn(move || {
+            for _ in 0..10 {
+                let metrics = Arc::new(VcpuMetrics::default());
+                let vcpu_id: u32 = 128;
+                METRICS
+                    .write()
+                    .unwrap()
+                    .vcpu
+                    .insert(vcpu_id, metrics.clone());
+            }
+        });
+
+        let read_thread = thread::spawn(move || {
+            for _ in 0..10 {
+                assert_eq!(
+                    METRICS
+                        .read()
+                        .unwrap()
+                        .vcpu
+                        .get(&vcpu_id)
+                        .unwrap()
+                        .failures
+                        .count(),
+                    0
+                );
+            }
+        });
+        write_thread.join().unwrap();
+        read_thread.join().unwrap();
+    }
 }
