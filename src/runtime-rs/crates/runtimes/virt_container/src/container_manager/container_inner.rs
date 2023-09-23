5725b3901413b35824def7f17ce4ba2f82f61adb
liuxu <liuxu623@gmail.com>
2023-09-23 22:46:50 +0800

    runtime-rs: fix exec hang when use not exists command

--- src/runtime-rs/crates/runtimes/virt_container/src/container_manager/container_inner.rs
+++ src/runtime-rs/crates/runtimes/virt_container/src/container_manager/container_inner.rs
@@ -82,16 +82,26 @@ impl ContainerInner {
             .get_mut(&process.exec_id)
             .ok_or_else(|| Error::ProcessNotFound(process.clone()))?;
 
-        self.agent
+        let err = self
+            .agent
             .exec_process(agent::ExecProcessRequest {
                 process_id: process.clone().into(),
                 string_user: None,
                 process: Some(exec.oci_process.clone()),
             })
-            .await
-            .context("exec process")?;
-        exec.process.set_status(ProcessStatus::Running).await;
-        Ok(())
+            .await;
+        match err {
+            Err(e) => {
+                error!(self.logger, "start_exec_process error: {:?}", e);
+                // notify all exit watcher
+                let _ = exec.process.exit_watcher_tx.take();
+                Err(e).context("exec process")
+            }
+            Ok(_) => {
+                exec.process.set_status(ProcessStatus::Running).await;
+                Ok(())
+            }
+        }
     }
 
     pub(crate) async fn win_resize_process(
