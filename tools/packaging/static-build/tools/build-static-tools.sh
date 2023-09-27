445fcf0bfee7718c71c472c739e12a247ab5f52b
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-09-27 13:54:38 +0200

    static-build: Add scripts to build content from src/tools
    As we'd like to ship the content from src/tools, we need to build them
    in the very same way we build the other components, and the first step
    is providing scripts that can build those inside a container.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

---
+++ tools/packaging/static-build/tools/build-static-tools.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2023 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${script_dir}/../../scripts/lib.sh"
+
+init_env() {
+	source "$HOME/.cargo/env"
+
+	export LIBC=musl
+	export LIBSECCOMP_LINK_TYPE=static
+	export LIBSECCOMP_LIB_PATH=/usr/lib
+
+	extra_rust_flags=" -C link-self-contained=yes"
+}
+
+build_tool_from_source() {
+	set -x
+	tool=${1}
+
+	echo "build ${tool} from source"
+	init_env
+
+	cd src/tools/${tool}
+	make
+}
+
+build_tool_from_source $@
