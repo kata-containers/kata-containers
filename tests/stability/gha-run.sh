ef49db59f77a5b556d9f0bcac9012d0e5299915f
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2023-10-06 18:41:43 +0000

    gha: Add general dependencies to stability tests
    This PR adds the general dependencies to stability tests.
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
    (cherry picked from commit 84e3d884e4c7dfde4e5bec38920af8952a9df60f)

--- tests/stability/gha-run.sh
+++ tests/stability/gha-run.sh
@@ -14,6 +14,27 @@ kata_tarball_dir="${2:-kata-artifacts}"
 stability_dir="$(dirname "$(readlink -f "$0")")"
 source "${stability_dir}/../common.bash"
 
+function install_dependencies() {
+	info "Installing the dependencies needed for running the containerd-stability tests"
+
+	declare -a system_deps=(
+		jq
+	)
+
+	sudo apt-get update
+	sudo apt-get -y install "${system_deps[@]}"
+
+	ensure_yq
+
+	declare -a github_deps
+	github_deps[0]="cri_containerd:$(get_from_kata_deps "externals.containerd.${CONTAINERD_VERSION}")"
+
+	for github_dep in "${github_deps[@]}"; do
+		IFS=":" read -r -a dep <<< "${github_dep}"
+		install_${dep[0]} "${dep[1]}"
+	done
+}
+
 function run() {
 	info "Running soak parallel stability tests using ${KATA_HYPERVISOR} hypervisor"
 
@@ -24,6 +45,7 @@ function run() {
 function main() {
 	action="${1:-}"
 	case "${action}" in
+		install-dependencies) install_dependencies ;;
 		install-kata) install_kata ;;
 		enabling-hypervisor) enabling_hypervisor ;;
 		run) run ;;
