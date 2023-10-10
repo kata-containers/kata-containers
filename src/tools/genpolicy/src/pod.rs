8c228fdd567335fc39465157e6d7b804abbdb44d
Dan Mihai <dmihai@microsoft.com>
2023-10-10 15:01:05 +0000

    policy: add support for readOnly volumeMounts
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- src/tools/genpolicy/src/pod.rs
+++ src/tools/genpolicy/src/pod.rs
@@ -394,6 +394,9 @@ pub struct VolumeMount {
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub subPathExpr: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub readOnly: Option<bool>,
     // TODO: additional fields.
 }
 
