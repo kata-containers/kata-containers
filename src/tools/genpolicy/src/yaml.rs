e0511b7d00ead047401ae35dadc8746317117523
Dan Mihai <dmihai@microsoft.com>
2023-10-18 21:05:40 +0000

    genpolicy: add support for ReplicationController
    Generate policy for K8s ReplicationController YAML.
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- src/tools/genpolicy/src/yaml.rs
+++ src/tools/genpolicy/src/yaml.rs
@@ -16,6 +16,7 @@ use crate::no_policy;
 use crate::pod;
 use crate::policy;
 use crate::replica_set;
+use crate::replication_controller;
 use crate::secret;
 use crate::settings;
 use crate::volume;
@@ -151,6 +152,15 @@ pub fn new_k8s_resource(
             .unwrap();
             debug!("{:#?}", &set);
             Ok((boxed::Box::new(set), header.kind))
+        }
+        "ReplicationController" => {
+            let controller: replication_controller::ReplicationController =
+                serde_ignored::deserialize(d, |path| {
+                    handle_unused_field(&path.to_string(), silent_unsupported_fields);
+                })
+                .unwrap();
+            debug!("{:#?}", &controller);
+            Ok((boxed::Box::new(controller), header.kind))
         }
         "Secret" => {
             let secret: secret::Secret = serde_ignored::deserialize(d, |path| {
