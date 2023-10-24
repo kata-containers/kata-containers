c42a2f2eda5fd17fd3d6cdcfeb3f23abe6b9ff5d
David Esparza <david.esparza.borquez@intel.com>
2023-10-24 09:27:32 -0600

    metrics: increase the number of attempts to stop kata
    This PR increases the number of attempts to stop kata components
    when it is required usually before starting a metrics test.
    
    Fixes: #8307
    
    Signed-off-by: David Esparza <david.esparza.borquez@intel.com>

--- tests/common.bash
+++ tests/common.bash
@@ -190,13 +190,17 @@ function clean_env_ctr()
 
 # Kills running shim and hypervisor components
 function kill_kata_components() {
+	local ATTEMPTS=2
 	local TIMEOUT="30s"
 	local PID_NAMES=( "containerd-shim-kata-v2" "qemu-system-x86_64" "cloud-hypervisor" )
 
 	sudo systemctl stop containerd
 	# iterate over the list of kata components and stop them
-	for PID_NAME in "${PID_NAMES[@]}"; do
-		[[ ! -z "$(pidof ${PID_NAME})" ]] && sudo killall "${PID_NAME}" > /dev/null 2>&1 || true
+	for (( i=1; i<=ATTEMPTS; i++ )); do
+		for PID_NAME in "${PID_NAMES[@]}"; do
+			[[ ! -z "$(pidof ${PID_NAME})" ]] && sudo killall "${PID_NAME}" >/dev/null 2>&1 || true
+		done
+		sleep 1
 	done
 	sudo timeout -s SIGKILL "${TIMEOUT}" systemctl start containerd
 }
