9cc6908b0920a5812d740d9f28d4dfb3a52667ef
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2023-11-13 17:40:13 +0000

    stability: Update stressng to run on the gha
    This PR updates the stressng test to run on the gha for kata CI.
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- tests/stability/gha-run.sh
+++ tests/stability/gha-run.sh
@@ -26,23 +26,7 @@ function install_dependencies() {
 	sudo apt-get -y install "${system_deps[@]}"
 
 	ensure_yq
-
-	# Add Docker's official GPG key:
-	sudo apt-get update
-	sudo apt-get -y install ca-certificates
-	sudo install -m 0755 -d /etc/apt/keyrings
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-	sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-	# Add the repository to Apt sources:
-	echo \
-		"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-		"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-		sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt-get update
-
-	sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
+	install_docker
 }
 
 function run() {
