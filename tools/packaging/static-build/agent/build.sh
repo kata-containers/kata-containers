254e9227cf3586a1888d8210fd713e663f159e2e
Zvonko Kaiser <zkaiser@nvidia.com>
2023-11-16 17:51:09 +0000

    tests: make shellcheck happy
    minor edits to make shellcheck happy quoting varaibles
    
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>

--- tools/packaging/static-build/agent/build.sh
+++ tools/packaging/static-build/agent/build.sh
@@ -17,16 +17,16 @@ source "${script_dir}/../../scripts/lib.sh"
 container_image="${AGENT_CONTAINER_BUILDER:-$(get_agent_image_name)}"
 [ "${CROSS_BUILD}" == "true" ] && container_image="${container_image}-cross-build"
 
-sudo docker pull --platform=linux/${target_arch} ${container_image} || \
+sudo docker pull --platform=linux/"${target_arch}" "${container_image}" || \
 	(sudo docker $BUILDX build $PLATFORM \
 	    	--build-arg RUST_TOOLCHAIN="$(get_from_kata_deps "languages.rust.meta.newest-version")" \
 		-t "${container_image}" "${script_dir}" && \
 	 # No-op unless PUSH_TO_REGISTRY is exported as "yes"
 	 push_to_registry "${container_image}")
-exit 1
+
 sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
-	--env DESTDIR=${DESTDIR} \
-	--env AGENT_POLICY=${AGENT_POLICY:-no} \
+	--env DESTDIR="${DESTDIR}" \
+	--env AGENT_POLICY="${AGENT_POLICY:-no}" \
 	-w "${repo_root_dir}" \
 	"${container_image}" \
 	bash -c "${agent_builder}"
