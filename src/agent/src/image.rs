9cddd5813c83539ebeef482c6437f47c97a3e338
ChengyuZhu6 <chengyu.zhu@intel.com>
2023-11-22 16:26:51 +0800

    agent/image: Enable image-rs crate to pull image inside guest
    With image-rs pull_image API, the downloaded container image layers
    will store at IMAGE_RS_WORK_DIR, and generated bundle dir with rootfs
    and config.json will be saved under CONTAINER_BASE/cid directory.
    
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>
    Co-authored-by: Arron Wang <arron.wang@intel.com>
    Co-authored-by: Jiang Liu <gerry@linux.alibaba.com>
    Co-authored-by: stevenhorsman <steven@uk.ibm.com>
    Co-authored-by: wllenyj <wllenyj@linux.alibaba.com>

--- src/agent/src/image.rs
+++ src/agent/src/image.rs
@@ -5,8 +5,20 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+use safe_path::scoped_join;
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+use std::sync::Arc;
+use std::path::PathBuf;
+
 use anyhow::{anyhow, Result};
+use image_rs::image::ImageClient;
 use tokio::sync::Mutex;
+
+use crate::rpc::CONTAINER_BASE;
+
+const KATA_IMAGE_WORK_DIR: &str = "/run/kata-containers/image/";
 
 #[rustfmt::skip]
 lazy_static! {
@@ -19,10 +31,19 @@ fn sl() -> slog::Logger {
 }
 
 #[derive(Clone)]
-pub struct ImageService {}
+pub struct ImageService {
+    image_client: Arc<Mutex<ImageClient>>,
+    images: Arc<Mutex<HashMap<String, String>>>,
+}
+
 impl ImageService {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            image_client: Arc::new(Mutex::new(ImageClient::new(PathBuf::from(
+                KATA_IMAGE_WORK_DIR,
+            )))),
+            images: Arc::new(Mutex::new(HashMap::new())),
+        }
     }
 
     /// Get the singleton instance of image service.
@@ -32,5 +53,56 @@ impl ImageService {
             .await
             .clone()
             .ok_or_else(|| anyhow!("image service is uninitialized"))
+    }
+
+    async fn add_image(&self, image: String, cid: String) {
+        self.images.lock().await.insert(image, cid);
+    }
+
+    /// pull_image is used for call image-rs to pull image in the guest.
+    /// # Parameters
+    /// - `image`: Image name (exp: quay.io/prometheus/busybox:latest)
+    /// - `cid`: Container id
+    /// - `image_metadata`: Annotations about the image (exp: "containerd.io/snapshot/cri.layer-digest": "sha256:24fb2886d6f6c5d16481dd7608b47e78a8e92a13d6e64d87d57cb16d5f766d63")
+    /// # Returns
+    /// - The image rootfs bundle path. (exp. /run/kata-containers/cb0b47276ea66ee9f44cc53afa94d7980b57a52c3f306f68cb034e58d9fbd3c6/images/rootfs)
+    pub async fn pull_image(
+        &self,
+        image: &str,
+        cid: &str,
+        image_metadata: &HashMap<String, String>,
+    ) -> Result<String> {
+        info!(sl(), "image metadata: {image_metadata:?}");
+        let bundle_base_dir = scoped_join(CONTAINER_BASE, cid)?;
+        fs::create_dir_all(&bundle_base_dir)?;
+        let bundle_path = scoped_join(&bundle_base_dir, "images")?;
+        fs::create_dir_all(&bundle_path)?;
+        info!(sl(), "pull image {image:?}, bundle path {bundle_path:?}");
+
+        let res = self
+            .image_client
+            .lock()
+            .await
+            .pull_image(image, &bundle_path, &None, &None)
+            .await;
+        match res {
+            Ok(image) => {
+                info!(
+                    sl(),
+                    "pull and unpack image {image:?}, cid: {cid:?} succeeded."
+                );
+            }
+            Err(e) => {
+                error!(
+                    sl(),
+                    "pull and unpack image {image:?}, cid: {cid:?} failed with {:?}.",
+                    e.to_string()
+                );
+                return Err(e);
+            }
+        };
+        self.add_image(String::from(image), String::from(cid)).await;
+        let image_bundle_path = scoped_join(&bundle_path, "rootfs")?;
+        Ok(image_bundle_path.as_path().display().to_string())
     }
 }
