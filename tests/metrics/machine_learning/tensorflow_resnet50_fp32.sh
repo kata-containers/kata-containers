56dddab04f94fa9c0876180e9655d01725710749
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2023-12-06 17:02:10 +0000

    metrics: Update command to run tensorflow resnet fp32 benchmark
    This PR updates the command needed to run the tensorflow benchmark.
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- tests/metrics/machine_learning/tensorflow_resnet50_fp32.sh
+++ tests/metrics/machine_learning/tensorflow_resnet50_fp32.sh
@@ -56,7 +56,7 @@ function create_start_script() {
 
 cat <<EOF >>"${script}"
 #!/bin/bash
-python3.8 models/benchmarks/launch_benchmark.py --benchmark-only --framework tensorflow --model-name resnet50 --precision fp32 --mode inference --in-graph /resnet50_fp32_pretrained_model.pb --batch-size 1 --num-intra-threads 16 >> results
+python3.10 models/benchmarks/launch_benchmark.py --benchmark-only --framework tensorflow --model-name resnet50 --precision fp32 --mode inference --in-graph /resnet50_fp32_pretrained_model.pb --batch-size 1 --num-intra-threads 16 >> results
 EOF
 	chmod +x "${script}"
 }
