d097667ac47049b109c68e0be60cc37448a484ff
Hyounggyu Choi <Hyounggyu.Choi@ibm.com>
2023-12-14 13:06:03 +0100

    runtime-rs: Separate init_config() from new() for struct VsockDevice
    As a follow-up for #8516, guest_cid and vhost_fd are not necessarily initialised
    via new(). Instead, the fields should be initialised later when they are really
    used to construct hypervisor's parameters.
    This commit is to separate init_config() from new() to initialise guest_cid
    and vhost_fd and leave only the assignment of id for the existing function.
    
    Fixes: #8671
    
    Signed-off-by: Hyounggyu Choi <Hyounggyu.Choi@ibm.com>

--- src/runtime-rs/crates/hypervisor/src/device/driver/virtio_vsock.rs
+++ src/runtime-rs/crates/hypervisor/src/device/driver/virtio_vsock.rs
@@ -117,15 +117,19 @@ nix::ioctl_write_ptr!(
 const CID_RETRY_COUNT: u32 = 50;
 
 impl VsockDevice {
-    pub async fn new(id: String) -> Result<Self> {
-        let (guest_cid, _vhost_fd) = generate_vhost_vsock_cid()
-            .await
-            .context("generate vhost vsock cid failed")?;
-
+    pub async fn new(id: String, config: &VsockConfig) -> Result<Self> {
         Ok(Self {
             id,
-            config: VsockConfig { guest_cid },
+            config: config.clone(),
         })
+    }
+
+    pub async fn init_config(&mut self) -> Result<File> {
+        let (guest_cid, vhost_fd) = generate_vhost_vsock_cid()
+            .await
+            .context("generate vhost vsock cid failed")?;
+        self.config.guest_cid = guest_cid;
+        Ok(vhost_fd)
     }
 }
 
