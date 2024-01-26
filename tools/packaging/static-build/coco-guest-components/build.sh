c91c60ed8e1667f2caafa03f935966185d87b735
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2024-01-26 14:30:07 +0100

    packaging: coco-guest-components: Allow building the project
    The Confidential Containers guest-components will, in the very short
    future, be part of the Kata Containers rootfs that's used by the
    Confidential Containers usecase.
    
    This commit introduces the ability to, standalone, build the component
    locally and as part of our CI, and this can be done by calling:
    `make coco-guest-components-tarball`
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>
    Signed-off-by: Linda Yu <linda.yu@intel.com>
    Co-authored-by: stevenhorsman <steven@uk.ibm.com>
    Co-authored-by: Jakob Naucke <jakob.naucke@ibm.com>
    Co-authored-by: Wang, Arron <arron.wang@intel.com>
    Co-authored-by: zhouliang121 <liang.a.zhou@linux.alibaba.com>
    Co-authored-by: Alex Carter <alex.carter@ibm.com>
    Co-authored-by: Suraj Deshmukh <suraj.deshmukh@microsoft.com>
    Co-authored-by: Xynnn007 <xynnn@linux.alibaba.com>

---
+++ tools/packaging/static-build/coco-guest-components/build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2024 Intel
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly coco_guest_components_builder="${script_dir}/build-static-coco-guest-components.sh"
+
+source "${script_dir}/../../scripts/lib.sh"
+
+coco_guest_components_repo="${coco_guest_components_repo:-}"
+coco_guest_components_version="${coco_guest_components_version:-}"
+coco_guest_components_toolchain="${coco_guest_components_toolchain:-}"
+package_output_dir="${package_output_dir:-}"
+
+[ -n "${coco_guest_components_repo}" ] || coco_guest_components_repo=$(get_from_kata_deps "externals.coco-guest-components.url")
+[ -n "${coco_guest_components_version}" ] || coco_guest_components_version=$(get_from_kata_deps "externals.coco-guest-components.version")
+[ -n "${coco_guest_components_toolchain}" ] || coco_guest_components_toolchain=$(get_from_kata_deps "externals.coco-guest-components.toolchain")
+
+[ -n "${coco_guest_components_repo}" ] || die "Failed to get coco-guest-components repo"
+[ -n "${coco_guest_components_version}" ] || die "Failed to get coco-guest-components version or commit"
+[ -n "${coco_guest_components_toolchain}" ] || die "Failed to get the rust toolchain to build coco-guest-components"
+
+container_image="${COCO_GUEST_COMPONENTS_CONTAINER_BUILDER:-$(get_coco_guest_components_image_name)}"
+[ "${CROSS_BUILD}" == "true" ] && container_image="${container_image}-cross-build"
+
+sudo docker pull ${container_image} || \
+	(sudo docker $BUILDX build $PLATFORM \
+	    	--build-arg RUST_TOOLCHAIN="${coco_guest_components_toolchain}" \
+		-t "${container_image}" "${script_dir}" && \
+	 # No-op unless PUSH_TO_REGISTRY is exported as "yes"
+	 push_to_registry "${container_image}")
+
+sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+	-w "${PWD}" \
+	--env TEE_PLATFORM=${TEE_PLATFORM:-all} \
+	--env coco_guest_components_repo="${coco_guest_components_repo}" \
+	--env coco_guest_components_version="${coco_guest_components_version}" \
+	"${container_image}" \
+	bash -c "${coco_guest_components_builder}"
