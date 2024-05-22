e71c7ab93291ddde4d1967ab6db129201a6114dc
ChengyuZhu6 <chengyu.zhu@intel.com>
2024-05-23 00:22:47 +0800

    agent/image: Remove functions about merging container spec for guest pull
    Let me explain why:
    
    In our previous approach, we implemented guest pull by passing PullImageRequest to the guest.
    However, this method  resulted in the loss of specifications essential for running the container,
    such as commands specified in YAML, during the CreateContainer stage. To address this,
    it is necessary to integrate the OCI specifications and process information
    from the image’s configuration with the container in guest pull.
    
    The snapshotter method does not care this issue. Nevertheless, a problem arises
    when two containers in the same pod attempt to pull the same image, like InitContainer.
    This is because the image service searches for the existing configuration,
    which resides in the guest. The configuration, associated with <image name, cid>,
    is stored in the directory /run/kata-containers/<cid>. Consequently, when the InitContainer finishes
    its task and terminates, the directory ceases to exist. As a result, during the creation
    of the application container, the OCI spec and process information cannot
    be merged due to the absence of the expected configuration file.
    
    Fixes: kata-containers#9665
    Fixes: kata-containers#9666
    Fixes: kata-containers#9667
    Fixes: kata-containers#9668
    
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>

--- src/agent/src/rpc.rs
+++ src/agent/src/rpc.rs
@@ -210,11 +210,6 @@ impl AgentService {
             "receive createcontainer, storages: {:?}", &req.storages
         );
 
-        // In case of pulling image inside guest, we need to merge the image bundle OCI spec
-        // into the container creation request OCI spec.
-        #[cfg(feature = "guest-pull")]
-        image::merge_bundle_oci(&mut oci).await?;
-
         // Some devices need some extra processing (the ones invoked with
         // --device for instance), and that's what this call is doing. It
         // updates the devices listed in the OCI spec, so that they actually
