6c9c0306ac6f208e1e3486dbb3fd5078bccc2201
Zvonko Kaiser <zkaiser@nvidia.com>
2024-05-28 11:31:59 +0000

    ci: pause-image no sudo
    Build pause-image without sudo docker this is not needed. This is part 5 of N
    
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>

--- tools/packaging/static-build/pause-image/build-static-pause-image.sh
+++ tools/packaging/static-build/pause-image/build-static-pause-image.sh
@@ -19,8 +19,8 @@ pull_pause_image_from_remote() {
 	echo "pull pause image from remote"
 
 	skopeo copy "${pause_image_repo}":"${pause_image_version}" oci:pause:"${pause_image_version}"
-	umoci unpack --image pause:"${pause_image_version}"  "${DESTDIR}/pause_bundle"
+	umoci unpack --rootless --image pause:"${pause_image_version}"  "${DESTDIR}/pause_bundle"
 	rm "${DESTDIR}/pause_bundle/umoci.json"
 }
 
-pull_pause_image_from_remote $@
+pull_pause_image_from_remote "$@"
