25c784c568b5b96e3211fecc5340a09cbec86515
Zvonko Kaiser <zkaiser@nvidia.com>
2024-05-28 11:52:01 +0000

    ci: shim-v2 no sudo
    Build shim-v2 without sudo docker this is not needed. This is part 6 of N
    
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>

--- tools/packaging/static-build/shim-v2/build.sh
+++ tools/packaging/static-build/shim-v2/build.sh
@@ -32,13 +32,13 @@ if [ "${MEASURED_ROOTFS}" == "yes" ]; then
 	[ -f "$root_hash_file" ] || \
 		die "Root hash file for measured rootfs not found at ${root_hash_file}"
 
-	root_hash=$(sudo sed -e 's/Root hash:\s*//g;t;d' "${root_hash_file}")
+	root_hash=$(sed -e 's/Root hash:\s*//g;t;d' "${root_hash_file}")
 	root_measure_config="rootfs_verity.scheme=dm-verity rootfs_verity.hash=${root_hash}"
 	EXTRA_OPTS+=" ROOTMEASURECONFIG=\"${root_measure_config}\""
 fi
 
-sudo docker pull ${container_image} || \
-	(sudo docker ${BUILDX} build ${PLATFORM}  \
+docker pull ${container_image} || \
+	(docker ${BUILDX} build ${PLATFORM}  \
 		--build-arg GO_VERSION="${GO_VERSION}" \
 		--build-arg RUST_VERSION="${RUST_VERSION}" \
 		-t "${container_image}" \
@@ -55,48 +55,50 @@ fi
 #Build rust project using cross build musl image to speed up
 [[ "${CROSS_BUILD}" == "true" && ${ARCH} != "s390x" ]] && container_image="messense/rust-musl-cross:${GCC_ARCH}-musl" && CC=${GCC_ARCH}-unknown-linux-musl-gcc
 
-sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	--env CROSS_BUILD=${CROSS_BUILD} \
 	--env ARCH=${ARCH} \
 	--env CC="${CC}" \
 	-w "${repo_root_dir}/src/runtime-rs" \
+	--user "$(id -u)":"$(id -g)" \
 	"${container_image}" \
-	bash -c "git config --global --add safe.directory ${repo_root_dir} && \
-		make clean-generated-files && make PREFIX=${PREFIX} QEMUCMD=qemu-system-${arch}"
+	bash -c "make clean-generated-files && make PREFIX=${PREFIX} QEMUCMD=qemu-system-${arch}"
 
-sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	--env CROSS_BUILD=${CROSS_BUILD} \
         --env ARCH=${ARCH} \
         --env CC="${CC}" \
 	-w "${repo_root_dir}/src/runtime-rs" \
+	--user "$(id -u)":"$(id -g)" \
 	"${container_image}" \
-	bash -c "git config --global --add safe.directory ${repo_root_dir} && make PREFIX="${PREFIX}" DESTDIR="${DESTDIR}" install"
+	bash -c "make PREFIX="${PREFIX}" DESTDIR="${DESTDIR}" install"
 
 [ "${CROSS_BUILD}" == "true" ] && container_image="${container_image_bk}-cross-build"
 
-sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	-w "${repo_root_dir}/src/runtime" \
+	--user "$(id -u)":"$(id -g)" \
 	"${container_image}" \
-	bash -c "git config --global --add safe.directory ${repo_root_dir} && \
-		make clean-generated-files && make PREFIX=${PREFIX} QEMUCMD=qemu-system-${arch} ${EXTRA_OPTS}"
+	bash -c "make clean-generated-files && make PREFIX=${PREFIX} QEMUCMD=qemu-system-${arch} ${EXTRA_OPTS}"
 
-sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	-w "${repo_root_dir}/src/runtime" \
+	--user "$(id -u)":"$(id -g)" \
 	"${container_image}" \
-	bash -c "git config --global --add safe.directory ${repo_root_dir} && make PREFIX="${PREFIX}" DESTDIR="${DESTDIR}" ${EXTRA_OPTS} install"
+	bash -c "make PREFIX="${PREFIX}" DESTDIR="${DESTDIR}" ${EXTRA_OPTS} install"
 
 for vmm in ${VMM_CONFIGS}; do
 	config_file="${DESTDIR}/${PREFIX}/share/defaults/kata-containers/configuration-${vmm}.toml"
 	if [ -f ${config_file} ]; then
 		if [ ${ARCH} == "ppc64le" ]; then
- 			sudo sed -i -e '/^image =/d' ${config_file}
- 			sudo sed -i 's/^# \(initrd =.*\)/\1/g' ${config_file}
+ 			sed -i -e '/^image =/d' ${config_file}
+ 			sed -i 's/^# \(initrd =.*\)/\1/g' ${config_file}
  		else
- 			sudo sed -i -e '/^initrd =/d' ${config_file}
+ 			sed -i -e '/^initrd =/d' ${config_file}
  		fi
 	fi
 done
 
 pushd "${DESTDIR}/${PREFIX}/share/defaults/kata-containers"
-	sudo ln -sf "configuration-qemu.toml" configuration.toml
+	ln -sf "configuration-qemu.toml" configuration.toml
 popd
