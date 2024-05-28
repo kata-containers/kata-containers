1e4cbc4fcd2d6a71b2d30f857a235b269e840f5e
Zvonko Kaiser <zkaiser@nvidia.com>
2024-05-28 09:03:14 +0000

    ci: guest-components wihout sudo
    Build guest-components without sudo docker this is not needed. This is part 2 of N
    
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>

--- tools/packaging/static-build/coco-guest-components/build.sh
+++ tools/packaging/static-build/coco-guest-components/build.sh
@@ -31,8 +31,8 @@ package_output_dir="${package_output_dir:-}"
 container_image="${COCO_GUEST_COMPONENTS_CONTAINER_BUILDER:-$(get_coco_guest_components_image_name)}"
 [ "${CROSS_BUILD}" == "true" ] && container_image="${container_image}-cross-build"
 
-sudo docker pull ${container_image} || \
-	(sudo docker $BUILDX build $PLATFORM \
+docker pull ${container_image} || \
+	(docker $BUILDX build $PLATFORM \
 	    	--build-arg RUST_TOOLCHAIN="${coco_guest_components_toolchain}" \
 		-t "${container_image}" "${script_dir}" && \
 	 # No-op unless PUSH_TO_REGISTRY is exported as "yes"
@@ -45,7 +45,7 @@ ATTESTER="none"
 # snp-attester and tdx-attester crates require packages only available on x86
 [ "$(uname -m)" == "x86_64" ] && ATTESTER="snp-attester,tdx-attester"
 
-sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	-w "${PWD}" \
 	--env DESTDIR="${DESTDIR}" \
 	--env TEE_PLATFORM=${TEE_PLATFORM:+"all"} \
@@ -53,5 +53,6 @@ sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	--env ATTESTER=${ATTESTER:-} \
 	--env coco_guest_components_repo="${coco_guest_components_repo}" \
 	--env coco_guest_components_version="${coco_guest_components_version}" \
+	--user "$(id -u)":"$(id -g)" \
 	"${container_image}" \
 	bash -c "${coco_guest_components_builder}"
