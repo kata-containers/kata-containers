1a2430a4d95c54c56d27a3583cb3a3c315b786a4
Konstantin Khlebnikov <koct9i@gmail.com>
2024-07-18 17:02:20 +0200

    govmm: setup qemu VM NUMA topology for initial CPUs and memory
    If NUMA topology is enabled:
    - groups CPUs into per NUMA node sockets
    - split memory into per NUMA node modules
    - report NUMA node for VCPU threads
    
    Signed-off-by: Konstantin Khlebnikov <koct9i@gmail.com>

--- src/runtime/pkg/katautils/config.go
+++ src/runtime/pkg/katautils/config.go
@@ -965,6 +965,7 @@ func newQemuHypervisorConfig(h hypervisor) (vc.HypervisorConfig, error) {
 		HugePages:                h.HugePages,
 		IOMMU:                    h.IOMMU,
 		IOMMUPlatform:            h.getIOMMUPlatform(),
+		NUMANodes:                h.defaultNUMANodes(),
 		FileBackedMemRootDir:     h.FileBackedMemRootDir,
 		FileBackedMemRootList:    h.FileBackedMemRootList,
 		Debug:                    h.Debug,
