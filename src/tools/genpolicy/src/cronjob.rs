88451d26d093c572951c0af22ac40770fb9e45d7
Saul Paredes <saulparedes@microsoft.com>
2024-08-12 13:12:06 -0700

    genpolicy: add support for cron jobs
    Add support for cron jobs
    
    Signed-off-by: Saul Paredes <saulparedes@microsoft.com>

--- src/tools/genpolicy/src/cronjob.rs
+++ src/tools/genpolicy/src/cronjob.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 Microsoft Corporation
+// Copyright (c) 2024 Microsoft Corporation
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -6,9 +6,9 @@
 // Allow K8s YAML field names.
 #![allow(non_snake_case)]
 
+use crate::job;
 use crate::obj_meta;
 use crate::pod;
-use crate::pod_template;
 use crate::policy;
 use crate::settings;
 use crate::utils::Config;
@@ -19,37 +19,63 @@ use protocols::agent;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 
-/// See Reference / Kubernetes API / Workload Resources / Job.
+/// See Reference / Kubernetes API / Workload Resources / CronJob.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Job {
+pub struct CronJob {
     apiVersion: String,
     kind: String,
     metadata: obj_meta::ObjectMeta,
-    spec: JobSpec,
-
+    spec: CronJobSpec,
     #[serde(skip)]
     doc_mapping: serde_yaml::Value,
 }
 
-/// See Reference / Kubernetes API / Workload Resources / Job.
+/// See Reference / Kubernetes API / Workload Resources / CronJob.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct JobSpec {
-    pub template: pod_template::PodTemplateSpec,
+pub struct CronJobSpec {
+    jobTemplate: JobTemplateSpec,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    concurrencyPolicy: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    failedJobsHistoryLimit: Option<i32>,
+
+    schedule: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    startingDeadlineSeconds: Option<i32>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    successfulJobsHistoryLimit: Option<i32>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    suspend: Option<bool>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    timeZone: Option<String>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     backoffLimit: Option<i32>,
     // TODO: additional fields.
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct JobTemplateSpec {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    metadata: Option<obj_meta::ObjectMeta>,
+    spec: job::JobSpec,
+}
+
 #[async_trait]
-impl yaml::K8sResource for Job {
+impl yaml::K8sResource for CronJob {
     async fn init(
         &mut self,
         config: &Config,
         doc_mapping: &serde_yaml::Value,
         _silent_unsupported_fields: bool,
     ) {
-        yaml::k8s_resource_init(&mut self.spec.template.spec, config).await;
+        yaml::k8s_resource_init(&mut self.spec.jobTemplate.spec.template.spec, config).await;
         self.doc_mapping = doc_mapping.clone();
     }
 
@@ -68,7 +94,7 @@ impl yaml::K8sResource for Job {
         container: &pod::Container,
         settings: &settings::Settings,
     ) {
-        if let Some(volumes) = &self.spec.template.spec.volumes {
+        if let Some(volumes) = &self.spec.jobTemplate.spec.template.spec.volumes {
             yaml::get_container_mounts_and_storages(
                 policy_mounts,
                 storages,
@@ -84,30 +110,41 @@ impl yaml::K8sResource for Job {
     }
 
     fn serialize(&mut self, policy: &str) -> String {
-        yaml::add_policy_annotation(&mut self.doc_mapping, "spec.template", policy);
+        yaml::add_policy_annotation(
+            &mut self.doc_mapping,
+            "spec.jobTemplate.spec.template",
+            policy,
+        );
         serde_yaml::to_string(&self.doc_mapping).unwrap()
     }
 
     fn get_containers(&self) -> &Vec<pod::Container> {
-        &self.spec.template.spec.containers
+        &self.spec.jobTemplate.spec.template.spec.containers
     }
 
     fn get_annotations(&self) -> &Option<BTreeMap<String, String>> {
-        if let Some(metadata) = &self.spec.template.metadata {
+        if let Some(metadata) = &self.spec.jobTemplate.spec.template.metadata {
             return &metadata.annotations;
         }
         &None
     }
 
     fn use_host_network(&self) -> bool {
-        if let Some(host_network) = self.spec.template.spec.hostNetwork {
+        if let Some(host_network) = self.spec.jobTemplate.spec.template.spec.hostNetwork {
             return host_network;
         }
         false
     }
 
     fn use_sandbox_pidns(&self) -> bool {
-        if let Some(shared) = self.spec.template.spec.shareProcessNamespace {
+        if let Some(shared) = self
+            .spec
+            .jobTemplate
+            .spec
+            .template
+            .spec
+            .shareProcessNamespace
+        {
             return shared;
         }
         false
