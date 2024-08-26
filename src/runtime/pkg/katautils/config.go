11ba8f05ca6502bf4f18e1d2d7629085dacc6076
Silenio Quarti <silenio_quarti@ca.ibm.com>
2024-08-26 10:17:40 -0400

    runtime: Allow machine_type in kata config for remote hypervisors
    Fixes: #10211
    
    Signed-off-by: Silenio Quarti <silenio_quarti@ca.ibm.com>

--- src/runtime/pkg/katautils/config.go
+++ src/runtime/pkg/katautils/config.go
@@ -1341,11 +1341,13 @@ func newStratovirtHypervisorConfig(h hypervisor) (vc.HypervisorConfig, error) {
 }
 
 func newRemoteHypervisorConfig(h hypervisor) (vc.HypervisorConfig, error) {
+	machineType := h.machineType()
 
 	return vc.HypervisorConfig{
 		RemoteHypervisorSocket:  h.getRemoteHypervisorSocket(),
 		RemoteHypervisorTimeout: h.getRemoteHypervisorTimeout(),
 		DisableGuestSeLinux:     true, // The remote hypervisor has a different guest, so Guest SELinux config doesn't work
+		HypervisorMachineType:   machineType,
 		SharedFS:                config.NoSharedFS,
 
 		// No valid value so avoid to append block device to list in kata_agent.appendDevices
