41943c7e52887b8318a71286a1262c3eb5184953
Ruoqing He <heruoqing@iscas.ac.cn>
2024-08-29 20:11:21 +0800

    dragonball: Introduce `riscv64` cpu fdt
    Introduce logic needed to create cpu node for `riscv64` architecture.
    
    Fixes: #10226
    
    Signed-off-by: Ruoqing He <heruoqing@iscas.ac.cn>

--- src/dragonball/src/dbs_boot/src/lib.rs
+++ src/dragonball/src/dbs_boot/src/lib.rs
@@ -15,6 +15,11 @@ mod aarch64;
 #[cfg(target_arch = "aarch64")]
 pub use aarch64::*;
 
+#[cfg(target_arch = "riscv64")]
+mod riscv64;
+#[cfg(target_arch = "riscv64")]
+pub use riscv64::*;
+
 /// Specialized [std::result::Result] for boot related operations.
 pub type Result<T> = std::result::Result<T, Error>;
 
