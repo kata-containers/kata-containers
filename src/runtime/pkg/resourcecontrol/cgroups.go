kata-containers/kata-containers

commit 4c2883f7e25615a789ce232236b225e2571248ca

vc: hypervisor: remove dependency on persist API
Today the hypervisor code in vc relies on persist pkg for two things:
1. To get the VM/run store path on the host filesystem,
2. For type definition of the Load/Save functions of the hypervisor
   interface.

For (1), we can simply remove the store interface from the hypervisor
config and replace it with just the path, since this is all we really
need. When we create a NewHypervisor structure, outside of the
hypervisor, we can populate this path.

For (2), rather than have the persist pkg define the structure, let's
let the hypervisor code (soon to be pkg) define the structure. persist
API already needs to call into hypervisor anyway; let's allow us to
define the structure.

We'll probably want to look at following similar pattern for other parts
of vc that we want to make independent of the persist API.

In doing this, we started an initial hypervisors pkg, to hold these
types (avoid a circular dependency between virtcontainers and persist
pkg). Next step will be to remove all other dependencies and move the
hypervisor specific code into this pkg, and out of virtcontaienrs.

Signed-off-by: Eric Ernst <eric_ernst@apple.com>

src/runtime/virtcontainers/clh.go
@@ -20,12 +20,12 @@ import (
 	"time"
 
 	"github.com/containerd/console"
-	persistapi "github.com/kata-containers/kata-containers/src/runtime/virtcontainers/persist/api"
 	chclient "github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/cloud-hypervisor/client"
 	"github.com/opencontainers/selinux/go-selinux/label"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 
+	hv "github.com/kata-containers/kata-containers/src/runtime/pkg/hypervisors"
 	"github.com/kata-containers/kata-containers/src/runtime/pkg/katautils/katatrace"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/device/config"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/types"
@@ -157,7 +157,6 @@ func (s *CloudHypervisorState) reset() {
 }
 
 type cloudHypervisor struct {
-	store     persistapi.PersistDriver
 	console   console.Console
 	virtiofsd Virtiofsd
 	APIClient clhClient
@@ -342,7 +341,7 @@ func (clh *cloudHypervisor) CreateVM(ctx context.Context, id string, networkNS N
 
 	clh.virtiofsd = &virtiofsd{
 		path:       clh.config.VirtioFSDaemon,
-		sourcePath: filepath.Join(getSharePath(clh.id)),
+		sourcePath: filepath.Join(GetSharePath(clh.id)),
 		socketPath: virtiofsdSocketPath,
 		extraArgs:  clh.config.VirtioFSExtraArgs,
 		debug:      clh.config.Debug,
@@ -374,7 +373,7 @@ func (clh *cloudHypervisor) StartVM(ctx context.Context, timeout int) error {
 
 	clh.Logger().WithField("function", "StartVM").Info("starting Sandbox")
 
-	vmPath := filepath.Join(clh.store.RunVMStoragePath(), clh.id)
+	vmPath := filepath.Join(clh.config.VMStorePath, clh.id)
 	err := os.MkdirAll(vmPath, DirMode)
 	if err != nil {
 		return err
@@ -747,7 +746,7 @@ func (clh *cloudHypervisor) toGrpc(ctx context.Context) ([]byte, error) {
 	return nil, errors.New("cloudHypervisor is not supported by VM cache")
 }
 
-func (clh *cloudHypervisor) Save() (s persistapi.HypervisorState) {
+func (clh *cloudHypervisor) Save() (s hv.HypervisorState) {
 	s.Pid = clh.state.PID
 	s.Type = string(ClhHypervisor)
 	s.VirtiofsdPid = clh.state.VirtiofsdPID
@@ -755,7 +754,7 @@ func (clh *cloudHypervisor) Save() (s persistapi.HypervisorState) {
 	return
 }
 
-func (clh *cloudHypervisor) Load(s persistapi.HypervisorState) {
+func (clh *cloudHypervisor) Load(s hv.HypervisorState) {
 	clh.state.PID = s.Pid
 	clh.state.VirtiofsdPID = s.VirtiofsdPid
 	clh.state.apiSocket = s.APISocket
@@ -893,15 +892,15 @@ func (clh *cloudHypervisor) GenerateSocket(id string) (interface{}, error) {
 }
 
 func (clh *cloudHypervisor) virtioFsSocketPath(id string) (string, error) {
-	return utils.BuildSocketPath(clh.store.RunVMStoragePath(), id, virtioFsSocket)
+	return utils.BuildSocketPath(clh.config.VMStorePath, id, virtioFsSocket)
 }
 
 func (clh *cloudHypervisor) vsockSocketPath(id string) (string, error) {
-	return utils.BuildSocketPath(clh.store.RunVMStoragePath(), id, clhSocket)
+	return utils.BuildSocketPath(clh.config.VMStorePath, id, clhSocket)
 }
 
 func (clh *cloudHypervisor) apiSocketPath(id string) (string, error) {
-	return utils.BuildSocketPath(clh.store.RunVMStoragePath(), id, clhAPISocket)
+	return utils.BuildSocketPath(clh.config.VMStorePath, id, clhAPISocket)
 }
 
 func (clh *cloudHypervisor) waitVMM(timeout uint) error {
@@ -1213,7 +1212,7 @@ func (clh *cloudHypervisor) cleanupVM(force bool) error {
 	}
 
 	// Cleanup vm path
-	dir := filepath.Join(clh.store.RunVMStoragePath(), clh.id)
+	dir := filepath.Join(clh.config.VMStorePath, clh.id)
 
 	// If it's a symlink, remove both dir and the target.
 	link, err := filepath.EvalSymlinks(dir)
@@ -1242,7 +1241,7 @@ func (clh *cloudHypervisor) cleanupVM(force bool) error {
 	}
 
 	if clh.config.VMid != "" {
-		dir = filepath.Join(clh.store.RunStoragePath(), clh.config.VMid)
+		dir = filepath.Join(clh.config.VMStorePath, clh.config.VMid)
 		if err := os.RemoveAll(dir); err != nil {
 			if !force {
 				return err

src/runtime/virtcontainers/sandbox.go
@@ -530,13 +530,15 @@ func newSandbox(ctx context.Context, sandboxConfig SandboxConfig, factory Factor
 	if s.store, err = persist.GetDriver(); err != nil || s.store == nil {
 		return nil, fmt.Errorf("failed to get fs persist driver: %v", err)
 	}
-
 	defer func() {
 		if retErr != nil {
 			s.Logger().WithError(retErr).Error("Create new sandbox failed")
 			s.store.Destroy(s.id)
 		}
 	}()
+
+	sandboxConfig.HypervisorConfig.VMStorePath = s.store.RunVMStoragePath()
+	sandboxConfig.HypervisorConfig.RunStorePath = s.store.RunStoragePath()
 
 	spec := s.GetPatchedOCISpec()
 	if spec != nil && spec.Process.SelinuxLabel != "" {

src/runtime/virtcontainers/persist/api/sandbox.go
@@ -6,6 +6,10 @@
 
 package persistapi
 
+import (
+	hv "github.com/kata-containers/kata-containers/src/runtime/pkg/hypervisors"
+)
+
 // ============= sandbox level resources =============
 
 // AgentState save agent state data
@@ -38,7 +42,7 @@ type SandboxState struct {
 	OverheadCgroupPath string
 
 	// HypervisorState saves hypervisor specific data
-	HypervisorState HypervisorState
+	HypervisorState hv.HypervisorState
 
 	// AgentState saves state data of agent
 	AgentState AgentState

src/runtime/cmd/kata-runtime/kata-env_test.go
@@ -1052,11 +1052,13 @@ func TestGetHypervisorInfoSocket(t *testing.T) {
 		{vc.QemuHypervisor, false},
 	}
 
+	config.HypervisorConfig.VMStorePath = "/foo"
+	config.HypervisorConfig.RunStorePath = "/bar"
+
 	for i, details := range hypervisors {
 		msg := fmt.Sprintf("hypervisor[%d]: %+v", i, details)
 
 		config.HypervisorType = details.hType
-
 		info, err := getHypervisorInfo(config)
 		assert.NoError(err, msg)
 

src/runtime/virtcontainers/acrn_test.go
@@ -199,11 +199,15 @@ func TestAcrnGetSandboxConsole(t *testing.T) {
 	assert.NoError(err)
 
 	a := &Acrn{
-		ctx:   context.Background(),
+		ctx: context.Background(),
+		config: HypervisorConfig{
+			VMStorePath:  store.RunVMStoragePath(),
+			RunStorePath: store.RunStoragePath(),
+		},
 		store: store,
 	}
 	sandboxID := "testSandboxID"
-	expected := filepath.Join(a.store.RunVMStoragePath(), sandboxID, consoleSocket)
+	expected := filepath.Join(store.RunVMStoragePath(), sandboxID, consoleSocket)
 
 	proto, result, err := a.GetVMConsole(a.ctx, sandboxID)
 	assert.NoError(err)
@@ -219,6 +223,10 @@ func TestAcrnCreateVM(t *testing.T) {
 
 	a := &Acrn{
 		store: store,
+		config: HypervisorConfig{
+			VMStorePath:  store.RunVMStoragePath(),
+			RunStorePath: store.RunStoragePath(),
+		},
 	}
 
 	sandbox := &Sandbox{

src/runtime/virtcontainers/clh_test.go
@@ -203,7 +203,10 @@ func TestCloudHypervisorCleanupVM(t *testing.T) {
 	assert.NoError(err, "persist.GetDriver() unexpected error")
 
 	clh := &cloudHypervisor{
-		store: store,
+		config: HypervisorConfig{
+			VMStorePath:  store.RunVMStoragePath(),
+			RunStorePath: store.RunStoragePath(),
+		},
 	}
 
 	err = clh.cleanupVM(true)
@@ -214,7 +217,7 @@ func TestCloudHypervisorCleanupVM(t *testing.T) {
 	err = clh.cleanupVM(true)
 	assert.NoError(err, "persist.GetDriver() unexpected error")
 
-	dir := filepath.Join(clh.store.RunVMStoragePath(), clh.id)
+	dir := filepath.Join(store.RunVMStoragePath(), clh.id)
 	os.MkdirAll(dir, os.ModePerm)
 
 	err = clh.cleanupVM(false)
@@ -235,9 +238,11 @@ func TestClhCreateVM(t *testing.T) {
 	store, err := persist.GetDriver()
 	assert.NoError(err)
 
+	clhConfig.VMStorePath = store.RunVMStoragePath()
+	clhConfig.RunStorePath = store.RunStoragePath()
+
 	clh := &cloudHypervisor{
 		config: clhConfig,
-		store:  store,
 	}
 
 	sandbox := &Sandbox{
@@ -261,11 +266,13 @@ func TestClooudHypervisorStartSandbox(t *testing.T) {
 	store, err := persist.GetDriver()
 	assert.NoError(err)
 
+	clhConfig.VMStorePath = store.RunVMStoragePath()
+	clhConfig.RunStorePath = store.RunStoragePath()
+
 	clh := &cloudHypervisor{
 		config:    clhConfig,
 		APIClient: &clhClientMock{},
 		virtiofsd: &virtiofsdMock{},
-		store:     store,
 	}
 
 	err = clh.StartVM(context.Background(), 10)
@@ -379,6 +386,11 @@ func TestClhGenerateSocket(t *testing.T) {
 	clh, ok := hypervisor.(*cloudHypervisor)
 	assert.True(ok)
 
+	clh.config = HypervisorConfig{
+		VMStorePath:  "/foo",
+		RunStorePath: "/bar",
+	}
+
 	clh.addVSock(1, "path")
 
 	s, err := clh.GenerateSocket("c")
@@ -391,7 +403,7 @@ func TestClhGenerateSocket(t *testing.T) {
 	assert.NotEmpty(hvsock.UdsPath)
 
 	// Path must be absolute
-	assert.True(strings.HasPrefix(hvsock.UdsPath, "/"))
+	assert.True(strings.HasPrefix(hvsock.UdsPath, "/"), "failed: socket path: %s", hvsock.UdsPath)
 
 	assert.NotZero(hvsock.Port)
 }

src/runtime/virtcontainers/fc.go
@@ -22,9 +22,9 @@ import (
 	"syscall"
 	"time"
 
+	hv "github.com/kata-containers/kata-containers/src/runtime/pkg/hypervisors"
 	"github.com/kata-containers/kata-containers/src/runtime/pkg/katautils/katatrace"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/device/config"
-	persistapi "github.com/kata-containers/kata-containers/src/runtime/virtcontainers/persist/api"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/firecracker/client"
 	models "github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/firecracker/client/models"
 	ops "github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/firecracker/client/operations"
@@ -1226,13 +1226,13 @@ func (fc *firecracker) toGrpc(ctx context.Context) ([]byte, error) {
 	return nil, errors.New("firecracker is not supported by VM cache")
 }
 
-func (fc *firecracker) Save() (s persistapi.HypervisorState) {
+func (fc *firecracker) Save() (s hv.HypervisorState) {
 	s.Pid = fc.info.PID
 	s.Type = string(FirecrackerHypervisor)
 	return
 }
 
-func (fc *firecracker) Load(s persistapi.HypervisorState) {
+func (fc *firecracker) Load(s hv.HypervisorState) {
 	fc.info.PID = s.Pid
 }
 

src/runtime/virtcontainers/hypervisor.go
@@ -14,9 +14,8 @@ import (
 	"strconv"
 	"strings"
 
+	hv "github.com/kata-containers/kata-containers/src/runtime/pkg/hypervisors"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/device/config"
-	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/persist"
-	persistapi "github.com/kata-containers/kata-containers/src/runtime/virtcontainers/persist/api"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/types"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/utils"
 )
@@ -185,28 +184,18 @@ func (hType *HypervisorType) String() string {
 	}
 }
 
-// NewHypervisor returns an hypervisor from and hypervisor type.
+// NewHypervisor returns an hypervisor from a hypervisor type.
 func NewHypervisor(hType HypervisorType) (Hypervisor, error) {
-	store, err := persist.GetDriver()
-	if err != nil {
-		return nil, err
-	}
 
 	switch hType {
 	case QemuHypervisor:
-		return &qemu{
-			store: store,
-		}, nil
+		return &qemu{}, nil
 	case FirecrackerHypervisor:
 		return &firecracker{}, nil
 	case AcrnHypervisor:
-		return &Acrn{
-			store: store,
-		}, nil
+		return &Acrn{}, nil
 	case ClhHypervisor:
-		return &cloudHypervisor{
-			store: store,
-		}, nil
+		return &cloudHypervisor{}, nil
 	case MockHypervisor:
 		return &mockHypervisor{}, nil
 	default:
@@ -344,6 +333,12 @@ type HypervisorConfig struct {
 	// VMid is the id of the VM that create the hypervisor if the VM is created by the factory.
 	// VMid is "" if the hypervisor is not created by the factory.
 	VMid string
+
+	// VMStorePath is the location on disk where VM information will persist
+	VMStorePath string
+
+	// VMStorePath is the location on disk where runtime information will persist
+	RunStorePath string
 
 	// SELinux label for the VM
 	SELinuxProcessLabel string
@@ -934,8 +929,8 @@ type Hypervisor interface {
 	toGrpc(ctx context.Context) ([]byte, error)
 	Check() error
 
-	Save() persistapi.HypervisorState
-	Load(persistapi.HypervisorState)
+	Save() hv.HypervisorState
+	Load(hv.HypervisorState)
 
 	// generate the socket to communicate the host and guest
 	GenerateSocket(id string) (interface{}, error)

commit 7f503410ed0a8963b9f8e8e9bb9c4aa9f0e58f9b

runtime/virtcontainer: remove cgroup directories on exit (#204)
* runtime/cgroup: fix incorrect error handling

Update should be executed after a successful Load().

Signed-off-by: Liu Bo <liub.liubo@gmail.com>

* Fix wrong store path during clh cleanup

commit 4c2883f7e25615a789ce232236b225e2571248ca introduced this problem.

s/clh.config.VMStorePath/clh.config.RunStorePath/g

Signed-off-by: Liu Bo <liub.liubo@gmail.com>

* runtime/cgroups: use cgroup v1 if sandboxCgroupOnly is false

Otherwise, the cgroup directories created by kata are not deleted
after kata pod exits.  The root cause is that on exit kata tries to
find systemd cgroup directories, however, cgroup directories are
created using cgroup v1 when sandboxCgroupOnly is false.

Signed-off-by: Liu Bo <liub.liubo@gmail.com>

* runtime/virtcontainer: remove cgroup directories on exit

In case of rootless, cgroup directories don't get deleted on exist and
these stale cgroups cause otel collectors's OOM since they need to
read all cgroup's content.

Signed-off-by: Liu Bo <liub.liubo@gmail.com>

---------

Signed-off-by: Liu Bo <liub.liubo@gmail.com>

src/runtime/pkg/resourcecontrol/cgroups.go
@@ -192,7 +192,7 @@ func NewSandboxResourceController(path string, resources *specs.LinuxResources, 
 
 	// Create systemd cgroup
 	if cgroups.Mode() == cgroups.Legacy || cgroups.Mode() == cgroups.Hybrid {
-		cgHierarchy, cgPath, err := cgroupHierarchy(path)
+		cgHierarchy, cgPath, err := cgroupHierarchy(path, sandboxCgroupOnly)
 		if err != nil {
 			return nil, err
 		}
@@ -200,18 +200,21 @@ func NewSandboxResourceController(path string, resources *specs.LinuxResources, 
 		// load created cgroup and update with resources
 		cg, err := cgroups.Load(cgHierarchy, cgPath)
 		if err != nil {
-			if cg.Update(&sandboxResources); err != nil {
-				return nil, err
-			}
+			controllerLogger.WithField("source", "cgroups").Errorf("failed to load cgroup, path: %v cgPath: %v", path, cgPath)
+			return nil, err
+		}
+		if cg.Update(&sandboxResources); err != nil {
+			return nil, err
 		}
 		cgroup = cg
 	} else if cgroups.Mode() == cgroups.Unified {
 		// load created cgroup and update with resources
 		cg, err := cgroupsv2.LoadSystemd(slice, unit)
 		if err != nil {
-			if cg.Update(cgroupsv2.ToResources(&sandboxResources)); err != nil {
-				return nil, err
-			}
+			return nil, err
+		}
+		if cg.Update(cgroupsv2.ToResources(&sandboxResources)); err != nil {
+			return nil, err
 		}
 		cgroup = cg
 	} else {
@@ -226,19 +229,20 @@ func NewSandboxResourceController(path string, resources *specs.LinuxResources, 
 	}, nil
 }
 
-func LoadResourceController(path string) (ResourceController, error) {
+func LoadResourceController(path string, sandboxCgroupOnly bool) (ResourceController, error) {
 	var err error
 	var cgroup interface{}
 
 	// load created cgroup and update with resources
 	if cgroups.Mode() == cgroups.Legacy || cgroups.Mode() == cgroups.Hybrid {
-		cgHierarchy, cgPath, err := cgroupHierarchy(path)
+		cgHierarchy, cgPath, err := cgroupHierarchy(path, sandboxCgroupOnly)
 		if err != nil {
 			return nil, err
 		}
 
 		cgroup, err = cgroups.Load(cgHierarchy, cgPath)
 		if err != nil {
+			controllerLogger.WithField("source", "cgroups").Errorf("cgPath: %v", cgPath)
 			return nil, err
 		}
 	} else if cgroups.Mode() == cgroups.Unified {
@@ -331,10 +335,10 @@ func (c *LinuxCgroup) Update(resources *specs.LinuxResources) error {
 	}
 }
 
-func (c *LinuxCgroup) MoveTo(path string) error {
+func (c *LinuxCgroup) MoveTo(path string, sandboxCgroupOnly bool) error {
 	switch cg := c.cgroup.(type) {
 	case cgroups.Cgroup:
-		cgHierarchy, cgPath, err := cgroupHierarchy(path)
+		cgHierarchy, cgPath, err := cgroupHierarchy(path, sandboxCgroupOnly)
 		if err != nil {
 			return err
 		}

src/runtime/pkg/resourcecontrol/controller.go
@@ -69,7 +69,7 @@ type ResourceController interface {
 	Update(*specs.LinuxResources) error
 
 	// MoveTo moves a controller to another one.
-	MoveTo(string) error
+	MoveTo(string, bool) error
 
 	// AddDevice adds a device resource to the controller.
 	AddDevice(string) error

src/runtime/pkg/resourcecontrol/utils_linux.go
@@ -55,8 +55,8 @@ func newProperty(name string, units interface{}) systemdDbus.Property {
 	}
 }
 
-func cgroupHierarchy(path string) (cgroups.Hierarchy, cgroups.Path, error) {
-	if !IsSystemdCgroup(path) {
+func cgroupHierarchy(path string, sandboxCgroupOnly bool) (cgroups.Hierarchy, cgroups.Path, error) {
+	if !IsSystemdCgroup(path) || !sandboxCgroupOnly {
 		return cgroups.V1, cgroups.StaticPath(path), nil
 	} else {
 		slice, unit, err := getSliceAndUnit(path)

src/runtime/virtcontainers/clh.go
@@ -1734,7 +1734,7 @@ func (clh *cloudHypervisor) cleanupVM(force bool) error {
 	}
 
 	if clh.config.VMid != "" {
-		dir = filepath.Join(clh.config.VMStorePath, clh.config.VMid)
+		dir = filepath.Join(clh.config.RunStorePath, clh.config.VMid)
 		if err := os.RemoveAll(dir); err != nil {
 			if !force {
 				return err

src/runtime/virtcontainers/sandbox.go
@@ -44,7 +44,6 @@ import (
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/annotations"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/compatoci"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/cpuset"
-	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/rootless"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/types"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/utils"
 
@@ -1031,10 +1030,8 @@ func (s *Sandbox) Delete(ctx context.Context) error {
 		}
 	}
 
-	if !rootless.IsRootless() {
-		if err := s.resourceControllerDelete(); err != nil {
-			s.Logger().WithError(err).Errorf("failed to cleanup the %s resource controllers", s.sandboxController)
-		}
+	if err := s.resourceControllerDelete(); err != nil {
+		s.Logger().WithError(err).Errorf("failed to cleanup the %s resource controllers", s.sandboxController)
 	}
 
 	if s.monitor != nil {
@@ -2596,13 +2593,13 @@ func (s *Sandbox) resourceControllerDelete() error {
 		return nil
 	}
 
-	sandboxController, err := resCtrl.LoadResourceController(s.state.SandboxCgroupPath)
+	sandboxController, err := resCtrl.LoadResourceController(s.state.SandboxCgroupPath, s.config.SandboxCgroupOnly)
 	if err != nil {
 		return err
 	}
 
 	resCtrlParent := sandboxController.Parent()
-	if err := sandboxController.MoveTo(resCtrlParent); err != nil {
+	if err := sandboxController.MoveTo(resCtrlParent, s.config.SandboxCgroupOnly); err != nil {
 		return err
 	}
 
@@ -2611,13 +2608,13 @@ func (s *Sandbox) resourceControllerDelete() error {
 	}
 
 	if s.state.OverheadCgroupPath != "" {
-		overheadController, err := resCtrl.LoadResourceController(s.state.OverheadCgroupPath)
+		overheadController, err := resCtrl.LoadResourceController(s.state.OverheadCgroupPath, s.config.SandboxCgroupOnly)
 		if err != nil {
 			return err
 		}
 
 		resCtrlParent := overheadController.Parent()
-		if err := s.overheadController.MoveTo(resCtrlParent); err != nil {
+		if err := s.overheadController.MoveTo(resCtrlParent, s.config.SandboxCgroupOnly); err != nil {
 			return err
 		}
 