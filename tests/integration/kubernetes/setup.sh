3bbf3c81c2330299a14fde03afa59224ddbb27d0
Fabiano Fidêncio <fabiano@fidencio.org>
2024-10-08 21:21:54 +0200

    ci: mariner: Use the image instead of the initrd
    As an image has been added for mariner as part of the commit 63c1f81c2,
    let's start using it in the CI, instead of using the initrd.
    
    Signed-off-by: Fabiano Fidêncio <fabiano@fidencio.org>

--- tests/integration/kubernetes/setup.sh
+++ tests/integration/kubernetes/setup.sh
@@ -99,19 +99,19 @@ add_annotations_to_yaml() {
 	esac
 }
 
-add_cbl_mariner_kernel_initrd_annotations() {
+add_cbl_mariner_specific_annotations() {
 	if [[ "${KATA_HOST_OS}" = "cbl-mariner" ]]; then
-		info "Add kernel and initrd path and annotations for cbl-mariner"
+		info "Add kernel and image path and annotations for cbl-mariner"
 		local mariner_annotation_kernel="io.katacontainers.config.hypervisor.kernel"
 		local mariner_kernel_path="/usr/share/cloud-hypervisor/vmlinux.bin"
 
-		local mariner_annotation_initrd="io.katacontainers.config.hypervisor.initrd"
-		local mariner_initrd_path="/opt/kata/share/kata-containers/kata-containers-initrd-mariner.img"
+		local mariner_annotation_image="io.katacontainers.config.hypervisor.image"
+		local mariner_image_path="/opt/kata/share/kata-containers/kata-containers-mariner.img"
 
 		for K8S_TEST_YAML in runtimeclass_workloads_work/*.yaml
 		do
 			add_annotations_to_yaml "${K8S_TEST_YAML}" "${mariner_annotation_kernel}" "${mariner_kernel_path}"
-			add_annotations_to_yaml "${K8S_TEST_YAML}" "${mariner_annotation_initrd}" "${mariner_initrd_path}"
+			add_annotations_to_yaml "${K8S_TEST_YAML}" "${mariner_annotation_image}" "${mariner_image_path}"
 		done
 	fi
 }
@@ -139,7 +139,7 @@ add_runtime_handler_annotations() {
 main() {
 	ensure_yq
 	reset_workloads_work_dir
-	add_cbl_mariner_kernel_initrd_annotations
+	add_cbl_mariner_specific_annotations
 	add_runtime_handler_annotations
 }
 
