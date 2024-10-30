c4089df9d2a4775777c50d043b22c5695dcceb53
Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>
2024-10-30 19:13:15 +0000

    gha: Add missing steps in Kata stability workflow
    This PR adds missing steps in the gha run script for the kata stability
    workflow.
    
    Signed-off-by: Gabriela Cervantes <gabriela.cervantes.tellez@intel.com>

--- tests/stability/gha-stability-run.sh
+++ tests/stability/gha-stability-run.sh
@@ -12,6 +12,8 @@ set -o pipefail
 stability_dir="$(dirname "$(readlink -f "$0")")"
 source "${stability_dir}/../common.bash"
 source "${stability_dir}/../metrics/lib/common.bash"
+source "${stability_dir}/../gha-run-k8s-common.sh"
+source "${stability_dir}/../integration/kubernetes/gha-run.sh"
 
 function run_tests() {
 	info "Running scability test using ${KATA_HYPERVISOR} hypervisor"
@@ -27,7 +29,18 @@ function run_tests() {
 function main() {
 	action="${1:-}"
 	case "${action}" in
+		install-azure-cli) install_azure_cli ;;
+		login-azure) login_azure ;;
+		create-cluster) create_cluster ;;
+		install-bats) install_bats ;;
+		install-kubectl) install_kubectl ;;
+		get-cluster-credentials) get_cluster_credentials ;;
+		deploy-snapshotter) deploy_snapshotter ;;
+		deploy-kata-aks) deploy_kata "aks" ;;
+		deploy-coco-kbs) deploy_coco_kbs ;;
+		install-kbs-client) install_kbs_client ;;
 		run-tests) run_tests ;;
+		delete-cluster) cleanup "aks" ;;
 		*) >&2 die "Invalid argument" ;;
 	esac
 }
