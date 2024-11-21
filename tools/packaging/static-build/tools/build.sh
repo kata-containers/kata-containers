fb87bf221f9b73c0b8915d6eef063ef94dcfd10a
Aurélien Bombo <abombo@microsoft.com>
2024-11-21 15:46:24 -0600

    ci: Implement build step for CSI driver
    This fully implements the compilation step for csi-kata-directvolume.
    This component can now be built by the CI running:
    
     $ cd tools/packaging/kata-deploy/local-build
     $ make csi-kata-directvolume-tarball
    
    A couple notes:
    
     * When installing the binary, we rename it from directvolplugin to
       csi-kata-directvolume on the fly to make it more readable.
     * We add go to the tools builder Dockerfile to support building this
       tool.
     * I've noticed the file install_libseccomp.sh gets created by the build
       process so I've added it to a .gitignore.
    
    Signed-off-by: Aurélien Bombo <abombo@microsoft.com>

--- tools/packaging/static-build/tools/build.sh
+++ tools/packaging/static-build/tools/build.sh
@@ -20,6 +20,7 @@ container_image="${TOOLS_CONTAINER_BUILDER:-$(get_tools_image_name)}"
 
 docker pull ${container_image} || \
 	(docker $BUILDX build $PLATFORM \
+	    	--build-arg GO_TOOLCHAIN="$(get_from_kata_deps ".languages.golang.meta.newest-version")" \
 	    	--build-arg RUST_TOOLCHAIN="$(get_from_kata_deps ".languages.rust.meta.newest-version")" \
 		-t "${container_image}" "${script_dir}" && \
 	 # No-op unless PUSH_TO_REGISTRY is exported as "yes"
