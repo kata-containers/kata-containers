ce37e7051d6a6ad9827b6abf7d36ee79356d77ab
Leonard Cohnen <lc@edgeless.systems>
2024-12-03 23:33:01 +0100

    policy: check the linux network namespace
    Peer pods have a linux namespace of type network. We want to make sure that all container in the same pod use the same namespace. Therefore, we add the first namespace path to the state and check all other requests against that.
    
    This commit also adds the corresponding integration test in the policy crate showcasing the benefit of having rust integration tests for the policy.
    
    Signed-off-by: Leonard Cohnen <lc@edgeless.systems>

--- src/agent/tests/main.rs
+++ src/agent/tests/main.rs
@@ -128,4 +128,9 @@ mod tests {
     async fn test_create_sandbox() {
         runtests::<CreateSandboxRequest>("createsandbox").await;
     }
+
+    #[tokio::test]
+    async fn test_create_container_network_namespace() {
+        runtests::<CreateContainerRequest>("createcontainer/network_namespace").await;
+    }
 }
