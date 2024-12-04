bea967232e8113eec8bf5c576455d57ac2240cea
Leonard Cohnen <lc@edgeless.systems>
2024-12-04 12:23:15 +0100

    genpolicy: set confidential_guest default to true
    Indicate that the default for genpolicy should be in a confidential guest setting.
    Most notably this switches the expected mount paths for emptyDir and configMaps
    around. This should make the manual fix in the CI integration test unnecessary.
    
    The setting was changed to false in order to unblock the then exising
    CI: https://github.com/kata-containers/kata-containers/commit/60ac3048e9d94fb86344e9f1ae9510dc92da669c
    
    We now should have the knobs to specifically disable the setting in the
    testcase.
    
    Signed-off-by: Leonard Cohnen <lc@edgeless.systems>

--- tests/integration/kubernetes/tests_common.sh
+++ tests/integration/kubernetes/tests_common.sh
@@ -85,7 +85,7 @@ adapt_common_policy_settings_coco() {
 	local settings_dir=$1
 
 	info "Adapting common policy settings for TDX, SNP, or the non-TEE development environment"
-	jq '.common.cpath = "/run/kata-containers" | .volumes.configMap.mount_point = "^$(cpath)/$(bundle-id)-[a-z0-9]{16}-"' "${settings_dir}/genpolicy-settings.json" > temp.json && sudo mv temp.json "${settings_dir}/genpolicy-settings.json"
+	jq '.common.cpath = "/run/kata-containers"' "${settings_dir}/genpolicy-settings.json" > temp.json && sudo mv temp.json "${settings_dir}/genpolicy-settings.json"
 }
 
 # adapt common policy settings for qemu-sev
@@ -93,7 +93,7 @@ adapt_common_policy_settings_for_sev() {
 	local settings_dir=$1
 
 	info "Adapting common policy settings for SEV"
-	jq '.kata_config.oci_version = "1.1.0-rc.1" | .common.cpath = "/run/kata-containers" | .volumes.configMap.mount_point = "^$(cpath)/$(bundle-id)-[a-z0-9]{16}-"' "${settings_dir}/genpolicy-settings.json" > temp.json && sudo mv temp.json "${settings_dir}/genpolicy-settings.json"
+	jq '.kata_config.oci_version = "1.1.0-rc.1" | .common.cpath = "/run/kata-containers"' "${settings_dir}/genpolicy-settings.json" > temp.json && sudo mv temp.json "${settings_dir}/genpolicy-settings.json"
 }
 
 # adapt common policy settings for pod VMs using "shared_fs = virtio-fs" (https://github.com/kata-containers/kata-containers/issues/10189)
