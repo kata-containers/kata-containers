cc9ecedaeaf201b37bf7f722366e0c2f21e50e0d
Zvonko Kaiser <zkaiser@nvidia.com>
2024-12-06 00:43:27 +0000

    qemu: Bump version, new options, add no_patches
    We want to have the latest QEMU version available
    which is as of this writing v9.1.2
    
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>
    
    qemu: Add new options for 9.1.2
    
    We need to fence specific options depending on the version
    and disable ones that are not needed anymore
    
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>
    
    qemu: Add no_patches.txt
    
    Since we do not have any patches for this version
    let's create the appropriate files.
    
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>

--- tools/packaging/scripts/configure-hypervisor.sh
+++ tools/packaging/scripts/configure-hypervisor.sh
@@ -222,9 +222,6 @@ generate_qemu_options() {
 
 	# Disabled options
 
-	# Disable block migration in the main migration stream
-	qemu_options+=(size:--disable-live-block-migration)
-
 	# braille support not required
 	qemu_options+=(size:--disable-brlapi)
 
@@ -397,6 +394,23 @@ generate_qemu_options() {
 	qemu_options+=(size:--disable-vhdx)
 	qemu_options+=(size:--disable-hv-balloon)
 
+	# Disable various features based on the qemu_version
+	if gt_eq "${qemu_version}" "9.1.0" ; then
+		# Disable Query Processing Library support
+		qemu_options+=(size:--disable-qpl)
+		# Disable UADK Library support
+		qemu_options+=(size:--disable-uadk)
+		# Disable syscall buffer debugging support
+		qemu_options+=(size:--disable-debug-remap)
+
+	fi
+
+	# Disable gio support
+	qemu_options+=(size:--disable-gio)
+	# Disable libdaxctl part of ndctl support
+	qemu_options+=(size:--disable-libdaxctl)
+	qemu_options+=(size:--disable-oss)
+
 	#---------------------------------------------------------------------
 	# Enabled options
 
@@ -409,6 +423,7 @@ generate_qemu_options() {
 
 	# Support Linux AIO (native)
 	qemu_options+=(size:--enable-linux-aio)
+	qemu_options+=(size:--enable-linux-io-uring)
 
 	# Support Ceph RADOS Block Device (RBD)
 	[ -z "${static}" ] && qemu_options+=(functionality:--enable-rbd)
@@ -427,7 +442,7 @@ generate_qemu_options() {
 	# for that architecture
 	if [ "$arch" == x86_64 ]; then
 		qemu_options+=(speed:--enable-avx2)
-		qemu_options+=(speed:--enable-avx512f)
+		qemu_options+=(speed:--enable-avx512bw)
 	else
 		qemu_options+=(speed:--disable-avx2)
 	fi
