4707883b4003645ecfd760a28f437ca1e54c79cf
Dan Mihai <dmihai@microsoft.com>
2025-01-10 20:07:41 +0000

    image: bump mariner guest version to 3.0
    Use Mariner 3.0 (a.k.a., Azure Linux 3.0) as the Guest CI image.
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 OS_NAME=cbl-mariner
-OS_VERSION=${OS_VERSION:-2.0}
+OS_VERSION=${OS_VERSION:-3.0}
 LIBC="gnu"
 PACKAGES="kata-packages-uvm"
 [ "$AGENT_INIT" = no ] && PACKAGES+=" systemd"
