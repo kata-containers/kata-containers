a698ae7039d60353ac9dc79328321ee2212e5c63
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-01-12 20:48:01 +0200

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -5,6 +5,6 @@ apt update
 cd /opt/deb/nvidia
 dpkg -i *.deb
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-565-open
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-560-open
 cd /opt/deb
 dpkg -i *.deb
