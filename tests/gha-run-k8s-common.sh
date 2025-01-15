4ee7f310b2e43f9d5e69432600e9ab1121f2f165
Aurélien Bombo <abombo@microsoft.com>
2025-01-15 16:24:03 -0600

    tests: Introduce new env variables to ease development
    It can be useful to set these variables during local testing:
    
     * AZ_REGION: Region for the cluster.
     * AZ_NODEPOOL_TAGS: Node pool tags for the cluster.
     * GENPOLICY_BINARY: Path to the genpolicy binary.
     * GENPOLICY_SETTINGS_DIR: Directory holding the genpolicy settings.
    
    I've also made it so that tests_common.sh modifies the duplicated
    genpolicy-settings.json (used for testing) instead of the original git-tracked
    one.
    
    Signed-off-by: Aurélien Bombo <abombo@microsoft.com>

--- tests/gha-run-k8s-common.sh
+++ tests/gha-run-k8s-common.sh
@@ -9,6 +9,8 @@ source "${tests_dir}/common.bash"
 kubernetes_dir="${tests_dir}/integration/kubernetes"
 helm_chart_dir="${repo_root_dir}/tools/packaging/kata-deploy/helm-chart/kata-deploy"
 
+AZ_REGION="${AZ_REGION:-eastus}"
+AZ_NODEPOOL_TAGS="${AZ_NODEPOOL_TAGS:-}"
 GENPOLICY_PULL_METHOD="${GENPOLICY_PULL_METHOD:-oci-distribution}"
 GH_PR_NUMBER="${GH_PR_NUMBER:-}"
 HELM_DEFAULT_INSTALLATION="${HELM_DEFAULT_INSTALLATION:-false}"
@@ -110,7 +112,7 @@ function create_cluster() {
 		"GENPOLICY_PULL_METHOD=${GENPOLICY_PULL_METHOD:0:1}")
 
 	az group create \
-		-l eastus \
+		-l "${AZ_REGION}" \
 		-n "${rg}"
 
 	# Required by e.g. AKS App Routing for KBS installation.
@@ -128,7 +130,8 @@ function create_cluster() {
 		--node-count 1 \
 		--generate-ssh-keys \
 		--tags "${tags[@]}" \
-		$([[ "${KATA_HOST_OS}" = "cbl-mariner" ]] && echo "--os-sku AzureLinux --workload-runtime KataMshvVmIsolation")
+		$([[ "${KATA_HOST_OS}" = "cbl-mariner" ]] && echo "--os-sku AzureLinux --workload-runtime KataMshvVmIsolation") \
+		$([[ -n "${AZ_NODEPOOL_TAGS}" ]] && echo "--nodepool-tags ${AZ_NODEPOOL_TAGS}")
 }
 
 function install_bats() {
