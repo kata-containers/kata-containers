9a1c11cfd05a594883bdd0757534c4447186a86a
Pavel Mores <pmores@redhat.com>
2025-01-30 17:08:38 +0100

    runtime-rs: add implementation of ProtectionDevice
    ProtectionDevice is a new device type whose implementation structure
    matches the one of other devices in the device module.  It is split into
    an inner "config" part which contains device details (we implement
    SEV/SEV-SNP for now) and the customary outer "device" part which just adds
    a device instance ID and the customary Device trait implementation.
    
    Signed-off-by: Pavel Mores <pmores@redhat.com>

--- src/runtime-rs/crates/hypervisor/src/device/device_manager.rs
+++ src/runtime-rs/crates/hypervisor/src/device/device_manager.rs
@@ -13,9 +13,9 @@ use tokio::sync::{Mutex, RwLock};
 
 use crate::{
     vhost_user_blk::VhostUserBlkDevice, BlockConfig, BlockDevice, HybridVsockDevice, Hypervisor,
-    NetworkDevice, ShareFsDevice, VfioDevice, VhostUserConfig, VhostUserNetDevice, VsockDevice,
-    KATA_BLK_DEV_TYPE, KATA_CCW_DEV_TYPE, KATA_MMIO_BLK_DEV_TYPE, KATA_NVDIMM_DEV_TYPE,
-    VIRTIO_BLOCK_CCW, VIRTIO_BLOCK_MMIO, VIRTIO_BLOCK_PCI, VIRTIO_PMEM,
+    NetworkDevice, ProtectionDevice, ShareFsDevice, VfioDevice, VhostUserConfig,
+    VhostUserNetDevice, VsockDevice, KATA_BLK_DEV_TYPE, KATA_CCW_DEV_TYPE, KATA_MMIO_BLK_DEV_TYPE,
+    KATA_NVDIMM_DEV_TYPE, VIRTIO_BLOCK_CCW, VIRTIO_BLOCK_MMIO, VIRTIO_BLOCK_PCI, VIRTIO_PMEM,
 };
 
 use super::{
@@ -250,7 +250,7 @@ impl DeviceManager {
                         return Some(device_id.to_string());
                     }
                 }
-                DeviceType::HybridVsock(_) | DeviceType::Vsock(_) => {
+                DeviceType::HybridVsock(_) | DeviceType::Vsock(_) | DeviceType::Protection(_) => {
                     continue;
                 }
             }
@@ -385,6 +385,13 @@ impl DeviceManager {
                 }
 
                 Arc::new(Mutex::new(ShareFsDevice::new(&device_id, config)))
+            }
+            DeviceConfig::ProtectionDevCfg(pconfig) => {
+                // No need to do find device for protection device.
+                Arc::new(Mutex::new(ProtectionDevice::new(
+                    device_id.clone(),
+                    pconfig,
+                )))
             }
         };
 
