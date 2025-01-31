bd3c93713f0a85c5c29459ec2f86fa631fca7aed
stevenhorsman <steven@uk.ibm.com>
2025-01-31 12:00:59 +0000

    kata-sys-util: Complete code move
    In #7236 the guest protection code was moved to kata-sys-utils,
    but some of it was left behind, and the adjustment to the new
    location wasn't completed, so the powerpc64 code doesn't
    build now we've fixed the cfg to test it.
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- src/tools/kata-ctl/src/arch/powerpc64/mod.rs
+++ src/tools/kata-ctl/src/arch/powerpc64/mod.rs
@@ -3,27 +3,26 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-use crate::types::*;
 #[cfg(all(target_arch = "powerpc64", target_endian = "little"))]
 pub use arch_specific::*;
 
 mod arch_specific {
     use crate::check;
+    use crate::types::CheckItem;
     use crate::utils;
     use anyhow::Result;
 
     pub const ARCH_CPU_VENDOR_FIELD: &str = "";
     pub const ARCH_CPU_MODEL_FIELD: &str = "model";
 
+    #[allow(dead_code)]
     pub fn check() -> Result<()> {
-        unimplemented!("Check not implemented in powerpc64le");
+        unimplemented!("Check not implemented in powerpc64");
     }
 
     pub fn get_checks() -> Option<&'static [CheckItem<'static>]> {
         None
     }
-
-    const PEF_SYS_FIRMWARE_DIR: &str = "/sys/firmware/ultravisor/";
 
     pub fn get_cpu_details() -> Result<(String, String)> {
         utils::get_generic_cpu_details(check::PROC_CPUINFO)
