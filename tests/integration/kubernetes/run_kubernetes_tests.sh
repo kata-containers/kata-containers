9fcf79605d270b2d2477f720ca1f2740c12953b0
Wainer dos Santos Moschetta <wainersm@redhat.com>
2025-02-14 19:51:04 -0300

    tests/k8s: split and save bats outputs in files
    Currently run_kubernetes_tests.sh sends all the bats outputs to stdout
    which can be very difficult to browse to find a problem, mainly on
    CI. With this change, each bats execution have its output sent to
    'reports/yyy-mm-dd-hh:mm:ss/<status>-<bats file>.log' where <status>
    is either 'ok' (tests passed) or 'not_ok' (some tests failed).
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- tests/integration/kubernetes/run_kubernetes_tests.sh
+++ tests/integration/kubernetes/run_kubernetes_tests.sh
@@ -6,6 +6,7 @@
 #
 
 set -e
+set -o pipefail
 
 kubernetes_dir=$(dirname "$(readlink -f "$0")")
 source "${kubernetes_dir}/../../common.bash"
@@ -128,16 +129,25 @@ fi
 
 ensure_yq
 
-info "Running tests with bats version: $(bats --version)"
+report_dir="${kubernetes_dir}/reports/$(date +'%F-%T')"
+mkdir -p "$report_dir"
+
+info "Running tests with bats version: $(bats --version). Save outputs to $report_dir"
 
 tests_fail=()
 for K8S_TEST_ENTRY in ${K8S_TEST_UNION[@]}
 do
 	info "$(kubectl get pods --all-namespaces 2>&1)"
 	info "Executing ${K8S_TEST_ENTRY}"
-	if ! bats --show-output-of-passing-tests "${K8S_TEST_ENTRY}"; then
+	# Output file will be prefixed with either "ok" or "not_ok" in case of
+	# success or fail, respectively.
+	out_file="${report_dir}/${K8S_TEST_ENTRY}.out"
+	if ! bats --show-output-of-passing-tests "${K8S_TEST_ENTRY}" | tee "$out_file"; then
 		tests_fail+=("${K8S_TEST_ENTRY}")
+		mv "${out_file}" "$(dirname "${out_file}")/not_ok-$(basename "${out_file}")"
 		[ "${K8S_TEST_FAIL_FAST}" = "yes" ] && break
+	else
+		mv "${out_file}" "$(dirname "${out_file}")/ok-$(basename "${out_file}")"
 	fi
 done
 
