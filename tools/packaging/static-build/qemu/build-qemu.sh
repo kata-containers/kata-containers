6d63f9eb4babdf773984f98201ef6ea9f5b68142
Zvonko Kaiser <zkaiser@nvidia.com>
2025-03-05 23:25:48 +0000

    shellcheck: fix build-qemu.sh
    Fix all warnings and errors
    
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>

--- tools/packaging/static-build/qemu/build-qemu.sh
+++ tools/packaging/static-build/qemu/build-qemu.sh
@@ -18,19 +18,27 @@ kata_static_build_scripts="${kata_static_build_dir}/scripts"
 
 ARCH=${ARCH:-$(uname -m)}
 
+QEMU_REPO="${QEMU_REPO:-}"
+QEMU_VERSION_NUM="${QEMU_VERSION_NUM:-}"
+QEMU_TARBALL="${QEMU_TARBALL:-}"
+QEMU_DESTDIR="${QEMU_DESTDIR:-}"
+HYPERVISOR_NAME="${HYPERVISOR_NAME:-}"
+PREFIX="${PREFIX:-}"
+PKGVERSION=${PKGVERSION:-}
+
 git clone --depth=1 "${QEMU_REPO}" qemu
 pushd qemu
 git fetch --depth=1 origin "${QEMU_VERSION_NUM}"
 git checkout FETCH_HEAD
 scripts/git-submodule.sh update meson capstone
-${kata_packaging_scripts}/patch_qemu.sh "${QEMU_VERSION_NUM}" "${kata_packaging_dir}/qemu/patches"
-if [ "$(uname -m)" != "${ARCH}" ] && [ "${ARCH}" == "s390x" ]; then
-       PREFIX="${PREFIX}" ${kata_packaging_scripts}/configure-hypervisor.sh -s "${HYPERVISOR_NAME}" "${ARCH}" | xargs ./configure  --with-pkgversion="${PKGVERSION}" --cc=s390x-linux-gnu-gcc --cross-prefix=s390x-linux-gnu- --prefix="${PREFIX}" --target-list=s390x-softmmu
+"${kata_packaging_scripts}"/patch_qemu.sh "${QEMU_VERSION_NUM}" "${kata_packaging_dir}/qemu/patches"
+if [[ "$(uname -m)" != "${ARCH}" ]] && [[ "${ARCH}" == "s390x" ]]; then
+       PREFIX="${PREFIX}" "${kata_packaging_scripts}"/configure-hypervisor.sh -s "${HYPERVISOR_NAME}" "${ARCH}" | xargs ./configure  --with-pkgversion="${PKGVERSION}" --cc=s390x-linux-gnu-gcc --cross-prefix=s390x-linux-gnu- --prefix="${PREFIX}" --target-list=s390x-softmmu
 else
-       PREFIX="${PREFIX}" ${kata_packaging_scripts}/configure-hypervisor.sh -s "${HYPERVISOR_NAME}" "${ARCH}" | xargs ./configure  --with-pkgversion="${PKGVERSION}"
+       PREFIX="${PREFIX}" "${kata_packaging_scripts}"/configure-hypervisor.sh -s "${HYPERVISOR_NAME}" "${ARCH}" | xargs ./configure  --with-pkgversion="${PKGVERSION}"
 fi
 make -j"$(nproc +--ignore 1)"
 make install DESTDIR="${QEMU_DESTDIR}"
 popd
-${kata_static_build_scripts}/qemu-build-post.sh
+"${kata_static_build_scripts}"/qemu-build-post.sh
 mv "${QEMU_DESTDIR}/${QEMU_TARBALL}" /share/
