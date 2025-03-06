04adcdace60fe6de37a3c3f6cb9347e6d7119435
Dan Mihai <dmihai@microsoft.com>
2025-03-06 18:45:47 +0000

    gha: always delete workspace on rebase error
    The workplace was already being deleted on non-x86_64 platforms, but
    x86_64 can be affected by the same problem too. That might have been
    the case with the SNP and TDX test runs from:
    
    https://github.com/kata-containers/kata-containers/actions/runs/13687511270/job/38313758751?pr=10973
    https://github.com/kata-containers/kata-containers/actions/runs/13687511270/job/38313760086?pr=10973
    
    Rebase worked fine for the same patch/PR on other platforms.
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- tests/git-helper.sh
+++ tests/git-helper.sh
@@ -19,13 +19,9 @@ function add_kata_bot_info() {
 function rebase_atop_of_the_latest_target_branch() {
 	if [ -n "${TARGET_BRANCH}" ]; then
 		echo "Rebasing atop of the latest ${TARGET_BRANCH}"
-		# Recover from any previous rebase left halfway
-		git rebase --abort 2> /dev/null || true
 		if ! git rebase origin/${TARGET_BRANCH}; then
-			# if GITHUB_WORKSPACE is defined and an architecture is not equal to x86_64
-			# (mostly self-hosted runners), then remove the repository
-			if [ -n "${GITHUB_WORKSPACE}" ] && [ "$(uname -m)" != "x86_64" ]; then
-				echo "Rebase failed, cleaning up a repository for self-hosted runners and exiting"
+			if [ -n "${GITHUB_WORKSPACE}" ] ; then
+				echo "Rebase failed, cleaning up the local repository and exiting"
 				cd "${GITHUB_WORKSPACE}"/..
 				sudo rm -rf "${GITHUB_WORKSPACE}"
 			else
