0d3f9fcee1c7bc65b7f79d45a9eda8eec3703905
Dan Mihai <dmihai@microsoft.com>
2025-03-18 23:29:09 +0000

    tests: tests_common: export variables used externally
    ShellCheck: export variables used outside of tests_common.sh - e.g.,
    
    warning: timeout appears unused. Verify use (or export if used externally). [SC2034]
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- tests/integration/kubernetes/tests_common.sh
+++ tests/integration/kubernetes/tests_common.sh
@@ -17,16 +17,16 @@ export container_images_agnhost_version="2.21"
 
 # Timeout options, mainly for use with waitForProcess(). Use them unless the
 # operation needs to wait longer.
-wait_time=90
-sleep_time=3
+export wait_time=90
+export sleep_time=3
 
 # Timeout for use with `kubectl wait`, unless it needs to wait longer.
 # Note: try to keep timeout and wait_time equal.
-timeout=90s
+export timeout=90s
 
 # issues that can't test yet.
-fc_limitations="https://github.com/kata-containers/documentation/issues/351"
-dragonball_limitations="https://github.com/kata-containers/kata-containers/issues/6621"
+export fc_limitations="https://github.com/kata-containers/documentation/issues/351"
+export dragonball_limitations="https://github.com/kata-containers/kata-containers/issues/6621"
 
 # Path to the kubeconfig file which is used by kubectl and other tools.
 # Note: the init script sets that variable but if you want to run the tests in
@@ -64,7 +64,7 @@ setup_common() {
 }
 
 get_pod_config_dir() {
-	pod_config_dir="${BATS_TEST_DIRNAME}/runtimeclass_workloads_work"
+	export pod_config_dir="${BATS_TEST_DIRNAME}/runtimeclass_workloads_work"
 	info "k8s configured to use runtimeclass"
 }
 
