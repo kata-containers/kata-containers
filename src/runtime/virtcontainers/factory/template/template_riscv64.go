4947938ce896cd2e769c2aef12390f6237f8d1aa
Ruoqing He <heruoqing@iscas.ac.cn>
2025-03-27 09:28:32 +0800

    runtime: Introduce riscv64 template for vm factory
    Set `templateDeviceStateSize` to 8 as other architectures did.
    
    Signed-off-by: Ruoqing He <heruoqing@iscas.ac.cn>

---
+++ src/runtime/virtcontainers/factory/template/template_riscv64.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2024 Institute of Software, CAS.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// template implements base vm factory with vm templating.
+
+package template
+
+// templateDeviceStateSize denotes device state size when
+// mount tmpfs.
+const templateDeviceStateSize = 8
