fc75aee13a5634b2971415a557f645a16c0936c2
Cameron Baird <cameronbaird@microsoft.com>
2025-04-04 23:12:17 +0000

    ci: Add CI tests for runAsGroup, GID policy
    Introduce tests to check for policy correctness on a redis deployment
    with 1. a pod-level securityContext 2. a container-level securityContext
    which shadows the pod-level securityContext 3. a pod-level
    securityContext which selects an existing user (nobody), causing a new GID to be selected.
    
    Redis is an interesting container image to test with because it includes
    a /etc/passwd file with existing user/group configuration of 1000:1000 baked in.
    
    Signed-off-by: Cameron Baird <cameronbaird@microsoft.com>

--- tests/integration/kubernetes/run_kubernetes_tests.sh
+++ tests/integration/kubernetes/run_kubernetes_tests.sh
@@ -73,6 +73,7 @@ else
 		"k8s-pod-quota.bats" \
 		"k8s-policy-hard-coded.bats" \
 		"k8s-policy-deployment.bats" \
+		"k8s-policy-deployment-sc.bats" \
 		"k8s-policy-job.bats" \
 		"k8s-policy-logs.bats" \
 		"k8s-policy-pod.bats" \
