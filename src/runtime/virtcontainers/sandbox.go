288fb38f552c11219913f897b0ca2c7739147b04
wushukun <wushukun@kuaishou.com>
2025-04-21 11:36:39 +0800

    fix: Failed to clean up cgroup dir when deleting Pod
    The Kata shim may not always be able to migrate the QEMU
    process to the parent cgroup during the MoveTo operation,
    which can lead to failure when deleting the cgroup.
    
    Therefore, we should add retry logic when deleting the cgroup
    to ensure that the cgroup directory can be cleaned up properly.
    
    Signed-off-by: wushukun <wushukun@kuaishou.com>

--- src/runtime/virtcontainers/sandbox.go
+++ src/runtime/virtcontainers/sandbox.go
@@ -20,6 +20,7 @@ import (
 	"strings"
 	"sync"
 	"syscall"
+	"time"
 
 	v1 "github.com/containerd/cgroups/stats/v1"
 	v2 "github.com/containerd/cgroups/v2/stats"
@@ -2611,8 +2612,15 @@ func (s *Sandbox) resourceControllerDelete() error {
 		return err
 	}
 
-	if err := sandboxController.Delete(); err != nil {
-		return err
+	delay := 1 * time.Second
+	maxDeletionAttempts := 5
+	for i := 0; i < maxDeletionAttempts; i++ {
+		if err := sandboxController.Delete(); err == nil {
+			break
+		} else if i == maxDeletionAttempts-1 {
+			return err
+		}
+		time.Sleep(delay)
 	}
 
 	if s.state.OverheadCgroupPath != "" {
@@ -2626,8 +2634,13 @@ func (s *Sandbox) resourceControllerDelete() error {
 			return err
 		}
 
-		if err := overheadController.Delete(); err != nil {
-			return err
+		for i := 0; i < maxDeletionAttempts; i++ {
+			if err := overheadController.Delete(); err == nil {
+				break
+			} else if i == maxDeletionAttempts-1 {
+				return err
+			}
+			time.Sleep(delay)
 		}
 	}
 
