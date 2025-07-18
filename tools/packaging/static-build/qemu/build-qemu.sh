564c8755864266379bbee9f102947604bc71b20a
Zvonko Kaiser <zkaiser@nvidia.com>
2025-07-18 03:24:20 +0000

    shellcheck: build-qemu.sh is clean
    Make shellcheck happy
    
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>

--- tools/packaging/static-build/qemu/build-qemu.sh
+++ tools/packaging/static-build/qemu/build-qemu.sh
@@ -8,7 +8,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Environment variables passed from container
 NUMA_ENABLED="${NUMA_ENABLED:-}"
+QEMU_REPO="${QEMU_REPO:-}"
+QEMU_VERSION_NUM="${QEMU_VERSION_NUM:-}"
+HYPERVISOR_NAME="${HYPERVISOR_NAME:-}"
+PKGVERSION="${PKGVERSION:-}"
+PREFIX="${PREFIX:-}"
+QEMU_DESTDIR="${QEMU_DESTDIR:-}"
+QEMU_TARBALL="${QEMU_TARBALL:-}"
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -25,14 +33,14 @@ pushd qemu
 git fetch --depth=1 origin "${QEMU_VERSION_NUM}"
 git checkout FETCH_HEAD
 scripts/git-submodule.sh update meson capstone
-${kata_packaging_scripts}/patch_qemu.sh "${QEMU_VERSION_NUM}" "${kata_packaging_dir}/qemu/patches"
-if [ "$(uname -m)" != "${ARCH}" ] && [ "${ARCH}" == "s390x" ]; then
-       PREFIX="${PREFIX}" ${kata_packaging_scripts}/configure-hypervisor.sh -s "${HYPERVISOR_NAME}" "${ARCH}" | xargs ./configure  --with-pkgversion="${PKGVERSION}" --cc=s390x-linux-gnu-gcc --cross-prefix=s390x-linux-gnu- --prefix="${PREFIX}" --target-list=s390x-softmmu
+"${kata_packaging_scripts}/patch_qemu.sh" "${QEMU_VERSION_NUM}" "${kata_packaging_dir}/qemu/patches"
+if [[ "$(uname -m)" != "${ARCH}" ]] && [[ "${ARCH}" == "s390x" ]]; then
+       PREFIX="${PREFIX}" "${kata_packaging_scripts}/configure-hypervisor.sh" -s "${HYPERVISOR_NAME}" "${ARCH}" | xargs ./configure  --with-pkgversion="${PKGVERSION}" --cc=s390x-linux-gnu-gcc --cross-prefix=s390x-linux-gnu- --prefix="${PREFIX}" --target-list=s390x-softmmu
 else
-       PREFIX="${PREFIX}" ${kata_packaging_scripts}/configure-hypervisor.sh -s "${HYPERVISOR_NAME}" "${ARCH}" | xargs ./configure  --with-pkgversion="${PKGVERSION}"
+       PREFIX="${PREFIX}" "${kata_packaging_scripts}/configure-hypervisor.sh" -s "${HYPERVISOR_NAME}" "${ARCH}" | xargs ./configure  --with-pkgversion="${PKGVERSION}"
 fi
 make -j"$(nproc +--ignore 1)"
 make install DESTDIR="${QEMU_DESTDIR}"
 popd
-${kata_static_build_scripts}/qemu-build-post.sh
+"${kata_static_build_scripts}/qemu-build-post.sh"
 mv "${QEMU_DESTDIR}/${QEMU_TARBALL}" /share/
