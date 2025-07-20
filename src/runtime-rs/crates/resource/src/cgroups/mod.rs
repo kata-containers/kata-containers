60e3679eb73503ddbf5965f1abb4071666a8450b
Xuewei Niu <niuxuewei.nxw@antgroup.com>
2025-07-20 11:15:50 +0800

    runtime-rs: Add full cgroups support on host
    Add full cgroups support on host. Cgroups are managed by `FsManager` and
    `SystemdManager`. As the names impies, the `FsManager` manages cgroups
    through cgroupfs, while the `SystemdManager` manages cgroups through
    systemd. The two manages support cgroup v1 and cgroup v2.
    
    Two types of cgroups path are supported:
    
    1. For colon paths, for example "foo.slice:bar:baz", the runtime manages
    cgroups by `SystemdManager`;
    2. For relative/absolute paths, the runtime manages cgroups by
    `FsManager`.
    
    vCPU threads are added into the sandbox cgroups in cgroup v1 + cgroupfs,
    others, cgroup v1 + systemd, cgroup v2 + cgroupfs, cgroup v2 + systemd, VMM
    process is added into the cgroups.
    
    The systemd doesn't provide a way to add thread to a unit. `add_thread()`
    in `SystemdManager` is equivalent to `add_process()`.
    
    Cgroup v2 supports threaded mode. However, we should enable threaded mode
    from leaf node to the root node (`/`) iteratively [1]. This means the
    runtime needs to modify the cgroups created by container runtime (e.g.
    containerd). Considering cgroupfs + cgroup v2 is not a common combination,
    its behavior is aligned with systemd + cgroup v2, which is not allowed to
    manage process at the thread level.
    
    1: https://www.kernel.org/doc/html/v4.18/admin-guide/cgroup-v2.html#threads
    
    Fixes: #11356
    
    Signed-off-by: Xuewei Niu <niuxuewei.nxw@antgroup.com>

--- src/runtime-rs/crates/resource/src/cgroups/mod.rs
+++ src/runtime-rs/crates/resource/src/cgroups/mod.rs
@@ -5,30 +5,19 @@
 //
 
 pub mod cgroup_persist;
+mod resource;
+pub use resource::CgroupsResource;
+mod resource_inner;
 mod utils;
 
-use anyhow::{anyhow, Context, Result};
-use async_trait::async_trait;
-use cgroup_persist::CgroupState;
-use cgroups_rs::{cgroup_builder::CgroupBuilder, Cgroup, CgroupPid, CpuResources, Resources};
-use hypervisor::Hypervisor;
+use anyhow::{anyhow, Result};
+use cgroups_rs::manager::is_systemd_cgroup;
+use hypervisor::HYPERVISOR_DRAGONBALL;
 use kata_sys_util::spec::load_oci_spec;
 use kata_types::config::TomlConfig;
-use oci::LinuxResources;
-use oci_spec::runtime as oci;
-use persist::sandbox_persist::Persist;
-use std::{
-    collections::{HashMap, HashSet},
-    error::Error,
-    io,
-    iter::FromIterator,
-    sync::Arc,
-};
-use tokio::sync::RwLock;
 
-use crate::ResourceUpdateOp;
+use crate::cgroups::cgroup_persist::CgroupState;
 
-const OS_ERROR_NO_SUCH_PROCESS: i32 = 3;
 const SANDBOXED_CGROUP_PATH: &str = "kata_sandboxed_pod";
 
 pub struct CgroupArgs {
@@ -44,7 +33,6 @@ pub struct CgroupConfig {
 
 impl CgroupConfig {
     fn new(sid: &str, toml_config: &TomlConfig) -> Result<Self> {
-        let overhead_path = utils::gen_overhead_path(sid);
         let path = if let Ok(spec) = load_oci_spec() {
             spec.linux()
                 .clone()
@@ -60,260 +48,38 @@ impl CgroupConfig {
         } else {
             format!("{}/{}", SANDBOXED_CGROUP_PATH, sid)
         };
+
+        let overhead_path = utils::gen_overhead_path(is_systemd_cgroup(&path), sid);
+
+        // Dragonball and runtime are the same process, so that the
+        // sandbox_cgroup_only is overwriten to true.
+        let sandbox_cgroup_only = if toml_config.runtime.hypervisor_name == HYPERVISOR_DRAGONBALL {
+            true
+        } else {
+            toml_config.runtime.sandbox_cgroup_only
+        };
+
         Ok(Self {
             path,
             overhead_path,
-            sandbox_cgroup_only: toml_config.runtime.sandbox_cgroup_only,
+            sandbox_cgroup_only,
         })
     }
-}
 
-pub struct CgroupsResource {
-    resources: Arc<RwLock<HashMap<String, Resources>>>,
-    cgroup_manager: Cgroup,
-    overhead_cgroup_manager: Option<Cgroup>,
-    cgroup_config: CgroupConfig,
-}
-
-impl CgroupsResource {
-    pub fn new(sid: &str, toml_config: &TomlConfig) -> Result<Self> {
-        let config = CgroupConfig::new(sid, toml_config)?;
-
-        // Create the sandbox cgroups manager (cgroups on Linux).
-        // Depending on the sandbox_cgroup_only value, this cgroup
-        // will either hold all the pod threads (sandbox_cgroup_only is true)
-        // or only the virtual CPU ones (sandbox_cgroup_only is false).
-        let hier = cgroups_rs::hierarchies::auto();
-        let cgroup_manager = CgroupBuilder::new(&config.path).build(hier)?;
-
-        // The shim configuration is requesting that we do not put all threads
-        // into the sandbox resource controller.
-        // We're creating an overhead controller, with no constraints. Everything but
-        // the vCPU threads will eventually make it there.
-        let overhead_cgroup_manager = if !config.sandbox_cgroup_only {
-            let hier = cgroups_rs::hierarchies::auto();
-            Some(CgroupBuilder::new(&config.overhead_path).build(hier)?)
-        } else {
-            None
-        };
-
-        // Add the runtime to the VMM sandbox resource controller
-
-        // By adding the runtime process to either the sandbox or overhead controller, we are making
-        // sure that any child process of the runtime (i.e. *all* processes serving a Kata pod)
-        // will initially live in this controller. Depending on the sandbox_cgroup_only settings, we will
-        // then move the vCPU threads between resource controllers.
-        let pid = CgroupPid { pid: 0 };
-        if let Some(manager) = overhead_cgroup_manager.as_ref() {
-            manager.add_task_by_tgid(pid).context("add task by tgid")?;
-        } else {
-            cgroup_manager
-                .add_task_by_tgid(pid)
-                .context("add task by tgid with sandbox only")?;
-        }
+    fn restore(state: &CgroupState) -> Result<Self> {
+        let path = state
+            .path
+            .as_ref()
+            .ok_or_else(|| anyhow!("cgroup path is missing in state"))?;
+        let overhead_path = state
+            .overhead_path
+            .as_ref()
+            .ok_or_else(|| anyhow!("overhead path is missing in state"))?;
 
         Ok(Self {
-            cgroup_manager,
-            resources: Arc::new(RwLock::new(HashMap::new())),
-            overhead_cgroup_manager,
-            cgroup_config: config,
-        })
-    }
-
-    /// delete will move the running processes in the cgroup_manager and
-    /// overhead_cgroup_manager to the parent and then delete the cgroups.
-    pub async fn delete(&self) -> Result<()> {
-        for cg_pid in self.cgroup_manager.procs() {
-            // For now, we can't guarantee that the process in cgroup_manager does still
-            // exist. Once it exit, we should ignore that error returned by remove_task_by_tgid
-            // to let it go.
-            if let Err(error) = self.cgroup_manager.remove_task_by_tgid(cg_pid) {
-                match error.source() {
-                    Some(err) => match err.downcast_ref::<io::Error>() {
-                        Some(e) => {
-                            if e.raw_os_error() != Some(OS_ERROR_NO_SUCH_PROCESS) {
-                                return Err(error.into());
-                            }
-                        }
-                        None => return Err(error.into()),
-                    },
-                    None => return Err(error.into()),
-                }
-            }
-        }
-
-        self.cgroup_manager
-            .delete()
-            .context("delete cgroup manager")?;
-
-        if let Some(overhead) = self.overhead_cgroup_manager.as_ref() {
-            for cg_pid in overhead.tasks() {
-                overhead.remove_task(cg_pid)?;
-            }
-            overhead.delete().context("delete overhead")?;
-        }
-
-        Ok(())
-    }
-
-    pub async fn update_cgroups(
-        &self,
-        cid: &str,
-        linux_resources: Option<&LinuxResources>,
-        op: ResourceUpdateOp,
-        h: &dyn Hypervisor,
-    ) -> Result<()> {
-        let new_resources = self.calc_resource(linux_resources);
-        let old_resources = self.update_resources(cid, new_resources.clone(), op).await;
-
-        if let Some(old_resource) = old_resources.clone() {
-            if old_resource == new_resources {
-                return Ok(());
-            }
-        }
-
-        match self.do_update_cgroups(h).await {
-            Err(e) => {
-                // if update failed, we should roll back the records in resources
-                let mut resources = self.resources.write().await;
-                match op {
-                    ResourceUpdateOp::Add => {
-                        resources.remove(cid);
-                    }
-                    ResourceUpdateOp::Update | ResourceUpdateOp::Del => {
-                        if let Some(old_resource) = old_resources {
-                            resources.insert(cid.to_owned(), old_resource);
-                        }
-                    }
-                }
-                Err(e)
-            }
-            Ok(()) => Ok(()),
-        }
-    }
-
-    async fn update_resources(
-        &self,
-        cid: &str,
-        new_resource: Resources,
-        op: ResourceUpdateOp,
-    ) -> Option<Resources> {
-        let mut resources = self.resources.write().await;
-        match op {
-            ResourceUpdateOp::Add | ResourceUpdateOp::Update => {
-                resources.insert(cid.to_owned(), new_resource.clone())
-            }
-            ResourceUpdateOp::Del => resources.remove(cid),
-        }
-    }
-
-    async fn do_update_cgroups(&self, h: &dyn Hypervisor) -> Result<()> {
-        let merged_resources = self.merge_resources().await;
-        self.cgroup_manager
-            .apply(&merged_resources)
-            .map_err(|e| anyhow!(e))?;
-
-        if self.overhead_cgroup_manager.is_some() {
-            // If we have an overhead controller, new vCPU threads would start there,
-            // as being children of the VMM PID.
-            // We need to constrain them by moving them into the sandbox controller.
-            self.constrain_hypervisor(h).await?
-        }
-
-        Ok(())
-    }
-
-    /// constrain_hypervisor will place the VMM and vCPU threads into resource controllers (cgroups on Linux).
-    async fn constrain_hypervisor(&self, h: &dyn Hypervisor) -> Result<()> {
-        let tids = h.get_thread_ids().await?;
-        let tids = tids.vcpus.values();
-
-        // All vCPU threads move to the sandbox controller.
-        for tid in tids {
-            self.cgroup_manager
-                .add_task(CgroupPid { pid: *tid as u64 })?
-        }
-
-        Ok(())
-    }
-
-    async fn merge_resources(&self) -> Resources {
-        let resources = self.resources.read().await;
-
-        let mut cpu_list: HashSet<String> = HashSet::new();
-        let mut mem_list: HashSet<String> = HashSet::new();
-
-        resources.values().for_each(|r| {
-            if let Some(cpus) = &r.cpu.cpus {
-                cpu_list.insert(cpus.clone());
-            }
-            if let Some(mems) = &r.cpu.mems {
-                mem_list.insert(mems.clone());
-            }
-        });
-
-        let cpu_resource = CpuResources {
-            cpus: Some(Vec::from_iter(cpu_list.into_iter()).join(",")),
-            mems: Some(Vec::from_iter(mem_list.into_iter()).join(",")),
-            ..Default::default()
-        };
-
-        Resources {
-            cpu: cpu_resource,
-            ..Default::default()
-        }
-    }
-
-    fn calc_cpu_resources(&self, linux_resources: Option<&LinuxResources>) -> CpuResources {
-        let cpus = linux_resources
-            .and_then(|res| res.cpu().clone())
-            .and_then(|cpu| cpu.cpus().clone());
-
-        let mems = linux_resources
-            .and_then(|res| res.cpu().clone())
-            .and_then(|cpu| cpu.mems().clone());
-
-        CpuResources {
-            cpus,
-            mems,
-            ..Default::default()
-        }
-    }
-
-    fn calc_resource(&self, linux_resources: Option<&LinuxResources>) -> Resources {
-        Resources {
-            cpu: self.calc_cpu_resources(linux_resources),
-            ..Default::default()
-        }
-    }
-}
-
-#[async_trait]
-impl Persist for CgroupsResource {
-    type State = CgroupState;
-    type ConstructorArgs = CgroupArgs;
-    /// Save a state of the component.
-    async fn save(&self) -> Result<Self::State> {
-        Ok(CgroupState {
-            path: Some(self.cgroup_config.path.clone()),
-            overhead_path: Some(self.cgroup_config.overhead_path.clone()),
-            sandbox_cgroup_only: self.cgroup_config.sandbox_cgroup_only,
-        })
-    }
-    /// Restore a component from a specified state.
-    async fn restore(
-        cgroup_args: Self::ConstructorArgs,
-        cgroup_state: Self::State,
-    ) -> Result<Self> {
-        let hier = cgroups_rs::hierarchies::auto();
-        let config = CgroupConfig::new(&cgroup_args.sid, &cgroup_args.config)?;
-        let path = cgroup_state.path.unwrap_or_default();
-        let cgroup_manager = Cgroup::load(hier, path.as_str());
-        Ok(Self {
-            cgroup_manager,
-            resources: Arc::new(RwLock::new(HashMap::new())),
-            overhead_cgroup_manager: None,
-            cgroup_config: config,
+            path: path.clone(),
+            overhead_path: overhead_path.clone(),
+            sandbox_cgroup_only: state.sandbox_cgroup_only,
         })
     }
 }
---
+++ src/runtime-rs/crates/resource/src/cgroups/resource_inner.rs
@@ -0,0 +1,298 @@
+// Copyright (c) 2019-2022 Alibaba Cloud
+// Copyright (c) 2019-2025 Ant Group
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+use std::collections::{HashMap, HashSet};
+use std::process;
+
+use anyhow::{anyhow, Context, Result};
+use cgroups_rs::manager::is_systemd_cgroup;
+use cgroups_rs::{CgroupPid, FsManager, Manager, SystemdManager};
+use hypervisor::Hypervisor;
+use oci_spec::runtime::{LinuxCpu, LinuxCpuBuilder, LinuxResources, LinuxResourcesBuilder};
+
+use crate::cgroups::utils::get_tgid_from_pid;
+use crate::cgroups::CgroupConfig;
+use crate::ResourceUpdateOp;
+
+pub type CgroupManager = Box<dyn Manager>;
+
+pub(crate) struct CgroupsResourceInner {
+    /// Container resources, key is container id, and value is resources.
+    resources: HashMap<String, LinuxResources>,
+    sandbox_cgroup: CgroupManager,
+    overhead_cgroup: Option<CgroupManager>,
+}
+
+impl CgroupsResourceInner {
+    /// Create cgroup managers according to the cgroup configuration.
+    ///
+    /// # Returns
+    ///
+    /// - `Ok((CgroupManager, Option<CgroupManager>))`: A tuple containing
+    ///   the sandbox cgroup manager and an optional overhead cgroup
+    ///   manager.
+    fn new_cgroup_managers(
+        config: &CgroupConfig,
+    ) -> Result<(CgroupManager, Option<CgroupManager>)> {
+        let use_systemd = is_systemd_cgroup(&config.path);
+        let sandbox_cgroup = if use_systemd {
+            let mut manager = SystemdManager::new(&config.path).context("new systemd manager")?;
+            // Set SIGTERM timeout to 5mins, so that the runtime has up to
+            // 5mins to do graceful shutdown. Exceeding this timeout, the
+            // systemd will forcibly kill the runtime by sending SIGKILL.
+            manager.set_term_timeout(300).context("set term timeout")?;
+            Box::new(manager) as Box<dyn Manager>
+        } else {
+            let manager = FsManager::new(&config.path).context("new fs manager")?;
+            Box::new(manager) as Box<dyn Manager>
+        };
+
+        let overhead_cgroup = if config.sandbox_cgroup_only {
+            None
+        } else if use_systemd {
+            let mut manager = SystemdManager::new(&config.overhead_path)
+                .context("new systemd manager for overhead")?;
+            manager
+                .set_term_timeout(300)
+                .context("set term timeout for overhead")?;
+            Some(Box::new(manager) as Box<dyn Manager>)
+        } else {
+            let manager =
+                FsManager::new(&config.overhead_path).context("new fs manager for overhead")?;
+            Some(Box::new(manager) as Box<dyn Manager>)
+        };
+
+        Ok((sandbox_cgroup, overhead_cgroup))
+    }
+
+    /// Create a new `CgroupsResourceInner` instance.
+    pub(crate) fn new(config: &CgroupConfig) -> Result<Self> {
+        let (mut sandbox_cgroup, mut overhead_cgroup) =
+            Self::new_cgroup_managers(config).context("create new cgroups")?;
+
+        // The runtime is prioritized to be added to the overhead cgroup.
+        let pid = CgroupPid::from(process::id() as u64);
+        if let Some(overhead_cgroup) = overhead_cgroup.as_mut() {
+            overhead_cgroup
+                .add_proc(pid)
+                .context("add runtime to overhead cgroup")?;
+        } else {
+            sandbox_cgroup
+                .add_proc(pid)
+                .context("add runtime to sandbox cgroup")?;
+        }
+
+        Ok(Self {
+            resources: HashMap::new(),
+            sandbox_cgroup,
+            overhead_cgroup,
+        })
+    }
+
+    pub(crate) fn restore(config: &CgroupConfig) -> Result<Self> {
+        let (sandbox_cgroup, overhead_cgroup) =
+            Self::new_cgroup_managers(config).context("restore cgroups")?;
+
+        Ok(Self {
+            resources: HashMap::new(),
+            sandbox_cgroup,
+            overhead_cgroup,
+        })
+    }
+}
+
+impl CgroupsResourceInner {
+    /// Add cpuset resources of all containers to the sandbox cgroup.
+    fn collect_resources(&self) -> Result<LinuxResources> {
+        let mut cpu_cpus = HashSet::new();
+        let mut cpu_mems = HashSet::new();
+
+        for res in self.resources.values() {
+            if let Some(cpu) = res.cpu() {
+                if let Some(cpus) = cpu.cpus() {
+                    cpu_cpus.insert(cpus.to_string());
+                }
+                if let Some(mems) = cpu.mems() {
+                    cpu_mems.insert(mems.to_string());
+                }
+            }
+        }
+
+        let mut resources_builder = LinuxResourcesBuilder::default();
+
+        let mut cpu_builder = LinuxCpuBuilder::default();
+        if !cpu_cpus.is_empty() {
+            cpu_builder = cpu_builder.cpus(cpu_cpus.into_iter().collect::<Vec<_>>().join(","));
+        }
+        if !cpu_mems.is_empty() {
+            cpu_builder = cpu_builder.mems(cpu_mems.into_iter().collect::<Vec<_>>().join(","));
+        }
+        let cpu = cpu_builder.build().context("build linux cpu")?;
+        if cpu != LinuxCpu::default() {
+            resources_builder = resources_builder.cpu(cpu);
+        }
+
+        let resources = resources_builder.build().context("build linux resources")?;
+
+        Ok(resources)
+    }
+
+    async fn move_vcpus_to_sandbox_cgroup(&mut self, hypervisor: &dyn Hypervisor) -> Result<usize> {
+        let hv_pids = hypervisor.get_thread_ids().await?;
+        let mut pids = hv_pids.vcpus.values();
+
+        // Use threaded mode only in cgroup v1 + cgroupfs
+        if !self.sandbox_cgroup.systemd() && !self.sandbox_cgroup.v2() {
+            for pid in pids {
+                let pid = CgroupPid::from(*pid as u64);
+                self.sandbox_cgroup
+                    .add_thread(pid)
+                    .with_context(|| format!("add vcpu pid {}", pid.pid))?
+            }
+        } else {
+            // No vCPU, exits early
+            let vcpu = match pids.next() {
+                Some(pid) => *pid,
+                None => return Ok(0),
+            };
+
+            let tgid = get_tgid_from_pid(vcpu as i32).context("get tgid from vCPU thread")? as u64;
+            self.sandbox_cgroup
+                .add_proc(CgroupPid::from(tgid))
+                .with_context(|| format!("add vcpu tgid {}", tgid))?;
+        }
+
+        Ok(hv_pids.vcpus.len())
+    }
+
+    async fn update_sandbox_cgroups(&mut self, hypervisor: &dyn Hypervisor) -> Result<bool> {
+        // The runtime is under overhead cgroup if available. The
+        // hypervisor as a child of the runtime is under the overhead
+        // cgroup by default. We should move VMM process/vCPU threads to
+        // the sandbox cgroup to prevent them from consuming excessive
+        // resources.
+        if self.overhead_cgroup.is_some() {
+            let vcpu_num = self
+                .move_vcpus_to_sandbox_cgroup(hypervisor)
+                .await
+                .context("move vcpus to sandbox cgroup")?;
+            // The cgroup managers will not create cgroups if no processes
+            // are added to it. `vcpu_num == 0` reflects that the
+            // hypervisor hasn't been started yet. We skip resource
+            // setting, as the sandbox cgroup might not be created yet.
+            if vcpu_num == 0 {
+                return Ok(false);
+            }
+        }
+
+        let sandbox_resources = self.collect_resources().context("collect resources")?;
+        self.sandbox_cgroup.set(&sandbox_resources).context("set")?;
+
+        Ok(true)
+    }
+}
+
+impl CgroupsResourceInner {
+    pub(crate) async fn delete(&mut self) -> Result<()> {
+        self.sandbox_cgroup
+            .destroy()
+            .context("destroy sandbox cgroup")?;
+
+        if let Some(overhead_cgroup) = self.overhead_cgroup.as_mut() {
+            overhead_cgroup
+                .destroy()
+                .context("destroy overhead cgroup")?;
+        }
+
+        Ok(())
+    }
+
+    pub(crate) async fn update(
+        &mut self,
+        cid: &str,
+        resources: Option<&LinuxResources>,
+        op: ResourceUpdateOp,
+        hypervisor: &dyn Hypervisor,
+    ) -> Result<()> {
+        let old = match op {
+            ResourceUpdateOp::Add | ResourceUpdateOp::Update => {
+                let resources = resources.ok_or_else(|| {
+                    anyhow::anyhow!("resources should not be empty for Add or Update operation")
+                })?;
+                let new = new_cpuset_resources(resources).context("new cpuset resources")?;
+                let old = self.resources.insert(cid.to_string(), new.clone());
+                // If the new resources are the same as the old ones, we
+                // can skip the update.
+                if let Some(old) = old.as_ref() {
+                    if old == &new {
+                        return Ok(());
+                    }
+                }
+                old
+            }
+            ResourceUpdateOp::Del => self.resources.remove(cid),
+        };
+
+        let ret = self
+            .update_sandbox_cgroups(hypervisor)
+            .await
+            .context("update sandbox cgroups");
+
+        // Rollback if the update fails
+        if ret.is_err() {
+            match op {
+                ResourceUpdateOp::Add => {
+                    self.resources.remove(cid);
+                }
+                ResourceUpdateOp::Update | ResourceUpdateOp::Del => {
+                    if let Some(old) = old {
+                        self.resources.insert(cid.to_string(), old);
+                    }
+                }
+            }
+        }
+
+        ret.map(|_| ())
+    }
+
+    pub(crate) async fn setup_after_start_vm(&mut self, hypervisor: &dyn Hypervisor) -> Result<()> {
+        let updated = self
+            .update_sandbox_cgroups(hypervisor)
+            .await
+            .context("update sandbox cgroups after start vm")?;
+
+        // There is an overhead cgroup and we are falling to move the vCPUs
+        // to the sandbox cgroup, it results in those threads being under
+        // the overhead cgroup, and allowing them to consume more resources
+        // than we have allocated for the sandbox.
+        if self.overhead_cgroup.is_some() && !updated {
+            return Err(anyhow!("hypervisor cannot be moved to sandbox cgroup"));
+        }
+
+        Ok(())
+    }
+}
+
+/// Copy cpu.cpus and cpu.mems from the given resources to new resources.
+fn new_cpuset_resources(resources: &LinuxResources) -> Result<LinuxResources> {
+    let cpu = resources.cpu();
+    let cpus = cpu.as_ref().and_then(|c| c.cpus().clone());
+    let mems = cpu.as_ref().and_then(|c| c.mems().clone());
+
+    let mut builder = LinuxCpuBuilder::default();
+    if let Some(cpus) = cpus {
+        builder = builder.cpus(cpus);
+    }
+    if let Some(mems) = mems {
+        builder = builder.mems(mems);
+    }
+    let linux_cpu = builder.build().context("build linux cpu")?;
+
+    let builder = LinuxResourcesBuilder::default().cpu(linux_cpu);
+    let resources = builder.build().context("build linux resources")?;
+
+    Ok(resources)
+}
