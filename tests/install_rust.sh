63515242c56ba563f0ef767979261d3fdfd454d2
Markus Rudy <mr@edgeless.systems>
2025-07-30 12:24:06 +0200

    tests: fix shellcheck findings in install_rust.sh
    Fixing the shellcheck issues first so that they are not coupled to the
    subsequent commit introducing Darwin support to the script.
    
    Signed-off-by: Markus Rudy <mr@edgeless.systems>

--- tests/install_rust.sh
+++ tests/install_rust.sh
@@ -9,21 +9,20 @@ set -o nounset
 set -o pipefail
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-script_name="$(basename "${BASH_SOURCE[0]}")"
 
 source "${script_dir}/common.bash"
 
 rustarch=$(arch_to_rust)
 
 version="${1:-""}"
-if [ -z "${version}" ]; then
+if [[ -z "${version}" ]]; then
 	version=$(get_from_kata_deps ".languages.rust.meta.newest-version")
 fi
 
 echo "Install rust ${version}"
 
 if ! command -v rustup > /dev/null; then
-	curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain ${version}
+	curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain "${version}"
 fi
 
 export PATH="${PATH}:${HOME}/.cargo/bin"
@@ -33,13 +32,13 @@ export PATH="${PATH}:${HOME}/.cargo/bin"
 ## with a different version toolchain.
 ## Even though the target version toolchain has been installed,
 ## this command will not take too long to run.
-rustup toolchain install ${version}
-rustup default ${version}
-if [ "${rustarch}" == "x86_64" ] || [ "${rustarch}" == "aarch64" ] ; then
-	rustup target add ${rustarch}-unknown-linux-musl
-	$([ "$(whoami)" != "root" ] && echo sudo) ln -sf /usr/bin/g++ /bin/musl-g++
+rustup toolchain install "${version}"
+rustup default "${version}"
+if [[ "${rustarch}" == "x86_64" || "${rustarch}" == "aarch64" ]] ; then
+	rustup target add "${rustarch}-unknown-linux-musl"
+	$([[ "$(whoami)" != "root" ]] && echo sudo) ln -sf /usr/bin/g++ /bin/musl-g++
 else
-	rustup target add ${rustarch}-unknown-linux-gnu
+	rustup target add "${rustarch}-unknown-linux-gnu"
 fi
 rustup component add rustfmt
 rustup component add clippy
