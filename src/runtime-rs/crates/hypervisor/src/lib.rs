c80ddd3fd9b986de65267dae33c570e28a9f3c82
Fupan Li <fupan.lfp@antgroup.com>
2025-08-25 16:21:09 +0800

    runtime-rs: make virtio-blk use the pci bus as default
    Since Dragonball's MMIO bus only supports legacy interrupts, while
    the PCI bus supports MSIX interrupts, to improve performance for block
    devices, virtio-blk devices are set to PCI bus mode by default.
    
    We had tested the virtio-blk's performance using the fio with the
    following commands:
    
    fio -filename=./test  -direct=1 -iodepth 32 -thread -rw=randrw
    -rwmixread=50 -ioengine=libaio -bs=4k -size=10G -numjobs=4
    -group_reporting -name=mytest
    
    When used the legacy interrupt, the io test is as below:
    
    read : io=20485MB, bw=195162KB/s, iops=48790, runt=107485msec
    write: io=20475MB, bw=195061KB/s, iops=48765, runt=107485msec
    
    Once switched to msix innterrupt, the io test is as below:
    
    read : io=20485MB, bw=260862KB/s, iops=65215, runt= 80414msec
    write: io=20475MB, bw=260727KB/s, iops=65181, runt= 80414msec
    
    We can get 34% performance improvement.
    
    Signed-off-by: Fupan Li <fupan.lfp@antgroup.com>

--- src/runtime-rs/crates/hypervisor/src/lib.rs
+++ src/runtime-rs/crates/hypervisor/src/lib.rs
@@ -69,6 +69,9 @@ pub const HYPERVISOR_REMOTE: &str = "remote";
 pub const DEFAULT_HYBRID_VSOCK_NAME: &str = "kata.hvsock";
 pub const JAILER_ROOT: &str = "root";
 
+/// default hotplug timeout
+const DEFAULT_HOTPLUG_TIMEOUT: u64 = 250;
+
 #[derive(PartialEq, Debug, Clone)]
 pub(crate) enum VmmState {
     NotReady,
