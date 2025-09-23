6fd95916b3b6ec46f743976f5db2142cdf7d150c
LandonTClipp <11232769+LandonTClipp@users.noreply.github.com>
2025-09-23 11:20:04 -0600

    fix

--- tools/osbuilder/dockerfiles/HGX/run.sh
+++ tools/osbuilder/dockerfiles/HGX/run.sh
@@ -13,7 +13,7 @@ export GOPATH=~/src/go
 export PATH=${PATH}:/usr/local/go/bin:${GOPATH}/bin
 
 kata_repo=github.com/kata-containers/kata-containers
-kata_repo_path=/${kata_repo}
+kata_repo_path=/
 
 log_header() {
 	/bin/echo -e "\n\e[1;42m${1}\e[0m"
