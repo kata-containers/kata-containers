53970ca8e404f43bda4c86a5f26b51283bcbbe0f
Joji Mekkattuparamban <jojim@nvidia.com>
2025-11-05 15:06:36 -0800

    shim: Add proto files for ttrpc handshake with device plugin
    Device cold plug is not possible with kata and Kubernetes currently because the upper layers (i.e. containerd and Kubelet) do not pass the consolidated device resource information to the kata-shim during Sandbox creation. There have been efforts to incorporate this into the CRI, but it is stalled for various reasons. While incorporating cold plug support into CRI and higher runtimes such as containerd is the desired approach, Confidential Containers require the cold plug support in order to work correctly with allocatable confidential devices.
    
    Signed-off-by: Joji Mekkattuparamban <jojim@nvidia.com>

--- src/runtime/hack/update-generated-runtime-proto.sh
+++ src/runtime/hack/update-generated-runtime-proto.sh
@@ -5,11 +5,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-CACHE_PATH="protocols/cache"
+set -o errexit -o pipefail -o nounset
 
-protoc \
-    -I=$GOPATH/src \
-    --proto_path=$CACHE_PATH \
-    --go_out=$CACHE_PATH \
-    --go-grpc_out=$CACHE_PATH \
-    $CACHE_PATH/cache.proto
+# Define the root directory for all proto files
+BASEDIR="$(dirname "$0")"
+cd ${BASEDIR}/..
+BASEPATH=`pwd`
+
+declare -A proto_files_dict
+proto_files_dict['protocols/cdiresolver/cdiresolver.proto']="ttrpc"
+proto_files_dict['protocols/cache/cache.proto']="grpc"
+
+for f in "${!proto_files_dict[@]}"; do
+	echo -e "\n   [golang] compiling ${f} ..."
+	PROTOPATH=$(dirname ${f})
+	PROTO="${proto_files_dict[${f}]}"
+	protoc \
+		-I="${PROTOPATH}":"${BASEPATH}/vendor" \
+		--go_out=paths=source_relative:${PROTOPATH} \
+		--go-${PROTO}_out=paths=source_relative:${PROTOPATH} \
+		${f}
+	echo -e "   [golang] ${f} compiled\n"
+done
