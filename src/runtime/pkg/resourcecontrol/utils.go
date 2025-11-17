e8eea1d23c9355e3881d90881b21260ae2634203
Alex Lyn <alex.lyn@antgroup.com>
2025-11-17 15:47:15 +0800

    runtime: Make csi-directvolume-kata works well within runtime
    As csi-directvolume-kata will use raw file on the host as the
    backend of a virtio-blk block device, and it is treated as just a
    regular file, which cause runtime label it unsupported device type.
    To correct such case, this patch supports it.
    
    Signed-off-by: Alex Lyn <alex.lyn@antgroup.com>

--- src/runtime/pkg/resourcecontrol/utils.go
+++ src/runtime/pkg/resourcecontrol/utils.go
@@ -16,7 +16,9 @@ import (
 )
 
 var (
-	ErrCgroupMode = errors.New("cgroup controller type error")
+	ErrCgroupMode      = errors.New("cgroup controller type error")
+	VirtualDeviceMajor = 254
+	VirtualDeviceMinor = 254
 )
 
 func DeviceToCgroupDeviceRule(device string) (*devices.Rule, error) {
@@ -35,16 +37,19 @@ func DeviceToCgroupDeviceRule(device string) (*devices.Rule, error) {
 	switch devType {
 	case unix.S_IFCHR:
 		deviceRule.Type = 'c'
-	case unix.S_IFBLK:
+		deviceRule.Major = int64(unix.Major(uint64(st.Rdev)))
+		deviceRule.Minor = int64(unix.Minor(uint64(st.Rdev)))
+	case unix.S_IFBLK: // When plugging a disk as a block device
 		deviceRule.Type = 'b'
+		deviceRule.Major = int64(unix.Major(uint64(st.Rdev)))
+		deviceRule.Minor = int64(unix.Minor(uint64(st.Rdev)))
+	case unix.S_IFREG: // When plugging a disk as a raw image file
+		deviceRule.Type = 'b'
+		deviceRule.Major = int64(VirtualDeviceMajor)
+		deviceRule.Minor = int64(VirtualDeviceMinor)
 	default:
-		return nil, fmt.Errorf("unsupported device type: %v", devType)
+		return nil, fmt.Errorf("unsupported type %v for device: %v", devType, device)
 	}
-
-	major := int64(unix.Major(uint64(st.Rdev)))
-	minor := int64(unix.Minor(uint64(st.Rdev)))
-	deviceRule.Major = major
-	deviceRule.Minor = minor
 
 	return &deviceRule, nil
 }
