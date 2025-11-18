0818c153abb069277c0d092bd922e4f13a35d375
Fabiano Fidêncio <ffidencio@nvidia.com>
2025-11-18 20:37:06 +0100

    scripts: Make runtimeclasses.sh ash-compatible
    Replace bash-specific syntax with ash-compatible alternatives:
    - Change shebang from bash to sh
    - Remove function keyword
    - Replace [[ == ]] with [ = ]
    - Replace array[@] with unquoted array variable for iteration
    
    Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>

--- tools/packaging/kata-deploy/scripts/runtimeclasses.sh
+++ tools/packaging/kata-deploy/scripts/runtimeclasses.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # Copyright (c) 2019 Intel Corporation
 #
 # SPDX-License-Identifier: Apache-2.0
@@ -8,11 +8,11 @@
 #   - yq
 #
 
-function adjust_shim_for_nfd() {
+adjust_shim_for_nfd() {
 	local shim="${1}"
 	local expand_runtime_classes_for_nfd="${2}"
 
-	if [[ "${expand_runtime_classes_for_nfd}" == "true" ]]; then
+	if [ "${expand_runtime_classes_for_nfd}" = "true" ]; then
 		file="/opt/kata-artifacts/runtimeclasses/kata-${shim}.yaml"
 		case "${shim}" in
 			*tdx*)
@@ -27,12 +27,12 @@ function adjust_shim_for_nfd() {
 	fi
 }
 
-function create_runtimeclasses() {
+create_runtimeclasses() {
 	echo "Creating the runtime classes"
 
 	local expand_runtime_classes_for_nfd="${1:-false}"
 
-	for shim in "${shims[@]}"; do
+	for shim in ${shims}; do
 		echo "Creating the kata-${shim} runtime class"
 		if [ -n "${MULTI_INSTALL_SUFFIX}" ]; then
 			sed -i -e "s|kata-${shim}|kata-${shim}-${MULTI_INSTALL_SUFFIX}|g" /opt/kata-artifacts/runtimeclasses/kata-${shim}.yaml
@@ -52,7 +52,7 @@ function create_runtimeclasses() {
 
 	done
 
-	if [[ "${CREATE_DEFAULT_RUNTIMECLASS}" == "true" ]]; then
+	if [ "${CREATE_DEFAULT_RUNTIMECLASS}" = "true" ]; then
 		if [ -n "${MULTI_INSTALL_SUFFIX}" ]; then
 			warn "CREATE_DEFAULT_RUNTIMECLASS is being ignored!"
 			warn "multi installation does not support creating a default runtime class"
@@ -68,15 +68,15 @@ function create_runtimeclasses() {
 	fi
 }
 
-function delete_runtimeclasses() {
+delete_runtimeclasses() {
 	echo "Deleting the runtime classes"
 
-	for shim in "${shims[@]}"; do
+	for shim in ${shims}; do
 		echo "Deleting the kata-${shim} runtime class"
 		canonical_shim_name="kata-${shim}"
 		shim_name="${canonical_shim_name}"
 		if [ -n "${MULTI_INSTALL_SUFFIX}" ]; then
-			shim_name+="-${MULTI_INSTALL_SUFFIX}"
+			shim_name="${shim_name}-${MULTI_INSTALL_SUFFIX}"
 			sed -i -e "s|${canonical_shim_name}|${shim_name}|g" /opt/kata-artifacts/runtimeclasses/${canonical_shim_name}.yaml
 		fi
 
@@ -84,7 +84,7 @@ function delete_runtimeclasses() {
 	done
 
 
-	if [[ "${CREATE_DEFAULT_RUNTIMECLASS}" == "true" ]]; then
+	if [ "${CREATE_DEFAULT_RUNTIMECLASS}" = "true" ]; then
 		if [ -n "${MULTI_INSTALL_SUFFIX}" ]; then
 			# There's nothing to be done here, as a default runtime class is never created
 			# for multi installations
