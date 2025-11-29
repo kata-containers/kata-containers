ae23eced119aff4d6a67ec1bef66ece43c47195b
Zvonko Kaiser <zkaiser@nvidia.com>
2025-11-29 00:03:26 +0000

    runtime-rs: Add Arm SMMUv3
    Arm SMMU is the Arm implementation  of an IOMMU, needed  for GBxxx
    NUMA systems.
    
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>

--- src/runtime-rs/crates/hypervisor/src/qemu/cmdline_generators_numa.rs
+++ src/runtime-rs/crates/hypervisor/src/qemu/cmdline_generators_numa.rs
@@ -74,30 +74,77 @@ impl ToQemuParams for PCIeExpanderBusDevice {
 /// that device
 #[derive(Debug, Default)]
 pub struct ACPIGenericInitiator {
-	id: String,
-	pci_dev: String,
-	node: u32,
+    id: String,
+    pci_dev: String,
+    node: u32,
 }
 
 impl ACPIGenericInitiator {
-	fn new(id: &str, pci_dev: &str, node: u32) -> Self {
-		ACPIGenericInitiator {
-			id: id.to_owned(),
-			pci_dev: pci_dev.to_owned(),
-			node,
-		}
-	}
+    fn new(id: &str, pci_dev: &str, node: u32) -> Self {
+        ACPIGenericInitiator {
+            id: id.to_owned(),
+            pci_dev: pci_dev.to_owned(),
+            node,
+        }
+    }
 }
 
 #[async_trait]
 impl ToQemuParams for ACPIGenericInitiator {
-	async fn qemu_params(&self) -> Result<Vec<String>> {
-		let mut device_params = Vec::new();
-		device_params.push(format!("{},id={}", "acpi-generic-initiator", self.id));
-		device_params.push(format!("pci-dev={}", self.pci_dev));
-		device_params.push(format!("node={}", self.node));
-		Ok(vec!["-object".to_owned(), device_params.join(",")])
-	}
+    async fn qemu_params(&self) -> Result<Vec<String>> {
+        let mut device_params = Vec::new();
+        device_params.push(format!("{},id={}", "acpi-generic-initiator", self.id));
+        device_params.push(format!("pci-dev={}", self.pci_dev));
+        device_params.push(format!("node={}", self.node));
+        Ok(vec!["-object".to_owned(), device_params.join(",")])
+    }
+}
+
+/// Arm SMMUv3 the IOMMU implementation for the Arm architecture
+/// needed for GBxxx NUMA architectures
+#[derive(Debug, Default)]
+pub struct ArmSMMUv3 {
+    primary_bus: String,
+    id: String,
+    accel: bool,
+    ats: bool,
+    ril: bool,
+    pasid: bool,
+    oas: u8,
+    cmdqv: bool,
+}
+
+impl ArmSMMUv3 {
+    fn new(primary_bus: &str, id: &str, cmdqv: bool) -> Self {
+        ArmSMMUv3 {
+            primary_bus: primary_bus.to_owned(),
+            id: id.to_owned(),
+            accel: true,
+            ats: true,
+            ril: true,
+            pasid: true,
+            oas: 48,
+            cmdqv,
+        }
+    }
+}
+
+#[async_trait]
+impl ToQemuParams for ArmSMMUv3 {
+    async fn qemu_params(&self) -> Result<Vec<String>> {
+        let mut device_params = Vec::new();
+        device_params.push(format!("{},primary-bus={}", "arm-smmuv3", self.primary_bus));
+        device_params.push(format!("id={}", self.id));
+        device_params.push(format!("accel={}", if self.accel { "on" } else { "off" }));
+        device_params.push(format!("ats={}", if self.ats { "on" } else { "off" }));
+        device_params.push(format!("ril={}", if self.ril { "on" } else { "off" }));
+        device_params.push(format!("pasid={}", if self.pasid { "on" } else { "off" }));
+        device_params.push(format!("oas={}", self.oas));
+        if self.cmdqv {
+            device_params.push(format!("cmdqv=on"));
+        }
+        Ok(vec!["-device".to_owned(), device_params.join(",")])
+    }
 }
 
 impl<'a> QemuCmdLine<'a> {
@@ -122,8 +169,32 @@ impl<'a> QemuCmdLine<'a> {
     }
     #[allow(dead_code)]
     pub fn add_acpi_generic_initiator(&mut self, id: &str, pci_dev: &str, node: u32) -> Result<()> {
+        let machine_type: &str = &self.config.machine_info.machine_type;
+        if !matches!(machine_type, "q35" | "virt") {
+            info!(
+                sl!(),
+                "ACPI Generic Initiator not supported for machine type: {}", machine_type
+            );
+            return Ok(());
+        }
+
         let acpi_generic_initiator = ACPIGenericInitiator::new(id, pci_dev, node);
         self.devices.push(Box::new(acpi_generic_initiator));
+
+        Ok(())
+    }
+    #[allow(dead_code)]
+    pub fn add_arm_smmuv3(&mut self, primary_bus: &str, id: &str, cmdqv: bool) -> Result<()> {
+        let machine_type: &str = &self.config.machine_info.machine_type;
+        if !matches!(machine_type, "virt") {
+            info!(
+                sl!(),
+                "Arm SMMUv3 not supported for machine type: {}", machine_type
+            );
+            return Ok(());
+        }
+        let arm_smmuv3 = ArmSMMUv3::new(primary_bus, id, cmdqv);
+        self.devices.push(Box::new(arm_smmuv3));
 
         Ok(())
     }
