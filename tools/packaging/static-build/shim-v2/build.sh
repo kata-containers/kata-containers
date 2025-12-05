1889f7b798b10af1b709db53eaf1d41fce436169
Fabiano Fidêncio <ffidencio@nvidia.com>
2025-12-05 09:14:29 +0100

    rootfs: Temporarily revert "gpu: Handle root_hash.txt correctly"
    This reverts commit e4a13b9a4ab76111ed47f65663f1edbe670cb86c, as it
    caused some issues with the GPU workflows.
    
    Reverting it is better, as it unblocks other PRs.
    
    Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>

--- tools/packaging/static-build/shim-v2/build.sh
+++ tools/packaging/static-build/shim-v2/build.sh
@@ -39,26 +39,14 @@ esac
 if [ "${MEASURED_ROOTFS}" == "yes" ]; then
 	info "Enable rootfs measurement config"
 
-	# Two VARIANTS (targets) that build a measured rootfs as of now are:
-	# - rootfs-image-confidential
-	# - rootfs-image-nvidia-gpu-confidential
-	#
-	found_any=""
-	for variant in confidential nvidia-gpu-confidential; do
-		root_hash_file="${repo_root_dir}/tools/packaging/kata-deploy/local-build/build/root_hash_${variant}.txt"
-		[ -f "$root_hash_file" ] || \
-			die "Root hash file for measured rootfs ${variant} not found at ${root_hash_file}"
+	root_hash_file="${repo_root_dir}/tools/packaging/kata-deploy/local-build/build/root_hash.txt"
 
-		found_any="yes"
+	[ -f "$root_hash_file" ] || \
+		die "Root hash file for measured rootfs not found at ${root_hash_file}"
 
-		root_hash=$(sed -e 's/Root hash:\s*//g;t;d' "${root_hash_file}")
-		root_measure_config="rootfs_verity.scheme=dm-verity rootfs_verity.hash=${root_hash}"
-
-		[ "${variant}" == "confidential" ] && EXTRA_OPTS+=" ROOTMEASURECONFIG=\"${root_measure_config}\""
-		[ "${variant}" == "nvidia-gpu-confidential" ] && EXTRA_OPTS+=" ROOTMEASURECONFIG_NV=\"${root_measure_config}\""
-
-	done
-	[ -z "${found_any}" ] && die "No root hash files found for shim-v2 with MEASURED_ROOTFS support, needs a rootfs with MEASURED_ROOTFS support"
+	root_hash=$(sed -e 's/Root hash:\s*//g;t;d' "${root_hash_file}")
+	root_measure_config="rootfs_verity.scheme=dm-verity rootfs_verity.hash=${root_hash}"
+	EXTRA_OPTS+=" ROOTMEASURECONFIG=\"${root_measure_config}\""
 fi
 
 docker pull ${container_image} || \
