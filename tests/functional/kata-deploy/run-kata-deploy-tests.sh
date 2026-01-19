cccb864d43ef3bf3374033d6ca917828ae615996
Fabiano Fidêncio <ffidencio@nvidia.com>
2026-01-19 18:26:43 +0100

    tests: Add common bats test runner function
    Add run_bats_tests() function to common.bash that provides consistent
    test execution and reporting across all test suites (k8s, nvidia,
    kata-deploy).
    
    This removes duplicated test runner code from run_kubernetes_tests.sh,
    run_kubernetes_nv_tests.sh, and run-kata-deploy-tests.sh.
    
    Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>

--- tests/functional/kata-deploy/run-kata-deploy-tests.sh
+++ tests/functional/kata-deploy/run-kata-deploy-tests.sh
@@ -6,9 +6,13 @@
 #
 
 set -e
+set -o pipefail
 
 kata_deploy_dir=$(dirname "$(readlink -f "$0")")
 source "${kata_deploy_dir}/../../common.bash"
+
+# Setting to "yes" enables fail fast, stopping execution at the first failed test.
+export BATS_TEST_FAIL_FAST="${BATS_TEST_FAIL_FAST:-no}"
 
 if [[ -n "${KATA_DEPLOY_TEST_UNION:-}" ]]; then
 	KATA_DEPLOY_TEST_UNION=("${KATA_DEPLOY_TEST_UNION}")
@@ -18,8 +22,4 @@ else
 	)
 fi
 
-info "Run tests"
-for KATA_DEPLOY_TEST_ENTRY in "${KATA_DEPLOY_TEST_UNION[@]}"
-do
-	bats --show-output-of-passing-tests "${KATA_DEPLOY_TEST_ENTRY}"
-done
+run_bats_tests "${kata_deploy_dir}" KATA_DEPLOY_TEST_UNION
