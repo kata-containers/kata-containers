29e7dd27f13458c199a2d60e61388792d22637df
tak-ka3 <takumi.hiraoka@acompany-ac.com>
2026-01-20 09:25:10 +0900

    runtime: Add -info flag support for containerd v2.0+
    Add support for the -info flag that containerd v2.0+ passes to shims.
    The flag outputs RuntimeInfo protobuf to stdout containing the shim
    name and version information.
    
    Fixes #12133
    
    Signed-off-by: tak-ka3 <takumi.hiraoka@acompany-ac.com>

--- src/runtime/cmd/containerd-shim-kata-v2/main.go
+++ src/runtime/cmd/containerd-shim-kata-v2/main.go
@@ -9,7 +9,9 @@ import (
 	"fmt"
 	"os"
 
+	containerdtypes "github.com/containerd/containerd/api/types"
 	shimapi "github.com/containerd/containerd/runtime/v2/shim"
+	"google.golang.org/protobuf/proto"
 
 	shim "github.com/kata-containers/kata-containers/src/runtime/pkg/containerd-shim-v2"
 	"github.com/kata-containers/kata-containers/src/runtime/pkg/katautils"
@@ -21,11 +23,34 @@ func shimConfig(config *shimapi.Config) {
 	config.NoSubreaper = true
 }
 
+func handleInfoFlag() {
+	info := &containerdtypes.RuntimeInfo{
+		Name: types.DefaultKataRuntimeName,
+		Version: &containerdtypes.RuntimeVersion{
+			Version:  katautils.VERSION,
+			Revision: katautils.COMMIT,
+		},
+	}
+
+	data, err := proto.Marshal(info)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to marshal RuntimeInfo: %v\n", err)
+		os.Exit(1)
+	}
+
+	os.Stdout.Write(data)
+	os.Exit(0)
+}
+
 func main() {
 
 	if len(os.Args) == 2 && os.Args[1] == "--version" {
 		fmt.Printf("%s containerd shim (Golang): id: %q, version: %s, commit: %v\n", katautils.PROJECT, types.DefaultKataRuntimeName, katautils.VERSION, katautils.COMMIT)
 		os.Exit(0)
+	}
+
+	if len(os.Args) == 2 && os.Args[1] == "-info" {
+		handleInfoFlag()
 	}
 
 	shimapi.Run(types.DefaultKataRuntimeName, shim.New, shimConfig)
