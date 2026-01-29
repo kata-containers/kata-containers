781de99ab198419f5935d43f03b9c8f6bd787748
Alex Lyn <alex.lyn@antgroup.com>
2026-01-29 11:59:09 +0800

    runtime-rs: Introduce a SNP policy field in ObjectSevSnpGuest
    A bitmask for the SNP guest policy is introduced in ObjectSevSnpGuest
    to help pass to Qemu cmdline.
    
    And defaults to 0x30000 (QEMU's default) to maintain standard behavior
    it just looks like as: "policy=0x30000"
    
    Signed-off-by: Alex Lyn <alex.lyn@antgroup.com>

--- src/runtime-rs/crates/hypervisor/src/qemu/cmdline_generator.rs
+++ src/runtime-rs/crates/hypervisor/src/qemu/cmdline_generator.rs
@@ -1863,6 +1863,7 @@ struct ObjectSevSnpGuest {
     reduced_phys_bits: u32,
     kernel_hashes: bool,
     host_data: Option<String>,
+    policy: u32,
     is_snp: bool,
 }
 
@@ -1874,6 +1875,7 @@ impl ObjectSevSnpGuest {
             reduced_phys_bits,
             kernel_hashes: true,
             host_data,
+            policy: 0x30000,
             is_snp,
         }
     }
@@ -1899,6 +1901,7 @@ impl ToQemuParams for ObjectSevSnpGuest {
                 "kernel-hashes={}",
                 if self.kernel_hashes { "on" } else { "off" }
             ));
+            params.push(format!("policy=0x{:x}", self.policy));
             if let Some(host_data) = &self.host_data {
                 params.push(format!("host-data={host_data}"))
             }
