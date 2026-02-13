7410156a3d681a935ef63fe8be1584b12ee131ee
stevenhorsman <steven@uk.ibm.com>
2026-02-13 16:24:30 +0000

    runtime: Avoid using fmt.Sprintf("%s", x)
    It's more efficient and concise to just call .String()
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- src/runtime/virtcontainers/tuntap_endpoint.go
+++ src/runtime/virtcontainers/tuntap_endpoint.go
@@ -170,7 +170,7 @@ func createTuntapNetworkEndpoint(idx int, ifName string, hwName net.HardwareAddr
 			Name: fmt.Sprintf("eth%d", idx),
 			TAPIface: NetworkInterface{
 				Name:     fmt.Sprintf("tap%d_kata", idx),
-				HardAddr: fmt.Sprintf("%s", hwName), //nolint:gosimple
+				HardAddr: hwName.String(),
 			},
 		},
 		EndpointType: TuntapEndpointType,
