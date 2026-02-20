770d00f5e106114c264b8de8ea2519dddd29b866
shwetha-s-poojary <shwetha.s-poojary@ibm.com>
2026-02-20 00:06:04 -0500

    qemu-runtime-rs: enable ppc64le support
    Add qemu-runtime-rs to the CI matrix.
    Enable Kubernetes tests on ppc64le for qemu-runtime-rs.
    Update the default shim configuration to include qemu-runtime-rs for ppc64le.
    Extend the Helm chart supportedArches list to include ppc64le.
    Enable shim-v2-rust support for ppc64le static tarball builds.
    Add shim-v2-rust to the ppc64le shim component configuration.
    Makefile changes to use initrd instead of image for ppc64le rust runtime
    Fix arch advertisement: split qemu-runtime-rs (amd64/arm64/s390x/ppc64le) from qemu-coco-dev-runtime-rs (amd64/arm64/s390x only)
    
    Signed-off-by: shwetha-s-poojary <shwetha.s-poojary@ibm.com>
    Co-authored-by: kishen-v  <kishen.viswanathan@ibm.com>

--- tests/gha-run-k8s-common.sh
+++ tests/gha-run-k8s-common.sh
@@ -765,7 +765,9 @@ function helm_helper() {
 					yq -i ".shims.${shim}.supportedArches = [\"amd64\"]" "${values_yaml}"
 				# qemu-coco-dev-runtime-rs is checked explicitly because
 				# qemu-coco-dev (Go runtime) does not support arm64.
-				elif [[ "${shim}" == "qemu-runtime-rs" ]] || [[ "${shim}" == "qemu-coco-dev-runtime-rs" ]]; then
+				elif [[ "${shim}" == "qemu-runtime-rs" ]]; then
+					yq -i ".shims.${shim}.supportedArches = [\"amd64\", \"arm64\", \"s390x\", \"ppc64le\"]" "${values_yaml}"
+				elif [[ "${shim}" == "qemu-coco-dev-runtime-rs" ]]; then
 					yq -i ".shims.${shim}.supportedArches = [\"amd64\", \"arm64\", \"s390x\"]" "${values_yaml}"
 				elif is_non_tee_hypervisor "${shim}"; then
 					yq -i ".shims.${shim}.supportedArches = [\"amd64\", \"s390x\"]" "${values_yaml}"
