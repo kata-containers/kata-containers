ca1a063a2dde4c236a246657dadfce9e684fc692
yangyubing <yangyubing@kuaishou.com>
2026-02-24 20:57:18 +0800

    runtime: ensure socket file is removed after shim deletion
    These socket files are created by kata-shim during
    StartShim and are expected to be cleaned up when
    the shim process exits normally.
    
    This commit ensures that the socket file is removed
    when `shim delete` is called, preventing resource
    leakage even if the shim exits unexpectedly.
    
    Signed-off-by: yangyubing <yangyubing@kuaishou.com>

--- src/runtime/pkg/containerd-shim-v2/delete_test.go
+++ src/runtime/pkg/containerd-shim-v2/delete_test.go
@@ -7,12 +7,17 @@
 package containerdshim
 
 import (
+	"context"
+	"os"
+	"path/filepath"
 	"testing"
 
 	taskAPI "github.com/containerd/containerd/api/runtime/task/v2"
 	"github.com/stretchr/testify/assert"
 
 	ktu "github.com/kata-containers/kata-containers/src/runtime/pkg/katatestutils"
+	vc "github.com/kata-containers/kata-containers/src/runtime/virtcontainers"
+	vcAnnotations "github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/annotations"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/compatoci"
 	"github.com/kata-containers/kata-containers/src/runtime/virtcontainers/pkg/vcmock"
 )
@@ -40,4 +45,105 @@ func TestDeleteContainerSuccessAndFail(t *testing.T) {
 	}
 	s.containers[testContainerID], err = newContainer(s, reqCreate, "", nil, true)
 	assert.NoError(err)
+}
+
+func TestCleanupPodContainerDoesNotRemoveSharedSocket(t *testing.T) {
+	assert := assert.New(t)
+
+	_, bundlePath, ociConfigFile := ktu.SetupOCIConfigFile(t)
+	spec, err := compatoci.ParseConfigJSON(bundlePath)
+	assert.NoError(err)
+
+	spec.Annotations = map[string]string{
+		vcAnnotations.ContainerTypeKey: string(vc.PodContainer),
+		vcAnnotations.SandboxIDKey:     testSandboxID,
+	}
+	err = ktu.WriteOCIConfigFile(spec, ociConfigFile)
+	assert.NoError(err)
+
+	err = os.MkdirAll(filepath.Join(bundlePath, "rootfs"), testFileMode)
+	assert.NoError(err)
+
+	socketPath := filepath.Join(t.TempDir(), "shared.sock")
+	err = os.WriteFile(socketPath, nil, testFileMode)
+	assert.NoError(err)
+
+	err = os.WriteFile(filepath.Join(bundlePath, "address"), []byte("unix://"+socketPath), testFileMode)
+	assert.NoError(err)
+
+	oldWd, err := os.Getwd()
+	assert.NoError(err)
+	defer func() {
+		_ = os.Chdir(oldWd)
+	}()
+	err = os.Chdir(bundlePath)
+	assert.NoError(err)
+
+	testingImpl.CleanupContainerFunc = func(ctx context.Context, sandboxID, containerID string, force bool) error {
+		return nil
+	}
+	defer func() {
+		testingImpl.CleanupContainerFunc = nil
+	}()
+
+	s := &service{
+		id:      testContainerID,
+		rootCtx: context.Background(),
+	}
+
+	_, err = s.Cleanup(context.Background())
+	assert.NoError(err)
+
+	_, err = os.Stat(socketPath)
+	assert.NoError(err)
+}
+
+func TestCleanupSandboxRemovesSocket(t *testing.T) {
+	assert := assert.New(t)
+
+	_, bundlePath, ociConfigFile := ktu.SetupOCIConfigFile(t)
+	spec, err := compatoci.ParseConfigJSON(bundlePath)
+	assert.NoError(err)
+
+	spec.Annotations = map[string]string{
+		vcAnnotations.ContainerTypeKey: string(vc.PodSandbox),
+	}
+	err = ktu.WriteOCIConfigFile(spec, ociConfigFile)
+	assert.NoError(err)
+
+	err = os.MkdirAll(filepath.Join(bundlePath, "rootfs"), testFileMode)
+	assert.NoError(err)
+
+	socketPath := filepath.Join(t.TempDir(), "sandbox.sock")
+	err = os.WriteFile(socketPath, nil, testFileMode)
+	assert.NoError(err)
+
+	err = os.WriteFile(filepath.Join(bundlePath, "address"), []byte("unix://"+socketPath), testFileMode)
+	assert.NoError(err)
+
+	oldWd, err := os.Getwd()
+	assert.NoError(err)
+	defer func() {
+		_ = os.Chdir(oldWd)
+	}()
+	err = os.Chdir(bundlePath)
+	assert.NoError(err)
+
+	testingImpl.CleanupContainerFunc = func(ctx context.Context, sandboxID, containerID string, force bool) error {
+		return nil
+	}
+	defer func() {
+		testingImpl.CleanupContainerFunc = nil
+	}()
+
+	s := &service{
+		id:      testSandboxID,
+		rootCtx: context.Background(),
+	}
+
+	_, err = s.Cleanup(context.Background())
+	assert.NoError(err)
+
+	_, err = os.Stat(socketPath)
+	assert.True(os.IsNotExist(err))
 }
