66e58d649091ceec03dea52161853f3b12fae518
stevenhorsman <steven@uk.ibm.com>
2026-02-26 16:36:59 +0000

    tests: Delete install_go.sh
    Having a script to install go is legacy from Jenkins, so
    delete it, so there is less code in our repo.
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- tests/integration/cri-containerd/gha-run.sh
+++ tests/integration/cri-containerd/gha-run.sh
@@ -45,9 +45,6 @@ function install_dependencies() {
 	sudo apt-get update
 	sudo apt-get -y install "${system_deps[@]}"
 
-	ensure_yq
-	"${repo_root_dir}/tests/install_go.sh" -p -f
-
 	# Dependency list of projects that we can install them
 	# directly from their releases on GitHub:
 	# - containerd
