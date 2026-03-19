9d4fcd37088e85c787457b955ea75a13cc1eee91
Aurélien Bombo <abombo@microsoft.com>
2026-03-19 16:53:47 -0500

    runtime-rs: Use fully qualified NixPath::is_empty()
    This avoids issues like below which are now errors in Rust 1.94:
    
    error: a method with this name may be added to the standard library in the future
       --> src/libs/kata-sys-util/src/mount.rs:265:12
        |
    265 |     if src.is_empty() {
        |            ^^^^^^^^
        |
        = warning: once this associated item is added to the standard library, the ambiguity may cause an error or change in behavior!
        = note: for more information, see issue #48919 <https://github.com/rust-lang/rust/issues/48919>
        = help: call with fully qualified syntax `nix::NixPath::is_empty(...)` to keep using the current method
    
    Signed-off-by: Aurélien Bombo <abombo@microsoft.com>

--- src/libs/kata-sys-util/src/mount.rs
+++ src/libs/kata-sys-util/src/mount.rs
@@ -225,7 +225,7 @@ pub fn create_mount_destination<S: AsRef<Path>, D: AsRef<Path>, R: AsRef<Path>>(
 /// Caller needs to ensure safety of the `dst` to avoid possible file path based attacks.
 pub fn bind_remount<P: AsRef<Path>>(dst: P, readonly: bool) -> Result<()> {
     let dst = dst.as_ref();
-    if dst.is_empty() {
+    if NixPath::is_empty(dst) {
         return Err(Error::NullMountPointPath);
     }
     let dst = dst
@@ -262,10 +262,10 @@ pub fn bind_mount_unchecked<S: AsRef<Path>, D: AsRef<Path>>(
 
     let src = src.as_ref();
     let dst = dst.as_ref();
-    if src.is_empty() {
+    if NixPath::is_empty(src) {
         return Err(Error::NullMountPointPath);
     }
-    if dst.is_empty() {
+    if NixPath::is_empty(dst) {
         return Err(Error::NullMountPointPath);
     }
     let abs_src = src
@@ -760,7 +760,7 @@ pub fn umount_timeout<P: AsRef<Path>>(path: P, timeout: u64) -> Result<()> {
 /// # Safety
 /// Caller needs to ensure safety of the `path` to avoid possible file path based attacks.
 pub fn umount_all<P: AsRef<Path>>(mountpoint: P, lazy_umount: bool) -> Result<()> {
-    if mountpoint.as_ref().is_empty() || !mountpoint.as_ref().exists() {
+    if NixPath::is_empty(mountpoint.as_ref()) || !mountpoint.as_ref().exists() {
         return Ok(());
     }
 
