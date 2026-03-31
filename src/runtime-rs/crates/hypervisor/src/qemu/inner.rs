f030f10fd174587c63a5f3e68e58b1d5360b4d76
Alex Lyn <alex.lyn@antgroup.com>
2026-03-31 17:17:30 +0800

    runtime-rs: Add format param to hotplug_block_device
    Add format parameter to hotplug_block_device for VMDK support.
    Implement VMDK format handling in QMP blockdev-add.
    
    Signed-off-by: Alex Lyn <alex.lyn@antgroup.com>

--- src/runtime-rs/crates/hypervisor/src/qemu/inner.rs
+++ src/runtime-rs/crates/hypervisor/src/qemu/inner.rs
@@ -689,14 +689,14 @@ impl QemuInner {
 
         let is_unaligned = !new_hotplugged_mem.is_multiple_of(guest_mem_block_size);
         if is_unaligned {
-            new_hotplugged_mem = ch_config::convert::checked_next_multiple_of(
-                new_hotplugged_mem,
-                guest_mem_block_size,
-            )
-            .ok_or(anyhow!(format!(
-                "alignment of {} B to the block size of {} B failed",
-                new_hotplugged_mem, guest_mem_block_size
-            )))?
+            new_hotplugged_mem = new_hotplugged_mem
+                .checked_add(guest_mem_block_size - 1)
+                .and_then(|v| v.checked_div(guest_mem_block_size))
+                .and_then(|v| v.checked_mul(guest_mem_block_size))
+                .ok_or(anyhow!(format!(
+                    "alignment of {} B to the block size of {} B failed",
+                    new_hotplugged_mem, guest_mem_block_size
+                )))?;
         }
         let new_hotplugged_mem = new_hotplugged_mem;
 
@@ -859,6 +859,7 @@ impl QemuInner {
                         block_device.config.is_direct,
                         block_device.config.is_readonly,
                         block_device.config.no_drop,
+                        &format!("{}", block_device.config.format),
                     )
                     .context("hotplug block device")?;
 
