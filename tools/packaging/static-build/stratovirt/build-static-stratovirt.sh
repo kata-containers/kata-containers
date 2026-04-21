a4ed797756f100631c1b60cfde393d8ddd095dc7
Fabiano Fidêncio <ffidencio@nvidia.com>
2026-04-21 18:03:29 +0200

    tools: Fix shellcheck issues in build-static-stratovirt.sh
    Fix shellcheck warnings and notes identified by running
    shellcheck --severity=style.
    
    Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>

--- tools/packaging/static-build/stratovirt/build-static-stratovirt.sh
+++ tools/packaging/static-build/stratovirt/build-static-stratovirt.sh
@@ -11,28 +11,28 @@ set -o pipefail
 ARCH=$(uname -m)
 
 # Currently, StratoVirt only support x86_64 and aarch64.
-[ "${ARCH}" != "x86_64" ] && [ "${ARCH}" != "aarch64" ] && exit
+[[ "${ARCH}" != "x86_64" ]] && [[ "${ARCH}" != "aarch64" ]] && exit
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# shellcheck source=/dev/null
 source "${script_dir}/../../scripts/lib.sh"
 
 info "Get stratovirt information from runtime versions.yaml"
 stratovirt_url="${stratovirt_url:-}"
-[ -n "$stratovirt_url" ] || stratovirt_url=$(get_from_kata_deps ".assets.hypervisor.stratovirt.url")
-[ -n "$stratovirt_url" ] || die "failed to get stratovirt url"
+[[ -n "${stratovirt_url}" ]] || stratovirt_url=$(get_from_kata_deps ".assets.hypervisor.stratovirt.url")
+[[ -n "${stratovirt_url}" ]] || die "failed to get stratovirt url"
 
 stratovirt_version="${stratovirt_version:-}"
-[ -n "$stratovirt_version" ] || stratovirt_version=$(get_from_kata_deps ".assets.hypervisor.stratovirt.version")
-[ -n "$stratovirt_version" ] || die "failed to get stratovirt version"
+[[ -n "${stratovirt_version}" ]] || stratovirt_version=$(get_from_kata_deps ".assets.hypervisor.stratovirt.version")
+[[ -n "${stratovirt_version}" ]] || die "failed to get stratovirt version"
 
 pull_stratovirt_released_binary() {
 	file_name="stratovirt-static-${stratovirt_version##*v}-${ARCH}"
 	download_url="${stratovirt_url}/releases/download/${stratovirt_version}/${file_name}.tar.gz"
 
-	curl -L ${download_url} -o ${file_name}.tar.gz
+	curl -L "${download_url}" -o "${file_name}.tar.gz"
 	mkdir -p static-stratovirt
-	tar zxvf ${file_name}.tar.gz -C static-stratovirt
+	tar zxvf "${file_name}.tar.gz" -C static-stratovirt
 }
 
 pull_stratovirt_released_binary
-
