ea27e2f021da9c4815d802deb6090c5904bc95b6
Fabiano Fidêncio <ffidencio@nvidia.com>
2026-04-21 18:38:03 +0200

    tools: Fix shellcheck issues in install_oras.sh
    Fix shellcheck warnings and notes identified by running
    shellcheck --severity=style.
    
    Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>

--- tools/packaging/kata-deploy/local-build/dockerbuild/install_oras.sh
+++ tools/packaging/kata-deploy/local-build/dockerbuild/install_oras.sh
@@ -12,7 +12,7 @@ set -o pipefail
 install_dest="/usr/local/bin"
 
 function get_installed_oras_version() {
-	oras version | grep Version | sed -e s/Version:// | tr -d [:blank:]
+	oras version | grep Version | sed -e s/Version:// | tr -d '[:blank:]'
 }
 
 oras_required_version="v1.2.0"
@@ -24,20 +24,20 @@ if command -v oras; then
 
 	echo "Proceeding to cleanup the previous installed version of ORAS, and install the version specified in the versions.yaml file"
 	oras_system_path=$(which oras)
-	sudo rm -f ${oras_system_path}
+	sudo rm -f "${oras_system_path}"
 fi
 
 arch=$(uname -m)
-if [ "${arch}" = "x86_64" ]; then
+if [[ "${arch}" = "x86_64" ]]; then
 	arch="amd64"
 fi
-if [ "${arch}" = "aarch64" ]; then
+if [[ "${arch}" = "aarch64" ]]; then
 	arch="arm64"
 fi
 oras_tarball="oras_${oras_required_version#v}_linux_${arch}.tar.gz"
 
 echo "Downloading ORAS ${oras_required_version}"
-curl -OL https://github.com/oras-project/oras/releases/download/${oras_required_version}/${oras_tarball}
+curl -OL "https://github.com/oras-project/oras/releases/download/${oras_required_version}/${oras_tarball}"
 
 echo "Installing ORAS to ${install_dest}"
 sudo mkdir -p "${install_dest}"
