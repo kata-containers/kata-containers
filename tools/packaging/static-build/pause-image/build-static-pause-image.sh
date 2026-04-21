ee5cd4920dd517ed1992ca7df2c9aedbe084bd2f
Fabiano Fidêncio <ffidencio@nvidia.com>
2026-04-21 18:03:29 +0200

    tools: Fix shellcheck issues in build-static-pause-image.sh
    Fix shellcheck warnings and notes identified by running
    shellcheck --severity=style.
    
    Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>

--- tools/packaging/static-build/pause-image/build-static-pause-image.sh
+++ tools/packaging/static-build/pause-image/build-static-pause-image.sh
@@ -10,15 +10,17 @@ set -o pipefail
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# shellcheck source=/dev/null
 source "${script_dir}/../../scripts/lib.sh"
 
-[ -n "$pause_image_repo" ] || die "failed to get pause-image repo"
-[ -n "$pause_image_version" ] || die "failed to get pause-image version"
+[[ -n "${pause_image_repo}" ]] || die "failed to get pause-image repo"
+[[ -n "${pause_image_version}" ]] || die "failed to get pause-image version"
 
 pull_pause_image_from_remote() {
 	echo "pull pause image from remote"
 
 	skopeo copy "${pause_image_repo}":"${pause_image_version}" oci:pause:"${pause_image_version}"
+	# shellcheck disable=SC2154
 	rm -rf "${DESTDIR}/pause_bundle"
 	umoci unpack --rootless --image pause:"${pause_image_version}"  "${DESTDIR}/pause_bundle"
 	rm "${DESTDIR}/pause_bundle/umoci.json"
