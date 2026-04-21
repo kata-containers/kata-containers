544d11dba2db82f020dad19182dd490ffffa149f
Fabiano Fidêncio <ffidencio@nvidia.com>
2026-04-21 18:03:29 +0200

    tests: Fix shellcheck issues in gha-adjust-to-use-prebuilt-components.sh
    Fix shellcheck warnings and notes identified by running
    shellcheck --severity=style.
    
    Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>

--- tests/gha-adjust-to-use-prebuilt-components.sh
+++ tests/gha-adjust-to-use-prebuilt-components.sh
@@ -19,18 +19,18 @@ function main() {
     artifacts_dir="${1:-}"
     asset="${2:-}"
 
-    if [ -z "${artifacts_dir}" ]; then
+    if [[ -z "${artifacts_dir}" ]]; then
         echo "The artefacts directory must be passed as the first argument to this script."
         exit 1
     fi
 
-    if [ -z "${asset}" ]; then
+    if [[ -z "${asset}" ]]; then
         echo "The asset must be passed as the second argument to this script."
         exit 1
     fi
 
-    mv ${artifacts_dir} ${build_dir}
-    sed -i "s/\(^${asset}-tarball:\).*/\1/g" ${base_dir}/Makefile
+    mv "${artifacts_dir}" "${build_dir}"
+    sed -i "s/\(^${asset}-tarball:\).*/\1/g" "${base_dir}/Makefile"
 }
 
 main "$@"
