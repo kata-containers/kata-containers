0105616ff20f1dcaea0b29d82af4825332411e19
Fabiano Fidêncio <ffidencio@nvidia.com>
2026-04-21 18:38:02 +0200

    tools: Fix shellcheck issues in config.sh
    Fix shellcheck warnings and notes identified by running
    shellcheck --severity=style.
    
    Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>

--- tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
@@ -4,9 +4,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+# shellcheck disable=SC2034
 OS_NAME=cbl-mariner
 OS_VERSION=${OS_VERSION:-3.0}
+# shellcheck disable=SC2034
 LIBC="gnu"
 PACKAGES="kata-packages-uvm"
-[ "$AGENT_INIT" = no ] && PACKAGES+=" systemd"
-[ "$SECCOMP" = yes ] && PACKAGES+=" libseccomp"
+# shellcheck disable=SC2154
+[[ "${AGENT_INIT}" = no ]] && PACKAGES+=" systemd"
+# shellcheck disable=SC2154
+[[ "${SECCOMP}" = yes ]] && PACKAGES+=" libseccomp"
