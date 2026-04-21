599a0944f0c8044f93ba9c1611d00ad62064a470
Fabiano Fidêncio <ffidencio@nvidia.com>
2026-04-21 18:03:29 +0200

    tools: Fix shellcheck issues in build.sh
    Fix shellcheck warnings and notes identified by running
    shellcheck --severity=style.
    
    Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>

--- tools/packaging/static-build/virtiofsd/build.sh
+++ tools/packaging/static-build/virtiofsd/build.sh
@@ -11,6 +11,7 @@ set -o pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly virtiofsd_builder="${script_dir}/build-static-virtiofsd.sh"
 
+# shellcheck source=/dev/null
 source "${script_dir}/../../scripts/lib.sh"
 
 ARCH=${ARCH:-$(uname -m)}
@@ -23,14 +24,14 @@ virtiofsd_toolchain="${virtiofsd_toolchain:-}"
 virtiofsd_zip="${virtiofsd_zip:-}"
 package_output_dir="${package_output_dir:-}"
 
-[ -n "${virtiofsd_repo}" ] || virtiofsd_repo=$(get_from_kata_deps ".externals.virtiofsd.url")
-[ -n "${virtiofsd_version}" ] || virtiofsd_version=$(get_from_kata_deps ".externals.virtiofsd.version")
-[ -n "${virtiofsd_toolchain}" ] || virtiofsd_toolchain=$(get_from_kata_deps ".externals.virtiofsd.toolchain")
-[ -n "${virtiofsd_zip}" ] || virtiofsd_zip=$(get_from_kata_deps ".externals.virtiofsd.meta.binary")
+[[ -n "${virtiofsd_repo}" ]] || virtiofsd_repo=$(get_from_kata_deps ".externals.virtiofsd.url")
+[[ -n "${virtiofsd_version}" ]] || virtiofsd_version=$(get_from_kata_deps ".externals.virtiofsd.version")
+[[ -n "${virtiofsd_toolchain}" ]] || virtiofsd_toolchain=$(get_from_kata_deps ".externals.virtiofsd.toolchain")
+[[ -n "${virtiofsd_zip}" ]] || virtiofsd_zip=$(get_from_kata_deps ".externals.virtiofsd.meta.binary")
 
-[ -n "${virtiofsd_repo}" ] || die "Failed to get virtiofsd repo"
-[ -n "${virtiofsd_version}" ] || die "Failed to get virtiofsd version or commit"
-[ -n "${virtiofsd_toolchain}" ] || die "Failed to get the rust toolchain to build virtiofsd"
+[[ -n "${virtiofsd_repo}" ]] || die "Failed to get virtiofsd repo"
+[[ -n "${virtiofsd_version}" ]] || die "Failed to get virtiofsd version or commit"
+[[ -n "${virtiofsd_toolchain}" ]] || die "Failed to get the rust toolchain to build virtiofsd"
 
 case ${ARCH} in
 	"aarch64")
@@ -51,15 +52,18 @@ case ${ARCH} in
 esac
 
 container_image="${VIRTIOFSD_CONTAINER_BUILDER:-$(get_virtiofsd_image_name)}"
-[ "${CROSS_BUILD}" == "true" ] && container_image="${container_image}-cross-build"
+# shellcheck disable=SC2154
+[[ "${CROSS_BUILD}" == "true" ]] && container_image="${container_image}-cross-build"
 
-docker pull ${container_image} || \
-	(docker $BUILDX build $PLATFORM \
+# shellcheck disable=SC2154,SC2086
+docker pull "${container_image}" || \
+	(docker ${BUILDX} build ${PLATFORM} \
 		--build-arg RUST_TOOLCHAIN="${virtiofsd_toolchain}" \
 		-t "${container_image}" "${script_dir}/${libc}" && \
 	 # No-op unless PUSH_TO_REGISTRY is exported as "yes"
 	 push_to_registry "${container_image}")
 
+# shellcheck disable=SC2154
 docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	-w "${PWD}" \
 	--env DESTDIR="${DESTDIR}" \
