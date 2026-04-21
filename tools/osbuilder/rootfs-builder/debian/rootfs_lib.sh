f21c902dfb5fd856b2a14184162929447146734b
Fabiano Fidêncio <ffidencio@nvidia.com>
2026-04-21 18:38:02 +0200

    tools: Fix shellcheck issues in rootfs_lib.sh
    Fix shellcheck warnings and notes identified by running
    shellcheck --severity=style.
    
    Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>

--- tools/osbuilder/rootfs-builder/debian/rootfs_lib.sh
+++ tools/osbuilder/rootfs-builder/debian/rootfs_lib.sh
@@ -5,4 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # NOTE: Re-using ubuntu rootfs lib, see 'ubuntu' folder for details.
-source ${script_dir}/ubuntu/$LIB_SH
+# shellcheck disable=SC2154
+# shellcheck source=/dev/null
+source "${script_dir}/ubuntu/${LIB_SH}"
