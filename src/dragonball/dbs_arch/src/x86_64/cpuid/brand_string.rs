f9c95a279edcf4bce60a441c911cf42d5d454b44
stevenhorsman <steven@uk.ibm.com>
2026-06-01 17:07:16 +0100

    dragonball: Remove unnecessary unsafe blocks in cpuid
    Rust 1.94 now warns about unnecessary unsafe blocks around
    __get_cpuid_max(), __cpuid_count(), and host_cpuid() calls.
    Remove the unsafe blocks as they are no longer needed.
    
    This fixes the following clippy warnings in dbs-arch:
    - warning: unnecessary `unsafe` block at brand_string.rs:106
    - warning: unnecessary `unsafe` block at brand_string.rs:114
    - warning: unnecessary `unsafe` block at common.rs:28
    - warning: unnecessary `unsafe` block at common.rs:36
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>
    Generated-By: IBM Bob

--- src/dragonball/dbs_arch/src/x86_64/cpuid/brand_string.rs
+++ src/dragonball/dbs_arch/src/x86_64/cpuid/brand_string.rs
@@ -103,7 +103,7 @@ impl BrandString {
     /// of the host CPU.
     fn from_host_cpuid() -> Result<Self, Error> {
         let mut this = Self::new();
-        let mut cpuid_regs = unsafe { host_cpuid(0x8000_0000) };
+        let mut cpuid_regs = host_cpuid(0x8000_0000);
 
         if cpuid_regs.eax < 0x8000_0004 {
             // Brand string not supported by the host CPU
@@ -111,7 +111,7 @@ impl BrandString {
         }
 
         for leaf in 0x8000_0002..=0x8000_0004 {
-            cpuid_regs = unsafe { host_cpuid(leaf) };
+            cpuid_regs = host_cpuid(leaf);
             this.set_reg_for_leaf(leaf, Reg::Eax, cpuid_regs.eax);
             this.set_reg_for_leaf(leaf, Reg::Ebx, cpuid_regs.ebx);
             this.set_reg_for_leaf(leaf, Reg::Ecx, cpuid_regs.ecx);
