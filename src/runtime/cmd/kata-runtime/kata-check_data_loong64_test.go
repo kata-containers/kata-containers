57dd1a8e331bbe7c18bb5191d538ec1d89bac342
zhaixiaojuan <zhaixiaojuan@loongson.cn>
2026-08-13 11:21:50 +0800

    Runtime: add LoongArch support

---
+++ src/runtime/cmd/kata-runtime/kata-check_data_loong64_test.go
@@ -0,0 +1,40 @@
+// Copyright (c) 2025 Institute of Software, CAS.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package main
+
+const testCPUInfoTemplate = `
+processor		: 0
+package			: 0
+core			: 0
+global_id		: 0
+CPU Family		: Loongson-64bit
+Model Name		: Loongson-3C5000
+CPU Revision		: 0x11
+FPU Revision		: 0x00
+CPU MHz			: 2200.00
+BogoMIPS		: 4400.00
+TLB Entries		: 2112
+Address Sizes		: 48 bits physical, 48 bits virtual
+ISA			: loongarch32r loongarch32s loongarch64
+Features		: cpucfg lam ual fpu lsx lasx crc32 lspw lvz
+Hardware Watchpoint	: yes, iwatch count: 8, dwatch count: 8
+
+processor		: 1
+package			: 0
+core			: 1
+global_id		: 1
+CPU Family		: Loongson-64bit
+Model Name		: Loongson-3C5000
+CPU Revision		: 0x11
+FPU Revision		: 0x00
+CPU MHz			: 2200.00
+BogoMIPS		: 4400.00
+TLB Entries		: 2112
+Address Sizes		: 48 bits physical, 48 bits virtual
+ISA			: loongarch32r loongarch32s loongarch64
+Features		: cpucfg lam ual fpu lsx lasx crc32 lspw lvz
+Hardware Watchpoint	: yes, iwatch count: 8, dwatch count: 8
+`
