d91b7e0727b1e21d61a1fdd7975e60835aabe0d9
stevenhorsman <steven@uk.ibm.com>
2026-09-14 15:18:36 +0100

    test: Bump kcli cluster image to 24.04
    Bump the kcli cluster worker image to 24.04 in line with the
    rest of our CI
    
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- tests/gha-run-k8s-common.sh
+++ tests/gha-run-k8s-common.sh
@@ -400,7 +400,7 @@ function create_cluster_kcli() {
 		-P ctlplanes="${CLUSTER_CONTROL_NODES:-1}" \
 		-P workers="${CLUSTER_WORKERS:-1}" \
 		-P network="${LIBVIRT_NETWORK:-default}" \
-		-P image="${CLUSTER_IMAGE:-ubuntu2204}" \
+		-P image="${CLUSTER_IMAGE:-ubuntu2404}" \
 		-P sdn=flannel \
 		-P nfs=false \
 		-P disk_size="${CLUSTER_DISK_SIZE:-20}" \
